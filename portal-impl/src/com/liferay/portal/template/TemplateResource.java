73ded60be8db942fdbef7ff91d0d56f991632468
tinatian <tian.tian@liferay.com>
2012-05-08 18:37:00 +0800

    LPS-25570 Add template resource loader

---
+++ portal-impl/src/com/liferay/portal/template/TemplateResource.java
@@ -0,0 +1,45 @@
+/**
+ * Copyright (c) 2000-2012 Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.template;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
+ * @author Tina Tian
+ */
+public class TemplateResource<T> {
+
+	public TemplateResource(
+		String name, T data, TemplateResourceLoader<T> templateResourceLoader) {
+
+		_data = data;
+		_name = name;
+		_templateResourceLoader = templateResourceLoader;
+	}
+
+	public InputStream getInputStream() throws IOException {
+		return _templateResourceLoader.getInputStream(_data);
+	}
+
+	public String getName() {
+		return _name;
+	}
+
+	private T _data;
+	private String _name;
+	private TemplateResourceLoader<T> _templateResourceLoader;
+
+}
