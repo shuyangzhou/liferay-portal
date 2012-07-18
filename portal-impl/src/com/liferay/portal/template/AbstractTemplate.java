408cbd67607e3d39e442db7eb88ce66fe505d713
tina.tian <tina.tian@liferay.com>
2012-07-18 18:11:06 +0800

    LPS-28491 Make sure all TemplateResources are stored in Levle1 cache.

--- portal-impl/src/com/liferay/portal/template/AbstractTemplate.java
+++ portal-impl/src/com/liferay/portal/template/AbstractTemplate.java
@@ -14,11 +14,18 @@
 
 package com.liferay.portal.template;
 
+import com.liferay.portal.deploy.sandbox.SandboxHandler;
+import com.liferay.portal.kernel.cache.MultiVMPoolUtil;
+import com.liferay.portal.kernel.cache.PortalCache;
 import com.liferay.portal.kernel.io.unsync.UnsyncStringWriter;
+import com.liferay.portal.kernel.template.StringTemplateResource;
 import com.liferay.portal.kernel.template.Template;
 import com.liferay.portal.kernel.template.TemplateException;
+import com.liferay.portal.kernel.template.TemplateManager;
 import com.liferay.portal.kernel.template.TemplateResource;
+import com.liferay.portal.kernel.template.TemplateResourceLoader;
 import com.liferay.portal.kernel.util.StringBundler;
+import com.liferay.portal.kernel.util.StringPool;
 
 import java.io.Writer;
 
@@ -32,7 +39,8 @@ public abstract class AbstractTemplate implements Template {
 	public AbstractTemplate(
 		TemplateResource templateResource,
 		TemplateResource errorTemplateResource,
-		TemplateContextHelper templateContextHelper) {
+		TemplateContextHelper templateContextHelper,
+		String templateResourceLoaderName, long interval) {
 
 		if (templateResource == null) {
 			throw new IllegalArgumentException("TemplateResource is null");
@@ -42,9 +50,16 @@ public abstract class AbstractTemplate implements Template {
 			throw new IllegalArgumentException("TemplateContextHelper is null");
 		}
 
+		if (templateResourceLoaderName == null) {
+			throw new IllegalArgumentException(
+				"Name of TemplateResourceLoader is null");
+		}
+
 		_templateResource = templateResource;
 		_errorTemplateResource = errorTemplateResource;
 		_templateContextHelper = templateContextHelper;
+
+		_cacheTemplateResource(templateResourceLoaderName, interval);
 	}
 
 	public void prepare(HttpServletRequest request) {
@@ -94,6 +109,64 @@ public abstract class AbstractTemplate implements Template {
 	protected abstract void processTemplate(
 			TemplateResource templateResource, Writer writer)
 		throws Exception;
+
+	private void _cacheTemplateResource(
+		String templateResourceLoaderName, long interval) {
+
+		if (interval == 0) {
+			return;
+		}
+
+		if (templateResourceLoaderName.equals(TemplateManager.VELOCITY) &&
+			_templateResource.getTemplateId().contains(
+				SandboxHandler.SANDBOX_MARKER)) {
+
+			return;
+		}
+
+		if (!(_templateResource instanceof StringTemplateResource) &&
+			!(_templateResource instanceof CacheTemplateResource)) {
+
+			_templateResource = new CacheTemplateResource(_templateResource);
+		}
+
+		String cacheName = TemplateResourceLoader.class.getName().concat(
+			StringPool.POUND).concat(templateResourceLoaderName);
+
+		PortalCache portalCache = MultiVMPoolUtil.getCache(cacheName);
+
+		Object object = portalCache.get(_templateResource.getTemplateId());
+
+		if ((object == null) || !_templateResource.equals(object)) {
+			portalCache.put(
+				_templateResource.getTemplateId(), _templateResource);
+		}
+
+		if (_errorTemplateResource == null) {
+			return;
+		}
+
+		if (templateResourceLoaderName.equals(TemplateManager.VELOCITY) &&
+			_errorTemplateResource.getTemplateId().contains(
+				SandboxHandler.SANDBOX_MARKER)) {
+
+			return;
+		}
+
+		if (!(_errorTemplateResource instanceof StringTemplateResource) &&
+			!(_errorTemplateResource instanceof CacheTemplateResource)) {
+
+			_errorTemplateResource = new CacheTemplateResource(
+				_errorTemplateResource);
+		}
+
+		object = portalCache.get(_errorTemplateResource.getTemplateId());
+
+		if ((object == null) || !_errorTemplateResource.equals(object)) {
+			portalCache.put(
+				_errorTemplateResource.getTemplateId(), _errorTemplateResource);
+		}
+	}
 
 	private TemplateResource _errorTemplateResource;
 	private TemplateContextHelper _templateContextHelper;
