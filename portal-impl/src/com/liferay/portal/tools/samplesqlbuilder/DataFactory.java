ee99f2301f28f22868dcbf20ed7eb7dd9db1f254
Tina Tian <tina.tian@liferay.com>
2013-05-21 11:03:05 +0800

    LPS-34467 Update sample data for Asset publisher test.

--- portal-impl/src/com/liferay/portal/tools/samplesqlbuilder/DataFactory.java
+++ portal-impl/src/com/liferay/portal/tools/samplesqlbuilder/DataFactory.java
@@ -71,9 +71,13 @@ import com.liferay.portlet.PortletPreferencesFactoryUtil;
 import com.liferay.portlet.asset.model.AssetCategory;
 import com.liferay.portlet.asset.model.AssetCategoryConstants;
 import com.liferay.portlet.asset.model.AssetEntry;
+import com.liferay.portlet.asset.model.AssetTag;
+import com.liferay.portlet.asset.model.AssetTagStats;
 import com.liferay.portlet.asset.model.AssetVocabulary;
 import com.liferay.portlet.asset.model.impl.AssetCategoryImpl;
 import com.liferay.portlet.asset.model.impl.AssetEntryImpl;
+import com.liferay.portlet.asset.model.impl.AssetTagImpl;
+import com.liferay.portlet.asset.model.impl.AssetTagStatsImpl;
 import com.liferay.portlet.asset.model.impl.AssetVocabularyImpl;
 import com.liferay.portlet.blogs.model.BlogsEntry;
 import com.liferay.portlet.blogs.model.BlogsStatsUser;
@@ -158,8 +162,11 @@ import java.text.Format;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Random;
+import java.util.Set;
 
 /**
  * @author Brian Wing Shun Chan
@@ -231,6 +238,7 @@ public class DataFactory {
 					_DEPENDENCIES_DIR + "ddm_structure_basic_document.xml")));
 
 		initAssetCateogries();
+		initAssetTags();
 		initCompany();
 		initDLFileEntryType();
 		initGroups();
@@ -253,14 +261,52 @@ public class DataFactory {
 		return _assetCategories;
 	}
 
-	public long getAssetCategoryId(long groupId, int currentIndex) {
-		int index = (currentIndex - 1) % _maxAssetCategoryCount;
+	public Set<Long> getAssetCategoryIds(long groupId) {
+		int assetCategoryCountPerGroup =
+			_maxAssetCategoryCount * _maxAssetVocabularyCount;
 
-		index = _maxAssetCategoryCount * ((int)groupId - 1) + index;
+		int startIndex = assetCategoryCountPerGroup * ((int)groupId - 1);
 
-		AssetCategory assetCategory = _assetCategories.get(index);
+		Set<Long> assetCategoryIds = new HashSet<Long>();
 
-		return assetCategory.getCategoryId();
+		for (int i = 0; i < _maxAssetEntryCategoryCount;) {
+			int index = _RANDOM.nextInt(assetCategoryCountPerGroup);
+
+			long assetCategoryId = _assetCategories.get(
+				startIndex + index).getCategoryId();
+
+			if (assetCategoryIds.add(assetCategoryId)) {
+				i++;
+			}
+		}
+
+		return assetCategoryIds;
+	}
+
+	public Set<Long> getAssetTagIds(long groupId) {
+		int startIndex = _maxAssetTagCount * ((int)groupId - 1);
+
+		Set<Long> assetTagIds = new HashSet<Long>();
+
+		for (int i = 0; i < _maxAssetEntryTagCount;) {
+			int index = _RANDOM.nextInt(_maxAssetTagCount);
+
+			long assetTagId = _assetTags.get(startIndex + index).getTagId();
+
+			if (assetTagIds.add(assetTagId)) {
+				i++;
+			}
+		}
+
+		return assetTagIds;
+	}
+
+	public List<AssetTag> getAssetTags() {
+		return _assetTags;
+	}
+
+	public List<AssetTagStats> getAssetTagStats() {
+		return _assetTagStats;
 	}
 
 	public List<AssetVocabulary> getAssetVocabularies() {
@@ -403,70 +449,82 @@ public class DataFactory {
 		_assetVocabularies = new ArrayList<AssetVocabulary>();
 		_assetCategories = new ArrayList<AssetCategory>();
 
-		StringBundler sb = new StringBundler(5);
-
-		sb.append("<?xml version=\"1.0\" encoding=\"UTF-8\"?><root ");
-		sb.append("available-locales=\"en_US\" default-locale=\"en_US\">");
-		sb.append("<Title language-id=\"en_US\">");
-		sb.append(PropsValues.ASSET_VOCABULARY_DEFAULT);
-		sb.append("</Title></root>");
-
-		String defaultTitle = sb.toString();
-
 		_assetVocabularies.add(
 			newAssetVocabulary(
 				_globalGroupId, _defaultUserId, null,
-				PropsValues.ASSET_VOCABULARY_DEFAULT, defaultTitle));
+				PropsValues.ASSET_VOCABULARY_DEFAULT));
 
 		for (int i = 1; i <= _maxGroupsCount; i++) {
 			long lastRightCategoryId = 2;
 
-			for (int j = 0; j < _maxAssetCategoryCount; j++) {
-				sb = new StringBundler(4);
+			for (int j = 0; j < _maxAssetVocabularyCount; j++) {
+				StringBundler sb = new StringBundler(4);
 
-				sb.append("Test_");
+				sb.append("TestVocabulary_");
 				sb.append(i);
 				sb.append(StringPool.UNDERLINE);
 				sb.append(j);
 
-				String name = sb.toString();
-
-				sb = new StringBundler(5);
-
-				sb.append("<?xml version=\"1.0\" encoding=\"UTF-8\"?><root ");
-				sb.append(
-					"available-locales=\"en_US\" default-locale=\"en_US\">");
-				sb.append("<Title language-id=\"en_US\">");
-				sb.append(name);
-				sb.append("</Title></root>");
-
-				String title = sb.toString();
-
 				AssetVocabulary assetVocabulary = newAssetVocabulary(
-					i, _sampleUserId, _SAMPLE_USER_NAME, name, title);
+					i, _sampleUserId, _SAMPLE_USER_NAME, sb.toString());
 
 				_assetVocabularies.add(assetVocabulary);
 
-				AssetCategory assetCategory = new AssetCategoryImpl();
+				long vocabularyId = assetVocabulary.getVocabularyId();
 
-				assetCategory.setUuid(SequentialUUID.generate());
-				assetCategory.setCategoryId(_counter.get());
-				assetCategory.setGroupId(i);
-				assetCategory.setCompanyId(_companyId);
-				assetCategory.setUserId(_sampleUserId);
-				assetCategory.setUserName(_SAMPLE_USER_NAME);
-				assetCategory.setCreateDate(new Date());
-				assetCategory.setModifiedDate(new Date());
-				assetCategory.setParentCategoryId(
-					AssetCategoryConstants.DEFAULT_PARENT_CATEGORY_ID);
-				assetCategory.setLeftCategoryId(lastRightCategoryId++);
-				assetCategory.setRightCategoryId(lastRightCategoryId++);
-				assetCategory.setName(name);
-				assetCategory.setTitle(title);
-				assetCategory.setVocabularyId(
-					assetVocabulary.getVocabularyId());
+				for (int k = 0; k < _maxAssetCategoryCount; k++) {
+					sb = new StringBundler(4);
 
-				_assetCategories.add(assetCategory);
+					sb.append("TestCategory_");
+					sb.append(vocabularyId);
+					sb.append(StringPool.UNDERLINE);
+					sb.append(k);
+
+					AssetCategory assetCategory = newAssetCategory(
+						i, lastRightCategoryId, sb.toString(), vocabularyId);
+
+					_assetCategories.add(assetCategory);
+				}
+			}
+		}
+	}
+
+	public void initAssetTags() {
+		_assetTags = new ArrayList<AssetTag>(_maxAssetTagCount);
+		_assetTagStats = new ArrayList<AssetTagStats>(_maxAssetTagCount * 3);
+
+		for (int i = 1; i <= _maxGroupsCount; i++) {
+			for (int j = 0; j < _maxAssetTagCount; j++) {
+				AssetTag assetTag = new AssetTagImpl();
+
+				assetTag.setTagId(_counter.get());
+				assetTag.setGroupId(i);
+				assetTag.setCompanyId(_companyId);
+				assetTag.setUserId(_sampleUserId);
+				assetTag.setUserName(_SAMPLE_USER_NAME);
+				assetTag.setCreateDate(new Date());
+				assetTag.setModifiedDate(new Date());
+				assetTag.setName("TestTag_" + i + "_" + j);
+
+				_assetTags.add(assetTag);
+
+				AssetTagStats assetTagStats = newAssetTagStats(
+					assetTag.getTagId(),
+					_classNamesMap.get(BlogsEntry.class.getName()));
+
+				_assetTagStats.add(assetTagStats);
+
+				assetTagStats = newAssetTagStats(
+					assetTag.getTagId(),
+					_classNamesMap.get(WikiPage.class.getName()));
+
+				_assetTagStats.add(assetTagStats);
+
+				assetTagStats = newAssetTagStats(
+					assetTag.getTagId(),
+					_classNamesMap.get(JournalArticle.class.getName()));
+
+				_assetTagStats.add(assetTagStats);
 			}
 		}
 	}
@@ -1467,6 +1525,12 @@ public class DataFactory {
 			String.valueOf(assetCategory.getCategoryId()), _sampleUserId);
 	}
 
+	public List<ResourcePermission> newResourcePermissions(AssetTag assetTag) {
+		return newResourcePermissions(
+			AssetTag.class.getName(), StringUtil.valueOf(assetTag.getTagId()),
+			_sampleUserId);
+	}
+
 	public List<ResourcePermission> newResourcePermissions(
 		AssetVocabulary assetVocabulary) {
 
@@ -1757,6 +1821,40 @@ public class DataFactory {
 		return userName;
 	}
 
+	protected AssetCategory newAssetCategory(
+		long groupId, long lastRightCategoryId, String name,
+		long vocabularyId) {
+
+		AssetCategory assetCategory = new AssetCategoryImpl();
+
+		assetCategory.setUuid(SequentialUUID.generate());
+		assetCategory.setCategoryId(_counter.get());
+		assetCategory.setGroupId(groupId);
+		assetCategory.setCompanyId(_companyId);
+		assetCategory.setUserId(_sampleUserId);
+		assetCategory.setUserName(_SAMPLE_USER_NAME);
+		assetCategory.setCreateDate(new Date());
+		assetCategory.setModifiedDate(new Date());
+		assetCategory.setParentCategoryId(
+			AssetCategoryConstants.DEFAULT_PARENT_CATEGORY_ID);
+		assetCategory.setLeftCategoryId(lastRightCategoryId++);
+		assetCategory.setRightCategoryId(lastRightCategoryId++);
+		assetCategory.setName(name);
+
+		StringBundler sb = new StringBundler(5);
+
+		sb.append("<?xml version=\"1.0\" encoding=\"UTF-8\"?><root ");
+		sb.append("available-locales=\"en_US\" default-locale=\"en_US\">");
+		sb.append("<Title language-id=\"en_US\">");
+		sb.append(name);
+		sb.append("</Title></root>");
+
+		assetCategory.setTitle(sb.toString());
+		assetCategory.setVocabularyId(vocabularyId);
+
+		return assetCategory;
+	}
+
 	protected AssetEntry newAssetEntry(
 		long groupId, Date createDate, Date modifiedDate, long classNameId,
 		long classPK, String uuid, long classTypeId, boolean visible,
@@ -1786,8 +1884,18 @@ public class DataFactory {
 		return assetEntry;
 	}
 
+	protected AssetTagStats newAssetTagStats(long tagId, long classNameId) {
+		AssetTagStats assetTagStats = new AssetTagStatsImpl();
+
+		assetTagStats.setTagStatsId(_counter.get());
+		assetTagStats.setTagId(tagId);
+		assetTagStats.setClassNameId(classNameId);
+
+		return assetTagStats;
+	}
+
 	protected AssetVocabulary newAssetVocabulary(
-		long grouId, long userId, String userName, String name, String title) {
+		long grouId, long userId, String userName, String name) {
 
 		AssetVocabulary assetVocabulary = new AssetVocabularyImpl();
 
@@ -1800,7 +1908,16 @@ public class DataFactory {
 		assetVocabulary.setCreateDate(new Date());
 		assetVocabulary.setModifiedDate(new Date());
 		assetVocabulary.setName(name);
-		assetVocabulary.setTitle(title);
+
+		StringBundler sb = new StringBundler(5);
+
+		sb.append("<?xml version=\"1.0\" encoding=\"UTF-8\"?><root ");
+		sb.append("available-locales=\"en_US\" default-locale=\"en_US\">");
+		sb.append("<Title language-id=\"en_US\">");
+		sb.append(name);
+		sb.append("</Title></root>");
+
+		assetVocabulary.setTitle(sb.toString());
 		assetVocabulary.setSettings(
 			"multiValued=true\\nselectedClassNameIds=0");
 
@@ -2137,12 +2254,16 @@ public class DataFactory {
 	private static final long _FUTURE_TIME =
 		System.currentTimeMillis() + Time.YEAR;
 
+	private static final Random _RANDOM = new Random();
+
 	private static final String _SAMPLE_USER_NAME = "Sample";
 
 	private Account _account;
 	private long _accountId;
 	private Role _administratorRole;
 	private List<AssetCategory> _assetCategories;
+	private List<AssetTag> _assetTags;
+	private List<AssetTagStats> _assetTagStats;
 	private List<AssetVocabulary> _assetVocabularies;
 	private String _baseDir;
 	private List<ClassName> _classNames;
