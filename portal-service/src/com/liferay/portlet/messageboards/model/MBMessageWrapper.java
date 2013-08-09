a9b54f00af2ec034b9a37d5e1e47685135d10c70
Zsolt Berentey <zsolt.berentey@liferay.com>
2013-08-09 15:24:56 +0200

    LPS-39077 Generated

--- portal-service/src/com/liferay/portlet/messageboards/model/MBMessageWrapper.java
+++ portal-service/src/com/liferay/portlet/messageboards/model/MBMessageWrapper.java
@@ -1181,13 +1181,6 @@ public class MBMessageWrapper implements MBMessage, ModelWrapper<MBMessage> {
 	}
 
 	@Override
-	public com.liferay.portal.model.ContainerModel getTrashContainer()
-		throws com.liferay.portal.kernel.exception.PortalException,
-			com.liferay.portal.kernel.exception.SystemException {
-		return _mbMessage.getTrashContainer();
-	}
-
-	@Override
 	public java.lang.String getWorkflowClassName() {
 		return _mbMessage.getWorkflowClassName();
 	}
@@ -1200,13 +1193,6 @@ public class MBMessageWrapper implements MBMessage, ModelWrapper<MBMessage> {
 	@Override
 	public boolean isFormatBBCode() {
 		return _mbMessage.isFormatBBCode();
-	}
-
-	@Override
-	public boolean isInTrashThread()
-		throws com.liferay.portal.kernel.exception.PortalException,
-			com.liferay.portal.kernel.exception.SystemException {
-		return _mbMessage.isInTrashThread();
 	}
 
 	@Override
