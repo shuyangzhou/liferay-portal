e346e8aa6c2ca58b8515d8b7e8e9966a903729a0
Tina Tian <tina.tian@liferay.com>
2014-01-08 18:40:09 +0800

    LPS-43180 Add concrete logic to AssetIndexer.

--- portal-impl/src/com/liferay/portlet/assetpublisher/util/AssetIndexer.java
+++ portal-impl/src/com/liferay/portlet/assetpublisher/util/AssetIndexer.java
@@ -18,11 +18,17 @@ import com.liferay.portal.kernel.search.BaseIndexer;
 import com.liferay.portal.kernel.search.BooleanQuery;
 import com.liferay.portal.kernel.search.Document;
 import com.liferay.portal.kernel.search.Field;
+import com.liferay.portal.kernel.search.Hits;
 import com.liferay.portal.kernel.search.SearchContext;
 import com.liferay.portal.kernel.search.Summary;
+import com.liferay.portal.kernel.util.GetterUtil;
+import com.liferay.portal.kernel.util.Tuple;
 import com.liferay.portal.util.PortletKeys;
 import com.liferay.portlet.asset.model.AssetEntry;
+import com.liferay.portlet.asset.service.AssetEntryLocalServiceUtil;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Locale;
 
 import javax.portlet.PortletURL;
@@ -37,14 +43,47 @@ public class AssetIndexer extends BaseIndexer {
 
 	public static final String PORTLET_ID = PortletKeys.ASSET_PUBLISHER;
 
+	public static final String[] SELECTED_FIELD_NAMES =
+		{Field.ENTRY_CLASS_NAME, Field.ENTRY_CLASS_PK};
+
 	@Override
 	public String[] getClassNames() {
 		return CLASS_NAMES;
 	}
 
 	@Override
+	public Tuple getEntries(Hits hits) {
+		List<AssetEntry> assetEntries = new ArrayList<AssetEntry>();
+
+		for (Document document : hits.getDocs()) {
+			String className = GetterUtil.getString(
+				document.get(Field.ENTRY_CLASS_NAME));
+			long classPK = GetterUtil.getLong(
+				document.get(Field.ENTRY_CLASS_PK));
+
+			try {
+				AssetEntry assetEntry = AssetEntryLocalServiceUtil.fetchEntry(
+					className, classPK);
+
+				if (assetEntry != null) {
+					assetEntries.add(assetEntry);
+				}
+			}
+			catch (Exception e) {
+			}
+		}
+
+		return new Tuple(assetEntries, hits.getLength());
+	}
+
+	@Override
 	public String getPortletId() {
 		return PORTLET_ID;
+	}
+
+	@Override
+	public String[] getSelectedFieldNames() {
+		return SELECTED_FIELD_NAMES;
 	}
 
 	@Override
