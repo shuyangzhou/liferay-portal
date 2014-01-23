e0517fc8edc8165a2c1a0e4cd0e483c119b0bc28
Tina Tian <tina.tian@liferay.com>
2014-01-23 16:49:14 +0800

    LPS-43608 Add PortalEventListener to support dectection of address.

--- portal-impl/src/com/liferay/portal/util/PortalImpl.java
+++ portal-impl/src/com/liferay/portal/util/PortalImpl.java
@@ -557,6 +557,15 @@ public class PortalImpl implements Portal {
 	}
 
 	@Override
+	public void addPortalEventListener(
+		PortalEventListener portalEventListener) {
+
+		if (!_portalEventListeners.contains(portalEventListener)) {
+			_portalEventListeners.add(portalEventListener);
+		}
+	}
+
+	@Override
 	public void addPortalPortEventListener(
 		PortalPortEventListener portalPortEventListener) {
 
@@ -6471,6 +6480,13 @@ public class PortalImpl implements Portal {
 	}
 
 	@Override
+	public void removePortalEventListener(
+		PortalEventListener portalEventListener) {
+
+		_portalEventListeners.remove(portalEventListener);
+	}
+
+	@Override
 	public void removePortalPortEventListener(
 		PortalPortEventListener portalPortEventListener) {
 
@@ -7868,6 +7884,8 @@ public class PortalImpl implements Portal {
 	private String _pathProxy;
 	private Map<String, Long> _plidToPortletIdMap =
 		new ConcurrentHashMap<String, Long>();
+	private List<PortalEventListener> _portalEventListeners =
+		new ArrayList<PortalEventListener>();
 	private final AtomicInteger _portalPort = new AtomicInteger(-1);
 	private List<PortalPortEventListener> _portalPortEventListeners =
 		new ArrayList<PortalPortEventListener>();
