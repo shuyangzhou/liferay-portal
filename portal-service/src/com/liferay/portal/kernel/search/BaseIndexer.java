ddeb75eed55364f833cb89864adc5c9d6e8ecb1f
Tina Tian <tina.tian@liferay.com>
2014-02-08 12:25:02 +0800

    LPS-44108 Rename classes.

--- portal-service/src/com/liferay/portal/kernel/search/BaseIndexer.java
+++ portal-service/src/com/liferay/portal/kernel/search/BaseIndexer.java
@@ -1648,7 +1648,7 @@ public abstract class BaseIndexer implements Indexer {
 	private boolean _stagingAware = true;
 
 	private class IndexerSearchResultPermissionFilter
-		extends SearchResultPermissionFilter {
+		extends BaseSearchResultPermissionFilter {
 
 		public IndexerSearchResultPermissionFilter(
 			PermissionChecker permissionChecker) {
