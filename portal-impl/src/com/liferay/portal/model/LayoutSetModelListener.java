732b71a1430ee8005aafd9f3179d9c2fb9d87580
willnewbury <william.newbury@liferay.com>
2014-08-13 16:45:49 -0700

    LPS-48598 Modified LayoutSetModelListener to account for rounding introduced earlier. This change causes date changes to propogate across LayoutSetPrototypes and layouts to ensure that proper handling of updates is enforced. Namely, this update ensures that a layoutSet will crosscheck the last merge time of its associated layouSetPrototype and layouts to ensure that its modified date is set higher than the maximum last merge time amongst these, and thus changes in these objects are propogated safely to the layoutSet.

--- portal-impl/src/com/liferay/portal/model/LayoutSetModelListener.java
+++ portal-impl/src/com/liferay/portal/model/LayoutSetModelListener.java
@@ -14,11 +14,26 @@
 
 package com.liferay.portal.model;
 
+import com.liferay.portal.ModelListenerException;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.util.DateUtil;
+import com.liferay.portal.kernel.util.GetterUtil;
+import com.liferay.portal.kernel.util.Time;
+import com.liferay.portal.model.impl.LayoutSetModelImpl;
+import com.liferay.portal.service.LayoutLocalServiceUtil;
+import com.liferay.portal.service.LayoutSetPrototypeLocalServiceUtil;
 import com.liferay.portal.servlet.filters.cache.CacheUtil;
+import com.liferay.portlet.sites.util.Sites;
+
+import java.util.Date;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 /**
  * @author Alexander Chow
  * @author Raymond Augé
+ * @author Will Newbury
  */
 public class LayoutSetModelListener extends BaseModelListener<LayoutSet> {
 
@@ -32,6 +47,20 @@ public class LayoutSetModelListener extends BaseModelListener<LayoutSet> {
 		clearCache(layoutSet);
 	}
 
+	@Override
+	public void onBeforeCreate(LayoutSet layoutSet)
+		throws ModelListenerException {
+
+		updateModifiedDate(layoutSet);
+	}
+
+	@Override
+	public void onBeforeUpdate(LayoutSet layoutSet)
+		throws ModelListenerException {
+
+		updateModifiedDate(layoutSet);
+	}
+
 	protected void clearCache(LayoutSet layoutSet) {
 		if (layoutSet == null) {
 			return;
@@ -39,6 +68,64 @@ public class LayoutSetModelListener extends BaseModelListener<LayoutSet> {
 
 		if (!layoutSet.isPrivateLayout()) {
 			CacheUtil.clearCache(layoutSet.getCompanyId());
+		}
+	}
+
+	private void updateModifiedDate(LayoutSet layoutSet) {
+		LayoutSetModelImpl layoutSetModelImpl = (LayoutSetModelImpl)layoutSet;
+
+		Date originalModifiedDate =
+			layoutSetModelImpl.getOriginalModifiedDate();
+
+		Date currentModifiedDate = layoutSet.getModifiedDate();
+
+		if ((currentModifiedDate == null) ||
+			currentModifiedDate.equals(originalModifiedDate)) {
+
+			return;
+		}
+
+		try {
+			currentModifiedDate = DateUtil.getDBSafeDate(currentModifiedDate);
+
+			long maxLastMergeTime = GetterUtil.getLong(
+				layoutSet.getSettingsProperty(Sites.LAST_MERGE_TIME));
+
+			List<Layout> layoutList = LayoutLocalServiceUtil.getLayouts(
+				layoutSet.getGroupId(), layoutSet.isPrivateLayout());
+
+			Set<Layout> layouts = new HashSet(layoutList);
+
+			maxLastMergeTime = MaxMergeTimeUtil.findMaxMergeTimeInLayouts(
+				layouts, maxLastMergeTime);
+
+			LayoutSetPrototype layoutSetPrototype = null;
+
+			long layoutSetPrototypeId = layoutSet.getLayoutSetPrototypeId();
+
+			if (layoutSetPrototypeId != 0) {
+				layoutSetPrototype =
+					LayoutSetPrototypeLocalServiceUtil.getLayoutSetPrototype(
+						layoutSet.getLayoutSetPrototypeId());
+
+				Date prototypeModifiedDate =
+					layoutSetPrototype.getModifiedDate();
+
+				long prototypeModifiedTime = prototypeModifiedDate.getTime();
+
+				if (prototypeModifiedTime > maxLastMergeTime) {
+					maxLastMergeTime = prototypeModifiedTime;
+				}
+			}
+
+			if (maxLastMergeTime >= currentModifiedDate.getTime()) {
+				currentModifiedDate = new Date(maxLastMergeTime + Time.SECOND);
+
+				layoutSet.setModifiedDate(currentModifiedDate);
+			}
+		}
+		catch (PortalException pe) {
+			throw new ModelListenerException(pe);
 		}
 	}
 
