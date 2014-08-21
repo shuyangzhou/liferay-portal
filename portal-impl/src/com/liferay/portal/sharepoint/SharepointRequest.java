545aeb511f3bf44c35861008c5fe2d2f0526eba3
willnewbury <william.newbury@liferay.com>
2014-08-21 12:03:08 -0700

    LPS-49294 Part 1: Modify or create constructors to ensure that all final variables are initialized

--- portal-impl/src/com/liferay/portal/sharepoint/SharepointRequest.java
+++ portal-impl/src/com/liferay/portal/sharepoint/SharepointRequest.java
@@ -42,17 +42,11 @@ public class SharepointRequest {
 			HttpServletRequest request, HttpServletResponse response, User user)
 		throws SharepointException {
 
-		_request = request;
-		_response = response;
-		_user = user;
-
-		_params.putAll(request.getParameterMap());
-
-		addParams();
+		this(request, response, StringPool.BLANK, user);
 	}
 
-	public SharepointRequest(String rootPath) {
-		_rootPath = rootPath;
+	public SharepointRequest(String rootPath) throws SharepointException {
+		this(null, null, rootPath, null);
 	}
 
 	public void addParam(String key, String value) {
@@ -161,12 +155,29 @@ public class SharepointRequest {
 		}
 	}
 
+	private SharepointRequest(
+			HttpServletRequest request, HttpServletResponse response,
+			String rootPath, User user)
+		throws SharepointException {
+
+		_rootPath = rootPath;
+
+		_request = request;
+		_response = response;
+		_user = user;
+
+		_params.putAll(request.getParameterMap());
+
+		addParams();
+	}
+
 	private byte[] _bytes;
-	private Map<String, String[]> _params = new HashMap<String, String[]>();
-	private HttpServletRequest _request;
-	private HttpServletResponse _response;
+	private final Map<String, String[]> _params =
+		new HashMap<String, String[]>();
+	private final HttpServletRequest _request;
+	private final HttpServletResponse _response;
 	private String _rootPath = StringPool.BLANK;
 	private SharepointStorage _storage;
-	private User _user;
+	private final User _user;
 
 }
