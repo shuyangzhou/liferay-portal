2bb6969c49fd52551f2c98a025e419377ad28829
willnewbury <william.newbury@liferay.com>
2014-08-21 17:28:24 -0700

    LPS-49294 Part 3: Use local variables in order to ensure that final variables are explicitly initialized and modified only once

--- portal-impl/src/com/liferay/portal/search/lucene/QueryTranslatorImpl.java
+++ portal-impl/src/com/liferay/portal/search/lucene/QueryTranslatorImpl.java
@@ -100,7 +100,7 @@ public class QueryTranslatorImpl implements QueryTranslator {
 					text = StringPool.QUOTE.concat(text).concat(
 						StringPool.QUOTE);
 
-					_textField.set(term, text);
+					_TEXT_FIELD.set(term, text);
 				}
 			}
 			catch (Exception e) {
@@ -118,7 +118,7 @@ public class QueryTranslatorImpl implements QueryTranslator {
 				if (text.matches("^\\s*\\*.*(?m)")) {
 					text = text.replaceFirst("\\*", StringPool.BLANK);
 
-					_textField.set(term, text);
+					_TEXT_FIELD.set(term, text);
 				}
 			}
 			catch (Exception e) {
@@ -127,19 +127,23 @@ public class QueryTranslatorImpl implements QueryTranslator {
 		}
 	}
 
+	private static final Field _TEXT_FIELD;
+
 	private static Log _log = LogFactoryUtil.getLog(QueryTranslatorImpl.class);
 
-	private static Field _textField = null;
-
 	static {
-		try {
-			_textField = Term.class.getDeclaredField("text");
+		Field textField = null;
 
-			_textField.setAccessible(true);
+		try {
+			textField = Term.class.getDeclaredField("text");
+
+			textField.setAccessible(true);
 		}
 		catch (Exception e) {
 			_log.error(e, e);
 		}
+
+		_TEXT_FIELD = textField;
 	}
 
 }
