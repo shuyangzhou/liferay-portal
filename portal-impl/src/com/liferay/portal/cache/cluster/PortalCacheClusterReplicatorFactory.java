225b07b04e150f3e86e49d45397a41bef52b3b87
tinatian <tina.tian@liferay.com>
2014-08-27 10:42:48 +0800

    LPS-49135 Apply our ListenerFactory mechanism to cluster link replicator, almost same logic with the ehcache one, will reomve ehcache dependency later.

---
+++ portal-impl/src/com/liferay/portal/cache/cluster/PortalCacheClusterReplicatorFactory.java
@@ -0,0 +1,37 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.cache.cluster;
+
+import com.liferay.portal.kernel.cache.ListenerFactory;
+
+import java.io.Serializable;
+
+import java.util.Properties;
+
+/**
+ * @author Tina Tian
+ */
+public class PortalCacheClusterReplicatorFactory
+	<K extends Serializable, V extends Serializable>
+		implements ListenerFactory<PortalCacheClusterReplicator<K, V>> {
+
+	@Override
+	public PortalCacheClusterReplicator<K, V> createListener(
+		Properties properties) {
+
+		return new PortalCacheClusterReplicator<K, V>(properties);
+	}
+
+}
