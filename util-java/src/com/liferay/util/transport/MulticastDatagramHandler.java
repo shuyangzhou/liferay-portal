7170f3dd70b763789e5864866eefb18bffbb4128
shuyangzhou <shuyang.zhou@liferay.com>
2014-09-09 15:46:33 -0700

    Use StreamUtil instead

--- util-java/src/com/liferay/util/transport/MulticastDatagramHandler.java
+++ util-java/src/com/liferay/util/transport/MulticastDatagramHandler.java
@@ -16,9 +16,9 @@ package com.liferay.util.transport;
 
 import com.liferay.portal.kernel.io.unsync.UnsyncByteArrayInputStream;
 import com.liferay.portal.kernel.io.unsync.UnsyncByteArrayOutputStream;
+import com.liferay.portal.kernel.util.StreamUtil;
 import com.liferay.portal.kernel.util.StringBundler;
 
-import java.io.InputStream;
 
 import java.net.DatagramPacket;
 
@@ -77,31 +77,10 @@ public class MulticastDatagramHandler implements DatagramHandler {
 	}
 
 	protected byte[] getUnzippedBytes(byte[] bytes) throws Exception {
-		UnsyncByteArrayOutputStream ubaos = new UnsyncByteArrayOutputStream(
-			bytes.length);
+		UnsyncByteArrayOutputStream ubaos = new UnsyncByteArrayOutputStream();
 
-		byte[] buffer = new byte[1500];
-
-		int c = 0;
-
-		try (InputStream is = new GZIPInputStream(
-				new UnsyncByteArrayInputStream(bytes))) {
-
-			while (true) {
-				if (c == -1) {
-					break;
-				}
-
-				c = is.read(buffer, 0, 1500);
-
-				if (c != -1) {
-					ubaos.write(buffer, 0, c);
-				}
-			}
-		}
-
-		ubaos.flush();
-		ubaos.close();
+		StreamUtil.transfer(
+			new GZIPInputStream(new UnsyncByteArrayInputStream(bytes)), ubaos);
 
 		return ubaos.toByteArray();
 	}
