0825236cc249fed841f8f0cd15337a4ff8b2967c
Miguel Angel Pastor Olivar <miguel.pastor@liferay.com>
2014-10-13 09:28:02 +0200

    LPS-50787 Abstraction to deal with resources independently of their location

---
+++ portal-service/src/com/liferay/portal/kernel/io/resource/loader/ClassLoaderResourceLoader.java
@@ -0,0 +1,44 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.kernel.io.resource.loader;
+
+import com.liferay.portal.kernel.io.resource.ClassPathResource;
+import com.liferay.portal.kernel.io.resource.Resource;
+
+/**
+ * @author Miguel Pastor
+ */
+public class ClassLoaderResourceLoader implements ResourceLoader {
+
+	public ClassLoaderResourceLoader(ClassLoader classLoader) {
+		if (classLoader != null) {
+			_classLoader = classLoader;
+		}
+		else {
+			_classLoader = ClassLoaderResourceLoader.class.getClassLoader();
+		}
+	}
+
+	public ClassLoader getClassLoader() {
+		return _classLoader;
+	}
+
+	public Resource getResource(String location) {
+		return new ClassPathResource(location, _classLoader);
+	}
+
+	private final ClassLoader _classLoader;
+
+}
