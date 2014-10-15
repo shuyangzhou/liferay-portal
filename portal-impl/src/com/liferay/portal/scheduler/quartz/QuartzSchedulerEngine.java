0ad0a4a2b95373a971eccac97c452578b573be84
tinatian <tina.tian@liferay.com>
2014-10-15 10:56:07 +0800

    LPS-50217 Remove ReceiverKey

--- portal-impl/src/com/liferay/portal/scheduler/quartz/QuartzSchedulerEngine.java
+++ portal-impl/src/com/liferay/portal/scheduler/quartz/QuartzSchedulerEngine.java
@@ -38,7 +38,6 @@ import com.liferay.portal.kernel.scheduler.StorageType;
 import com.liferay.portal.kernel.scheduler.TriggerFactoryUtil;
 import com.liferay.portal.kernel.scheduler.TriggerState;
 import com.liferay.portal.kernel.scheduler.TriggerType;
-import com.liferay.portal.kernel.scheduler.messaging.ReceiverKey;
 import com.liferay.portal.kernel.scheduler.messaging.SchedulerEventMessageListenerWrapper;
 import com.liferay.portal.kernel.scheduler.messaging.SchedulerResponse;
 import com.liferay.portal.kernel.util.CharPool;
@@ -432,7 +431,7 @@ public class QuartzSchedulerEngine implements SchedulerEngine {
 				message = message.clone();
 			}
 
-			registerMessageListeners(jobName, groupName, destination, message);
+			registerMessageListeners(destination, message);
 
 			schedule(
 				scheduler, storageType, quartzTrigger, description, destination,
@@ -949,8 +948,7 @@ public class QuartzSchedulerEngine implements SchedulerEngine {
 	}
 
 	protected void registerMessageListeners(
-			String jobName, String groupName, String destinationName,
-			Message message)
+			String destinationName, Message message)
 		throws SchedulerException {
 
 		String messageListenerClassName = message.getString(
@@ -991,8 +989,6 @@ public class QuartzSchedulerEngine implements SchedulerEngine {
 		SchedulerEventMessageListenerWrapper schedulerEventListenerWrapper =
 			new SchedulerEventMessageListenerWrapper();
 
-		schedulerEventListenerWrapper.setGroupName(groupName);
-		schedulerEventListenerWrapper.setJobName(jobName);
 		schedulerEventListenerWrapper.setMessageListener(
 			schedulerEventListener);
 
@@ -1004,8 +1000,6 @@ public class QuartzSchedulerEngine implements SchedulerEngine {
 		message.put(
 			MESSAGE_LISTENER_UUID,
 			schedulerEventListenerWrapper.getMessageListenerUUID());
-
-		message.put(RECEIVER_KEY, new ReceiverKey(jobName, groupName));
 	}
 
 	protected void schedule(
