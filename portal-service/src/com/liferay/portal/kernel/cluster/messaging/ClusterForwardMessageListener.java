151421efb95d2bc3948cb2b8ee50b5066bd1ce1c
Preston-Crary <preston.crary@liferay.com>
2014-11-10 09:35:16 -0800

    LPS-51428 Only forward messages once the portal has started

--- portal-service/src/com/liferay/portal/kernel/cluster/messaging/ClusterForwardMessageListener.java
+++ portal-service/src/com/liferay/portal/kernel/cluster/messaging/ClusterForwardMessageListener.java
@@ -19,15 +19,35 @@ import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.messaging.Message;
 import com.liferay.portal.kernel.messaging.MessageBusUtil;
+import com.liferay.portal.kernel.util.BasePortalLifecycle;
 import com.liferay.portal.kernel.util.Validator;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author Shuyang Zhou
+ * @author Preston Crary
  */
-public class ClusterForwardMessageListener implements ClusterMessageListener {
+public class ClusterForwardMessageListener extends BasePortalLifecycle
+	implements ClusterMessageListener {
+
+	public ClusterForwardMessageListener() {
+		registerPortalLifecycle(METHOD_INIT);
+	}
 
 	@Override
 	public void receive(Message message) {
+		if (!_portalStartupComplete) {
+			synchronized (this) {
+				if (!_portalStartupComplete) {
+					_startupMessageBuffer.add(message);
+
+					return;
+				}
+			}
+		}
+
 		String destinationName = message.getDestinationName();
 
 		if (Validator.isNotNull(destinationName)) {
@@ -50,7 +70,28 @@ public class ClusterForwardMessageListener implements ClusterMessageListener {
 		}
 	}
 
+	@Override
+	protected void doPortalDestroy() throws Exception {
+	}
+
+	@Override
+	protected void doPortalInit() throws Exception {
+		synchronized (this) {
+			_portalStartupComplete = true;
+		}
+
+		for (Message message : _startupMessageBuffer) {
+			receive(message);
+		}
+
+		_startupMessageBuffer.clear();
+	}
+
 	private static final Log _log = LogFactoryUtil.getLog(
 		ClusterForwardMessageListener.class);
+
+	private volatile boolean _portalStartupComplete = false;
+	private final List<Message> _startupMessageBuffer =
+		new ArrayList<Message>();
 
 }
