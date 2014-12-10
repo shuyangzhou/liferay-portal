6c55f027e328f11b83088d659c529c12bed4bf01
Cristina González Castellano <cristina.gonzalez@liferay.com>
2014-12-10 10:28:14 +0100

    LPS-50675 Create an event runner adaptor to manage the event BeforeClass, Before, AfterClass, After, Test that this extension needs

--- modules/test/arquillian-extension/src/com/liferay/arquillian/extension/internal/init/InitLiferayContextImpl.java
+++ modules/test/arquillian-extension/src/com/liferay/arquillian/extension/internal/init/InitLiferayContextImpl.java
@@ -12,29 +12,25 @@
  * details.
  */
 
-package com.liferay.arquillian.extension.internal.observer;
+package com.liferay.arquillian.extension.internal.init;
 
 import com.liferay.arquillian.extension.internal.descriptor.SpringDescriptor;
-import com.liferay.arquillian.extension.internal.event.LiferayContextCreatedEvent;
 import com.liferay.portal.test.jdbc.ResetDatabaseUtilDataSource;
 import com.liferay.portal.util.InitUtil;
 
 import java.util.ArrayList;
 import java.util.List;
 
-import org.jboss.arquillian.core.api.Event;
 import org.jboss.arquillian.core.api.Instance;
 import org.jboss.arquillian.core.api.annotation.Inject;
-import org.jboss.arquillian.core.api.annotation.Observes;
 import org.jboss.arquillian.core.spi.ServiceLoader;
-import org.jboss.arquillian.test.spi.event.suite.BeforeClass;
 
 /**
  * @author Cristina González
  */
-public class InitializeLiferayTestEnvironment {
+public class InitLiferayContextImpl implements InitLiferayContext {
 
-	public void beforeClass(@Observes BeforeClass beforeClass) {
+	public void init() {
 		System.setProperty("catalina.base", ".");
 
 		ResetDatabaseUtilDataSource.initialize();
@@ -46,14 +42,12 @@ public class InitializeLiferayTestEnvironment {
 		if (System.getProperty("external-properties") == null) {
 			System.setProperty("external-properties", "portal-test.properties");
 		}
-
-		_event.fire(new LiferayContextCreatedEvent(beforeClass.getTestClass()));
 	}
 
 	protected List<String> getConfigLocations() {
 		List<String> configLocations = new ArrayList<String>();
 
-		ServiceLoader serviceLoader = _instance.get();
+		ServiceLoader serviceLoader = _serviceLoaderInstance.get();
 
 		List<SpringDescriptor> springDescriptors =
 			(List<SpringDescriptor>)serviceLoader.all(SpringDescriptor.class);
@@ -66,9 +60,6 @@ public class InitializeLiferayTestEnvironment {
 	}
 
 	@Inject
-	private Event<LiferayContextCreatedEvent> _event;
-
-	@Inject
-	private Instance<ServiceLoader> _instance;
+	private Instance<ServiceLoader> _serviceLoaderInstance;
 
 }
