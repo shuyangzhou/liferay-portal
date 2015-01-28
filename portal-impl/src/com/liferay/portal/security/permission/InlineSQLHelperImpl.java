d4ceacf90a3630bd7813d0d4984cb9c38d8a79ca
Preston-Crary <preston.crary@liferay.com>
2015-01-27 16:47:50 -0800

    LPS-52664 Logically equivelent using IN instead of OR chaining, having two loops isn't nessessary since LPS-43611 moved the logic that made them different

--- portal-impl/src/com/liferay/portal/security/permission/InlineSQLHelperImpl.java
+++ portal-impl/src/com/liferay/portal/security/permission/InlineSQLHelperImpl.java
@@ -27,9 +27,7 @@ import com.liferay.portal.service.ResourceTypePermissionLocalServiceUtil;
 import com.liferay.portal.util.PropsValues;
 import com.liferay.util.dao.orm.CustomSQLUtil;
 
-import java.util.ArrayList;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
 
 /**
@@ -544,72 +542,35 @@ public class InlineSQLHelperImpl implements InlineSQLHelper {
 
 		permissionJoin += CustomSQLUtil.get(JOIN_RESOURCE_PERMISSION);
 
-		StringBundler sb = new StringBundler();
+		StringBundler sb = new StringBundler(10);
 
-		sb.append("(((InlineSQLResourcePermission.primKey = CAST_TEXT(");
+		sb.append("((InlineSQLResourcePermission.primKey = CAST_TEXT(");
 		sb.append(classPKField);
-		sb.append(")) AND ((");
-
-		boolean hasPreviousViewableGroup = false;
-
-		List<Long> viewableGroupIds = new ArrayList<>();
-
-		for (int j = 0; j < groupIds.length; j++) {
-			long groupId = groupIds[j];
-
-			if (!permissionChecker.hasPermission(
-					groupId, className, 0, ActionKeys.VIEW)) {
-
-				if ((j > 0) && hasPreviousViewableGroup) {
-					sb.append(" OR ");
-				}
-
-				hasPreviousViewableGroup = true;
-
-				sb.append(StringPool.OPEN_PARENTHESIS);
-
-				if (Validator.isNull(groupIdField)) {
-					sb.append(
-						classPKField.substring(
-							0, classPKField.lastIndexOf(CharPool.PERIOD)));
-					sb.append(".groupId = ");
-				}
-				else {
-					sb.append(groupIdField);
-					sb.append(" = ");
-				}
-
-				sb.append(groupId);
-				sb.append(StringPool.CLOSE_PARENTHESIS);
-			}
-			else {
-				viewableGroupIds.add(groupId);
-			}
-		}
-
 		sb.append(StringPool.CLOSE_PARENTHESIS);
 
-		if (!viewableGroupIds.isEmpty()) {
-			for (Long viewableGroupId : viewableGroupIds) {
-				sb.append(" OR (");
+		sb.append(") AND (");
 
-				if (Validator.isNull(groupIdField)) {
-					sb.append(
-						classPKField.substring(
-							0, classPKField.lastIndexOf(CharPool.PERIOD)));
-					sb.append(".groupId = ");
-				}
-				else {
-					sb.append(groupIdField);
-					sb.append(" = ");
-				}
-
-				sb.append(viewableGroupId);
-				sb.append(StringPool.CLOSE_PARENTHESIS);
-			}
+		if (Validator.isNull(groupIdField)) {
+			sb.append(
+				classPKField.substring(
+					0, classPKField.lastIndexOf(CharPool.PERIOD)));
+			sb.append(".groupId");
+		}
+		else {
+			sb.append(groupIdField);
 		}
 
-		sb.append(")))");
+		if (groupIds.length > 1) {
+			sb.append(" IN (");
+			sb.append(StringUtil.merge(groupIds));
+			sb.append(StringPool.CLOSE_PARENTHESIS);
+		}
+		else {
+			sb.append(" = ");
+			sb.append(groupIds[0]);
+		}
+
+		sb.append("))");
 
 		String roleIdsOrOwnerIdSQL = getRoleIdsOrOwnerIdSQL(
 			permissionChecker, groupIds, userIdField);
