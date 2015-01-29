04abf9c34da0bcfcd7514c0acd2aa0aa475df4ca
tinatian <tina.tian@liferay.com>
2015-01-29 12:43:59 +0800

    LPS-53157 Fix unit tests.

--- portal-impl/test/unit/com/liferay/portal/cluster/ClusterExecutorImplTest.java
+++ portal-impl/test/unit/com/liferay/portal/cluster/ClusterExecutorImplTest.java
@@ -267,9 +267,6 @@ public class ClusterExecutorImplTest extends BaseClusterExecutorImplTestCase {
 
 			clusterRequest = ClusterRequest.createUnicastRequest(null, address);
 
-			clusterExecutorImpl.memberJoined(
-				address, new ClusterNode(PortalUUIDUtil.generate()));
-
 			try {
 				clusterExecutorImpl.execute(clusterRequest);
 
