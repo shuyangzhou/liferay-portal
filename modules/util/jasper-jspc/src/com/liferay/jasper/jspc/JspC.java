b728ab829464d0875f3a7fd9168eebd6544710bd
will.newbury <william.newbury@liferay.com>
2015-03-12 09:38:56 -0700

    LPS-53897 modify liferay JspC to account for expressions even in portlets without web.xmls

--- modules/util/jasper-jspc/src/com/liferay/jasper/jspc/JspC.java
+++ modules/util/jasper-jspc/src/com/liferay/jasper/jspc/JspC.java
@@ -11,14 +11,24 @@
  * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
  * details.
  */
-
 package com.liferay.jasper.jspc;
 
+import com.liferay.portal.kernel.util.ReflectionUtil;
 import java.io.File;
+import java.io.PrintWriter;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 import java.util.Iterator;
+import javax.servlet.ServletContext;
 
 import org.apache.jasper.JasperException;
+import org.apache.jasper.compiler.JspRuntimeContext;
+import org.apache.jasper.compiler.TagPluginManager;
+import org.apache.jasper.compiler.TldLocationsCache;
+import org.apache.jasper.servlet.JspCServletContext;
 
 /**
  * @author Shuyang Zhou
@@ -26,6 +36,8 @@ import org.apache.jasper.JasperException;
 public class JspC extends org.apache.jasper.JspC {
 
 	public static void main(String[] args) {
+		System.out.println("In Main");
+
 		JspC jspc = new JspC();
 
 		try {
@@ -44,6 +56,8 @@ public class JspC extends org.apache.jasper.JspC {
 
 	@Override
 	public void scanFiles(File baseDir) throws JasperException {
+		System.out.println("In Scan");
+
 		super.scanFiles(baseDir);
 
 		Iterator<String> iterator = pages.iterator();
@@ -55,6 +69,76 @@ public class JspC extends org.apache.jasper.JspC {
 				iterator.remove();
 			}
 		}
+	}
+
+	@Override
+	protected void initServletContext() {
+		try {
+			context = new JspCServletContext(new PrintWriter(System.out),
+				new URL("file:" + uriRoot.replace('\\', '/') + '/'));
+			tldLocationsCache = TldLocationsCache.getInstance(context);
+		}
+		catch (MalformedURLException me) {
+			System.out.println("**" + me);
+		}
+		rctxt = new JspRuntimeContext(context, this);
+		jspConfig = new JspConfig(context);
+
+		tagPluginManager = new TagPluginManager(context);
+	}
+
+	public class JspConfig extends org.apache.jasper.compiler.JspConfig {
+
+		public JspConfig(ServletContext ctxt) {
+			super(ctxt);
+		}
+
+		public void initialize() {
+			try {
+				Class<?> clazz = this.getClass().getSuperclass();
+
+				Field field = ReflectionUtil.getDeclaredField(
+					clazz, "initialized");
+
+				if (field.getBoolean(this)) {
+					return;
+				}
+
+				Method init = ReflectionUtil.getDeclaredMethod(
+					clazz, "init");
+
+				init.invoke(this);
+
+				JspProperty defaultProperty = new JspProperty(
+					null, null, null, null, null, null, "true", null, null,
+					null, "false");
+
+				field = ReflectionUtil.getDeclaredField(
+					clazz, "defaultJspProperty");
+
+				field.setAccessible(true);
+
+				field.set(this, defaultProperty);
+			}
+			catch (Exception e) {
+				ReflectionUtil.throwException(e);
+			}
+		}
+
+		@Override
+		public boolean isJspPage(String uri) throws JasperException {
+			initialize();
+
+			return super.isJspPage(uri);
+		}
+
+		@Override
+		public JspProperty findJspProperty(String uri) throws JasperException {
+			initialize();
+
+			return super.findJspProperty(uri);
+		}
+
 	}
 
 }
