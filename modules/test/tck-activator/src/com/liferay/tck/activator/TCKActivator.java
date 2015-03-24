71c6822f08a89e14a0c2982afafe04ad74441791
Matthew Tambara <matthew.tambara@liferay.com>
2015-03-24 15:37:44 -0700

    LPS-54488 Create TCK module to enable tck tests

---
+++ modules/test/tck-activator/src/com/liferay/tck/activator/TCKActivator.java
@@ -0,0 +1,53 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.tck.activator;
+
+import com.liferay.portal.struts.StrutsActionRegistryUtil;
+
+import javax.servlet.ServletContext;
+
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.ConfigurationPolicy;
+import org.osgi.service.component.annotations.Deactivate;
+import org.osgi.service.component.annotations.Modified;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Matthew Tambara
+ */
+@Component(
+	configurationPolicy = ConfigurationPolicy.OPTIONAL
+)
+public class TCKActivator {
+
+	@Activate
+	@Modified
+	protected void activate() {
+		StrutsActionRegistryUtil.register("/portal/tck", _TCKStrutsAction);
+	}
+
+	@Deactivate
+	protected void deactivate() {
+		StrutsActionRegistryUtil.unregister("/portal/tck");
+	}
+
+	@Reference(target = "(original.bean=*)", unbind = "-")
+	protected void setServletContext(ServletContext servletContext) {
+	}
+
+	private final TCKStrutsAction _TCKStrutsAction = new TCKStrutsAction();
+
+}
