c8c9ed8242538c551dea69212ed25c50e3147f52
shuyangzhou <shuyang.zhou@liferay.com>
2015-05-06 15:07:32 -0700

    LPS-55159 Centralize synthetic class to InstrumentationAgent

--- portal-test/src/com/liferay/portal/kernel/test/rule/CodeCoverageAssertor.java
+++ portal-test/src/com/liferay/portal/kernel/test/rule/CodeCoverageAssertor.java
@@ -94,8 +94,6 @@ public class CodeCoverageAssertor implements TestRule {
 
 		appendAssertClasses(assertClasses);
 
-		_purgeSyntheticClasses(assertClasses);
-
 		try {
 			_ASSERT_COVERAGE_METHOD.invoke(
 				null, _includeInnerClasses,
@@ -135,18 +133,6 @@ public class CodeCoverageAssertor implements TestRule {
 		return clazz.getClassLoader();
 	}
 
-	private static void _purgeSyntheticClasses(List<Class<?>> assertClasses) {
-		Iterator<Class<?>> iterator = assertClasses.iterator();
-
-		while (iterator.hasNext()) {
-			Class<?> assertClass = iterator.next();
-
-			if (assertClass.isSynthetic()) {
-				iterator.remove();
-			}
-		}
-	}
-
 	private String[] _generateIncludes(String mainClassName) throws Exception {
 		List<Class<?>> assertClasses = new ArrayList<>();
 
@@ -181,8 +167,6 @@ public class CodeCoverageAssertor implements TestRule {
 
 			appendAssertClassesMethod.invoke(reloadedObject, assertClasses);
 		}
-
-		_purgeSyntheticClasses(assertClasses);
 
 		String[] includes = new String[assertClasses.size()];
 
