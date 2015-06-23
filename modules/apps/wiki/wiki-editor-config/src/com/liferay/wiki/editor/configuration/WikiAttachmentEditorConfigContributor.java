8c3657d843c9245eab8b4c2d183ba11f78f52efa
shuyangzhou <shuyang.zhou@liferay.com>
2015-06-22 23:36:42 -0700

    Revert "LPS-56485 WikiAttachmentEditor needs to have a higher service ranking compared to the default EditorConfigContributors like ImageEditorConfigContributors"
    This reverts commit 6cff5a9d20f1088b6bf24cdcc508364694a0e9da.

--- modules/apps/wiki/wiki-editor-config/src/com/liferay/wiki/editor/configuration/WikiAttachmentEditorConfigContributor.java
+++ modules/apps/wiki/wiki-editor-config/src/com/liferay/wiki/editor/configuration/WikiAttachmentEditorConfigContributor.java
@@ -43,8 +43,7 @@ import org.osgi.service.component.annotations.Reference;
 	property = {
 		"javax.portlet.name=" + WikiPortletKeys.WIKI,
 		"javax.portlet.name=" + WikiPortletKeys.WIKI_ADMIN,
-		"javax.portlet.name=" + WikiPortletKeys.WIKI_DISPLAY,
-		"service.ranking:Integer=100"
+		"javax.portlet.name=" + WikiPortletKeys.WIKI_DISPLAY
 	},
 	service = EditorConfigContributor.class
 )
