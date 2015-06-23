d3342081340a1f8b29afa1c89f5a5deecf88fbad
shuyangzhou <shuyang.zhou@liferay.com>
2015-06-22 23:37:31 -0700

    Revert "LPS-53194 Edit Javadoc 2"
    This reverts commit 6bf83bce6ea8c8fac238a8152670cbe77995249a.

--- portal-service/src/com/liferay/portal/service/ServiceContext.java
+++ portal-service/src/com/liferay/portal/service/ServiceContext.java
@@ -871,9 +871,10 @@ public class ServiceContext implements Cloneable, Serializable {
 	}
 
 	/**
-	 * Merges all of the specified service context's non-<code>null</code>
-	 * attributes, attributes greater than <code>0</code>, and fields (except
-	 * the request) with this service context object.
+	 * Merges all attributes and fields (except for the request) of the
+	 * specified service context with this service context object. Attributes
+	 * and fields that are <code>null</code> or less than <code>1</code> are not
+	 * merged.
 	 *
 	 * @param serviceContext the service context object to be merged
 	 */
