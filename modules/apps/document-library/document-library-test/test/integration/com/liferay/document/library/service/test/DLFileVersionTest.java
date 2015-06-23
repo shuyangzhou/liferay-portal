94ca5ebd136921c30542b9fb3dfd8a3590f10fd8
shuyangzhou <shuyang.zhou@liferay.com>
2015-06-22 23:35:36 -0700

    Revert "LPS-56533 Capture failed deletion of private working copy"
    This reverts commit 5cf7f53b14dd5329365b91383999e051583b9137.

--- modules/apps/document-library/document-library-test/test/integration/com/liferay/document/library/service/test/DLFileVersionTest.java
+++ modules/apps/document-library/document-library-test/test/integration/com/liferay/document/library/service/test/DLFileVersionTest.java
@@ -40,8 +40,6 @@ import com.liferay.portal.security.permission.PermissionChecker;
 import com.liferay.portal.security.permission.PermissionThreadLocal;
 import com.liferay.portal.security.permission.SimplePermissionChecker;
 import com.liferay.portal.service.ServiceContext;
-import com.liferay.portal.test.log.CaptureAppender;
-import com.liferay.portal.test.log.Log4JLoggerTestUtil;
 import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
 import com.liferay.portal.util.PortalUtil;
 import com.liferay.portlet.documentlibrary.NoSuchFolderException;
@@ -54,7 +52,6 @@ import com.liferay.portlet.documentlibrary.model.DLFolderConstants;
 import com.liferay.portlet.documentlibrary.service.DLAppServiceUtil;
 import com.liferay.portlet.documentlibrary.service.DLFileEntryTypeLocalServiceUtil;
 import com.liferay.portlet.documentlibrary.service.DLFileVersionLocalServiceUtil;
-import com.liferay.portlet.documentlibrary.store.BaseStore;
 import com.liferay.portlet.dynamicdatamapping.model.DDMStructure;
 import com.liferay.portlet.dynamicdatamapping.storage.DDMFormValues;
 import com.liferay.portlet.dynamicdatamapping.storage.Field;
@@ -75,9 +72,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
-
-import org.apache.log4j.Level;
-import org.apache.log4j.spi.LoggingEvent;
 
 import org.junit.After;
 import org.junit.Assert;
@@ -150,9 +144,6 @@ public class DLFileVersionTest {
 
 		_fileVersion = DLFileVersionLocalServiceUtil.getFileVersion(
 			fileEntry.getFileEntryId(), DLFileEntryConstants.VERSION_DEFAULT);
-
-		_captureAppender = Log4JLoggerTestUtil.configureLog4JLogger(
-			BaseStore.class.getName(), Level.WARN);
 	}
 
 	@After
@@ -171,12 +162,6 @@ public class DLFileVersionTest {
 			RoleConstants.GUEST, "com.liferay.portlet.documentlibrary",
 			ResourceConstants.SCOPE_GROUP, String.valueOf(_group.getGroupId()),
 			ActionKeys.VIEW);
-
-		List<LoggingEvent> loggingEvents = _captureAppender.getLoggingEvents();
-
-		Assert.assertTrue(loggingEvents.isEmpty());
-
-		_captureAppender.close();
 	}
 
 	@Test
@@ -206,8 +191,6 @@ public class DLFileVersionTest {
 		fileEntry = DLAppServiceUtil.getFileEntry(fileEntry.getFileEntryId());
 
 		Assert.assertEquals("2.0", fileEntry.getVersion());
-
-		checkLogForFileDeletion(4);
 	}
 
 	@Test
@@ -220,8 +203,6 @@ public class DLFileVersionTest {
 
 		Assert.assertNotEquals(
 			DLFileEntryConstants.VERSION_DEFAULT, fileEntry.getVersion());
-
-		checkLogForFileDeletion(1);
 	}
 
 	@Test
@@ -234,8 +215,6 @@ public class DLFileVersionTest {
 
 		Assert.assertNotEquals(
 			DLFileEntryConstants.VERSION_DEFAULT, fileEntry.getVersion());
-
-		checkLogForFileDeletion(1);
 	}
 
 	@Test
@@ -251,8 +230,6 @@ public class DLFileVersionTest {
 
 		Assert.assertNotEquals(
 			DLFileEntryConstants.VERSION_DEFAULT, fileEntry.getVersion());
-
-		checkLogForFileDeletion(1);
 	}
 
 	@Test
@@ -270,8 +247,6 @@ public class DLFileVersionTest {
 
 		Assert.assertNotEquals(
 			DLFileEntryConstants.VERSION_DEFAULT, fileEntry.getVersion());
-
-		checkLogForFileDeletion(1);
 	}
 
 	@Test
@@ -287,8 +262,6 @@ public class DLFileVersionTest {
 
 		Assert.assertNotEquals(
 			DLFileEntryConstants.VERSION_DEFAULT, fileEntry.getVersion());
-
-		checkLogForFileDeletion(1);
 	}
 
 	@Test
@@ -301,8 +274,6 @@ public class DLFileVersionTest {
 
 		Assert.assertEquals(
 			DLFileEntryConstants.VERSION_DEFAULT, fileEntry.getVersion());
-
-		checkLogForFileDeletion(1);
 	}
 
 	@Test
@@ -315,8 +286,6 @@ public class DLFileVersionTest {
 
 		Assert.assertNotEquals(
 			DLFileEntryConstants.VERSION_DEFAULT, fileEntry.getVersion());
-
-		checkLogForFileDeletion(1);
 	}
 
 	@Test
@@ -329,28 +298,6 @@ public class DLFileVersionTest {
 
 		Assert.assertNotEquals(
 			DLFileEntryConstants.VERSION_DEFAULT, fileEntry.getVersion());
-
-		checkLogForFileDeletion(1);
-	}
-
-	protected void checkLogForFileDeletion(int size) {
-		List<LoggingEvent> loggingEvents = _captureAppender.getLoggingEvents();
-
-		Assert.assertEquals(size, loggingEvents.size());
-
-		for (LoggingEvent loggingEvent : loggingEvents) {
-			String message = (String)loggingEvent.getMessage();
-
-			Assert.assertTrue(
-				message.startsWith(
-					"Unable to delete file {companyId=" +
-						_fileVersion.getCompanyId()));
-			Assert.assertTrue(
-				message.endsWith(
-					"versionLabel=PWC} because it does not exist"));
-		}
-
-		loggingEvents.clear();
 	}
 
 	protected Field createField(DDMStructure ddmStructure, String name) {
@@ -572,7 +519,6 @@ public class DLFileVersionTest {
 		}
 	}
 
-	private CaptureAppender _captureAppender;
 	private long _contractDLFileEntryTypeId;
 	private DLFileVersion _fileVersion;
 
