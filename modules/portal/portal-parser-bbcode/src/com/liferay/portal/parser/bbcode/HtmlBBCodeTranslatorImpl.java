d4dcaac8572d824c02f75b1c1494288b011e4e1f
shuyangzhou <shuyang.zhou@liferay.com>
2015-06-22 23:36:33 -0700

    Revert "LPS-56411 Fix the displayed issue after publish the thread"
    This reverts commit c4668a5c1ba1d8cf62c47d450228f3fb4cb23890.

--- modules/portal/portal-parser-bbcode/src/com/liferay/portal/parser/bbcode/HtmlBBCodeTranslatorImpl.java
+++ modules/portal/portal-parser-bbcode/src/com/liferay/portal/parser/bbcode/HtmlBBCodeTranslatorImpl.java
@@ -57,12 +57,6 @@ public class HtmlBBCodeTranslatorImpl implements BBCodeTranslator {
 		_listStyles.put("i", "list-style: lower-roman outside;");
 		_listStyles.put("I", "list-style: upper-roman outside;");
 
-		_listBulletedStyles = new HashMap<>();
-
-		_listBulletedStyles.put("circle", "list-style: circle outside;");
-		_listBulletedStyles.put("disc", "list-style: disc outside;");
-		_listBulletedStyles.put("square", "list-style: square outside;");
-
 		_excludeNewLineTypes = new HashMap<>();
 
 		_excludeNewLineTypes.put("*", BBCodeParser.TYPE_TAG_START_END);
@@ -463,13 +457,13 @@ public class HtmlBBCodeTranslatorImpl implements BBCodeTranslator {
 
 		String listAttribute = bbCodeItem.getAttribute();
 
-		if (_listStyles.get(listAttribute) != null ) {
+		if (listAttribute != null) {
 			listStyle = _listStyles.get(listAttribute);
 
 			tag = "ol";
 		}
 		else {
-			listStyle = _listBulletedStyles.get(listAttribute);
+			listStyle = "list-style: disc outside;";
 
 			tag = "ul";
 		}
@@ -779,7 +773,6 @@ public class HtmlBBCodeTranslatorImpl implements BBCodeTranslator {
 	private final Pattern _imagePattern = Pattern.compile(
 		"^(?:https?://|/)[-;/?:@&=+$,_.!~*'()%0-9a-z]{1,2048}$",
 		Pattern.CASE_INSENSITIVE);
-	private final Map<String, String> _listBulletedStyles;
 	private final Map<String, String> _listStyles;
 	private final Pattern _tagPattern = Pattern.compile(
 		"^/?(?:b|center|code|colou?r|email|i|img|justify|left|pre|q|quote|" +
