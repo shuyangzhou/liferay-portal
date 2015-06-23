eab992e963d5ca96f82a82c80beda1845f7d814e
shuyangzhou <shuyang.zhou@liferay.com>
2015-06-22 23:36:02 -0700

    Revert "LPS-56383 Upgrade: use literals for class names and table names in core upgrades"
    This reverts commit 4e1a961fd686700051d7045537affa44851d008c.

--- portal-impl/src/com/liferay/portal/upgrade/v6_0_2/UpgradeExpando.java
+++ portal-impl/src/com/liferay/portal/upgrade/v6_0_2/UpgradeExpando.java
@@ -20,6 +20,8 @@ import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.upgrade.UpgradeProcess;
 import com.liferay.portal.util.PortalUtil;
 import com.liferay.portlet.expando.model.ExpandoTableConstants;
+import com.liferay.portlet.journal.model.JournalArticle;
+import com.liferay.portlet.journal.model.impl.JournalArticleImpl;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
@@ -91,8 +93,8 @@ public class UpgradeExpando extends UpgradeProcess {
 	@Override
 	protected void doUpgrade() throws Exception {
 		updateTables(
-			"com.liferay.portlet.journal.model.JournalArticle",
-			"JournalArticle", "id_");
+			JournalArticle.class.getName(), JournalArticleImpl.TABLE_NAME,
+			"id_");
 
 		updateTables("com.liferay.wiki.model.WikiPage", "WikiPage", "pageId");
 	}
