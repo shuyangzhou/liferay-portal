37b621f91ff07e6e4087ecefbab9aa6b87d8e7e4
shuyangzhou <shuyang.zhou@liferay.com>
2015-06-22 22:45:53 -0700

    Revert "LPS-56383 Services: move Journal Service Test classes to test module"
    This reverts commit a6fc4cd4d84a07a58c25a4f4117aa84d8ca84ebe.

--- portal-impl/test/integration/com/liferay/portlet/journal/service/persistence/test/JournalFeedPersistenceTest.java
+++ portal-impl/test/integration/com/liferay/portlet/journal/service/persistence/test/JournalFeedPersistenceTest.java
@@ -12,7 +12,7 @@
  * details.
  */
 
-package com.liferay.journal.service.persistence.test;
+package com.liferay.portlet.journal.service.persistence.test;
 
 import com.liferay.portal.kernel.dao.orm.ActionableDynamicQuery;
 import com.liferay.portal.kernel.dao.orm.DynamicQuery;
@@ -34,11 +34,13 @@ import com.liferay.portal.kernel.util.Time;
 import com.liferay.portal.kernel.util.Validator;
 import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
 import com.liferay.portal.test.rule.PersistenceTestRule;
-import com.liferay.journal.NoSuchFeedException;
-import com.liferay.journal.model.JournalFeed;
-import com.liferay.journal.service.JournalFeedLocalServiceUtil;
-import com.liferay.journal.service.persistence.JournalFeedPersistence;
-import com.liferay.journal.service.persistence.JournalFeedUtil;
+
+import com.liferay.portlet.journal.NoSuchFeedException;
+import com.liferay.portlet.journal.model.JournalFeed;
+import com.liferay.portlet.journal.service.JournalFeedLocalServiceUtil;
+import com.liferay.portlet.journal.service.persistence.JournalFeedPersistence;
+import com.liferay.portlet.journal.service.persistence.JournalFeedUtil;
+
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
@@ -46,6 +48,7 @@ import org.junit.Rule;
 import org.junit.Test;
 
 import java.io.Serializable;
+
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Iterator;
