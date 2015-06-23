80574152c86f8849e7f5e99e04780b5636526527
shuyangzhou <shuyang.zhou@liferay.com>
2015-06-22 23:38:31 -0700

    Revert "LPS-56274 Use try with resources"
    This reverts commit a37650bf7a77f0739b79e657d828280e03f77e80.

--- util-java/src/com/liferay/util/dao/orm/CustomSQL.java
+++ util-java/src/com/liferay/util/dao/orm/CustomSQL.java
@@ -28,6 +28,7 @@ import com.liferay.portal.kernel.util.OrderByComparator;
 import com.liferay.portal.kernel.util.PortalClassLoaderUtil;
 import com.liferay.portal.kernel.util.PropsKeys;
 import com.liferay.portal.kernel.util.PropsUtil;
+import com.liferay.portal.kernel.util.StreamUtil;
 import com.liferay.portal.kernel.util.StringBundler;
 import com.liferay.portal.kernel.util.StringPool;
 import com.liferay.portal.kernel.util.StringUtil;
@@ -752,11 +753,13 @@ public class CustomSQL {
 	protected void read(ClassLoader classLoader, String source)
 		throws Exception {
 
-		try (InputStream is = classLoader.getResourceAsStream(source)) {
-			if (is == null) {
-				return;
-			}
+		InputStream is = classLoader.getResourceAsStream(source);
 
+		if (is == null) {
+			return;
+		}
+
+		try {
 			if (_log.isDebugEnabled()) {
 				_log.debug("Loading " + source);
 			}
@@ -780,6 +783,9 @@ public class CustomSQL {
 					_sqlPool.put(id, content);
 				}
 			}
+		}
+		finally {
+			StreamUtil.cleanUp(is);
 		}
 	}
 
