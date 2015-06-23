237ae56f1d2890fe1b77f212762ebba1160ceb82
shuyangzhou <shuyang.zhou@liferay.com>
2015-06-22 22:45:03 -0700

    Revert "LPS-56280 rename for clarity"
    This reverts commit f1f8b7bde2d9a9542d3a2137ec58003a08effe80.

--- portal-service/test/unit/com/liferay/portal/kernel/servlet/ServletContextUtilTest.java
+++ portal-service/test/unit/com/liferay/portal/kernel/servlet/ServletContextUtilTest.java
@@ -59,26 +59,26 @@ public class ServletContextUtilTest {
 
 	@Test
 	public void testGetRootURIWithEmptyPath() throws Exception {
-		doTestGetRootURI(StringPool.BLANK, getURI(StringPool.SLASH));
+		getRootURI(StringPool.BLANK, getURI(StringPool.SLASH));
 	}
 
 	@Test(expected = MalformedURLException.class)
 	public void testGetRootURIWithInvalidCharacters() throws Exception {
-		doTestGetRootURI(_URI_WITH_INVALID_CHARACTERS, null);
+		getRootURI(_URI_WITH_INVALID_CHARACTERS, null);
 	}
 
 	@Test
 	public void testGetRootURIWithReservedCharacters() throws Exception {
 		String path = _URI_WITH_RESERVED_CHARACTERS;
 
-		doTestGetRootURI(path, getURI(path));
+		getRootURI(path, getURI(path));
 	}
 
 	@Test
 	public void testGetRootURIWithUnreservedCharacters() throws Exception {
 		String path = _URI_WITH_UNRESERVED_CHARACTERS;
 
-		doTestGetRootURI(path, getURI(path));
+		getRootURI(path, getURI(path));
 	}
 
 	protected void doTestGetResourceURI(String resourceURL) throws Exception {
@@ -91,7 +91,7 @@ public class ServletContextUtilTest {
 		Assert.assertEquals(null, uri.getFragment());
 	}
 
-	protected void doTestGetRootURI(String path, URI uri) throws Exception {
+	protected void getRootURI(String path, URI uri) throws Exception {
 		ServletContext servletContext = getServletContext(path);
 
 		URI rootURI = ServletContextUtil.getRootURI(servletContext);
