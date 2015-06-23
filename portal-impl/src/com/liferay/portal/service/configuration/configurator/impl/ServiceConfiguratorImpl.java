629b532c06ae8f42f2c7d76a86a82593eb481228
shuyangzhou <shuyang.zhou@liferay.com>
2015-06-22 22:45:08 -0700

    Revert "LPS-56544 No longer needed (PluginPackageHotDeployListener.initLogger() is kept only to support legacy plugins)"
    This reverts commit ca02d2bf7d5c887f3244e025ae1db81c5e71eda4.

--- portal-impl/src/com/liferay/portal/service/configuration/configurator/impl/ServiceConfiguratorImpl.java
+++ portal-impl/src/com/liferay/portal/service/configuration/configurator/impl/ServiceConfiguratorImpl.java
@@ -33,6 +33,7 @@ import com.liferay.portal.service.configuration.configurator.ServiceConfigurator
 import com.liferay.registry.Registry;
 import com.liferay.registry.RegistryUtil;
 import com.liferay.registry.ServiceRegistrar;
+import com.liferay.util.log4j.Log4JUtil;
 
 import java.net.URL;
 import java.util.HashMap;
@@ -67,6 +68,8 @@ public class ServiceConfiguratorImpl implements ServiceConfigurator {
 			ClassLoader classLoader)
 		throws Exception {
 
+		initLog4J(classLoader);
+
 		initServiceComponent(serviceComponentConfiguration, classLoader);
 
 		reconfigureCaches(classLoader);
@@ -100,6 +103,11 @@ public class ServiceConfiguratorImpl implements ServiceConfigurator {
 		}
 
 		return classLoader.getResource(cacheConfigurationLocation);
+	}
+
+	protected void initLog4J(ClassLoader classLoader) {
+		Log4JUtil.configureLog4J(
+			classLoader.getResource("META-INF/portal-log4j.xml"));
 	}
 
 	protected void initServiceComponent(
