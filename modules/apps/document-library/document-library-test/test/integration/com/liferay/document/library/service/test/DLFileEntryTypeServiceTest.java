57f0348d542f4c2746e53692f385702cfd9a148e
shuyangzhou <shuyang.zhou@liferay.com>
2015-06-22 22:39:03 -0700

    Revert "LPS-56567 Add SynchronousDestinationTestRule"
    This reverts commit be3682ec8a21686111160c1ea8cf6f1605dfa4e8.

--- modules/apps/document-library/document-library-test/test/integration/com/liferay/document/library/service/test/DLFileEntryTypeServiceTest.java
+++ modules/apps/document-library/document-library-test/test/integration/com/liferay/document/library/service/test/DLFileEntryTypeServiceTest.java
@@ -22,8 +22,6 @@ import com.liferay.portal.kernel.repository.model.FileEntry;
 import com.liferay.portal.kernel.repository.model.Folder;
 import com.liferay.portal.kernel.test.rule.AggregateTestRule;
 import com.liferay.portal.kernel.test.rule.DeleteAfterTestRun;
-import com.liferay.portal.kernel.test.rule.Sync;
-import com.liferay.portal.kernel.test.rule.SynchronousDestinationTestRule;
 import com.liferay.portal.kernel.test.util.GroupTestUtil;
 import com.liferay.portal.kernel.test.util.RandomTestUtil;
 import com.liferay.portal.kernel.test.util.ServiceContextTestUtil;
@@ -72,15 +70,12 @@ import org.junit.runner.RunWith;
  * @author Alexander Chow
  */
 @RunWith(Arquillian.class)
-@Sync
 public class DLFileEntryTypeServiceTest {
 
 	@ClassRule
 	@Rule
 	public static final AggregateTestRule aggregateTestRule =
-		new AggregateTestRule(
-			new LiferayIntegrationTestRule(),
-			SynchronousDestinationTestRule.INSTANCE);
+		new LiferayIntegrationTestRule();
 
 	@Before
 	public void setUp() throws Exception {
