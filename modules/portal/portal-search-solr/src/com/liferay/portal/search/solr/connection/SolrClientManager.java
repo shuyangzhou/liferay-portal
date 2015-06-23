9c2489652bd7846269b5c27925b5d94702afe88a
shuyangzhou <shuyang.zhou@liferay.com>
2015-06-22 23:38:37 -0700

    Revert "LPS-56459 src format"
    This reverts commit 6b3d568f9a06f0aabdfc58d3a5dc1001ad2ed8c2.

--- modules/portal/portal-search-solr/src/com/liferay/portal/search/solr/connection/SolrClientManager.java
+++ modules/portal/portal-search-solr/src/com/liferay/portal/search/solr/connection/SolrClientManager.java
@@ -30,6 +30,7 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
+import org.apache.http.client.HttpClient;
 import org.apache.solr.client.solrj.SolrClient;
 
 import org.osgi.service.component.ComponentContext;
@@ -95,6 +96,8 @@ public class SolrClientManager {
 			throw new IllegalStateException(
 				"No HTTP client factory for " + authMode);
 		}
+
+		HttpClient readerHttpClient = httpClientFactory.createInstance();
 
 		_solrClient = solrClientFactory.getSolrClient(
 			_solrConfiguration, httpClientFactory);
