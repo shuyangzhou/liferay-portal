3467e50e12886e5d56d00664dec57922d8bdbb76
shuyangzhou <shuyang.zhou@liferay.com>
2015-06-22 23:39:51 -0700

    Revert "LPS-55518 SF"
    This reverts commit 77cacaa89b41f2c9f17549f3d37267a167ed6c0f.

--- modules/frontend/frontend-editors-web/src/com/liferay/frontend/editors/web/editor/configuration/CKEditorConfigContributor.java
+++ modules/frontend/frontend-editors-web/src/com/liferay/frontend/editors/web/editor/configuration/CKEditorConfigContributor.java
@@ -118,12 +118,9 @@ public class CKEditorConfigContributor extends BaseCKEditorConfigContributor {
 			"{element: 'pre', name: 'Preformatted Text'}",
 			"{element: 'cite', name: 'Cited Work'}",
 			"{element: 'code', name: 'Computer Code'}",
-			"{attributes: {'class': 'portlet-msg-info'}, element: 'div', " +
-				"name: 'Info Message'}",
-			"{attributes: {'class': 'portlet-msg-alert'}, element: 'div', " +
-				"name: 'Alert Message'}",
-			"{attributes: {'class': 'portlet-msg-error'}, element: 'div', " +
-				"name: 'Error Message'}"
+			"{attributes: {'class': 'portlet-msg-info'}, element: 'div', name: 'Info Message'}",
+			"{attributes: {'class': 'portlet-msg-alert'}, element: 'div', name: 'Alert Message'}",
+			"{attributes: {'class': 'portlet-msg-error'}, element: 'div', name: 'Error Message'}"
 		};
 
 		for (String styleFormat : styleFormats) {
