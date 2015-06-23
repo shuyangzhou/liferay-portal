cc55b1ca5f58c2420261233ba39c6785ce12b74b
shuyangzhou <shuyang.zhou@liferay.com>
2015-06-22 23:40:15 -0700

    Revert "LPS-56271 VerifyUUIDTest"
    This reverts commit 4bb370846a60ad03ff48c6d157d0097f95b0abd5.

--- portal-impl/test/integration/com/liferay/portal/verify/VerifyUUIDTest.java
+++ portal-impl/test/integration/com/liferay/portal/verify/VerifyUUIDTest.java
@@ -54,12 +54,6 @@ public class VerifyUUIDTest extends BaseVerifyProcessTestCase {
 					"Unable to process runnable: Unknown column 'Unknown' " +
 						"in 'field list'",
 				expectedType = ExpectedType.EXACT
-			),
-			@ExpectedLog(
-				expectedLog =
-					"Unable to process runnable: ERROR: column \"unknown\" " +
-						"does not exist_  Position: 8 [Sanitized]",
-				expectedType = ExpectedType.EXACT
 			)
 		},
 		level = "ERROR", loggerClass = ThrowableAwareRunnable.class
@@ -86,12 +80,6 @@ public class VerifyUUIDTest extends BaseVerifyProcessTestCase {
 		expectedLogs = {
 			@ExpectedLog(
 				expectedLog = "Unable to process runnable: Table ",
-				expectedType = ExpectedType.PREFIX
-			),
-			@ExpectedLog(
-				expectedLog =
-					"Unable to process runnable: ERROR: relation " +
-						"\"unknown\" does not exist_  Position: 21 [Sanitized]",
 				expectedType = ExpectedType.PREFIX
 			)
 		},
@@ -131,12 +119,6 @@ public class VerifyUUIDTest extends BaseVerifyProcessTestCase {
 			@ExpectedLog(
 				expectedLog =
 					"Unable to process runnable: Table ",
-				expectedType = ExpectedType.PREFIX
-			),
-			@ExpectedLog(
-				expectedLog =
-					"Unable to process runnable: ERROR: relation \"unknown\" " +
-						"does not exist_  Position: 21 [Sanitized]",
 				expectedType = ExpectedType.PREFIX
 			)
 		},
