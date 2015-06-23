81a5bf0e0fccdcc0a19cf599f557d60c95f0e22f
shuyangzhou <shuyang.zhou@liferay.com>
2015-06-22 23:39:48 -0700

    Revert "LPS-55518 SF"
    This reverts commit c34a2f7677cb596c6b6b15d48d676056d9c62d3c.

--- modules/frontend/frontend-editors-web/src/com/liferay/frontend/editors/web/editor/configuration/BaseCKEditorConfigContributor.java
+++ modules/frontend/frontend-editors-web/src/com/liferay/frontend/editors/web/editor/configuration/BaseCKEditorConfigContributor.java
@@ -31,6 +31,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * @author Ambrin Chaudhary
  */
+
 public class BaseCKEditorConfigContributor extends BaseEditorConfigContributor {
 
 	@Override
