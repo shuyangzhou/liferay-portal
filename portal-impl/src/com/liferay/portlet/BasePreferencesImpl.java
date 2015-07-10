bfaa1104be9ed4796923834d77dca02c439809be
Matthew Tambara <matthew.tambara@liferay.com>
2015-07-10 16:00:11 -0700

    LPS-56505 Add reloadable store in case of StaleObjectStateException

--- portal-impl/src/com/liferay/portlet/BasePreferencesImpl.java
+++ portal-impl/src/com/liferay/portlet/BasePreferencesImpl.java
@@ -25,10 +25,13 @@ import java.util.Collections;
 import java.util.Enumeration;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.Callable;
 import java.util.concurrent.ConcurrentHashMap;
 
 import javax.portlet.ReadOnlyException;
 import javax.portlet.ValidatorException;
+
+import org.hibernate.StaleObjectStateException;
 
 /**
  * @author Alexander Chow
@@ -214,6 +217,22 @@ public abstract class BasePreferencesImpl implements Serializable {
 
 	public abstract void store() throws IOException, ValidatorException;
 
+	protected boolean causedByStaleObjectException(Throwable t) {
+		Throwable cause = t.getCause();
+
+		while ((t != null) && !t.equals(cause)) {
+			if (t instanceof StaleObjectStateException) {
+				return true;
+			}
+
+			t = cause;
+
+			cause = t.getCause();
+		}
+
+		return false;
+	}
+
 	protected String getActualValue(String value) {
 		if ((value == null) || value.equals(_NULL_VALUE)) {
 			return null;
@@ -304,6 +323,30 @@ public abstract class BasePreferencesImpl implements Serializable {
 		}
 
 		return false;
+	}
+
+	protected void reload() throws UnsupportedOperationException {
+		throw new UnsupportedOperationException();
+	};
+
+	protected Object reloadableStore(Callable callable) throws Exception {
+		while (true) {
+			try {
+				callable.call();
+
+				store();
+
+				return null;
+			}
+			catch (Exception e) {
+				if (causedByStaleObjectException(e)) {
+					reload();
+				}
+				else {
+					throw e;
+				}
+			}
+		}
 	}
 
 	protected void setOriginalXML(String xml) {
