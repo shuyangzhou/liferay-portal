1b566a1d5a7145f2113f599263f5953417cfc84b
Matthew Tambara <matthew.tambara@liferay.com>
2015-08-31 10:14:25 -0700

    LPS-58278 Missed a few

--- portal-impl/test/integration/com/liferay/portal/dao/orm/SQLNullTest.java
+++ portal-impl/test/integration/com/liferay/portal/dao/orm/SQLNullTest.java
@@ -92,7 +92,7 @@ import org.junit.Test;
  * 			<code>FALSE</code><sup>*</sup>
  * 		</td>
  *		<td>
- * 			<code>NULL</code>
+ * 			<code>NULL</code><sup>*</sup>
  * 		</td>
  * 	</tr>
  *
@@ -113,7 +113,7 @@ import org.junit.Test;
  * 			<code>TRUE</code><sup>*</sup>
  * 		</td>
  *		<td>
- * 			<code>NULL</code>
+ * 			<code>NULL</code><sup>*</sup>
  * 		</td>
  * 	</tr>
  *
@@ -224,7 +224,7 @@ import org.junit.Test;
  * 			<code>TRUE</code><sup>*</sup>
  * 		</td>
  * 		<td>
- * 			<code>NULL</code>
+ * 			<code>NULL</code><sup>*</sup>
  * 		</td>
  * 	</tr>
  *
@@ -245,7 +245,7 @@ import org.junit.Test;
  * 			<code>FALSE</code><sup>*</sup>
  * 		</td>
  * 		<td>
- * 			<code>NULL</code>
+ * 			<code>NULL</code><sup>*</sup>
  * 		</td>
  * 	</tr>
  *
@@ -308,7 +308,7 @@ import org.junit.Test;
  *			<code>FALSE</code>
  * 		</td>
  * 		<td>
- * 			<code>NULL</code>
+ * 			<code>NULL</code><sup>*</sup>
  * 		</td>
  * 	</tr>
  *
@@ -329,7 +329,7 @@ import org.junit.Test;
  * 			<code>TRUE</code>
  * 		</td>
  * 		<td>
- * 			<code>NULL</code>
+ * 			<code>NULL</code><sup>*</sup>
  * 		</td>
  * 	</tr>
  *
@@ -356,7 +356,7 @@ import org.junit.Test;
  * 			<code>FALSE</code>
  * 		</td>
  * 		<td>
- * 			<code>NULL</code>
+ * 			<code>NULL</code><sup>*</sup>
  * 		</td>
  * 	</tr>
  *
@@ -377,7 +377,7 @@ import org.junit.Test;
  * 			<code>TRUE</code>
  * 		</td>
  * 		<td>
- * 			<code>NULL</code>
+ * 			<code>NULL</code><sup>*</sup>
  * 		</td>
  * 	</tr>
  *
