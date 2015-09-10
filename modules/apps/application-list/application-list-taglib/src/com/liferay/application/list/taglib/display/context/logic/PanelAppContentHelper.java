shuyangzhou/liferay-portal

commit 3ced98b92982f4b8d83c99948e796337f54b1593

LPS-57261 dispatching to core

modules/apps/application-list/application-list-taglib/src/com/liferay/application/list/taglib/display/context/logic/PanelAppContentHelper.java
@@ -15,7 +15,6 @@
 package com.liferay.application.list.taglib.display.context.logic;
 
 import com.liferay.portal.kernel.template.StringTemplateResource;
-import com.liferay.portal.kernel.util.JavaConstants;
 import com.liferay.portal.kernel.util.StringBundler;
 import com.liferay.portal.kernel.util.Validator;
 import com.liferay.portal.kernel.util.WebKeys;
@@ -26,7 +25,7 @@ import com.liferay.portal.model.Theme;
 import com.liferay.portal.service.LayoutTemplateLocalServiceUtil;
 import com.liferay.portal.service.PortletLocalServiceUtil;
 import com.liferay.portal.theme.ThemeDisplay;
-import com.liferay.portlet.PortletRequestImpl;
+import com.liferay.portal.util.PortalUtil;
 
 import java.io.Writer;
 
@@ -74,15 +73,8 @@ public class PanelAppContentHelper {
 		if (Validator.isNotNull(velocityTemplateId) &&
 			Validator.isNotNull(content)) {
 
-			HttpServletRequest request = _request;
-
-			PortletRequestImpl portletRequestImpl =
-				(PortletRequestImpl)_request.getAttribute(
-					JavaConstants.JAVAX_PORTLET_REQUEST);
-
-			if (portletRequestImpl != null) {
-				request = portletRequestImpl.getOriginalHttpServletRequest();
-			}
+			HttpServletRequest request = PortalUtil.getOriginalServletRequest(
+				_request);
 
 			StringBundler sb = RuntimePageUtil.getProcessedTemplate(
 				request, _response, getPortletId(),

portal-impl/src/com/liferay/portal/model/impl/LayoutTypeControllerImpl.java
@@ -18,6 +18,7 @@ import com.liferay.portal.kernel.configuration.Filter;
 import com.liferay.portal.kernel.exception.SystemException;
 import com.liferay.portal.kernel.io.unsync.UnsyncStringWriter;
 import com.liferay.portal.kernel.servlet.BrowserSnifferUtil;
+import com.liferay.portal.kernel.servlet.DirectRequestDispatcherFactoryUtil;
 import com.liferay.portal.kernel.util.GetterUtil;
 import com.liferay.portal.kernel.util.ParamUtil;
 import com.liferay.portal.kernel.util.PropsKeys;
@@ -125,7 +126,8 @@ public class LayoutTypeControllerImpl implements LayoutTypeController {
 			WebKeys.CTX);
 
 		RequestDispatcher requestDispatcher =
-			servletContext.getRequestDispatcher(getEditPage());
+			DirectRequestDispatcherFactoryUtil.getRequestDispatcher(
+				servletContext, getEditPage());
 
 		UnsyncStringWriter unsyncStringWriter = new UnsyncStringWriter();
 
@@ -151,7 +153,8 @@ public class LayoutTypeControllerImpl implements LayoutTypeController {
 		String path = getViewPath(portletId, BrowserSnifferUtil.isWap(request));
 
 		RequestDispatcher requestDispatcher =
-			servletContext.getRequestDispatcher(path);
+			DirectRequestDispatcherFactoryUtil.getRequestDispatcher(
+				servletContext, path);
 
 		UnsyncStringWriter unsyncStringWriter = new UnsyncStringWriter();
 

portal-impl/src/com/liferay/portal/servlet/DirectRequestDispatcher.java
@@ -30,8 +30,11 @@ import javax.servlet.http.HttpServletRequest;
  */
 public class DirectRequestDispatcher implements RequestDispatcher {
 
-	public DirectRequestDispatcher(Servlet servlet, String queryString) {
+	public DirectRequestDispatcher(
+		Servlet servlet, String path, String queryString) {
+
 		_servlet = servlet;
+		_path = path;
 		_queryString = queryString;
 	}
 
@@ -51,12 +54,16 @@ public class DirectRequestDispatcher implements RequestDispatcher {
 			ServletRequest servletRequest, ServletResponse servletResponse)
 		throws IOException, ServletException {
 
+		servletRequest.setAttribute(RequestDispatcher.INCLUDE_PATH_INFO, null);
+		servletRequest.setAttribute(
+			RequestDispatcher.INCLUDE_SERVLET_PATH, _path);
 		servletRequest = DynamicServletRequest.addQueryString(
 			(HttpServletRequest)servletRequest, _queryString);
 
 		_servlet.service(servletRequest, servletResponse);
 	}
 
+	private final String _path;
 	private final String _queryString;
 	private final Servlet _servlet;
 

portal-impl/src/com/liferay/portal/servlet/DirectRequestDispatcherFactoryImpl.java
@@ -117,7 +117,7 @@ public class DirectRequestDispatcherFactoryImpl
 			}
 
 			requestDispatcher = new DirectRequestDispatcher(
-				servlet, queryString);
+				servlet, path, queryString);
 		}
 
 		return _pacl.getRequestDispatcher(servletContext, requestDispatcher);

portal-impl/src/com/liferay/portal/servlet/DirectServletPathRegisterDispatcher.java
@@ -50,6 +50,9 @@ public class DirectServletPathRegisterDispatcher implements RequestDispatcher {
 			ServletRequest servletRequest, ServletResponse servletResponse)
 		throws IOException, ServletException {
 
+		servletRequest.setAttribute(RequestDispatcher.INCLUDE_PATH_INFO, null);
+		servletRequest.setAttribute(
+			RequestDispatcher.INCLUDE_SERVLET_PATH, _path);
 		servletRequest.setAttribute(WebKeys.SERVLET_PATH, _path);
 
 		_requestDispatcher.include(servletRequest, servletResponse);

util-taglib/src/com/liferay/taglib/ui/ToggleTag.java
@@ -14,6 +14,7 @@
 
 package com.liferay.taglib.ui;
 
+import com.liferay.portal.kernel.servlet.DirectRequestDispatcherFactoryUtil;
 import com.liferay.portal.kernel.util.DeterminateKeyGenerator;
 import com.liferay.portal.kernel.util.StringPool;
 import com.liferay.portal.kernel.util.Validator;
@@ -116,7 +117,8 @@ public class ToggleTag extends IncludeTag {
 			"liferay-ui:toggle:defaultMessage", defaultMessage);
 
 		RequestDispatcher requestDispatcher =
-			servletContext.getRequestDispatcher(page);
+			DirectRequestDispatcherFactoryUtil.getRequestDispatcher(
+				servletContext, page);
 
 		requestDispatcher.include(request, response);
 	}

util-taglib/src/com/liferay/taglib/util/PortalIncludeUtil.java
@@ -14,6 +14,7 @@
 
 package com.liferay.taglib.util;
 
+import com.liferay.portal.kernel.servlet.DirectRequestDispatcherFactoryUtil;
 import com.liferay.portal.kernel.util.WebKeys;
 import com.liferay.taglib.servlet.PipingServletResponse;
 
@@ -57,7 +58,8 @@ public class PortalIncludeUtil {
 			WebKeys.CTX);
 
 		RequestDispatcher requestDispatcher =
-			servletContext.getRequestDispatcher(path);
+			DirectRequestDispatcherFactoryUtil.getRequestDispatcher(
+				servletContext, path);
 
 		requestDispatcher.include(
 			request, new PipingServletResponse(response, pageContext.getOut()));

commit dcdd994c232ec8fbd34c83a9f80846999f5cca78

LPS-58357 LPS-57261 Wrong dispatching request selecting, caused by 3ced98b92982f4b8d83c99948e796337f54b1593

modules/apps/application-list/application-list-taglib/src/com/liferay/application/list/taglib/display/context/logic/PanelAppContentHelper.java
@@ -15,6 +15,7 @@
 package com.liferay.application.list.taglib.display.context.logic;
 
 import com.liferay.portal.kernel.template.StringTemplateResource;
+import com.liferay.portal.kernel.util.JavaConstants;
 import com.liferay.portal.kernel.util.StringBundler;
 import com.liferay.portal.kernel.util.Validator;
 import com.liferay.portal.kernel.util.WebKeys;
@@ -26,6 +27,7 @@ import com.liferay.portal.service.LayoutTemplateLocalServiceUtil;
 import com.liferay.portal.service.PortletLocalServiceUtil;
 import com.liferay.portal.theme.ThemeDisplay;
 import com.liferay.portal.util.PortalUtil;
+import com.liferay.portlet.PortletRequestImpl;
 
 import java.io.Writer;
 
@@ -73,8 +75,18 @@ public class PanelAppContentHelper {
 		if (Validator.isNotNull(velocityTemplateId) &&
 			Validator.isNotNull(content)) {
 
-			HttpServletRequest request = PortalUtil.getOriginalServletRequest(
-				_request);
+			PortletRequestImpl portletRequestImpl =
+				(PortletRequestImpl)_request.getAttribute(
+					JavaConstants.JAVAX_PORTLET_REQUEST);
+
+			HttpServletRequest request = null;
+
+			if (portletRequestImpl == null) {
+				request = PortalUtil.getOriginalServletRequest(_request);
+			}
+			else {
+				request = portletRequestImpl.getOriginalHttpServletRequest();
+			}
 
 			StringBundler sb = RuntimePageUtil.getProcessedTemplate(
 				request, _response, getPortletId(),