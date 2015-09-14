72707e50bf64464ee9a1f4afb0c2cb493bd250e2
Matthew Tambara <matthew.tambara@liferay.com>
2015-09-14 14:14:19 -0700

    LPS-58278 Remove useless methods

--- portal-impl/src/com/liferay/portal/service/impl/PortletPreferencesLocalServiceImpl.java
+++ portal-impl/src/com/liferay/portal/service/impl/PortletPreferencesLocalServiceImpl.java
@@ -369,7 +369,7 @@ public class PortletPreferencesLocalServiceImpl
 		return portletPreferences;
 	}
 
-	protected javax.portlet.PortletPreferences doGetPreferences(
+	protected javax.portlet.PortletPreferences getPreferences(
 		long companyId, long ownerId, int ownerType, long plid,
 		String portletId, String defaultPreferences, boolean strict) {
 
@@ -409,15 +409,6 @@ public class PortletPreferencesLocalServiceImpl
 				portletPreferences.getPreferences());
 
 		return portletPreferencesImpl;
-	}
-
-	protected javax.portlet.PortletPreferences getPreferences(
-		long companyId, long ownerId, int ownerType, long plid,
-		String portletId, String defaultPreferences, boolean strict) {
-
-		return doGetPreferences(
-			companyId, ownerId, ownerType, plid, portletId, defaultPreferences,
-			strict);
 	}
 
 	private static final Log _log = LogFactoryUtil.getLog(
