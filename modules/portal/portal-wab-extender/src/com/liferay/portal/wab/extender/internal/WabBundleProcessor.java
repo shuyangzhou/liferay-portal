cc2b150581fd0a22ffc4fc712a8e0db25bbf6997
shuyangzhou <shuyang.zhou@liferay.com>
2015-10-13 17:13:09 -0700

    More output

--- modules/portal/portal-wab-extender/src/com/liferay/portal/wab/extender/internal/WabBundleProcessor.java
+++ modules/portal/portal-wab-extender/src/com/liferay/portal/wab/extender/internal/WabBundleProcessor.java
@@ -550,6 +550,15 @@ public class WabBundleProcessor implements ServletContextListener {
 				ServletContextListener.class,
 				servletContextListenerExceptionAdaptor, properties);
 
+			Object service = _bundleContext.getService(serviceRegistration.getReference());
+
+			if (service != servletContextListenerExceptionAdaptor) {
+				System.out.println("##########WabBundleProcessor this is not possible, added as " + servletContextListenerExceptionAdaptor + ", got as " + service + ", " + listenerDefinition.getEventListener() + ", for " + _bundleContext.getBundle());
+			}
+			else {
+				System.out.println("##########WabBundleProcessor get after add " + listenerDefinition.getEventListener() + ", for " + _bundleContext.getBundle());
+			}
+
 			Exception exception =
 				servletContextListenerExceptionAdaptor.getException();
 
