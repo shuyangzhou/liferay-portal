shuyangzhou/liferay-portal

commit 440d5c5e5a9ede001c62d4d44f24fb67646993e2

LPS-27165 Source formatting

portal-impl/src/com/liferay/portal/upgrade/v6_1_0/UpgradeImageGallery.java
@@ -23,8 +23,11 @@ import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.upgrade.UpgradeProcess;
 import com.liferay.portal.kernel.util.FileUtil;
+import com.liferay.portal.kernel.util.GetterUtil;
 import com.liferay.portal.kernel.util.MimeTypesUtil;
 import com.liferay.portal.kernel.util.PortalClassLoaderUtil;
+import com.liferay.portal.kernel.util.PropsKeys;
+import com.liferay.portal.kernel.util.PropsUtil;
 import com.liferay.portal.kernel.util.StringBundler;
 import com.liferay.portal.kernel.util.StringPool;
 import com.liferay.portal.kernel.util.Validator;
@@ -40,6 +43,7 @@ import com.liferay.portlet.documentlibrary.util.ImageProcessorUtil;
 import java.io.InputStream;
 
 import java.sql.Connection;
+import java.sql.DatabaseMetaData;
 import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -475,6 +479,80 @@ public class UpgradeImageGallery extends UpgradeProcess {
 		}
 	}
 
+	protected void removeConflictingIGPermissions(
+			String igResourceName, String dlResourceName)
+		throws Exception {
+
+		Connection con = null;
+		PreparedStatement ps = null;
+		ResultSet rs = null;
+
+		try {
+			con = DataAccess.getConnection();
+
+			DatabaseMetaData databaseMetaData = con.getMetaData();
+
+			boolean supportsBatchUpdates =
+				databaseMetaData.supportsBatchUpdates();
+
+			StringBundler sb = new StringBundler();
+
+			sb.append("select companyId, scope, primKey, roleId from ");
+			sb.append("ResourcePermission where name = ?");
+
+			String sql = sb.toString();
+
+			ps = con.prepareStatement(sql);
+
+			ps.setString(1, igResourceName);
+
+			rs = ps.executeQuery();
+
+			sb = new StringBundler();
+
+			sb.append("delete from ResourcePermission where name = ? and ");
+			sb.append("companyId = ? and scope = ? and primKey = ? and ");
+			sb.append("roleId = ?");
+
+			sql = sb.toString();
+
+			ps = con.prepareStatement(sql);
+
+			int count = 0;
+
+			while (rs.next()) {
+				ps.setString(1, dlResourceName);
+				ps.setLong(2, rs.getLong("companyId"));
+				ps.setLong(3, rs.getLong("scope"));
+				ps.setLong(4, rs.getLong("primKey"));
+				ps.setLong(5, rs.getLong("roleId"));
+
+				if (supportsBatchUpdates) {
+					ps.addBatch();
+
+					if (count == _BATCH_SIZE) {
+						ps.executeBatch();
+
+						count = 0;
+					}
+					else {
+						count++;
+					}
+				}
+				else {
+				 	ps.executeUpdate();
+				}
+			}
+
+			if (supportsBatchUpdates && (count > 0)) {
+				ps.executeBatch();
+			}
+		}
+		finally {
+			DataAccess.cleanUp(con, ps, rs);
+		}
+	}
+
 	protected void updateIGFolderEntries() throws Exception {
 		Connection con = null;
 		PreparedStatement ps = null;
@@ -526,37 +604,12 @@ public class UpgradeImageGallery extends UpgradeProcess {
 	}
 
 	protected void updateIGFolderPermissions() throws Exception {
-		runSQL(
-			"delete from ResourcePermission where " +
-				"name = 'com.liferay.portlet.imagegallery.model.IGFolder' " +
-					"and primKey = '0'");
+		removeConflictingIGPermissions(
+			_IG_FOLDER_CLASS_NAME, DLFolder.class.getName());
 
-		runSQL(
-			"delete from ResourcePermission where " +
-				"name = 'com.liferay.portlet.imagegallery.model.IGFolder' " +
-					"and primKey = companyId");
-
-		Connection con = null;
-		PreparedStatement ps = null;
-		ResultSet rs = null;
-
-		try {
-			con = DataAccess.getConnection();
-
-			StringBundler sb = new StringBundler(4);
-
-			sb.append("update ResourcePermission set name = '");
-			sb.append(DLFolder.class.getName());
-			sb.append("' where name = 'com.liferay.portlet.imagegallery.");
-			sb.append("model.IGFolder'");
-
-			ps = con.prepareStatement(sb.toString());
-
-			ps.executeUpdate();
-		}
-		finally {
-			DataAccess.cleanUp(con, ps, rs);
-		}
+		runSQL("update ResourcePermission set name = '" +
+			DLFolder.class.getName() +
+				"' where name = '" + _IG_FOLDER_CLASS_NAME + "'");
 	}
 
 	protected void updateIGImageEntries() throws Exception {
@@ -679,19 +732,20 @@ public class UpgradeImageGallery extends UpgradeProcess {
 	}
 
 	protected void updateIGImagePermissions() throws Exception {
-		runSQL(
-			"delete from ResourcePermission where name = '" +
-				_IG_IMAGE_CLASS_NAME + "' and primKey = '0'");
-
-		runSQL(
-			"delete from ResourcePermission where name = '" +
-				_IG_IMAGE_CLASS_NAME + "' and primKey = companyId");
+		removeConflictingIGPermissions(
+			_IG_IMAGE_CLASS_NAME, DLFileEntry.class.getName());
 
 		runSQL(
 			"update ResourcePermission set name = '" +
 				DLFileEntry.class.getName() + "' where name = '" +
 					_IG_IMAGE_CLASS_NAME + "'");
 	}
+
+	private static final int _BATCH_SIZE = GetterUtil.getInteger(
+		PropsUtil.get(PropsKeys.HIBERNATE_JDBC_BATCH_SIZE), 20);
+
+	private static final String _IG_FOLDER_CLASS_NAME =
+		"com.liferay.portlet.imagegallery.model.IGFolder";
 
 	private static final String _IG_IMAGE_CLASS_NAME =
 		"com.liferay.portlet.imagegallery.model.IGImage";

commit 118c83bd5841fbceb08e271f70eac92e92dfd3c9

LPS-6 The first ps got overwritten by the second ps, leaving the first one never closed. Caused by 440d5c5e5a9ede001c62d4d44f24fb67646993e2. Rename the second ps to ps2, preapre to fix the unclosed statement issue, no logic change.

portal-impl/src/com/liferay/portal/upgrade/v6_1_0/UpgradeImageGallery.java
@@ -353,7 +353,7 @@ public class UpgradeImageGallery extends UpgradeProcess {
 
 			rs = ps.executeQuery();
 
-			ps = con.prepareStatement(
+			PreparedStatement ps2 = con.prepareStatement(
 				"delete from ResourcePermission where name = ? and " +
 					"companyId = ? and scope = ? and primKey = ? and " +
 						"roleId = ?");
@@ -361,17 +361,17 @@ public class UpgradeImageGallery extends UpgradeProcess {
 			int count = 0;
 
 			while (rs.next()) {
-				ps.setString(1, dlResourceName);
-				ps.setLong(2, rs.getLong("companyId"));
-				ps.setInt(3, rs.getInt("scope"));
-				ps.setString(4, rs.getString("primKey"));
-				ps.setLong(5, rs.getLong("roleId"));
+				ps2.setString(1, dlResourceName);
+				ps2.setLong(2, rs.getLong("companyId"));
+				ps2.setInt(3, rs.getInt("scope"));
+				ps2.setString(4, rs.getString("primKey"));
+				ps2.setLong(5, rs.getLong("roleId"));
 
 				if (supportsBatchUpdates) {
-					ps.addBatch();
+					ps2.addBatch();
 
 					if (count == PropsValues.HIBERNATE_JDBC_BATCH_SIZE) {
-						ps.executeBatch();
+						ps2.executeBatch();
 
 						count = 0;
 					}
@@ -380,12 +380,12 @@ public class UpgradeImageGallery extends UpgradeProcess {
 					}
 				}
 				else {
-					ps.executeUpdate();
+					ps2.executeUpdate();
 				}
 			}
 
 			if (supportsBatchUpdates && (count > 0)) {
-				ps.executeBatch();
+				ps2.executeBatch();
 			}
 		}
 		finally {