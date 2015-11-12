b4763ce23a28db5b95b26541155ea4853d62a541
Preston-Crary <preston.crary@liferay.com>
2015-11-12 14:59:54 -0800

    LPS-56075 Remove expensive hasGroup check when we always add the group anyway.

--- modules/portal/portal-search/src/main/java/com/liferay/portal/search/internal/SearchPermissionCheckerImpl.java
+++ modules/portal/portal-search/src/main/java/com/liferay/portal/search/internal/SearchPermissionCheckerImpl.java
@@ -248,15 +248,14 @@ public class SearchPermissionCheckerImpl implements SearchPermissionChecker {
 			groupIdsToRoles);
 
 		return doGetPermissionFilter_6(
-			companyId, groupIds, userId, permissionChecker, className,
-			booleanFilter, groups, roles, groupIdsToRoles);
+			companyId, userId, permissionChecker, className, booleanFilter,
+			groups, roles, groupIdsToRoles);
 	}
 
 	protected BooleanFilter doGetPermissionFilter_6(
-			long companyId, long[] groupIds, long userId,
-			PermissionChecker permissionChecker, String className,
-			BooleanFilter booleanFilter, Set<Group> groups, Set<Role> roles,
-			Map<Long, List<Role>> groupIdsToRoles)
+			long companyId, long userId, PermissionChecker permissionChecker,
+			String className, BooleanFilter booleanFilter, Set<Group> groups,
+			Set<Role> roles, Map<Long, List<Role>> groupIdsToRoles)
 		throws Exception {
 
 		BooleanFilter permissionBooleanFilter = new BooleanFilter();
@@ -333,18 +332,6 @@ public class SearchPermissionCheckerImpl implements SearchPermissionChecker {
 				}
 			}
 
-			if (ArrayUtil.isNotEmpty(groupIds)) {
-				for (long groupId : groupIds) {
-					if (_resourcePermissionLocalService.hasResourcePermission(
-							companyId, className, ResourceConstants.SCOPE_GROUP,
-							String.valueOf(groupId), role.getRoleId(),
-							ActionKeys.VIEW)) {
-
-						groupsTermsFilter.addValue(String.valueOf(groupId));
-					}
-				}
-			}
-
 			rolesTermsFilter.addValue(String.valueOf(role.getRoleId()));
 		}
 
@@ -418,13 +405,7 @@ public class SearchPermissionCheckerImpl implements SearchPermissionChecker {
 			groups.addAll(userBag.getUserOrgGroups());
 		}
 		else {
-			for (long groupId : groupIds) {
-				if (_groupLocalService.hasUserGroup(userId, groupId)) {
-					Group group = _groupLocalService.getGroup(groupId);
-
-					groups.add(group);
-				}
-			}
+			groups.addAll(_groupLocalService.getGroups(groupIds));
 		}
 
 		if (permissionChecker.isSignedIn()) {
