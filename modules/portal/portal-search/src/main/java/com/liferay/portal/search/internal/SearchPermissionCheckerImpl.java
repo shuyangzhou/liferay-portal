a3bf47259589a80ea200942853b6bbe1f0a46e82
Preston-Crary <preston.crary@liferay.com>
2015-11-12 14:46:25 -0800

    LPS-56075 Fix test failure, we do not need to index the permission fields if we are permission unaware.

--- modules/portal/portal-search/src/main/java/com/liferay/portal/search/internal/SearchPermissionCheckerImpl.java
+++ modules/portal/portal-search/src/main/java/com/liferay/portal/search/internal/SearchPermissionCheckerImpl.java
@@ -395,7 +395,9 @@ public class SearchPermissionCheckerImpl implements SearchPermissionChecker {
 
 		Indexer<?> indexer = _indexerRegistry.nullSafeGetIndexer(resourceName);
 
-		indexer.reindex(resourceName, GetterUtil.getLong(resourceClassPK));
+		if (indexer.isPermissionAware()) {
+			indexer.reindex(resourceName, GetterUtil.getLong(resourceClassPK));
+		}
 	}
 
 	protected void populate(
