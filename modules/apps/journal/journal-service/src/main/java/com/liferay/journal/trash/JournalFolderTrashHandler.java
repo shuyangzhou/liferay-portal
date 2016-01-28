360b6c039539bc09c1d2c70ae29c86cd3fee9af5
Preston-Crary <preston.crary@liferay.com>
2016-01-27 17:34:43 -0800

    LPS-61952 Asset: Update java file usages

--- modules/apps/journal/journal-service/src/main/java/com/liferay/journal/trash/JournalFolderTrashHandler.java
+++ modules/apps/journal/journal-service/src/main/java/com/liferay/journal/trash/JournalFolderTrashHandler.java
@@ -14,6 +14,8 @@
 
 package com.liferay.journal.trash;
 
+import com.liferay.asset.kernel.AssetRendererFactoryRegistryUtil;
+import com.liferay.asset.kernel.model.AssetRendererFactory;
 import com.liferay.journal.exception.InvalidDDMStructureException;
 import com.liferay.journal.model.JournalFolder;
 import com.liferay.journal.model.JournalFolderConstants;
@@ -29,8 +31,6 @@ import com.liferay.portal.kernel.trash.TrashRenderer;
 import com.liferay.portal.kernel.util.Validator;
 import com.liferay.portal.model.ContainerModel;
 import com.liferay.portal.service.ServiceContext;
-import com.liferay.portlet.asset.AssetRendererFactoryRegistryUtil;
-import com.liferay.portlet.asset.model.AssetRendererFactory;
 import com.liferay.portlet.trash.exception.RestoreEntryException;
 import com.liferay.portlet.trash.model.TrashEntry;
 import com.liferay.portlet.trash.model.TrashEntryConstants;
