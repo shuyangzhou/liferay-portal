cfe05617543e4b8e8c61c5acf66d0fd28b089d82
shuyangzhou <shuyang.zhou@liferay.com>
2016-02-04 09:45:57 -0800

    LPS-61952 Regenerate

--- portal-service/src/com/liferay/portal/service/UserLocalServiceUtil.java
+++ portal-service/src/com/liferay/portal/service/UserLocalServiceUtil.java
@@ -1101,7 +1101,7 @@ public class UserLocalServiceUtil {
 	}
 
 	public static com.liferay.portal.kernel.dao.orm.ExportActionableDynamicQuery getExportActionableDynamicQuery(
-		com.liferay.portlet.exportimport.lar.PortletDataContext portletDataContext) {
+		com.liferay.exportimport.kernel.lar.PortletDataContext portletDataContext) {
 		return getService().getExportActionableDynamicQuery(portletDataContext);
 	}
 
