39d909b65d87f5cddc3c6a00848448d32cbe4cb2
Preston-Crary <preston.crary@liferay.com>
2016-02-12 17:42:36 -0800

    LPS-56075 Pass roleIdArrays instead of querrying one by one. If there are R roles and G groups we were doing (2 * R * G + 2) queries, now we do (2 * G + 2) queries.

--- modules/portal-search/portal-search/src/main/java/com/liferay/portal/search/internal/SearchPermissionCheckerImpl.java
+++ modules/portal-search/portal-search/src/main/java/com/liferay/portal/search/internal/SearchPermissionCheckerImpl.java
@@ -66,6 +66,7 @@ import org.osgi.service.component.annotations.Reference;
  * @author Bruno Farache
  * @author Raymond Augé
  * @author Amos Fong
+ * @author Preston Crary
  */
 @Component(immediate = true, service = SearchPermissionChecker.class)
 public class SearchPermissionCheckerImpl implements SearchPermissionChecker {
@@ -252,63 +253,71 @@ public class SearchPermissionCheckerImpl implements SearchPermissionChecker {
 			Field.GROUP_ROLE_ID);
 		TermsFilter rolesTermsFilter = new TermsFilter(Field.ROLE_ID);
 
+		List<Long> roleIds = new ArrayList<>(roles.size());
+		List<Long> regularRoleIds = new ArrayList<>();
+
 		for (Role role : roles) {
-			if (_resourcePermissionLocalService.hasResourcePermission(
-					companyId, className, ResourceConstants.SCOPE_COMPANY,
-					String.valueOf(companyId), role.getRoleId(),
-					ActionKeys.VIEW)) {
+			roleIds.add(role.getRoleId());
 
-				return booleanFilter;
-			}
-
-			if ((role.getType() == RoleConstants.TYPE_REGULAR) &&
-				_resourcePermissionLocalService.hasResourcePermission(
-					companyId, className,
-					ResourceConstants.SCOPE_GROUP_TEMPLATE,
-					String.valueOf(GroupConstants.DEFAULT_PARENT_GROUP_ID),
-					role.getRoleId(), ActionKeys.VIEW)) {
-
-				return booleanFilter;
-			}
-
-			for (Group group : groups) {
-				if (permissionChecker.isGroupAdmin(
-						group.getGroupId()) ||
-					_resourcePermissionLocalService.hasResourcePermission(
-						companyId, className, ResourceConstants.SCOPE_GROUP,
-						String.valueOf(group.getGroupId()), role.getRoleId(),
-						ActionKeys.VIEW)) {
-
-					groupsTermsFilter.addValue(
-						String.valueOf(group.getGroupId()));
-				}
-
-				if ((role.getType() != RoleConstants.TYPE_REGULAR) &&
-					_resourcePermissionLocalService.hasResourcePermission(
-						companyId, className,
-						ResourceConstants.SCOPE_GROUP_TEMPLATE,
-						String.valueOf(GroupConstants.DEFAULT_PARENT_GROUP_ID),
-						role.getRoleId(), ActionKeys.VIEW)) {
-
-					Set<Role> groupRoles = groupIdsToRoles.get(
-						group.getGroupId());
-
-					if (groupRoles.contains(role)) {
-						groupsTermsFilter.addValue(
-							String.valueOf(group.getGroupId()));
-					}
-				}
-
-				Set<Role> groupRoles = groupIdsToRoles.get(group.getGroupId());
-
-				for (Role groupRole : groupRoles) {
-					groupRolesTermsFilter.addValue(
-						group.getGroupId() + StringPool.DASH +
-							groupRole.getRoleId());
-				}
+			if (role.getType() == RoleConstants.TYPE_REGULAR) {
+				regularRoleIds.add(role.getRoleId());
 			}
 
 			rolesTermsFilter.addValue(String.valueOf(role.getRoleId()));
+		}
+
+		long[] roleIdsArray = ArrayUtil.toLongArray(roleIds);
+
+		if (_resourcePermissionLocalService.hasResourcePermission(
+				companyId, className, ResourceConstants.SCOPE_COMPANY,
+				String.valueOf(companyId), roleIdsArray, ActionKeys.VIEW)) {
+
+			return booleanFilter;
+		}
+
+		if (_resourcePermissionLocalService.hasResourcePermission(
+				companyId, className, ResourceConstants.SCOPE_GROUP_TEMPLATE,
+				String.valueOf(GroupConstants.DEFAULT_PARENT_GROUP_ID),
+				ArrayUtil.toLongArray(regularRoleIds), ActionKeys.VIEW)) {
+
+			return booleanFilter;
+		}
+
+		for (Group group : groups) {
+			if (permissionChecker.isGroupAdmin(
+					group.getGroupId()) ||
+				_resourcePermissionLocalService.hasResourcePermission(
+					companyId, className, ResourceConstants.SCOPE_GROUP,
+					String.valueOf(group.getGroupId()), roleIdsArray,
+					ActionKeys.VIEW)) {
+
+				groupsTermsFilter.addValue(String.valueOf(group.getGroupId()));
+			}
+
+			Set<Role> groupRoles = groupIdsToRoles.get(group.getGroupId());
+
+			long[] groupRoleIds = new long[groupRoles.size()];
+
+			int i = 0;
+
+			for (Role groupRole : groupRoles) {
+				groupRoleIds[i++] = groupRole.getRoleId();
+			}
+
+			if (_resourcePermissionLocalService.hasResourcePermission(
+					companyId, className,
+					ResourceConstants.SCOPE_GROUP_TEMPLATE,
+					String.valueOf(GroupConstants.DEFAULT_PARENT_GROUP_ID),
+					groupRoleIds, ActionKeys.VIEW)) {
+
+				groupsTermsFilter.addValue(String.valueOf(group.getGroupId()));
+			}
+
+			for (Role groupRole : groupRoles) {
+				groupRolesTermsFilter.addValue(
+					group.getGroupId() + StringPool.DASH +
+						groupRole.getRoleId());
+			}
 		}
 
 		if (!groupsTermsFilter.isEmpty()) {
