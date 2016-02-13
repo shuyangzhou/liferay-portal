8b52e08b33fcb8b147f4c8c3fce834ef52b1e3bb
Preston-Crary <preston.crary@liferay.com>
2016-02-12 17:05:14 -0800

    LPS-56075 revert LPS-52099 wrong fix

--- modules/portal-search/portal-search/src/main/java/com/liferay/portal/search/internal/SearchPermissionCheckerImpl.java
+++ modules/portal-search/portal-search/src/main/java/com/liferay/portal/search/internal/SearchPermissionCheckerImpl.java
@@ -432,6 +432,8 @@ public class SearchPermissionCheckerImpl implements SearchPermissionChecker {
 				_userGroupRoleLocalService.getUserGroupRoles(userId));
 		}
 		else {
+			groups.addAll(userBag.getGroups());
+
 			for (long groupId : groupIds) {
 				if (_groupLocalService.hasUserGroup(userId, groupId)) {
 					Group group = _groupLocalService.getGroup(groupId);
