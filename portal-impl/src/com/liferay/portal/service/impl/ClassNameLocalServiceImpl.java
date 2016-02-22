fd04a45080de64efcf02dd5eebc5d36229c1bbbc
shuyangzhou <shuyang.zhou@liferay.com>
2016-02-22 00:42:16 -0800

    Temp solution

--- portal-impl/src/com/liferay/portal/service/impl/ClassNameLocalServiceImpl.java
+++ portal-impl/src/com/liferay/portal/service/impl/ClassNameLocalServiceImpl.java
@@ -54,6 +54,7 @@ public class ClassNameLocalServiceImpl
 	@Override
 	@Transactional(propagation = Propagation.SUPPORTS, readOnly = true)
 	public void checkClassNames() {
+		_classNames.clear();
 		List<ClassName> classNames = classNamePersistence.findAll();
 
 		for (ClassName className : classNames) {
@@ -141,7 +142,7 @@ public class ClassNameLocalServiceImpl
 
 	@Override
 	public void invalidate() {
-		_classNames.clear();
+//		_classNames.clear();
 
 		new Exception("**********Class name cache is cleared by : ").printStackTrace(System.out);
 	}
