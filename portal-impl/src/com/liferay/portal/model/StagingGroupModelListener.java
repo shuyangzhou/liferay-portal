5b5dc25f483b1cdc3be74c02b83effb1c58856c8
arbetts <andrew.betts@liferay.com>
2016-05-13 12:35:29 -0700

    LPS-61795 throw an exception if we are adding an association to a staged group; associations should only be made on the live site

---
+++ portal-impl/src/com/liferay/portal/model/StagingGroupModelListener.java
@@ -0,0 +1,49 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.model;
+
+import com.liferay.portal.kernel.exception.ModelListenerException;
+import com.liferay.portal.kernel.model.BaseModelListener;
+import com.liferay.portal.kernel.model.Group;
+import com.liferay.portal.kernel.service.persistence.GroupUtil;
+
+/**
+ * @author Andrew Betts
+ */
+public class StagingGroupModelListener extends BaseModelListener<Group> {
+
+	@Override
+	public void onBeforeAddAssociation(
+			Object groupId, String associationClassName,
+			Object associationClassPK)
+		throws ModelListenerException {
+
+		Group group = GroupUtil.fetchByPrimaryKey((long)groupId);
+
+		if ((group != null) && group.isStagingGroup()) {
+			throw new StagingGroupMembershipException(
+				"Associations cannot be added to Staging Groups");
+		}
+	}
+
+	public static class StagingGroupMembershipException
+		extends ModelListenerException {
+
+		public StagingGroupMembershipException(String msg) {
+			super(msg);
+		}
+	}
+
+}
