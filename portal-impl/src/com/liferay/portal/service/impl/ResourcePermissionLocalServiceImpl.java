shuyangzhou/liferay-portal

commit 687e02b417e49554a6fd9305b8bd8d89879faedc

LPS-67801 simplify

portal-impl/src/com/liferay/portal/service/impl/ResourcePermissionLocalServiceImpl.java
@@ -1389,21 +1389,27 @@ public class ResourcePermissionLocalServiceImpl
 
 			List<ResourcePermission> resourcePermissions = new ArrayList<>();
 
-			int i = 0;
+			int batchSize = 1000;
+			int start = 0;
 
-			while (i < (roleIds.length - 1000)) {
+			while (start < (roleIds.length - batchSize)) {
 				resourcePermissions.addAll(
 					resourcePermissionPersistence.findByC_N_S_P_R(
 						companyId, name, scope, primKey,
-						ArrayUtil.subset(roleIds, i, i + 1000)));
+						ArrayUtil.subset(roleIds, start, start + batchSize)));
 
-				i += 1000;
+				start += batchSize;
 			}
 
 			resourcePermissions.addAll(
 				resourcePermissionPersistence.findByC_N_S_P_R(
 					companyId, name, scope, primKey,
-					ArrayUtil.subset(roleIds, i, roleIds.length)));
+					ArrayUtil.subset(roleIds, start, roleIds.length)));
+
+			resourcePermissions.addAll(
+				resourcePermissionPersistence.findByC_N_S_P_R(
+					companyId, name, scope, primKey,
+					ArrayUtil.subset(roleIds, start, roleIds.length)));
 
 			for (ResourcePermission resourcePermission : resourcePermissions) {
 				long roleId = resourcePermission.getRoleId();

commit a5154f57d31f9a81510a5553ebda3bf567a2c066

LPS-67801 Remove second add. Caused by 687e02b417e49554a6fd9305b8bd8d89879faedc

portal-impl/src/com/liferay/portal/service/impl/ResourcePermissionLocalServiceImpl.java
@@ -1406,11 +1406,6 @@ public class ResourcePermissionLocalServiceImpl
 					companyId, name, scope, primKey,
 					ArrayUtil.subset(roleIds, start, roleIds.length)));
 
-			resourcePermissions.addAll(
-				resourcePermissionPersistence.findByC_N_S_P_R(
-					companyId, name, scope, primKey,
-					ArrayUtil.subset(roleIds, start, roleIds.length)));
-
 			for (ResourcePermission resourcePermission : resourcePermissions) {
 				long roleId = resourcePermission.getRoleId();
 				String[] actionIds = roleIdsToActionIds.remove(roleId);