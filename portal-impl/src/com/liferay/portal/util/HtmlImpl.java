495db268a98fd924959f812465c1f6693da7a088
Preston-Crary <preston.crary@liferay.com>
2016-09-15 14:04:16 -0700

    LPS-68190 SF

--- portal-impl/src/com/liferay/portal/util/HtmlImpl.java
+++ portal-impl/src/com/liferay/portal/util/HtmlImpl.java
@@ -252,7 +252,7 @@ public class HtmlImpl implements Html {
 				if ((mode == ESCAPE_MODE_CSS) && (i < (text.length() - 1))) {
 					char nextChar = text.charAt(i + 1);
 
-					if ((nextChar >= CharPool.NUMBER_0) &&
+					if ((CharPool.NUMBER_0 <= nextChar) &&
 						(nextChar <= CharPool.NUMBER_9)) {
 
 						sb.append(CharPool.SPACE);
@@ -841,9 +841,9 @@ public class HtmlImpl implements Html {
 	}
 
 	private boolean _isUnicodeCompatibilityCharacter(char c) {
-		if (((c >= '\u007f') && (c <= '\u0084')) ||
-			((c >= '\u0086') && (c <= '\u009f')) ||
-			((c >= '\ufdd0') && (c <= '\ufdef'))) {
+		if ((('\u007f' <= c) && (c <= '\u0084')) ||
+			(('\u0086' <= c) && (c <= '\u009f')) ||
+			(('\ufdd0' <= c) && (c <= '\ufdef'))) {
 
 			return true;
 		}
