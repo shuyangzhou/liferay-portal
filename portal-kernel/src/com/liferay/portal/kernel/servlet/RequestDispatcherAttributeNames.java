b22dbada358815cb93d5cd5148a8a089eb557ffd
Preston-Crary <preston.crary@liferay.com>
2016-12-08 18:02:36 -0800

    LPS-69632 Add protection against hash set lookup.

--- portal-kernel/src/com/liferay/portal/kernel/servlet/RequestDispatcherAttributeNames.java
+++ portal-kernel/src/com/liferay/portal/kernel/servlet/RequestDispatcherAttributeNames.java
@@ -27,10 +27,20 @@ import javax.portlet.MimeResponse;
 public class RequestDispatcherAttributeNames {
 
 	public static boolean contains(String name) {
+		if ((name == null) || (name.length() < _attributeNamesLowerBound) ||
+			(name.length() > _attributeNamesUpperBound) ||
+			!_attributeNamesHash[name.length() - _attributeNamesLowerBound]) {
+
+			return false;
+		}
+
 		return _attributeNames.contains(name);
 	}
 
 	private static final Set<String> _attributeNames = new HashSet<>();
+	private static final boolean[] _attributeNamesHash;
+	private static final int _attributeNamesLowerBound;
+	private static final int _attributeNamesUpperBound;
 
 	static {
 		_attributeNames.add(JavaConstants.JAVAX_SERVLET_FORWARD_CONTEXT_PATH);
@@ -44,6 +54,29 @@ public class RequestDispatcherAttributeNames {
 		_attributeNames.add(JavaConstants.JAVAX_SERVLET_INCLUDE_REQUEST_URI);
 		_attributeNames.add(JavaConstants.JAVAX_SERVLET_INCLUDE_SERVLET_PATH);
 		_attributeNames.add(MimeResponse.MARKUP_HEAD_ELEMENT);
+
+		int lowerBound = Integer.MAX_VALUE;
+		int upperBound = 0;
+
+		for (String name : _attributeNames) {
+			if (name.length() < lowerBound) {
+				lowerBound = name.length();
+			}
+
+			if (name.length() > upperBound) {
+				upperBound = name.length();
+			}
+		}
+
+		_attributeNamesLowerBound = lowerBound;
+		_attributeNamesUpperBound = upperBound;
+
+		_attributeNamesHash = new boolean[upperBound - lowerBound + 1];
+
+		for (String name : _attributeNames) {
+			_attributeNamesHash[name.length() - _attributeNamesLowerBound] =
+				true;
+		}
 	}
 
 }
