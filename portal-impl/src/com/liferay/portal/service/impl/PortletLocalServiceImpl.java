33d88345f38dc38aaed16d17c81c0320afac75a1
Matthew Tambara <matthew.tambara@liferay.com>
2017-03-23 11:35:43 -0700

    LPS-9 Extract out

--- portal-impl/src/com/liferay/portal/service/impl/PortletLocalServiceImpl.java
+++ portal-impl/src/com/liferay/portal/service/impl/PortletLocalServiceImpl.java
@@ -427,9 +427,7 @@ public class PortletLocalServiceImpl extends PortletLocalServiceBaseImpl {
 			new ArrayList<>();
 
 		for (Portlet portlet : getPortlets()) {
-			if (!portlet.isActive() || !portlet.isInclude() ||
-				!portlet.isReady() || portlet.isUndeployedPortlet()) {
-
+			if (!_isValid(portlet)) {
 				continue;
 			}
 
@@ -475,10 +473,7 @@ public class PortletLocalServiceImpl extends PortletLocalServiceBaseImpl {
 			Portlet portlet = _portletsMap.get(
 				portletIdFriendlyURLMapperComposite.getPortletId());
 
-			if ((portlet == null) || !portlet.isActive() ||
-				!portlet.isInclude() || !portlet.isReady() ||
-				portlet.isUndeployedPortlet()) {
-
+			if (!_isValid(portlet)) {
 				continue;
 			}
 
@@ -494,9 +489,7 @@ public class PortletLocalServiceImpl extends PortletLocalServiceBaseImpl {
 		List<FriendlyURLMapper> friendlyURLMappers = new ArrayList<>();
 
 		for (Portlet portlet : getPortlets()) {
-			if (!portlet.isActive() || !portlet.isInclude() ||
-				!portlet.isReady() || portlet.isUndeployedPortlet()) {
-
+			if (!_isValid(portlet)) {
 				continue;
 			}
 
@@ -940,6 +933,16 @@ public class PortletLocalServiceImpl extends PortletLocalServiceBaseImpl {
 		finally {
 			clearCache();
 		}
+	}
+
+	private boolean _isValid(Portlet portlet) {
+		if ((portlet == null) || !portlet.isActive() || !portlet.isInclude() ||
+			!portlet.isReady() || portlet.isUndeployedPortlet()) {
+
+			return false;
+		}
+
+		return true;
 	}
 
 	@Override
