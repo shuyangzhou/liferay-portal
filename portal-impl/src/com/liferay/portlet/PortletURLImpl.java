f92cdd581b61b8dae9b5b5ecde042a14f594f7a2
Preston-Crary <preston.crary@liferay.com>
2017-03-22 17:25:01 -0700

    LPS-71441 Only encode namespace when it might used.

--- portal-impl/src/com/liferay/portlet/PortletURLImpl.java
+++ portal-impl/src/com/liferay/portlet/PortletURLImpl.java
@@ -939,46 +939,48 @@ public class PortletURLImpl
 
 		int previousSbIndex = sb.index();
 
-		String namespace = getNamespace();
+		if (!renderParams.isEmpty()) {
+			String namespace = getNamespace();
 
-		String encodedNamespace = HttpUtil.encodeURL(namespace);
+			String encodedNamespace = HttpUtil.encodeURL(namespace);
 
-		for (Map.Entry<String, String[]> entry : renderParams.entrySet()) {
-			String name = entry.getKey();
-			String[] values = entry.getValue();
+			for (Map.Entry<String, String[]> entry : renderParams.entrySet()) {
+				String name = entry.getKey();
+				String[] values = entry.getValue();
 
-			if (isParameterIncludedInPath(name)) {
-				continue;
-			}
-
-			String publicRenderParameterName = getPublicRenderParameterName(
-				name);
-
-			if (Validator.isNotNull(publicRenderParameterName)) {
-				name = publicRenderParameterName;
-			}
-
-			boolean appendNamespace = false;
-
-			if (!PortalUtil.isReservedParameter(name) &&
-				!name.startsWith(
-					PortletQName.PUBLIC_RENDER_PARAMETER_NAMESPACE) &&
-				!name.startsWith(namespace)) {
-
-				appendNamespace = true;
-			}
-
-			name = HttpUtil.encodeURL(name);
-
-			for (String value : values) {
-				if (appendNamespace) {
-					sb.append(encodedNamespace);
+				if (isParameterIncludedInPath(name)) {
+					continue;
 				}
 
-				sb.append(name);
-				sb.append(StringPool.EQUAL);
-				sb.append(processValue(key, value));
-				sb.append(StringPool.AMPERSAND);
+				String publicRenderParameterName = getPublicRenderParameterName(
+					name);
+
+				if (Validator.isNotNull(publicRenderParameterName)) {
+					name = publicRenderParameterName;
+				}
+
+				boolean appendNamespace = false;
+
+				if (!PortalUtil.isReservedParameter(name) &&
+					!name.startsWith(
+						PortletQName.PUBLIC_RENDER_PARAMETER_NAMESPACE) &&
+					!name.startsWith(namespace)) {
+
+					appendNamespace = true;
+				}
+
+				name = HttpUtil.encodeURL(name);
+
+				for (String value : values) {
+					if (appendNamespace) {
+						sb.append(encodedNamespace);
+					}
+
+					sb.append(name);
+					sb.append(StringPool.EQUAL);
+					sb.append(processValue(key, value));
+					sb.append(StringPool.AMPERSAND);
+				}
 			}
 		}
 
