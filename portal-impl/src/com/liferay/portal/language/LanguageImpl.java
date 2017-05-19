7e82aa14c6a35cb9c518a585c381f908ccd37697
Preston-Crary <preston.crary@liferay.com>
2017-05-19 11:23:20 -0700

    LPS-72578 Extract method.

--- portal-impl/src/com/liferay/portal/language/LanguageImpl.java
+++ portal-impl/src/com/liferay/portal/language/LanguageImpl.java
@@ -861,6 +861,17 @@ public class LanguageImpl implements Language, Serializable {
 			resourceBundle = portletConfig.getResourceBundle(locale);
 		}
 
+		return _get(locale, resourceBundle, key, defaultValue);
+	}
+
+	private String _get(
+		Locale locale, ResourceBundle resourceBundle, String key,
+		String defaultValue) {
+
+		if (key == null) {
+			return defaultValue;
+		}
+
 		if ((resourceBundle != null) && resourceBundle.containsKey(key)) {
 			return _get(resourceBundle, key);
 		}
