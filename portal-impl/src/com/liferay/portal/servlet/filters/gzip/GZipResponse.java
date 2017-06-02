8712d4531e856747218042b1b5c1ce904a4554ad
shuyangzhou <shuyang.zhou@liferay.com>
2017-06-01 23:25:00 -0700

    LPS-X Change solution again :( This is weaker, but at least portable for all appserver.

--- portal-impl/src/com/liferay/portal/servlet/filters/gzip/GZipResponse.java
+++ portal-impl/src/com/liferay/portal/servlet/filters/gzip/GZipResponse.java
@@ -25,11 +25,6 @@ import com.liferay.portal.util.PropsValues;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Map.Entry;
 import java.util.zip.GZIPOutputStream;
 
 import javax.servlet.ServletOutputStream;
@@ -45,16 +40,6 @@ public class GZipResponse extends HttpServletResponseWrapper {
 
 	public GZipResponse(HttpServletResponse response) {
 		super(response);
-
-		// Clear previous content length setting. GZip response does not buffer
-		// output to get final content length. The response will be chunked
-		// unless an outer filter calculates the content length.
-
-		response.setContentLength(-1);
-
-		// Setting the header after finishResponse is too late
-
-		response.addHeader(HttpHeaders.CONTENT_ENCODING, _GZIP);
 	}
 
 	public void finishResponse() throws IOException {
@@ -113,120 +98,11 @@ public class GZipResponse extends HttpServletResponseWrapper {
 	public void setContentLength(int contentLength) {
 	}
 
-	@Override
-	public void setHeader(String name, String value) {
-		if (HttpHeaders.CONTENT_LENGTH.equals(name)) {
-			return;
-		}
-
-		super.setHeader(name, value);
-	}
-
-	@Override
-	@SuppressWarnings("deprecation")
-	public void setStatus(int sc, String sm) {
-		super.setStatus(sc, sm);
-
-		if (super.getStatus() != SC_OK) {
-			_disableGZip();
-		}
-	}
-
-	@Override
-	public void setStatus(int sc) {
-		setStatus(sc, null);
-	}
-
-	@Override
-	public void sendRedirect(String location) throws IOException {
-		_disableGZip();
-
-		super.sendRedirect(location);
-	}
-
-	@Override
-	public void sendError(int sc) throws IOException {
-		sendError(sc, null);
-	}
-
-	@Override
-	public void sendError(int sc, String msg) throws IOException {
-		_disableGZip();
-
-		super.sendError(sc, msg);
-	}
-
-	private void _disableGZip() {
-		if ((_servletOutputStream != null) || isCommitted()) {
-			return;
-		}
-
-		HttpServletResponse rootHttpServletResponse =
-			(HttpServletResponse)getResponse();
-
-		while (rootHttpServletResponse instanceof HttpServletResponseWrapper) {
-			HttpServletResponseWrapper httpServletResponseWrapper =
-				(HttpServletResponseWrapper)rootHttpServletResponse;
-
-			rootHttpServletResponse =
-				(HttpServletResponse)httpServletResponseWrapper.getResponse();
-		}
-
-		Map<String, Collection<String>> headerMap = new HashMap<>();
-
-		for (String headerName : rootHttpServletResponse.getHeaderNames()) {
-			Collection<String> headerValues =
-				rootHttpServletResponse.getHeaders(headerName);
-
-			if (headerName.equals(HttpHeaders.CONTENT_ENCODING)) {
-				headerValues = new ArrayList<>(headerValues);
-
-				headerValues.remove(_GZIP);
-
-				if (headerValues.isEmpty()) {
-					continue;
-				}
-			}
-
-			headerMap.put(headerName, headerValues);
-		}
-
-		int statusCode = rootHttpServletResponse.getStatus();
-
-		super.reset();
-
-		rootHttpServletResponse.setStatus(statusCode);
-
-		for (Entry<String, Collection<String>> entry : headerMap.entrySet()) {
-			String headerName = entry.getKey();
-
-			for (String headerValue : entry.getValue()) {
-				rootHttpServletResponse.addHeader(headerName, headerValue);
-			}
-		}
-
-		Collection<String> headerValues = rootHttpServletResponse.getHeaders(
-			HttpHeaders.CONTENT_ENCODING);
-
-		if (headerValues.contains(_GZIP)) {
-
-			// Reponse is not committed, but we failed to remove the gzip
-			// header, that could only mean we are in include mode which we
-			// could not disable gzip.
-
-			return;
-		}
-
-		_disabled = true;
-	}
-
-	private boolean _disabled;
-
 	private ServletOutputStream _createGZipServletOutputStream(
 			ServletOutputStream servletOutputStream)
 		throws IOException {
 
-		if (_isGZipContentType() || _disabled) {
+		if (_isGZipContentType() || !_setGZipHeader()) {
 			return servletOutputStream;
 		}
 
@@ -240,6 +116,39 @@ public class GZipResponse extends HttpServletResponseWrapper {
 		};
 
 		return new ServletOutputStreamAdapter(gzipOutputStream);
+	}
+
+	private boolean _setGZipHeader() {
+		if (isCommitted() || getStatus() != SC_OK) {
+			return false;
+		}
+
+		HttpServletResponse rootHttpServletResponse =
+			(HttpServletResponse)getResponse();
+
+		while (rootHttpServletResponse instanceof HttpServletResponseWrapper) {
+			HttpServletResponseWrapper httpServletResponseWrapper =
+				(HttpServletResponseWrapper)rootHttpServletResponse;
+
+			rootHttpServletResponse =
+				(HttpServletResponse)httpServletResponseWrapper.getResponse();
+		}
+
+		rootHttpServletResponse.setHeader(HttpHeaders.CONTENT_ENCODING, _GZIP);
+
+		if (!_GZIP.equals(
+				rootHttpServletResponse.getHeader(
+					HttpHeaders.CONTENT_ENCODING))) {
+
+			// Unable to set header, it means we are in include mode, give up
+			// gzip
+
+			return false;
+		}
+
+		rootHttpServletResponse.setContentLength(-1);
+
+		return true;
 	}
 
 	private boolean _isGZipContentType() {
