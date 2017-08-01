8b55ad80810ba5d4d8400cc9e5181231309c8764
Matthew Tambara <matthew.tambara@liferay.com>
2017-08-01 09:45:04 -0700

    LPS-73760 Refreshing an LPKG will reinstall all contained jars

--- modules/apps/static/portal-lpkg-deployer/portal-lpkg-deployer/src/main/java/com/liferay/portal/lpkg/deployer/internal/LPKGBundleTrackerCustomizer.java
+++ modules/apps/static/portal-lpkg-deployer/portal-lpkg-deployer/src/main/java/com/liferay/portal/lpkg/deployer/internal/LPKGBundleTrackerCustomizer.java
@@ -172,8 +172,18 @@ public class LPKGBundleTrackerCustomizer
 						continue;
 					}
 
-					Bundle newBundle = _bundleContext.installBundle(
-						url.getPath(), url.openStream());
+					String location = url.getPath();
+
+					Bundle newBundle = _bundleContext.getBundle(location);
+
+					if (newBundle != null) {
+						bundles.add(newBundle);
+
+						continue;
+					}
+
+					newBundle = _bundleContext.installBundle(
+						location, url.openStream());
 
 					BundleStartLevel bundleStartLevel = newBundle.adapt(
 						BundleStartLevel.class);
@@ -199,13 +209,23 @@ public class LPKGBundleTrackerCustomizer
 					continue;
 				}
 
+				String location = url.getPath();
+
+				Bundle newBundle = _bundleContext.getBundle(location);
+
+				if (newBundle != null) {
+					bundles.add(newBundle);
+
+					continue;
+				}
+
 				// Install a wrapper bundle for this WAR bundle. The wrapper
 				// bundle defers the WAR bundle installation until the WAB
 				// protocol handler is ready. The installed WAR bundle is always
 				// tied its wrapper bundle. When the wrapper bundle is
 				// uninstalled, its wrapped WAR bundle will also be unintalled.
 
-				Bundle newBundle = _bundleContext.installBundle(
+				newBundle = _bundleContext.installBundle(
 					url.getPath(), _toWARWrapperBundle(bundle, url));
 
 				BundleStartLevel bundleStartLevel = newBundle.adapt(
@@ -263,6 +283,35 @@ public class LPKGBundleTrackerCustomizer
 	@Override
 	public void modifiedBundle(
 		Bundle bundle, BundleEvent bundleEvent, List<Bundle> bundles) {
+
+		if (bundleEvent.getType() != BundleEvent.UNRESOLVED) {
+			return;
+		}
+
+		List<Bundle> newBundles = addingBundle(bundle, bundleEvent);
+
+		bundles.removeAll(newBundles);
+
+		for (Bundle installedBundle : bundles) {
+			try {
+				installedBundle.uninstall();
+
+				if (_log.isInfoEnabled()) {
+					_log.info(
+						"Uninstalled " + installedBundle + "because " + bundle +
+							" was updated");
+				}
+			}
+			catch (BundleException be) {
+				_log.error("Unable to uninstall " + installedBundle, be);
+			}
+		}
+
+		bundles.clear();
+
+		if (newBundles != null) {
+			bundles.addAll(newBundles);
+		}
 	}
 
 	@Override
