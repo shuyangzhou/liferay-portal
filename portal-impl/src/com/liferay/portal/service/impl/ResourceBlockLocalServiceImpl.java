60fadeb1ad83257d11898355a76ac52d84c35765
Preston-Crary <preston.crary@liferay.com>
2017-08-14 17:07:18 -0700

    LPS-74348 Cascade deprecations.

--- portal-impl/src/com/liferay/portal/service/impl/ResourceBlockLocalServiceImpl.java
+++ portal-impl/src/com/liferay/portal/service/impl/ResourceBlockLocalServiceImpl.java
@@ -79,7 +79,9 @@ import javax.sql.DataSource;
  *
  * @author Connor McKay
  * @author Shuyang Zhou
+ * @deprecated As of 7.0.0, with no direct replacement
  */
+@Deprecated
 public class ResourceBlockLocalServiceImpl
 	extends ResourceBlockLocalServiceBaseImpl {
 
