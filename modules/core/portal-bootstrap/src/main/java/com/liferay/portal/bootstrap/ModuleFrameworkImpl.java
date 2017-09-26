shuyangzhou/liferay-portal

commit 9f00c707171becedac884a2664787d99ef4c2786

LPS-74737 Correctly calculate the jarPath from location, otherwise we will end up reinstalling static bundles for no reason.

modules/core/portal-bootstrap/src/main/java/com/liferay/portal/bootstrap/ModuleFrameworkImpl.java
@@ -1216,10 +1216,9 @@ public class ModuleFrameworkImpl implements ModuleFramework {
 				continue;
 			}
 
-			Path filePath = Paths.get(
-				location.substring(0, location.indexOf(StringPool.QUESTION)));
+			URI uri = new URI(location);
 
-			if (jarPaths.contains(filePath)) {
+			if (jarPaths.contains(Paths.get(uri.getPath()))) {
 				bundles.put(bundle.getLocation(), bundle);
 
 				continue;

commit 41cff6aaed163cf0f792d4ca549655fb9bf5a569

LPS-74899 Convert to a file to remove leading slash on Windows. Caused by 9f00c707171becedac884a2664787d99ef4c2786

modules/core/portal-bootstrap/src/main/java/com/liferay/portal/bootstrap/ModuleFrameworkImpl.java
@@ -1218,7 +1218,9 @@ public class ModuleFrameworkImpl implements ModuleFramework {
 
 			URI uri = new URI(location);
 
-			if (jarPaths.contains(Paths.get(uri.getPath()))) {
+			File file = new File(uri.getPath());
+
+			if (jarPaths.contains(file.toPath())) {
 				bundles.put(bundle.getLocation(), bundle);
 
 				continue;