b35a1288efb0a3d70fd0b24a51d6e1cfceee66ad
Preston-Crary <preston.crary@liferay.com>
2017-09-28 16:58:24 -0700

    LPS-75010 Fix StringUtil code coverage.

--- modules/core/petra/petra-string/src/test/java/com/liferay/petra/string/StringUtilTest.java
+++ modules/core/petra/petra-string/src/test/java/com/liferay/petra/string/StringUtilTest.java
@@ -16,8 +16,9 @@ package com.liferay.petra.string;
 
 import com.liferay.portal.kernel.test.rule.CodeCoverageAssertor;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
 
 import org.junit.Assert;
 import org.junit.ClassRule;
@@ -27,6 +28,7 @@ import org.junit.Test;
  * @author Alexander Chow
  * @author Shuyang Zhou
  * @author Hugo Huijser
+ * @author Preston Crary
  */
 public class StringUtilTest {
 
@@ -35,106 +37,409 @@ public class StringUtilTest {
 		new CodeCoverageAssertor();
 
 	@Test
-	public void testMerge() {
-		Assert.assertEquals(
-			"1,2,3", StringUtil.merge(new String[] {"1", " 2 ", "3"}));
-		Assert.assertEquals("1", StringUtil.merge(new String[] {"1"}));
-		Assert.assertEquals("", StringUtil.merge(new String[0]));
-		Assert.assertEquals(
-			"true,false,true",
-			StringUtil.merge(new boolean[] {true, false, true}));
+	public void testConstructor() throws Exception {
+		new StringUtil();
+	}
+
+	@Test
+	public void testMergeEmpty() {
+		Assert.assertSame(StringPool.BLANK, StringUtil.merge(new boolean[0]));
+		Assert.assertSame(StringPool.BLANK, StringUtil.merge(new char[0]));
+		Assert.assertSame(
+			StringPool.BLANK, StringUtil.merge(Collections.emptyList()));
+		Assert.assertSame(StringPool.BLANK, StringUtil.merge(new double[0]));
+		Assert.assertSame(StringPool.BLANK, StringUtil.merge(new float[0]));
+		Assert.assertSame(StringPool.BLANK, StringUtil.merge(new int[0]));
+		Assert.assertSame(StringPool.BLANK, StringUtil.merge(new long[0]));
+		Assert.assertSame(StringPool.BLANK, StringUtil.merge(new short[0]));
+		Assert.assertSame(StringPool.BLANK, StringUtil.merge(new String[0]));
+	}
+
+	@Test
+	public void testMergeNull() {
+		Assert.assertNull(StringUtil.merge((boolean[])null));
+		Assert.assertNull(StringUtil.merge((char[])null));
+		Assert.assertNull(StringUtil.merge((Collection<?>)null));
+		Assert.assertNull(StringUtil.merge((double[])null));
+		Assert.assertNull(StringUtil.merge((float[])null));
+		Assert.assertNull(StringUtil.merge((int[])null));
+		Assert.assertNull(StringUtil.merge((long[])null));
+		Assert.assertNull(StringUtil.merge((short[])null));
+		Assert.assertNull(StringUtil.merge((String[])null));
+	}
+
+	@Test
+	public void testMergeOne() {
 		Assert.assertEquals("true", StringUtil.merge(new boolean[] {true}));
 		Assert.assertEquals(
-			"1.1,2.2,3.3", StringUtil.merge(new double[] {1.1, 2.2, 3.3}));
-		Assert.assertEquals("1.1", StringUtil.merge(new double[] {1.1}));
-		Assert.assertEquals("1,2,3", StringUtil.merge(new int[] {1, 2, 3}));
-		Assert.assertEquals("1", StringUtil.merge(new int[] {1}));
-		Assert.assertEquals("1,2,3", StringUtil.merge(new long[] {1, 2, 3}));
-		Assert.assertEquals("1", StringUtil.merge(new long[] {1}));
+			"A", StringUtil.merge(new char[] {CharPool.UPPER_CASE_A}));
+		Assert.assertEquals(
+			"test", StringUtil.merge(Collections.singletonList("test")));
+		Assert.assertEquals("3.14", StringUtil.merge(new double[] {3.14}));
+		Assert.assertEquals("3.14", StringUtil.merge(new float[] {3.14F}));
+		Assert.assertEquals("10", StringUtil.merge(new int[] {10}));
+		Assert.assertEquals("10", StringUtil.merge(new long[] {10L}));
+		Assert.assertEquals("10", StringUtil.merge(new short[] {10}));
+		Assert.assertEquals("test", StringUtil.merge(new String[] {"test"}));
 	}
 
 	@Test
-	public void testReplaceCharStringArrays() {
+	public void testMergeTwo() {
 		Assert.assertEquals(
-			"Hello World,HELLO WORLD,Hello World",
-			StringUtil.replace(
-				"Hello World/HI WORLD/Hello World",
-				new char[] {CharPool.SLASH, CharPool.UPPER_CASE_I},
-				new String[] {StringPool.COMMA, "ELLO"}));
+			"true,false", StringUtil.merge(new boolean[] {true, false}));
 		Assert.assertEquals(
-			"Hello World,HELLO WORLD,Hello World",
-			StringUtil.replace(
-				"Hello World/HI WORLD/Hello World",
-				new char[] {
-					CharPool.SLASH, CharPool.SLASH, CharPool.UPPER_CASE_I,
-					CharPool.UPPER_CASE_I
-				},
-				new String[] {
-					StringPool.COMMA, StringPool.COMMA, "ELLO", "ELLO"
-				}));
+			"A,B",
+			StringUtil.merge(
+				new char[] {CharPool.UPPER_CASE_A, CharPool.UPPER_CASE_B}));
+		Assert.assertEquals(
+			"test,test2", StringUtil.merge(Arrays.asList("test", "test2")));
+		Assert.assertEquals(
+			"3.14,2.72", StringUtil.merge(new double[] {3.14, 2.72}));
+		Assert.assertEquals(
+			"3.14,2.72", StringUtil.merge(new float[] {3.14F, 2.72F}));
+		Assert.assertEquals("10,-10", StringUtil.merge(new int[] {10, -10}));
+		Assert.assertEquals("10,-10", StringUtil.merge(new long[] {10L, -10L}));
+		Assert.assertEquals("10,-10", StringUtil.merge(new short[] {10, -10}));
+		Assert.assertEquals(
+			"test,test2", StringUtil.merge(new String[] {"test", "test2"}));
 	}
 
 	@Test
-	public void testReplaceEmptyString() {
+	public void testReplace() {
 		Assert.assertEquals(
-			"Hello World HELLO WORLD Hello World",
+			"b.b",
 			StringUtil.replace(
-				"Hello World HELLO WORLD Hello World", "", "Aloha"));
+				"a.a", CharPool.LOWER_CASE_A, CharPool.LOWER_CASE_B));
+		Assert.assertEquals(
+			".b.b.",
+			StringUtil.replace(
+				".a.a.", CharPool.LOWER_CASE_A, CharPool.LOWER_CASE_B));
+
+		Assert.assertEquals(
+			"beta.beta",
+			StringUtil.replace("a.a", CharPool.LOWER_CASE_A, "beta"));
+		Assert.assertEquals(
+			".beta.beta.",
+			StringUtil.replace(".a.a.", CharPool.LOWER_CASE_A, "beta"));
+
+		Assert.assertEquals(
+			"b.b",
+			StringUtil.replace(
+				"a.a", new char[] {CharPool.LOWER_CASE_A},
+				new char[] {CharPool.LOWER_CASE_B}));
+		Assert.assertEquals(
+			".b.b.",
+			StringUtil.replace(
+				".a.a.", new char[] {CharPool.LOWER_CASE_A},
+				new char[] {CharPool.LOWER_CASE_B}));
+
+		Assert.assertEquals(
+			"beta.beta",
+			StringUtil.replace(
+				"a.a", new char[] {CharPool.LOWER_CASE_A},
+				new String[] {"beta"}));
+		Assert.assertEquals(
+			".beta.beta.",
+			StringUtil.replace(
+				".a.a.", new char[] {CharPool.LOWER_CASE_A},
+				new String[] {"beta"}));
+
+		Assert.assertEquals(
+			"beta.beta",
+			StringUtil.replace(
+				"alpha.alpha", new String[] {"alpha"}, new String[] {"beta"}));
+		Assert.assertEquals(
+			".beta.beta.",
+			StringUtil.replace(
+				".alpha.alpha.", new String[] {"alpha"},
+				new String[] {"beta"}));
+
+		Assert.assertEquals(
+			"beta.beta", StringUtil.replace("alpha.alpha", "alpha", "beta"));
+		Assert.assertEquals(
+			".beta.beta.",
+			StringUtil.replace(".alpha.alpha.", "alpha", "beta"));
+	}
+
+	@Test
+	public void testReplaceEmpty() {
+		Assert.assertSame(
+			StringPool.BLANK,
+			StringUtil.replace(
+				StringPool.BLANK, CharPool.UPPER_CASE_A,
+				CharPool.UPPER_CASE_B));
+		Assert.assertSame(
+			StringPool.BLANK,
+			StringUtil.replace(
+				StringPool.BLANK, CharPool.UPPER_CASE_A, "beta"));
+		Assert.assertSame(
+			StringPool.BLANK,
+			StringUtil.replace(
+				StringPool.BLANK, new char[] {CharPool.UPPER_CASE_A},
+				new char[] {CharPool.UPPER_CASE_B}));
+		Assert.assertSame(
+			StringPool.BLANK,
+			StringUtil.replace(
+				StringPool.BLANK, new char[] {CharPool.UPPER_CASE_A},
+				new String[] {"beta"}));
+		Assert.assertSame(
+			StringPool.BLANK,
+			StringUtil.replace(
+				StringPool.BLANK, new String[] {"alpha"},
+				new String[] {"beta"}));
+		Assert.assertSame(
+			StringPool.BLANK,
+			StringUtil.replace(StringPool.BLANK, "alpha", "beta"));
+
+		String s = "A";
+
+		Assert.assertSame(s, StringUtil.replace(s, StringPool.BLANK, "beta"));
 	}
 
 	@Test
 	public void testReplaceMap() {
-		Map<String, String> map = new HashMap<>();
-
-		map.put("Hallo", "Hello");
-		map.put("Wirld", "World");
-
 		Assert.assertEquals(
-			"Hello World",
-			StringUtil.replace("AB Hallo CD AB Wirld CD", "AB ", " CD", map));
-		Assert.assertEquals(
-			"Hello World",
+			"beta",
 			StringUtil.replace(
-				"Hello World", StringPool.BLANK, StringPool.BLANK, map));
+				"${alpha}", "${", "}",
+				Collections.singletonMap("alpha", "beta")));
+		Assert.assertEquals(
+			"alpha}",
+			StringUtil.replace(
+				"alpha}", "${", "}",
+				Collections.singletonMap("alpha", "beta")));
+		Assert.assertEquals(
+			"${alpha",
+			StringUtil.replace(
+				"${alpha", "${", "}",
+				Collections.singletonMap("alpha", "beta")));
 	}
 
 	@Test
-	public void testReplaceSpaceString() {
+	public void testReplaceMapEmpty() {
 		Assert.assertEquals(
-			"HelloWorldHELLOWORLDHelloWorld",
+			StringPool.BLANK,
 			StringUtil.replace(
-				"Hello World HELLO WORLD Hello World", " ", StringPool.BLANK));
+				StringPool.BLANK, "${", "}", Collections.emptyMap()));
+		Assert.assertEquals(
+			"test",
+			StringUtil.replace(
+				"test", StringPool.BLANK, "}", Collections.emptyMap()));
+		Assert.assertEquals(
+			"test",
+			StringUtil.replace(
+				"test", "${", StringPool.BLANK, Collections.emptyMap()));
+		Assert.assertEquals(
+			"test",
+			StringUtil.replace("test", "${", "}", Collections.emptyMap()));
 	}
 
 	@Test
-	public void testReplaceString() {
+	public void testReplaceMapNull() {
+		Assert.assertNull(
+			StringUtil.replace(null, "${", "}", Collections.emptyMap()));
 		Assert.assertEquals(
-			"Aloha World HELLO WORLD Aloha World",
+			"${test}",
+			StringUtil.replace("${test}", null, "}", Collections.emptyMap()));
+		Assert.assertEquals(
+			"${test}",
+			StringUtil.replace("${test}", "${", null, Collections.emptyMap()));
+		Assert.assertEquals(
+			"${test}", StringUtil.replace("${test}", "${", "}", null));
+		Assert.assertEquals(
+			"test",
 			StringUtil.replace(
-				"Hello World HELLO WORLD Hello World", "Hello", "Aloha"));
+				"${test}", "${", "}", Collections.singletonMap("test", null)));
 	}
 
 	@Test
-	public void testReplaceStringArray() {
-		Assert.assertEquals(
-			"Aloha World ALOHA WORLD Aloha World",
+	public void testReplaceNull() {
+		String s = "alpha";
+
+		Assert.assertNull(
 			StringUtil.replace(
-				"Hello World HELLO WORLD Hello World",
-				new String[] {"Hello", "HELLO"},
-				new String[] {"Aloha", "ALOHA"}));
+				null, CharPool.UPPER_CASE_A, CharPool.UPPER_CASE_B));
+
+		Assert.assertNull(
+			StringUtil.replace(null, CharPool.UPPER_CASE_A, "beta"));
+		Assert.assertNull(StringUtil.replace(s, CharPool.UPPER_CASE_A, null));
+
+		Assert.assertNull(
+			StringUtil.replace(
+				null, new char[] {CharPool.UPPER_CASE_A},
+				new char[] {CharPool.UPPER_CASE_B}));
+		Assert.assertNull(
+			StringUtil.replace(s, null, new char[] {CharPool.UPPER_CASE_B}));
+		Assert.assertNull(
+			StringUtil.replace(
+				s, new char[] {CharPool.UPPER_CASE_A}, (char[])null));
+
+		Assert.assertNull(
+			StringUtil.replace(null, new char[] {'A'}, new String[] {"beta"}));
+		Assert.assertNull(
+			StringUtil.replace(s, (char[])null, new String[] {"beta"}));
+		Assert.assertNull(
+			StringUtil.replace(s, new char[] {'A'}, (String[])null));
+
+		Assert.assertNull(
+			StringUtil.replace(
+				null, new String[] {"alpha"}, new String[] {"beta"}));
+		Assert.assertNull(
+			StringUtil.replace(s, (String[])null, new String[] {"beta"}));
+		Assert.assertNull(StringUtil.replace(s, new String[] {"alpha"}, null));
+
+		Assert.assertNull(StringUtil.replace(null, "alpha", "beta"));
+		Assert.assertSame(s, StringUtil.replace(s, null, "beta"));
+		Assert.assertSame(
+			StringPool.BLANK, StringUtil.replace("alpha", "alpha", null));
 	}
 
 	@Test
-	public void testSplit() {
+	public void testReplaceOneChange() {
+		Assert.assertEquals(
+			"B",
+			StringUtil.replace(
+				"A", CharPool.UPPER_CASE_A, CharPool.UPPER_CASE_B));
+		Assert.assertEquals(
+			"beta", StringUtil.replace("A", CharPool.UPPER_CASE_A, "beta"));
+		Assert.assertEquals(
+			"B",
+			StringUtil.replace(
+				"A", new char[] {CharPool.UPPER_CASE_A},
+				new char[] {CharPool.UPPER_CASE_B}));
+		Assert.assertEquals(
+			"beta",
+			StringUtil.replace(
+				"A", new char[] {CharPool.UPPER_CASE_A},
+				new String[] {"beta"}));
+		Assert.assertEquals(
+			"beta",
+			StringUtil.replace(
+				"alpha", new String[] {"alpha"}, new String[] {"beta"}));
+		Assert.assertEquals(
+			"beta", StringUtil.replace("alpha", "alpha", "beta"));
+	}
+
+	@Test
+	public void testReplaceOneNoChange() {
+		String s = "beta";
+
+		Assert.assertSame(
+			s,
+			StringUtil.replace(
+				s, CharPool.UPPER_CASE_A, CharPool.UPPER_CASE_B));
+		Assert.assertSame(
+			s, StringUtil.replace(s, CharPool.UPPER_CASE_A, "beta"));
+		Assert.assertSame(
+			s,
+			StringUtil.replace(
+				s, new char[] {CharPool.UPPER_CASE_A},
+				new char[] {CharPool.UPPER_CASE_B}));
+		Assert.assertSame(
+			s,
+			StringUtil.replace(
+				s, new char[] {CharPool.UPPER_CASE_A}, new String[] {"beta"}));
+		Assert.assertSame(
+			s,
+			StringUtil.replace(
+				s, new String[] {"alpha"}, new String[] {"beta"}));
+		Assert.assertSame(s, StringUtil.replace(s, "alpha", "beta"));
+	}
+
+	@Test
+	public void testReplaceWrongNumberOfReplacements() {
+		String s = "A";
+
+		Assert.assertSame(
+			s,
+			StringUtil.replace(
+				s, new char[] {CharPool.UPPER_CASE_A, CharPool.UPPER_CASE_A},
+				new char[] {CharPool.UPPER_CASE_B}));
+		Assert.assertSame(
+			s,
+			StringUtil.replace(
+				s, new char[] {CharPool.UPPER_CASE_A, CharPool.UPPER_CASE_A},
+				new String[] {"B"}));
+		Assert.assertSame(
+			s,
+			StringUtil.replace(s, new String[] {"A", "A"}, new String[] {"B"}));
+	}
+
+	@Test
+	public void testSplitEmpty() {
 		Assert.assertArrayEquals(
-			new String[] {"Alice", "Bob", "Charlie"},
-			StringUtil.split("Alice,Bob,Charlie"));
+			new String[0], StringUtil.split(StringPool.BLANK));
 		Assert.assertArrayEquals(
-			new String[] {"First", "Second", "Third"},
-			StringUtil.split("First;Second;Third", ';'));
+			new String[0], StringUtil.split(StringPool.THREE_SPACES));
+
 		Assert.assertArrayEquals(
-			new String[] {"One", "Two", "Three"},
-			StringUtil.split("OnexTwoxThree", 'x'));
+			new String[0],
+			StringUtil.split(StringPool.BLANK, StringPool.BLANK));
+		Assert.assertArrayEquals(
+			new String[0],
+			StringUtil.split(StringPool.THREE_SPACES, StringPool.BLANK));
+		Assert.assertArrayEquals(
+			new String[0],
+			StringUtil.split(
+				StringPool.TRIPLE_PERIOD, StringPool.TRIPLE_PERIOD));
+	}
+
+	@Test
+	public void testSplitNull() {
+		Assert.assertArrayEquals(new String[0], StringUtil.split(null));
+
+		Assert.assertArrayEquals(new String[0], StringUtil.split(null, null));
+		Assert.assertArrayEquals(new String[0], StringUtil.split("test", null));
+	}
+
+	@Test
+	public void testSplitOne() {
+		Assert.assertArrayEquals(
+			new String[] {"test"}, StringUtil.split("test"));
+		Assert.assertArrayEquals(
+			new String[] {"test"}, StringUtil.split("test,"));
+
+		Assert.assertArrayEquals(
+			new String[] {"test...test2"},
+			StringUtil.split("test...test2", StringPool.COMMA));
+		Assert.assertArrayEquals(
+			new String[] {"test...test2"},
+			StringUtil.split("test...test2", StringPool.COMMA_AND_SPACE));
+		Assert.assertArrayEquals(
+			new String[] {"test"},
+			StringUtil.split("test", StringPool.TRIPLE_PERIOD));
+		Assert.assertArrayEquals(
+			new String[] {"test"},
+			StringUtil.split("test...", StringPool.TRIPLE_PERIOD));
+	}
+
+	@Test
+	public void testSplitTwo() {
+		Assert.assertArrayEquals(
+			new String[] {"test", "test2"}, StringUtil.split("test,test2"));
+
+		Assert.assertArrayEquals(
+			new String[] {"test", "test2"},
+			StringUtil.split("test...test2", StringPool.TRIPLE_PERIOD));
+	}
+
+	@Test
+	public void testToHexString() {
+		Assert.assertEquals("a", StringUtil.toHexString(Integer.valueOf(10)));
+		Assert.assertEquals("40", StringUtil.toHexString(Integer.valueOf(64)));
+		Assert.assertEquals(
+			"7fffffff", StringUtil.toHexString(Integer.MAX_VALUE));
+		Assert.assertEquals(
+			"80000000", StringUtil.toHexString(Integer.MIN_VALUE));
+
+		Assert.assertEquals("b", StringUtil.toHexString(Long.valueOf(11)));
+		Assert.assertEquals("41", StringUtil.toHexString(Long.valueOf(65)));
+		Assert.assertEquals(
+			"7fffffffffffffff", StringUtil.toHexString(Long.MAX_VALUE));
+		Assert.assertEquals(
+			"8000000000000000", StringUtil.toHexString(Long.MIN_VALUE));
+
+		Assert.assertSame("test", StringUtil.toHexString("test"));
 	}
 
 }
