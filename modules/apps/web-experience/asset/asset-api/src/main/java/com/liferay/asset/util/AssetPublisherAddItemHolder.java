6a726168c0ee03e02fa995364dcbb3d8e5994af4
Matthew Tambara <matthew.tambara@liferay.com>
2017-10-19 10:15:03 -0700

    LPS-75330 Move to asset-api

--- modules/apps/web-experience/asset/asset-api/src/main/java/com/liferay/asset/util/AssetPublisherAddItemHolder.java
+++ modules/apps/web-experience/asset/asset-api/src/main/java/com/liferay/asset/util/AssetPublisherAddItemHolder.java
@@ -12,7 +12,7 @@
  * details.
  */
 
-package com.liferay.asset.util.impl;
+package com.liferay.asset.util;
 
 import com.liferay.portal.kernel.language.LanguageUtil;
 import com.liferay.portal.kernel.security.permission.ResourceActionsUtil;
