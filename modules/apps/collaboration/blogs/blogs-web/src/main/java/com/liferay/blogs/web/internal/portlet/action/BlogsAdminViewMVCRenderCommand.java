432c8c7b774cbd0d31f9c46ade5f137124a488e7
tomwang2011 <tong.wang@liferay.com>
2017-10-26 10:45:00 -0700

    LPS-75465 Apply usages

--- modules/apps/collaboration/blogs/blogs-web/src/main/java/com/liferay/blogs/web/internal/portlet/action/BlogsAdminViewMVCRenderCommand.java
+++ modules/apps/collaboration/blogs/blogs-web/src/main/java/com/liferay/blogs/web/internal/portlet/action/BlogsAdminViewMVCRenderCommand.java
@@ -14,8 +14,8 @@
 
 package com.liferay.blogs.web.internal.portlet.action;
 
-import com.liferay.blogs.web.constants.BlogsPortletKeys;
-import com.liferay.blogs.web.constants.BlogsWebKeys;
+import com.liferay.blogs.constants.BlogsPortletKeys;
+import com.liferay.blogs.constants.BlogsWebKeys;
 import com.liferay.document.library.display.context.DLMimeTypeDisplayContext;
 import com.liferay.portal.kernel.portlet.bridges.mvc.MVCRenderCommand;
 
