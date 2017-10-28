3f750a7bd579dcb9a5edf29528720a0c1d379878
Preston-Crary <preston.crary@liferay.com>
2017-10-27 17:33:01 -0700

    LPS-75306 Move away from ABCPermission classes with static methods. Separate staging permission concerns into separate class that should be instantiated in the web.

--- portal-kernel/src/com/liferay/portal/kernel/security/permission/ResourcePermissionCheckerUtil.java
+++ portal-kernel/src/com/liferay/portal/kernel/security/permission/ResourcePermissionCheckerUtil.java
@@ -26,8 +26,16 @@ public class ResourcePermissionCheckerUtil {
 		PermissionChecker permissionChecker, String className, long classPK,
 		String actionId) {
 
+		PortletPermissionHelper portletPermissionHelper =
+			_portletPermissionHelpers.getService(className);
+
+		if (portletPermissionHelper != null) {
+			return portletPermissionHelper.contains(
+				permissionChecker, classPK, actionId);
+		}
+
 		ResourcePermissionChecker resourcePermissionChecker =
-			_serviceTrackerMap.getService(className);
+			_resourcePermissionCheckers.getService(className);
 
 		if (resourcePermissionChecker == null) {
 			return null;
@@ -43,8 +51,13 @@ public class ResourcePermissionCheckerUtil {
 		return null;
 	}
 
+	private static final ServiceTrackerMap<String, PortletPermissionHelper>
+		_portletPermissionHelpers =
+			ServiceTrackerCollections.openSingleValueMap(
+				PortletPermissionHelper.class, "resource.name");
 	private static final ServiceTrackerMap<String, ResourcePermissionChecker>
-		_serviceTrackerMap = ServiceTrackerCollections.openSingleValueMap(
-			ResourcePermissionChecker.class, "resource.name");
+		_resourcePermissionCheckers =
+			ServiceTrackerCollections.openSingleValueMap(
+				ResourcePermissionChecker.class, "resource.name");
 
 }
