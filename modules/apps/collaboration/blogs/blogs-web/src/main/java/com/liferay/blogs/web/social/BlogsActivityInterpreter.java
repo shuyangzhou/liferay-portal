734761434c26522e244f9a957478ef440f9fa365
Preston-Crary <preston.crary@liferay.com>
2017-11-01 18:04:29 -0700

    LPS-75306 Update usages in blogs-web.

--- modules/apps/collaboration/blogs/blogs-web/src/main/java/com/liferay/blogs/web/social/BlogsActivityInterpreter.java
+++ modules/apps/collaboration/blogs/blogs-web/src/main/java/com/liferay/blogs/web/social/BlogsActivityInterpreter.java
@@ -17,9 +17,9 @@ package com.liferay.blogs.web.social;
 import com.liferay.blogs.constants.BlogsPortletKeys;
 import com.liferay.blogs.model.BlogsEntry;
 import com.liferay.blogs.service.BlogsEntryLocalService;
-import com.liferay.blogs.service.permission.BlogsEntryPermission;
 import com.liferay.blogs.social.BlogsActivityKeys;
 import com.liferay.portal.kernel.security.permission.PermissionChecker;
+import com.liferay.portal.kernel.security.permission.checker.ModelPermission;
 import com.liferay.portal.kernel.service.ServiceContext;
 import com.liferay.portal.kernel.util.AggregateResourceBundleLoader;
 import com.liferay.portal.kernel.util.FastDateFormatFactoryUtil;
@@ -174,7 +174,7 @@ public class BlogsActivityInterpreter extends BaseSocialActivityInterpreter {
 			String actionId, ServiceContext serviceContext)
 		throws Exception {
 
-		return BlogsEntryPermission.contains(
+		return _modelPermission.contains(
 			permissionChecker, activity.getClassPK(), actionId);
 	}
 
@@ -199,6 +199,10 @@ public class BlogsActivityInterpreter extends BaseSocialActivityInterpreter {
 	private static final String[] _CLASS_NAMES = {BlogsEntry.class.getName()};
 
 	private BlogsEntryLocalService _blogsEntryLocalService;
+
+	@Reference(target = "(model.class.name=com.liferay.blogs.model.BlogsEntry)")
+	private ModelPermission<BlogsEntry> _modelPermission;
+
 	private ResourceBundleLoader _resourceBundleLoader;
 
 }
