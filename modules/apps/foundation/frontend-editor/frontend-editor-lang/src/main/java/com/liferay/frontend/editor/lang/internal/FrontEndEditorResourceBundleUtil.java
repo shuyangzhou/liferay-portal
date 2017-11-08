87998f671d49cd707513487cdfb7f64b865d08a6
Matthew Tambara <matthew.tambara@liferay.com>
2017-11-08 13:49:43 -0800

    LPS-75788 Move to internal

--- modules/apps/foundation/frontend-editor/frontend-editor-lang/src/main/java/com/liferay/frontend/editor/lang/internal/FrontEndEditorResourceBundleUtil.java
+++ modules/apps/foundation/frontend-editor/frontend-editor-lang/src/main/java/com/liferay/frontend/editor/lang/internal/FrontEndEditorResourceBundleUtil.java
@@ -12,7 +12,7 @@
  * details.
  */
 
-package com.liferay.frontend.editor.lang;
+package com.liferay.frontend.editor.lang.internal;
 
 import com.liferay.portal.kernel.language.LanguageUtil;
 import com.liferay.portal.kernel.util.AggregateResourceBundleLoader;
