222012bc4325b2a4af118b33c5a2ad4d4833584c
Matthew Tambara <matthew.tambara@liferay.com>
2017-11-08 12:11:17 -0800

    LPS-75786 Move test and resources

--- modules/apps/foundation/frontend-css/frontend-css-rtl-servlet/src/test/java/com/liferay/frontend/css/rtl/servlet/internal/converter/CSSRTLConverterTest.java
+++ modules/apps/foundation/frontend-css/frontend-css-rtl-servlet/src/test/java/com/liferay/frontend/css/rtl/servlet/internal/converter/CSSRTLConverterTest.java
@@ -12,7 +12,7 @@
  * details.
  */
 
-package com.liferay.frontend.css.rtl.converter;
+package com.liferay.frontend.css.rtl.servlet.internal.converter;
 
 import com.liferay.frontend.css.rtl.servlet.internal.converter.CSSRTLConverter;
 import com.helger.commons.charset.CCharset;
