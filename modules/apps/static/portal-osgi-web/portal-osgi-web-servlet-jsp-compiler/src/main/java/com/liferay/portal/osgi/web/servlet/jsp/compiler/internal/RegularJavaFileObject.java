47baedb187ace551bd9e8ba234f72c78eec50bab
Minhchau Dang <minhchau.dang@liferay.com>
2017-11-10 15:55:50 -0800

    LPS-75833 Add copyright and author

--- modules/apps/static/portal-osgi-web/portal-osgi-web-servlet-jsp-compiler/src/main/java/com/liferay/portal/osgi/web/servlet/jsp/compiler/internal/RegularJavaFileObject.java
+++ modules/apps/static/portal-osgi-web/portal-osgi-web-servlet-jsp-compiler/src/main/java/com/liferay/portal/osgi/web/servlet/jsp/compiler/internal/RegularJavaFileObject.java
@@ -1,3 +1,17 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
 package com.liferay.portal.osgi.web.servlet.jsp.compiler.internal;
 
 import java.io.IOException;
@@ -8,6 +22,9 @@ import java.net.URI;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
+/**
+ * @author Minhchau Dang
+ */
 public class RegularJavaFileObject extends BaseJavaFileObject {
 
 	public RegularJavaFileObject(String className, Path path) {
