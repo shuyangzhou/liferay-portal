shuyangzhou/liferay-portal

commit 5b4b09ab01db00e285083a7e4942542118c11a09

LPS-74944 Creates Asset Helper interface only with useful methods from AssetUtil

modules/apps/web-experience/asset/asset-api/src/main/java/com/liferay/asset/util/AssetHelper.java
@@ -1,0 +1,112 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.asset.util;
+
+import com.liferay.asset.kernel.model.AssetEntry;
+import com.liferay.asset.kernel.model.AssetTag;
+import com.liferay.asset.kernel.service.persistence.AssetEntryQuery;
+import com.liferay.portal.kernel.model.Layout;
+import com.liferay.portal.kernel.portlet.LiferayPortletRequest;
+import com.liferay.portal.kernel.portlet.LiferayPortletResponse;
+import com.liferay.portal.kernel.search.BaseModelSearchResult;
+import com.liferay.portal.kernel.search.Hits;
+import com.liferay.portal.kernel.search.SearchContext;
+import com.liferay.portal.kernel.util.CharPool;
+
+import java.io.Serializable;
+
+import java.util.List;
+import java.util.Locale;
+import java.util.Map;
+import java.util.Set;
+import java.util.TimeZone;
+
+import javax.portlet.PortletURL;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * @author Eudaldo Alonso
+ */
+public interface AssetHelper {
+
+	public static final int ASSET_ENTRY_ABSTRACT_LENGTH = 200;
+
+	public static final char[] INVALID_CHARACTERS = {
+		CharPool.AMPERSAND, CharPool.APOSTROPHE, CharPool.AT,
+		CharPool.BACK_SLASH, CharPool.CLOSE_BRACKET, CharPool.CLOSE_CURLY_BRACE,
+		CharPool.COLON, CharPool.COMMA, CharPool.EQUAL, CharPool.GREATER_THAN,
+		CharPool.FORWARD_SLASH, CharPool.LESS_THAN, CharPool.NEW_LINE,
+		CharPool.OPEN_BRACKET, CharPool.OPEN_CURLY_BRACE, CharPool.PERCENT,
+		CharPool.PIPE, CharPool.PLUS, CharPool.POUND, CharPool.PRIME,
+		CharPool.QUESTION, CharPool.QUOTE, CharPool.RETURN, CharPool.SEMICOLON,
+		CharPool.SLASH, CharPool.STAR, CharPool.TILDE
+	};
+
+	public Set<String> addLayoutTags(
+		HttpServletRequest request, List<AssetTag> tags);
+
+	public PortletURL getAddPortletURL(
+			LiferayPortletRequest liferayPortletRequest,
+			LiferayPortletResponse liferayPortletResponse, long groupId,
+			String className, long classTypeId, long[] allAssetCategoryIds,
+			String[] allAssetTagNames, String redirect)
+		throws Exception;
+
+	public String getAddURLPopUp(
+		long groupId, long plid, PortletURL addPortletURL,
+		boolean addDisplayPageParameter, Layout layout);
+
+	public List<AssetEntry> getAssetEntries(Hits hits);
+
+	public String getAssetKeywords(String className, long classPK);
+
+	public List getAssetPublisherAddItemHolders(
+			LiferayPortletRequest liferayPortletRequest,
+			LiferayPortletResponse liferayPortletResponse, long groupId,
+			long[] classNameIds, long[] classTypeIds,
+			long[] allAssetCategoryIds, String[] allAssetTagNames,
+			String redirect)
+		throws Exception;
+
+	public Hits search(
+			HttpServletRequest request, AssetEntryQuery assetEntryQuery,
+			int start, int end)
+		throws Exception;
+
+	public Hits search(
+			SearchContext searchContext, AssetEntryQuery assetEntryQuery,
+			int start, int end)
+		throws Exception;
+
+	public BaseModelSearchResult<AssetEntry> searchAssetEntries(
+			AssetEntryQuery assetEntryQuery, long[] assetCategoryIds,
+			String[] assetTagNames, Map<String, Serializable> attributes,
+			long companyId, String keywords, Layout layout, Locale locale,
+			long scopeGroupId, TimeZone timeZone, long userId, int start,
+			int end)
+		throws Exception;
+
+	public BaseModelSearchResult<AssetEntry> searchAssetEntries(
+			HttpServletRequest request, AssetEntryQuery assetEntryQuery,
+			int start, int end)
+		throws Exception;
+
+	public BaseModelSearchResult<AssetEntry> searchAssetEntries(
+			SearchContext searchContext, AssetEntryQuery assetEntryQuery,
+			int start, int end)
+		throws Exception;
+
+}

commit 0fc2d33638a9ef3f56551d521657fb2227eb6900

LPS-74944 Add back type to list. Caused by 5b4b09ab01db00e285083a7e4942542118c11a09

modules/apps/web-experience/asset/asset-api/src/main/java/com/liferay/asset/util/AssetHelper.java
@@ -73,7 +73,7 @@ public interface AssetHelper {
 
 	public String getAssetKeywords(String className, long classPK);
 
-	public List getAssetPublisherAddItemHolders(
+	public List<AssetPublisherAddItemHolder> getAssetPublisherAddItemHolders(
 			LiferayPortletRequest liferayPortletRequest,
 			LiferayPortletResponse liferayPortletResponse, long groupId,
 			long[] classNameIds, long[] classTypeIds,