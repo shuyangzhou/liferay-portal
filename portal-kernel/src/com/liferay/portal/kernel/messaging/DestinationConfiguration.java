6209f242c8bd00ee923297992662b842922f566c
Jesse Rao <jesse.rao@liferay.com>
2017-12-04 17:25:54 -0500

    LPS-73701 Decouple MessageBusException from kernel

--- portal-kernel/src/com/liferay/portal/kernel/messaging/DestinationConfiguration.java
+++ portal-kernel/src/com/liferay/portal/kernel/messaging/DestinationConfiguration.java
@@ -14,8 +14,8 @@
 
 package com.liferay.portal.kernel.messaging;
 
-import com.liferay.portal.kernel.concurrent.RejectedExecutionHandler;
 import com.liferay.petra.string.StringBundler;
+import com.liferay.portal.kernel.concurrent.RejectedExecutionHandler;
 
 import java.io.Serializable;
 
