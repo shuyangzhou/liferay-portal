327d842488fb8317d81c693d13b142074815539e
Preston-Crary <preston.crary@liferay.com>
2017-12-21 17:19:10 -0800

    LPS-76626 Update exception message to provide more information and add hidden flag to adjust the check in case Oracle 12.1 isn't the database that should be limiting the length.

--- modules/util/portal-tools-service-builder/src/main/java/com/liferay/portal/tools/service/builder/ServiceBuilder.java
+++ modules/util/portal-tools-service-builder/src/main/java/com/liferay/portal/tools/service/builder/ServiceBuilder.java
@@ -636,6 +636,9 @@ public class ServiceBuilder {
 
 			_apiPackagePath = GetterUtil.getString(
 				rootElement.attributeValue("api-package-path"), packagePath);
+			_nameLengthLimit = GetterUtil.getInteger(
+				rootElement.attributeValue("name-length-limit"),
+				_nameLengthLimit);
 			_oldServiceOutputPath =
 				_apiDirName + "/" + StringUtil.replace(packagePath, '.', '/');
 			_outputPath =
@@ -4142,12 +4145,17 @@ public class ServiceBuilder {
 
 		String tableName = entityMapping.getTable();
 
-		if (tableName.length() > _TABLE_NAME_MAX_LENGTH) {
+		if (tableName.length() > _nameLengthLimit) {
 			throw new ServiceBuilderException(
 				StringBundler.concat(
 					"Unable to create entity mapping \"", tableName,
 					"\" because table name exceeds ",
-					String.valueOf(_TABLE_NAME_MAX_LENGTH), " characters"));
+					String.valueOf(_nameLengthLimit), " characters, some ",
+					"databases do not allow table names longer than ",
+					String.valueOf(_nameLengthLimit), " characters. To ",
+					"disable this warning set the \"service-builder\" ",
+					"attribute \"name-length-limit\" to the max length that ",
+					"your database supports"));
 		}
 
 		sb.append(tableName);
@@ -4162,14 +4170,19 @@ public class ServiceBuilder {
 
 				String colDBName = col.getDBName();
 
-				if (colDBName.length() > _COLUMN_NAME_MAX_LENGTH) {
+				if (colDBName.length() > _nameLengthLimit) {
 					throw new ServiceBuilderException(
 						StringBundler.concat(
 							"Unable to create entity mapping \"", tableName,
 							"\" because column name \"", colDBName,
-							"\" exceeds ",
-							String.valueOf(_COLUMN_NAME_MAX_LENGTH),
-							" characters"));
+							"\" exceeds ", String.valueOf(_nameLengthLimit),
+							" characters, some databases do not allow column ",
+							"names longer than ",
+							String.valueOf(_nameLengthLimit), " characters. ",
+							"To disable this warning set the ",
+							"\"service-builder\" attribute ",
+							"\"name-length-limit\" to the max length that ",
+							"your database supports"));
 				}
 
 				String colType = col.getType();
@@ -4275,12 +4288,17 @@ public class ServiceBuilder {
 
 		String tableName = entity.getTable();
 
-		if (tableName.length() > _TABLE_NAME_MAX_LENGTH) {
+		if (tableName.length() > _nameLengthLimit) {
 			throw new ServiceBuilderException(
 				StringBundler.concat(
 					"Unable to create entity \"", tableName,
 					"\" because table name exceeds ",
-					String.valueOf(_TABLE_NAME_MAX_LENGTH), " characters"));
+					String.valueOf(_nameLengthLimit), " characters, some ",
+					"databases do not allow table names longer than ",
+					String.valueOf(_nameLengthLimit), " characters. To ",
+					"disable this warning set the \"service-builder\" ",
+					"attribute \"name-length-limit\" to the max length that ",
+					"your database supports"));
 		}
 
 		sb.append(tableName);
@@ -4292,13 +4310,17 @@ public class ServiceBuilder {
 
 			String colDBName = col.getDBName();
 
-			if (colDBName.length() > _COLUMN_NAME_MAX_LENGTH) {
+			if (colDBName.length() > _nameLengthLimit) {
 				throw new ServiceBuilderException(
 					StringBundler.concat(
 						"Unable to create entity \"", tableName,
 						"\" because column name \"", colDBName, "\" exceeds ",
-						String.valueOf(_COLUMN_NAME_MAX_LENGTH),
-						" characters"));
+						String.valueOf(_nameLengthLimit), " characters, some ",
+						"databases do not allow column names longer than ",
+						String.valueOf(_nameLengthLimit),
+						" characters. To disable this warning set the ",
+						"\"service-builder\" attribute \"name-length-limit\" ",
+						"to the max length that your database supports"));
 			}
 
 			String colType = col.getType();
@@ -6086,8 +6108,6 @@ public class ServiceBuilder {
 		entity.setResolved();
 	}
 
-	private static final int _COLUMN_NAME_MAX_LENGTH = 30;
-
 	private static final int _DEFAULT_COLUMN_MAX_LENGTH = 75;
 
 	private static final int _SESSION_TYPE_LOCAL = 1;
@@ -6097,8 +6117,6 @@ public class ServiceBuilder {
 	private static final String _SPRING_NAMESPACE_BEANS = "beans";
 
 	private static final String _SQL_CREATE_TABLE = "create table ";
-
-	private static final int _TABLE_NAME_MAX_LENGTH = 30;
 
 	private static final String _TMP_DIR = System.getProperty("java.io.tmpdir");
 
@@ -6137,6 +6155,7 @@ public class ServiceBuilder {
 	private String _modelHintsFileName;
 	private Set<String> _modifiedFileNames = new HashSet<>();
 	private boolean _mvccEnabled;
+	private int _nameLengthLimit = 30;
 	private String _oldServiceOutputPath;
 	private boolean _osgiModule;
 	private String _outputPath;
