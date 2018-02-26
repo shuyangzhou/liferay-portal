shuyangzhou/liferay-portal

commit 59c185b539ece8a2c33f5881eaaefd618376cd39

LPS-55586 Create configuration for RequestHeaderAutoLogin

portal-impl/src/com/liferay/portal/verify/VerifyProperties.java
@@ -1089,9 +1089,13 @@ public class VerifyProperties extends VerifyProcess {
 		// Request Header
 
 		new String[] {
-			"request.header.auth.hosts.allowed",
-			"request.header.auth.hosts.allowed",
-			"com.liferay.portal.security.auth"
+			"request.header.auth.hosts.allowed", "authHostsAllowed",
+			"com.liferay.portal.security.auto.login.requestheader"
+		},
+
+		new String[] {
+			"request.header.auth.import.from.ldap", "importFromLDAP",
+			"com.liferay.portal.security.auto.login.requestheader"
 		},
 
 		// RSS

modules/portal-security/portal-security-auto-login/bnd.bnd
@@ -1,6 +1,8 @@
 Bundle-Name: Liferay Portal Security Auto Login
 Bundle-SymbolicName: com.liferay.portal.security.auto.login
 Bundle-Version: 1.0.0
-Export-Package: com.liferay.portal.security.auto.login
+Export-Package: \
+	com.liferay.portal.security.auto.login,\
+	com.liferay.portal.security.auto.login.requestheader
 Import-Package: *
 Include-Resource: classes

modules/portal-security/portal-security-auto-login/src/com/liferay/portal/security/auto/login/requestheader/RequestHeaderAutoLogin.java
@@ -12,64 +12,56 @@
  * details.
  */
 
-package com.liferay.portal.security.auto.login;
+package com.liferay.portal.security.auto.login.requestheader;
 
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.security.auto.login.AutoLogin;
 import com.liferay.portal.kernel.security.auto.login.BaseAutoLogin;
 import com.liferay.portal.kernel.servlet.HttpHeaders;
-import com.liferay.portal.kernel.util.PropsKeys;
+import com.liferay.portal.kernel.settings.CompanyServiceSettingsLocator;
+import com.liferay.portal.kernel.settings.SettingsException;
+import com.liferay.portal.kernel.settings.SettingsFactory;
 import com.liferay.portal.kernel.util.StringPool;
 import com.liferay.portal.kernel.util.StringUtil;
 import com.liferay.portal.kernel.util.Validator;
 import com.liferay.portal.model.User;
+import com.liferay.portal.security.auto.login.requestheader.configuration.RequestHeaderAutoLoginConfiguration;
+import com.liferay.portal.security.auto.login.requestheader.constants.RequestHeaderAutoLoginConstants;
 import com.liferay.portal.security.exportimport.UserImporterUtil;
 import com.liferay.portal.security.sso.SSOUtil;
 import com.liferay.portal.service.UserLocalServiceUtil;
 import com.liferay.portal.util.PortalUtil;
-import com.liferay.portal.util.PrefsPropsUtil;
-import com.liferay.portal.util.PropsValues;
 
 import java.util.HashSet;
-import java.util.Map;
 import java.util.Set;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
 
 /**
  * @author Brian Wing Shun Chan
  * @author Wesley Gong
  */
 @Component(
-	immediate = true,
-	property = {"request.header.auth.hosts.allowed=255.255.255.255"},
-	service = AutoLogin.class
+	configurationPid = "com.liferay.portal.security.auto.login.requestheader.configuration.RequestHeaderAutoLoginConfiguration",
+	immediate = true, service = AutoLogin.class
 )
 public class RequestHeaderAutoLogin extends BaseAutoLogin {
-
-	@Activate
-	public void activate(Map<String, String> properties) {
-		String[] hostsAllowedArray = StringUtil.split(
-			properties.get("request.header.auth.hosts.allowed"));
-
-		for (int i = 0; i < hostsAllowedArray.length; i++) {
-			_hostsAllowed.add(hostsAllowedArray[i]);
-		}
-	}
 
 	@Override
 	protected String[] doLogin(
 			HttpServletRequest request, HttpServletResponse response)
 		throws Exception {
 
+		long companyId = PortalUtil.getCompanyId(request);
+
 		String remoteAddr = request.getRemoteAddr();
 
-		if (SSOUtil.isAccessAllowed(request, _hostsAllowed)) {
+		if (isAccessAllowed(companyId, request)) {
 			if (_log.isDebugEnabled()) {
 				_log.debug("Access allowed for " + remoteAddr);
 			}
@@ -82,8 +74,6 @@ public class RequestHeaderAutoLogin extends BaseAutoLogin {
 			return null;
 		}
 
-		long companyId = PortalUtil.getCompanyId(request);
-
 		String screenName = request.getHeader(HttpHeaders.LIFERAY_SCREEN_NAME);
 
 		if (Validator.isNull(screenName)) {
@@ -92,10 +82,7 @@ public class RequestHeaderAutoLogin extends BaseAutoLogin {
 
 		User user = null;
 
-		if (PrefsPropsUtil.getBoolean(
-				companyId, PropsKeys.REQUEST_HEADER_AUTH_IMPORT_FROM_LDAP,
-				PropsValues.REQUEST_HEADER_AUTH_IMPORT_FROM_LDAP)) {
-
+		if (isLDAPImportEnabled(companyId)) {
 			try {
 				user = UserImporterUtil.importUser(
 					companyId, StringPool.BLANK, screenName);
@@ -118,9 +105,68 @@ public class RequestHeaderAutoLogin extends BaseAutoLogin {
 		return credentials;
 	}
 
+	protected boolean isAccessAllowed(
+		long companyId, HttpServletRequest request) {
+
+		RequestHeaderAutoLoginConfiguration configuration = _getConfiguration(
+			companyId);
+
+		if (configuration == null) {
+			return false;
+		}
+
+		String[] hostsAllowedArray = StringUtil.split(
+			configuration.authHostsAllowed());
+
+		Set<String> hostsAllowed = new HashSet<>();
+
+		for (int i = 0; i < hostsAllowedArray.length; i++) {
+			hostsAllowed.add(hostsAllowedArray[i]);
+		}
+
+		return SSOUtil.isAccessAllowed(request, hostsAllowed);
+	}
+
+	protected boolean isLDAPImportEnabled(long companyId) {
+		RequestHeaderAutoLoginConfiguration configuration = _getConfiguration(
+			companyId);
+
+		if (configuration == null) {
+			return false;
+		}
+
+		return configuration.importFromLDAP();
+	}
+
+	@Reference
+	protected void setSettingsFactory(SettingsFactory settingsFactory) {
+		_settingsFactory = settingsFactory;
+	}
+
+	private RequestHeaderAutoLoginConfiguration _getConfiguration(
+		long companyId) {
+
+		try {
+			RequestHeaderAutoLoginConfiguration configuration =
+				_settingsFactory.getSettings(
+					RequestHeaderAutoLoginConfiguration.class,
+					new CompanyServiceSettingsLocator(
+						companyId,
+						RequestHeaderAutoLoginConstants.SERVICE_NAME));
+
+			return configuration;
+		}
+		catch (SettingsException se) {
+			_log.error(
+				"Unable to get RequestHeaderAutoLogin configuration", se);
+		}
+
+		return null;
+	}
+
 	private static final Log _log = LogFactoryUtil.getLog(
 		RequestHeaderAutoLogin.class);
 
-	private final Set<String> _hostsAllowed = new HashSet<>();
+	private volatile SettingsFactory _settingsFactory;
 
 }

modules/portal-security/portal-security-auto-login/src/com/liferay/portal/security/auto/login/requestheader/configuration/RequestHeaderAutoLoginConfiguration.java
@@ -1,0 +1,43 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.security.auto.login.requestheader.configuration;
+
+import aQute.bnd.annotation.metatype.Meta;
+
+/**
+ * @author Tomas Polesovsky
+ */
+@Meta.OCD(
+	id = "com.liferay.portal.security.auto.login.requestheader.configuration.RequestHeaderAutoLoginConfiguration"
+)
+public interface RequestHeaderAutoLoginConfiguration {
+
+	@Meta.AD(
+		deflt = "255.255.255.255", description =
+			"Input a list of comma delimited IPs that can automatically " +
+			"authenticate via request headers. Input a blank list to allow " +
+			"any IP to automatically authenticate via request headers. " +
+			"SERVER_IP will be replaced with the IP of the host server."
+	)
+	public String authHostsAllowed();
+
+	@Meta.AD(
+		deflt = "false",
+		description = "Set this to true to automatically import users from LDAP if they do not exist in the portal.",
+		required = false
+	)
+	public boolean importFromLDAP();
+
+}

modules/portal-security/portal-security-auto-login/src/com/liferay/portal/security/auto/login/requestheader/constants/RequestHeaderAutoLoginConstants.java
@@ -1,0 +1,25 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.security.auto.login.requestheader.constants;
+
+/**
+ * @author Tomas Polesovsky
+ */
+public class RequestHeaderAutoLoginConstants {
+
+	public static final String SERVICE_NAME =
+		"com.liferay.portal.security.auto.login.requestheader";
+
+}

modules/portal-security/portal-security-auto-login/src/com/liferay/portal/security/auto/login/requestheader/settings/RequestHeaderAutoLoginCompanyServiceConfigurationBeanDeclaration.java
@@ -1,0 +1,34 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.security.auto.login.requestheader.settings;
+
+import com.liferay.portal.kernel.settings.definition.ConfigurationBeanDeclaration;
+import com.liferay.portal.security.auto.login.requestheader.configuration.RequestHeaderAutoLoginConfiguration;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Mika Koivisto
+ */
+@Component
+public class RequestHeaderAutoLoginCompanyServiceConfigurationBeanDeclaration
+	implements ConfigurationBeanDeclaration {
+
+	@Override
+	public Class<?> getConfigurationBeanClass() {
+		return RequestHeaderAutoLoginConfiguration.class;
+	}
+
+}

modules/portal-security/portal-security-auto-login/src/com/liferay/portal/security/auto/login/requestheader/settings/RequestHeaderAutoLoginCompanyServiceSettingsIdMapping.java
@@ -1,0 +1,40 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.security.auto.login.requestheader.settings;
+
+import com.liferay.portal.kernel.settings.definition.SettingsIdMapping;
+import com.liferay.portal.security.auto.login.requestheader.configuration.RequestHeaderAutoLoginConfiguration;
+import com.liferay.portal.security.auto.login.requestheader.constants.RequestHeaderAutoLoginConstants;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Mika Koivisto
+ */
+@Component
+public class RequestHeaderAutoLoginCompanyServiceSettingsIdMapping
+	implements SettingsIdMapping {
+
+	@Override
+	public Class<?> getConfigurationBeanClass() {
+		return RequestHeaderAutoLoginConfiguration.class;
+	}
+
+	@Override
+	public String getSettingsId() {
+		return RequestHeaderAutoLoginConstants.SERVICE_NAME;
+	}
+
+}

portal-impl/src/portal.properties
@@ -3379,10 +3379,10 @@
     # Set this to true to automatically import users from LDAP if they do not
     # exist in the portal. The property "auto.login.hooks" must contain a
     # reference to the class
-    # com.liferay.portal.security.auto.login.RequestHeaderAutoLogin to enable
-    # request header authentication.
+    # com.liferay.portal.security.auto.login.requestheader.RequestHeaderAutoLogin
+    # to enable request header authentication.
     #
-    request.header.auth.import.from.ldap=false
+    #request.header.auth.import.from.ldap=false
 
     #
     # Input a list of comma delimited IPs that can automatically authenticate

commit 1e88e1b4f4d6de0baa591622ca28dd22cdff9585

LPS-X Bad bundle symbolic name, caused by 59c185b539ece8a2c33f5881eaaefd618376cd39

portal-impl/src/com/liferay/portal/verify/VerifyProperties.java
@@ -1447,12 +1447,12 @@ public class VerifyProperties extends VerifyProcess {
 
 		new String[] {
 			"request.header.auth.hosts.allowed", "authHostsAllowed",
-			"com.liferay.portal.security.auto.login.request.header"
+			"com.liferay.portal.security.auto.login"
 		},
 
 		new String[] {
 			"request.header.auth.import.from.ldap", "importFromLDAP",
-			"com.liferay.portal.security.auto.login.request.header"
+			"com.liferay.portal.security.auto.login"
 		},
 
 		// RSS