c024ff0b45637f363a214594aae7a649eca1437c
arthurchan35 <arthurchan35@gmail.com>
2018-04-11 09:41:39 +0800

    LPS-79508 fix tests

--- portal-impl/test/unit/com/liferay/portal/util/HttpImplTest.java
+++ portal-impl/test/unit/com/liferay/portal/util/HttpImplTest.java
@@ -384,18 +384,19 @@ public class HttpImplTest extends PowerMockito {
 		Assert.assertEquals(
 			"#^&://abc.com", _httpImpl.removeProtocol("#^&://abc.com"));
 		Assert.assertEquals(
-			"^&://abc.com", _httpImpl.removeProtocol("/^&://abc.com"));
+			"/^&://abc.com", _httpImpl.removeProtocol("/^&://abc.com"));
 		Assert.assertEquals(
 			"ftp.foo.com", _httpImpl.removeProtocol("ftp://ftp.foo.com"));
 		Assert.assertEquals(
-			"foo.com", _httpImpl.removeProtocol("http://///foo.com"));
-		Assert.assertEquals("foo.com", _httpImpl.removeProtocol("////foo.com"));
+			"///foo.com", _httpImpl.removeProtocol("http://///foo.com"));
+		Assert.assertEquals(
+			"////foo.com", _httpImpl.removeProtocol("////foo.com"));
 		Assert.assertEquals(
 			"foo.com", _httpImpl.removeProtocol("http://http://foo.com"));
 		Assert.assertEquals(
-			"www.google.com", _httpImpl.removeProtocol("/\\www.google.com"));
+			"/\\www.google.com", _httpImpl.removeProtocol("/\\www.google.com"));
 		Assert.assertEquals(
-			"www.google.com",
+			"/\\//\\/www.google.com",
 			_httpImpl.removeProtocol("/\\//\\/www.google.com"));
 		Assert.assertEquals(
 			"/path/name", _httpImpl.removeProtocol("/path/name"));
