4d480fb24df79182cd8023ac1f9e7bbcf42b987e
tomwang2011 <tong.wang@liferay.com>
2018-04-12 09:37:38 -0700

    LPS-77684 When the body is blank, we are returning subject according to b649a46e18c84d69d587d1e72c24323ccb6e4899

--- modules/apps/collaboration/message-boards/message-boards-test/src/testIntegration/java/com/liferay/message/boards/service/test/MBMessageLocalServiceTest.java
+++ modules/apps/collaboration/message-boards/message-boards-test/src/testIntegration/java/com/liferay/message/boards/service/test/MBMessageLocalServiceTest.java
@@ -37,6 +37,7 @@ import com.liferay.portal.kernel.test.util.ServiceContextTestUtil;
 import com.liferay.portal.kernel.test.util.TestPropsValues;
 import com.liferay.portal.kernel.util.DateFormatFactoryUtil;
 import com.liferay.portal.kernel.util.FileUtil;
+import com.liferay.portal.kernel.util.HtmlUtil;
 import com.liferay.portal.kernel.util.ObjectValuePair;
 import com.liferay.portal.kernel.util.Time;
 import com.liferay.portal.kernel.workflow.WorkflowConstants;
@@ -155,7 +156,7 @@ public class MBMessageLocalServiceTest {
 			serviceContext);
 
 		Assert.assertEquals(subject, message.getSubject());
-		Assert.assertEquals(StringPool.BLANK, message.getBody());
+		Assert.assertEquals(HtmlUtil.escape(subject), message.getBody());
 	}
 
 	@Test
