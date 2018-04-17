shuyangzhou/liferay-portal

commit 6d14ee950f015cdbbf18e76323671ea12fde862f

LPS-79679 Fix incorrect naming

portal-impl/src/com/liferay/portal/servlet/MainServlet.java
@@ -1109,7 +1109,7 @@ public class MainServlet extends ActionServlet {
 			return false;
 		}
 
-		_inactiveRequesthandler.processInactiveRequest(
+		_inactiveRequestHandler.processInactiveRequest(
 			request, response,
 			"this-instance-is-inactive-please-contact-the-administrator");
 
@@ -1147,7 +1147,7 @@ public class MainServlet extends ActionServlet {
 			return false;
 		}
 
-		_inactiveRequesthandler.processInactiveRequest(
+		_inactiveRequestHandler.processInactiveRequest(
 			request, response,
 			"this-site-is-inactive-please-contact-the-administrator");
 
@@ -1320,7 +1320,7 @@ public class MainServlet extends ActionServlet {
 			messageKey = "the-system-is-shutdown-please-try-again-later";
 		}
 
-		_inactiveRequesthandler.processInactiveRequest(
+		_inactiveRequestHandler.processInactiveRequest(
 			request, response, messageKey);
 
 		return true;
@@ -1405,7 +1405,7 @@ public class MainServlet extends ActionServlet {
 
 	private static final Log _log = LogFactoryUtil.getLog(MainServlet.class);
 
-	private static volatile InactiveRequestHandler _inactiveRequesthandler =
+	private static volatile InactiveRequestHandler _inactiveRequestHandler =
 		ServiceProxyFactory.newServiceTrackedInstance(
 			InactiveRequestHandler.class, MainServlet.class,
 			"_inactiveRequesthandler", false);

modules/apps/collaboration/document-library/document-library-web/src/main/java/com/liferay/document/library/web/internal/display/context/logic/UIItemsBuilder.java
@@ -278,11 +278,11 @@ public class UIItemsBuilder {
 		sb.append(selectFileVersionURL.toString());
 		sb.append("');");
 
-		JavaScriptMenuItem javascriptMenuItem = _addJavaScriptUIItem(
+		JavaScriptMenuItem javaScriptMenuItem = _addJavaScriptUIItem(
 			new JavaScriptMenuItem(), menuItems, DLUIItemKeys.COMPARE_TO,
 			"compare-to", sb.toString());
 
-		javascriptMenuItem.setData(data);
+		javaScriptMenuItem.setData(data);
 
 		String javaScript =
 			"/com/liferay/document/library/web/display/context/dependencies" +
@@ -307,7 +307,7 @@ public class UIItemsBuilder {
 
 		template.processTemplate(unsyncStringWriter);
 
-		javascriptMenuItem.setJavaScript(unsyncStringWriter.toString());
+		javaScriptMenuItem.setJavaScript(unsyncStringWriter.toString());
 	}
 
 	public void addDeleteMenuItem(List<MenuItem> menuItems)
@@ -652,7 +652,7 @@ public class UIItemsBuilder {
 		String onClick = StringBundler.concat(
 			getNamespace(), "openDocument('", webDavURL, "');");
 
-		JavaScriptMenuItem javascriptMenuItem = _addJavaScriptUIItem(
+		JavaScriptMenuItem javaScriptMenuItem = _addJavaScriptUIItem(
 			new JavaScriptMenuItem(), menuItems, DLUIItemKeys.OPEN_IN_MS_OFFICE,
 			"open-in-ms-office", onClick);
 
@@ -680,7 +680,7 @@ public class UIItemsBuilder {
 
 		template.processTemplate(unsyncStringWriter);
 
-		javascriptMenuItem.setJavaScript(unsyncStringWriter.toString());
+		javaScriptMenuItem.setJavaScript(unsyncStringWriter.toString());
 	}
 
 	public void addOpenInMsOfficeToolbarItem(List<ToolbarItem> toolbarItems)
@@ -856,11 +856,11 @@ public class UIItemsBuilder {
 		portletURL.setParameter(
 			"fileEntryId", String.valueOf(_fileEntry.getFileEntryId()));
 
-		JavaScriptMenuItem javascriptMenuItem = new JavaScriptMenuItem();
+		JavaScriptMenuItem javaScriptMenuItem = new JavaScriptMenuItem();
 
-		javascriptMenuItem.setKey(DLUIItemKeys.CHECKIN);
-		javascriptMenuItem.setLabel("checkin");
-		javascriptMenuItem.setOnClick(
+		javaScriptMenuItem.setKey(DLUIItemKeys.CHECKIN);
+		javaScriptMenuItem.setLabel("checkin");
+		javaScriptMenuItem.setOnClick(
 			StringBundler.concat(
 				getNamespace(), "showVersionDetailsDialog('",
 				String.valueOf(portletURL), "');"));
@@ -893,9 +893,9 @@ public class UIItemsBuilder {
 
 		template.processTemplate(unsyncStringWriter);
 
-		javascriptMenuItem.setJavaScript(unsyncStringWriter.toString());
+		javaScriptMenuItem.setJavaScript(unsyncStringWriter.toString());
 
-		return javascriptMenuItem;
+		return javaScriptMenuItem;
 	}
 
 	public boolean isOpenInMsOfficeActionAvailable() throws PortalException {

modules/apps/collaboration/wiki/wiki-engine-creole/src/test/java/com/liferay/wiki/engine/creole/AntlrCreoleParserTest.java
@@ -902,11 +902,11 @@ public class AntlrCreoleParserTest {
 	}
 
 	protected WikiPageNode getWikiPageNode(String fileName) throws Exception {
-		_creole10parser = getCreole10Parser(fileName);
+		_creole10Parser = getCreole10Parser(fileName);
 
-		_creole10parser.wikipage();
+		_creole10Parser.wikipage();
 
-		return _creole10parser.getWikiPageNode();
+		return _creole10Parser.getWikiPageNode();
 	}
 
 	protected BaseListNode parseBaseListNode(String fileName) throws Exception {
@@ -919,7 +919,7 @@ public class AntlrCreoleParserTest {
 		return (BaseListNode)listNode.getChildASTNode(0);
 	}
 
-	private Creole10Parser _creole10parser;
+	private Creole10Parser _creole10Parser;
 	private WikiEngineCreoleComponentProvider
 		_wikiEngineCreoleComponentProvider;
 

modules/apps/collaboration/wiki/wiki-engine-creole/src/test/java/com/liferay/wiki/engine/creole/TranslationToXHTMLTest.java
@@ -767,11 +767,11 @@ public class TranslationToXHTMLTest {
 	}
 
 	protected WikiPageNode getWikiPageNode(String fileName) throws Exception {
-		_creole10parser = getCreole10Parser(fileName);
+		_creole10Parser = getCreole10Parser(fileName);
 
-		_creole10parser.wikipage();
+		_creole10Parser.wikipage();
 
-		return _creole10parser.getWikiPageNode();
+		return _creole10Parser.getWikiPageNode();
 	}
 
 	protected String toUnix(String text) {
@@ -785,7 +785,7 @@ public class TranslationToXHTMLTest {
 
 	private static final String _NEW_LINE = StringPool.NEW_LINE;
 
-	private Creole10Parser _creole10parser;
+	private Creole10Parser _creole10Parser;
 	private WikiEngineCreoleComponentProvider
 		_wikiEngineCreoleComponentProvider;
 	private final XhtmlTranslationVisitor _xhtmlTranslationVisitor =

modules/apps/collaboration/wiki/wiki-engine-creole/src/test/java/com/liferay/wiki/engine/creole/util/test/CreoleTestUtil.java
@@ -60,12 +60,12 @@ public class CreoleTestUtil {
 	public static WikiPageNode getWikiPageNode(
 		String fileName, Class<?> clazz) {
 
-		Creole10Parser creole10parser = null;
+		Creole10Parser creole10Parser = null;
 
 		try {
-			creole10parser = getCreole10Parser(fileName, clazz);
+			creole10Parser = getCreole10Parser(fileName, clazz);
 
-			creole10parser.wikipage();
+			creole10Parser.wikipage();
 		}
 		catch (IOException ioe) {
 			throw new RuntimeException("File " + fileName + " does not exist");
@@ -74,7 +74,7 @@ public class CreoleTestUtil {
 			throw new RuntimeException("Uanble to parse " + fileName);
 		}
 
-		return creole10parser.getWikiPageNode();
+		return creole10Parser.getWikiPageNode();
 	}
 
 	protected static Creole10Parser getCreole10Parser(

modules/apps/collaboration/wiki/wiki-web/src/main/java/com/liferay/wiki/web/internal/display/context/DefaultWikiListPagesDisplayContext.java
@@ -670,10 +670,10 @@ public class DefaultWikiListPagesDisplayContext
 		throws PortalException {
 
 		try {
-			JavaScriptMenuItem javascriptMenuItem = new JavaScriptMenuItem();
+			JavaScriptMenuItem javaScriptMenuItem = new JavaScriptMenuItem();
 
-			javascriptMenuItem.setKey(WikiUIItemKeys.PRINT);
-			javascriptMenuItem.setLabel("print");
+			javaScriptMenuItem.setKey(WikiUIItemKeys.PRINT);
+			javaScriptMenuItem.setLabel("print");
 
 			StringBundler sb = new StringBundler(5);
 
@@ -698,9 +698,9 @@ public class DefaultWikiListPagesDisplayContext
 			sb.append("menubar=1,resizable=1,scrollbars=yes,status=0,");
 			sb.append("toolbar=0,top=180,width=640');");
 
-			javascriptMenuItem.setOnClick(sb.toString());
+			javaScriptMenuItem.setOnClick(sb.toString());
 
-			menuItems.add(javascriptMenuItem);
+			menuItems.add(javaScriptMenuItem);
 		}
 		catch (WindowStateException wse) {
 		}

modules/apps/forms-and-workflow/calendar/calendar-service/src/main/java/com/liferay/calendar/internal/util/CalendarICalDataHandler.java
@@ -213,12 +213,12 @@ public class CalendarICalDataHandler implements CalendarDataHandler {
 
 		// Recurrence
 
-		RRule rrule = (RRule)vEvent.getProperty(Property.RRULE);
+		RRule rRule = (RRule)vEvent.getProperty(Property.RRULE);
 
 		String recurrence = StringPool.BLANK;
 
-		if (rrule != null) {
-			recurrence = StringUtil.trim(rrule.toString());
+		if (rRule != null) {
+			recurrence = StringUtil.trim(rRule.toString());
 
 			PropertyList propertyList = vEvent.getProperties(Property.EXDATE);
 

modules/apps/forms-and-workflow/dynamic-data-mapping/dynamic-data-mapping-api/src/main/java/com/liferay/dynamic/data/mapping/webdav/DDMWebDAV.java
@@ -29,7 +29,7 @@ public interface DDMWebDAV {
 
 	public static final String TYPE_TEMPLATES = "Templates";
 
-	public int addResource(WebDAVRequest webDavRequest, long classNameId)
+	public int addResource(WebDAVRequest webDAVRequest, long classNameId)
 		throws Exception;
 
 	public int deleteResource(

commit d1f8a03142d8f73472c1908587f6504a231764ce

LPS-79679 Fix SF caused error from 6d14ee950f015cdbbf18e76323671ea12fde862f

portal-impl/src/com/liferay/portal/servlet/MainServlet.java
@@ -1408,7 +1408,7 @@ public class MainServlet extends ActionServlet {
 	private static volatile InactiveRequestHandler _inactiveRequestHandler =
 		ServiceProxyFactory.newServiceTrackedInstance(
 			InactiveRequestHandler.class, MainServlet.class,
-			"_inactiveRequesthandler", false);
+			"_inactiveRequestHandler", false);
 
 	private ServiceRegistration<ModuleServiceLifecycle>
 		_moduleServiceLifecycleServiceRegistration;