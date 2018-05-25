ebdafb81c7166eb5dca95e7f54a1860a33047a17
Neil Griffin <neil.griffin.scm@gmail.com>
2018-05-25 18:21:36 -0400

    LPS-80949 Portlet 3.0: Portlet container must invoke the HEADER_PHASE of the portlet lifecycle

--- portal-impl/src/com/liferay/portal/util/PortalImpl.java
+++ portal-impl/src/com/liferay/portal/util/PortalImpl.java
@@ -4607,7 +4607,11 @@ public class PortalImpl implements Portal {
 		PortletResponseImpl portletResponseImpl =
 			PortletResponseImpl.getPortletResponseImpl(portletResponse);
 
-		return ((RenderResponseImpl)portletResponseImpl).getTitle();
+		if (portletResponseImpl instanceof RenderResponseImpl) {
+			return ((RenderResponseImpl)portletResponseImpl).getTitle();
+		}
+
+		return null;
 	}
 
 	@Override
