6977c9a485caf5642970fcf633e0983943809fd4
Lily.Chi <lily.chi@liferay.com>
2018-06-07 14:21:20 +0800

    Revert "LPS-74555 portal-search-elasticsearch6-impl: with Aggregation Filtering we may get nested aggregations"
    This reverts commit 040c8846e8441e47837e0eb1ba1e2aa7b3f0e719.

--- modules/apps/portal-search-elasticsearch6/portal-search-elasticsearch6-impl/src/main/java/com/liferay/portal/search/elasticsearch6/internal/facet/FacetCollectorFactory.java
+++ modules/apps/portal-search-elasticsearch6/portal-search-elasticsearch6-impl/src/main/java/com/liferay/portal/search/elasticsearch6/internal/facet/FacetCollectorFactory.java
@@ -17,9 +17,7 @@ package com.liferay.portal.search.elasticsearch6.internal.facet;
 import com.liferay.portal.kernel.search.facet.collector.FacetCollector;
 
 import org.elasticsearch.search.aggregations.Aggregation;
-import org.elasticsearch.search.aggregations.Aggregations;
 import org.elasticsearch.search.aggregations.bucket.MultiBucketsAggregation;
-import org.elasticsearch.search.aggregations.bucket.filter.InternalFilter;
 import org.elasticsearch.search.aggregations.bucket.range.Range;
 
 /**
@@ -28,14 +26,6 @@ import org.elasticsearch.search.aggregations.bucket.range.Range;
 public class FacetCollectorFactory {
 
 	public FacetCollector getFacetCollector(Aggregation aggregation) {
-		if (aggregation instanceof InternalFilter) {
-			InternalFilter internalFilter = (InternalFilter)aggregation;
-
-			Aggregations aggregations = internalFilter.getAggregations();
-
-			return getFacetCollector(aggregations.get(aggregation.getName()));
-		}
-
 		if (aggregation instanceof Range) {
 			return new RangeFacetCollector((Range)aggregation);
 		}
