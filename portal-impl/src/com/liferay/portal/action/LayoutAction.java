37a12cc6614402b278adf998bc6c1528aaf7c729
Neil Griffin <neil.griffin.scm@gmail.com>
2018-06-09 17:58:13 -0400

    LPS-81855 Portlet 3.0: Default to standards-based behavior for generating render data during a hub partial action

--- portal-impl/src/com/liferay/portal/action/LayoutAction.java
+++ portal-impl/src/com/liferay/portal/action/LayoutAction.java
@@ -232,7 +232,8 @@ public class LayoutAction extends Action {
 
 	protected String getRenderStateJSON(
 			HttpServletRequest request, HttpServletResponse response,
-			ThemeDisplay themeDisplay, LayoutTypePortlet layoutTypePortlet)
+			ThemeDisplay themeDisplay, String portletId,
+			LayoutTypePortlet layoutTypePortlet)
 		throws Exception {
 
 		Map<String, RenderData> renderDataMap = new HashMap<>();
@@ -240,19 +241,25 @@ public class LayoutAction extends Action {
 		List<Portlet> allPortlets = layoutTypePortlet.getAllPortlets();
 
 		for (Portlet curPortlet : allPortlets) {
-			BufferCacheServletResponse bufferCacheServletResponse =
-				new BufferCacheServletResponse(response);
+			String curPortletId = curPortlet.getPortletId();
 
-			PortletContainerUtil.preparePortlet(request, curPortlet);
+			if (curPortletId.equals(portletId) ||
+				curPortlet.isPartialActionServeResource()) {
 
-			PortletContainerUtil.serveResource(
-				request, bufferCacheServletResponse, curPortlet);
+				BufferCacheServletResponse bufferCacheServletResponse =
+					new BufferCacheServletResponse(response);
 
-			RenderData renderData = new RenderData(
-				bufferCacheServletResponse.getContentType(),
-				bufferCacheServletResponse.getString());
+				PortletContainerUtil.preparePortlet(request, curPortlet);
 
-			renderDataMap.put(curPortlet.getPortletId(), renderData);
+				PortletContainerUtil.serveResource(
+					request, bufferCacheServletResponse, curPortlet);
+
+				RenderData renderData = new RenderData(
+					bufferCacheServletResponse.getContentType(),
+					bufferCacheServletResponse.getString());
+
+				renderDataMap.put(curPortletId, renderData);
+			}
 		}
 
 		return RenderStateUtil.generateJSON(
@@ -354,7 +361,7 @@ public class LayoutAction extends Action {
 						if (layoutTypePortlet != null) {
 							renderStateJSON = getRenderStateJSON(
 								request, response, themeDisplay,
-								layoutTypePortlet);
+								portlet.getPortletId(), layoutTypePortlet);
 						}
 					}
 
