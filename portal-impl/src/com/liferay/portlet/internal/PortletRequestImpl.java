shuyangzhou/liferay-portal

commit 55bae239277a558e9422d73b45d2f65276b6c136

LPS-81861 SF

portal-impl/src/com/liferay/portlet/internal/PortletRequestImpl.java
@@ -683,7 +683,8 @@ public abstract class PortletRequestImpl implements LiferayPortletRequest {
 			else if (themeDisplay.isLifecycleAction()) {
 				_triggeredByActionURL = true;
 
-				if (getLifecycle().equals(PortletRequest.ACTION_PHASE)) {
+				if (Objects.equals(
+						getLifecycle(), PortletRequest.ACTION_PHASE)) {
 
 					// Request was triggered by an action URL and is being
 					// processed by
@@ -718,8 +719,10 @@ public abstract class PortletRequestImpl implements LiferayPortletRequest {
 					continue;
 				}
 
-				if (getLifecycle().equals(PortletRequest.HEADER_PHASE) ||
-					getLifecycle().equals(PortletRequest.RENDER_PHASE)) {
+				if (Objects.equals(
+						getLifecycle(), PortletRequest.HEADER_PHASE) ||
+					Objects.equals(
+						getLifecycle(), PortletRequest.RENDER_PHASE)) {
 
 					if (privateRenderParameters == null) {
 						privateRenderParameters = new HashMap<>();
@@ -856,6 +859,7 @@ public abstract class PortletRequestImpl implements LiferayPortletRequest {
 		_plid = plid;
 
 		Set<String> publicRenderParameterNames = new HashSet<>();
+
 		Set<PublicRenderParameter> publicRenderParameters =
 			portlet.getPublicRenderParameters();
 
@@ -868,7 +872,7 @@ public abstract class PortletRequestImpl implements LiferayPortletRequest {
 
 		Map<String, String[]> allRenderParameters = new LinkedHashMap<>();
 
-		if (getLifecycle().equals(PortletRequest.RESOURCE_PHASE)) {
+		if (Objects.equals(getLifecycle(), PortletRequest.RESOURCE_PHASE)) {
 			for (PublicRenderParameter publicRenderParameter :
 					publicRenderParameters) {
 
@@ -1347,11 +1351,11 @@ public abstract class PortletRequestImpl implements LiferayPortletRequest {
 					PortletQName.PRIVATE_RENDER_PARAMETER_NAMESPACE.length();
 
 				if (pos >= 0) {
-					name = name.substring(
-						0,
-						pos).concat(
-							name.substring(
-								pos + privateRenderParameterNamespaceLength));
+					name = name.substring(0, pos);
+					
+					name = name.concat(
+						name.substring(
+							pos + privateRenderParameterNamespaceLength));
 				}
 			}
 

commit dba2a30d0527d54aaafa4b3a4fbb7d5e08cb3836

LPS-81861 Fixed IndexOutOfBoundsException caused by SF in 55bae239277a558e9422d73b45d2f65276b6c136

portal-impl/src/com/liferay/portlet/internal/PortletRequestImpl.java
@@ -1351,11 +1351,13 @@ public abstract class PortletRequestImpl implements LiferayPortletRequest {
 					PortletQName.PRIVATE_RENDER_PARAMETER_NAMESPACE.length();
 
 				if (pos >= 0) {
-					name = name.substring(0, pos);
+					String privateRenderParamName = name.substring(0, pos);
 
-					name = name.concat(
+					privateRenderParamName = privateRenderParamName.concat(
 						name.substring(
 							pos + privateRenderParameterNamespaceLength));
+
+					name = privateRenderParamName;
 				}
 			}
 