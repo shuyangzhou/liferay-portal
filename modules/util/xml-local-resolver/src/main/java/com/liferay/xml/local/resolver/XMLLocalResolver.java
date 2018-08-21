e9c250139829e6e7faf85094cfef68fd8724c30b
Peter Shin <peter.shin@liferay.com>
2018-08-21 11:12:21 -0700

    LPS-84621 XML Local Resolver

---
+++ modules/util/xml-local-resolver/src/main/java/com/liferay/xml/local/resolver/XMLLocalResolver.java
@@ -0,0 +1,47 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.xml.local.resolver;
+
+import java.io.IOException;
+
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
+
+import org.xmlresolver.tools.ResolvingXMLReader;
+
+/**
+ * @author Peter Shin
+ */
+public class XMLLocalResolver extends ResolvingXMLReader {
+
+	public XMLLocalResolver() {
+	}
+
+	@Override
+	public InputSource resolveEntity(String publicId, String systemId)
+		throws IOException, SAXException {
+
+		return super.resolveEntity(publicId, systemId);
+	}
+
+	@Override
+	public InputSource resolveEntity(
+			String name, String publicId, String baseURI, String systemId)
+		throws IOException, SAXException {
+
+		return super.resolveEntity(name, publicId, baseURI, systemId);
+	}
+
+}
