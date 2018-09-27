f3349b0dd7ec3d14dfea87b2f90e31280a6ca63a
arthurchan35 <arthurchan35@gmail.com>
2018-09-27 15:29:35 +0800

    LPS-83456 throw specific exception

--- portal-kernel/src/com/liferay/portal/kernel/settings/SettingsLocatorHelper.java
+++ portal-kernel/src/com/liferay/portal/kernel/settings/SettingsLocatorHelper.java
@@ -14,6 +14,8 @@
 
 package com.liferay.portal.kernel.settings;
 
+import com.liferay.portal.kernel.exception.NoSuchPortletPreferencesException;
+
 import aQute.bnd.annotation.ProviderType;
 
 /**
@@ -59,12 +61,12 @@ public interface SettingsLocatorHelper {
 	public Settings getPortletInstancePortletPreferencesSettings(
 			long companyId, long ownerId, int ownerType, long plid,
 			String portletId, Settings parentSettings)
-		throws Exception;
+		throws NoSuchPortletPreferencesException;
 
 	public Settings getPortletInstancePortletPreferencesSettings(
 			long companyId, long plid, String portletId,
 			Settings parentSettings)
-		throws Exception;
+		throws NoSuchPortletPreferencesException;
 
 	public Settings getServerSettings(String settingsId);
 
