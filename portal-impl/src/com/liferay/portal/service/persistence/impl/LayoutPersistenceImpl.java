dbf383c6d7c2fe8085a3d5e9867e3a8931f41824
Preston-Crary <preston.crary@liferay.com>
2018-12-05 14:25:47 -0800

    LPS-88217 Build Service.

--- portal-impl/src/com/liferay/portal/service/persistence/impl/LayoutPersistenceImpl.java
+++ portal-impl/src/com/liferay/portal/service/persistence/impl/LayoutPersistenceImpl.java
@@ -54,7 +54,6 @@ import com.liferay.portal.model.impl.LayoutModelImpl;
 
 import java.io.Serializable;
 
-import java.lang.reflect.Field;
 import java.lang.reflect.InvocationHandler;
 
 import java.util.ArrayList;
@@ -11988,26 +11987,6 @@ public class LayoutPersistenceImpl extends BasePersistenceImpl<Layout>
 
 		setModelImplClass(LayoutImpl.class);
 		setEntityCacheEnabled(LayoutModelImpl.ENTITY_CACHE_ENABLED);
-
-		try {
-			Field field = BasePersistenceImpl.class.getDeclaredField(
-					"_dbColumnNames");
-
-			field.setAccessible(true);
-
-			Map<String, String> dbColumnNames = new HashMap<String, String>();
-
-			dbColumnNames.put("uuid", "uuid_");
-			dbColumnNames.put("type", "type_");
-			dbColumnNames.put("hidden", "hidden_");
-
-			field.set(this, dbColumnNames);
-		}
-		catch (Exception e) {
-			if (_log.isDebugEnabled()) {
-				_log.debug(e, e);
-			}
-		}
 	}
 
 	/**
