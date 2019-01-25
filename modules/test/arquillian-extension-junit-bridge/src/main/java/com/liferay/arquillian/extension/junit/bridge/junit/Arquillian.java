2fa34ca5ffab56f97284bdc6a3bbd5570796454b
shuyangzhou <shuyang.zhou@liferay.com>
2019-01-25 10:33:04 -0800

    LPS-X No longer needs to record after junit exception.

--- modules/test/arquillian-extension-junit-bridge/src/main/java/com/liferay/arquillian/extension/junit/bridge/junit/Arquillian.java
+++ modules/test/arquillian-extension-junit-bridge/src/main/java/com/liferay/arquillian/extension/junit/bridge/junit/Arquillian.java
@@ -25,7 +25,6 @@ import java.util.Collections;
 import java.util.List;
 
 import java.util.concurrent.atomic.AtomicInteger;
-import org.jboss.arquillian.junit.State;
 import org.jboss.arquillian.junit.event.AfterRules;
 import org.jboss.arquillian.junit.event.BeforeRules;
 import org.jboss.arquillian.test.spi.LifecycleMethodExecutor;
@@ -214,7 +213,6 @@ public class Arquillian extends BlockJUnit4ClassRunner {
 
                @Override
                public void evaluate() throws Throwable {
-                   State.caughtExceptionAfterJunit(null);
                    final AtomicInteger integer = new AtomicInteger();
                    List<Throwable> exceptions = new ArrayList<Throwable>();
 
@@ -228,22 +226,15 @@ public class Arquillian extends BlockJUnit4ClassRunner {
                        }));
                        // If AroundRules (includes lifecycles) were not executed, invoke only lifecycles+test
                        if(integer.get() == 0) {
-                           try {
-                               stmtwithLifecycle.evaluate();
-                           } catch(Throwable t) {
-                               State.caughtExceptionAfterJunit(t);
-                               throw t;
-                           }
+                            stmtwithLifecycle.evaluate();
                        }
                    } catch(Throwable t) {
-                       State.caughtExceptionAfterJunit(t);
                        exceptions.add(t);
                    }
                    finally {
                        try {
                            _testRunnerAdaptor.fireCustomLifecycle(new AfterRules(test, method.getMethod(), LifecycleMethodExecutor.NO_OP));
                        } catch(Throwable t) {
-                           State.caughtExceptionAfterJunit(t);
                            exceptions.add(t);
                        }
                    }
@@ -278,15 +269,8 @@ public class Arquillian extends BlockJUnit4ClassRunner {
 						public void invoke(Object... parameters)
 							throws Throwable {
 
-							try {
-								frameworkMethod.invokeExplosively(
-									testObject, parameters);
-							}
-							catch (Throwable t) {
-								State.caughtTestException(t);
-
-								throw t;
-							}
+							frameworkMethod.invokeExplosively(
+								testObject, parameters);
 						}
 
 						@Override
