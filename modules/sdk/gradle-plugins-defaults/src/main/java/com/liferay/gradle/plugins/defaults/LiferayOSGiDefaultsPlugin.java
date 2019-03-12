e00b96371563ddaf4dd669c992f9d534d68cb0da
Preston-Crary <preston.crary@liferay.com>
2019-03-12 15:11:06 -0700

    LPS-92016 Reorder class path to fix compile for tests. Only saw this issue because test requires at least OSGi 6 and portal/lib/bnd.jar has OSGi 5 included.

--- modules/sdk/gradle-plugins-defaults/src/main/java/com/liferay/gradle/plugins/defaults/LiferayOSGiDefaultsPlugin.java
+++ modules/sdk/gradle-plugins-defaults/src/main/java/com/liferay/gradle/plugins/defaults/LiferayOSGiDefaultsPlugin.java
@@ -2754,8 +2754,8 @@ public class LiferayOSGiDefaultsPlugin implements Plugin<Project> {
 
 		sourceSet.setRuntimeClasspath(
 			FileUtil.join(
-				compileClasspathConfiguration, portalConfiguration,
-				sourceSet.getRuntimeClasspath(), portalTestConfiguration));
+				compileClasspathConfiguration, sourceSet.getRuntimeClasspath(),
+				portalTestConfiguration, portalConfiguration));
 	}
 
 	private void _configureSourceSetTestIntegration(
@@ -2774,13 +2774,13 @@ public class LiferayOSGiDefaultsPlugin implements Plugin<Project> {
 
 		sourceSet.setCompileClasspath(
 			FileUtil.join(
-				portalConfiguration, compileOnlyConfiguration,
-				sourceSet.getCompileClasspath(), portalTestConfiguration));
+				compileOnlyConfiguration, sourceSet.getCompileClasspath(),
+				portalTestConfiguration, portalConfiguration));
 
 		sourceSet.setRuntimeClasspath(
 			FileUtil.join(
-				portalConfiguration, compileOnlyConfiguration,
-				sourceSet.getRuntimeClasspath(), portalTestConfiguration));
+				compileOnlyConfiguration, sourceSet.getRuntimeClasspath(),
+				portalTestConfiguration, portalConfiguration));
 	}
 
 	private void _configureTaskBaselineSyncReleaseVersions(
