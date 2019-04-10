d72c7af095a8b04d421b44d45b0f893eafcc61c4
shuyangzhou <shuyang.zhou@liferay.com>
2019-04-10 16:49:49 -0700

    LPS-93172 Take advantage of the major version bump.

--- portal-test-integration/src/com/liferay/portal/test/rule/LiferayIntegrationTestRule.java
+++ portal-test-integration/src/com/liferay/portal/test/rule/LiferayIntegrationTestRule.java
@@ -15,7 +15,6 @@
 package com.liferay.portal.test.rule;
 
 import com.liferay.petra.log4j.Log4JUtil;
-import com.liferay.portal.kernel.process.ClassPathUtil;
 import com.liferay.portal.kernel.test.rule.AggregateTestRule;
 import com.liferay.portal.kernel.test.rule.BaseTestRule;
 import com.liferay.portal.kernel.test.rule.SynchronousDestinationTestRule;
@@ -126,8 +125,6 @@ public class LiferayIntegrationTestRule extends AggregateTestRule {
 								DialectDetector.class.getName(),
 								Level.INFO.toString(), false);
 
-							ClassPathUtil.initializeClassPaths(
-								new MockServletContext());
 							PortalClassPathUtil.initializeClassPaths(
 								new MockServletContext());
 
