1414bb9f7a7518aaa2a91130dd16bcdcc4fe5f73
tina.tian <tina.tian@liferay.com>
2019-05-07 16:37:12 -0700

    LPS-94205 These test methods should be unit test

--- portal-impl/test/integration/com/liferay/portal/util/PortalImplTest.java
+++ portal-impl/test/integration/com/liferay/portal/util/PortalImplTest.java
@@ -14,7 +14,6 @@
 
 package com.liferay.portal.util;
 
-import com.liferay.portal.kernel.servlet.PersistentHttpServletRequestWrapper;
 import com.liferay.portal.kernel.test.rule.AggregateTestRule;
 import com.liferay.portal.kernel.test.util.RandomTestUtil;
 import com.liferay.portal.kernel.upload.UploadServletRequest;
@@ -31,7 +30,6 @@ import com.liferay.portal.util.test.PortletContainerTestUtil;
 import java.io.InputStream;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletRequestWrapper;
 
 import org.junit.AfterClass;
 import org.junit.Assert;
@@ -63,56 +61,6 @@ public class PortalImplTest {
 	@AfterClass
 	public static void tearDownClass() {
 		_atomicState.close();
-	}
-
-	@Test
-	public void testGetOriginalServletRequest() {
-		HttpServletRequest request = new MockHttpServletRequest();
-
-		Assert.assertSame(
-			request, PortalUtil.getOriginalServletRequest(request));
-
-		HttpServletRequestWrapper requestWrapper1 =
-			new HttpServletRequestWrapper(request);
-
-		Assert.assertSame(
-			request, PortalUtil.getOriginalServletRequest(requestWrapper1));
-
-		HttpServletRequestWrapper requestWrapper2 =
-			new HttpServletRequestWrapper(requestWrapper1);
-
-		Assert.assertSame(
-			request, PortalUtil.getOriginalServletRequest(requestWrapper2));
-
-		HttpServletRequestWrapper requestWrapper3 =
-			new PersistentHttpServletRequestWrapper1(requestWrapper2);
-
-		HttpServletRequest originalRequest =
-			PortalUtil.getOriginalServletRequest(requestWrapper3);
-
-		Assert.assertSame(
-			PersistentHttpServletRequestWrapper1.class,
-			originalRequest.getClass());
-		Assert.assertNotSame(requestWrapper3, originalRequest);
-		Assert.assertSame(request, getWrappedRequest(originalRequest));
-
-		HttpServletRequestWrapper requestWrapper4 =
-			new PersistentHttpServletRequestWrapper2(requestWrapper3);
-
-		originalRequest = PortalUtil.getOriginalServletRequest(requestWrapper4);
-
-		Assert.assertSame(
-			PersistentHttpServletRequestWrapper2.class,
-			originalRequest.getClass());
-		Assert.assertNotSame(requestWrapper4, originalRequest);
-
-		originalRequest = getWrappedRequest(originalRequest);
-
-		Assert.assertSame(
-			PersistentHttpServletRequestWrapper1.class,
-			originalRequest.getClass());
-		Assert.assertNotSame(requestWrapper3, originalRequest);
-		Assert.assertSame(request, getWrappedRequest(originalRequest));
 	}
 
 	@Test
@@ -190,54 +138,6 @@ public class PortalImplTest {
 		Assert.assertTrue(_atomicState.isSet());
 	}
 
-	@Test
-	public void testIsValidResourceId() {
-		Assert.assertTrue(PortalUtil.isValidResourceId("/view.jsp"));
-		Assert.assertFalse(
-			PortalUtil.isValidResourceId("/META-INF/MANIFEST.MF"));
-		Assert.assertFalse(
-			PortalUtil.isValidResourceId("/META-INF\\MANIFEST.MF"));
-		Assert.assertFalse(
-			PortalUtil.isValidResourceId("\\META-INF/MANIFEST.MF"));
-		Assert.assertFalse(
-			PortalUtil.isValidResourceId("\\META-INF\\MANIFEST.MF"));
-		Assert.assertFalse(PortalUtil.isValidResourceId("/WEB-INF/web.xml"));
-		Assert.assertFalse(PortalUtil.isValidResourceId("/WEB-INF\\web.xml"));
-		Assert.assertFalse(PortalUtil.isValidResourceId("\\WEB-INF/web.xml"));
-		Assert.assertFalse(PortalUtil.isValidResourceId("\\WEB-INF\\web.xml"));
-	}
-
-	protected HttpServletRequest getWrappedRequest(
-		HttpServletRequest httpServletRequest) {
-
-		HttpServletRequestWrapper requestWrapper =
-			(HttpServletRequestWrapper)httpServletRequest;
-
-		return (HttpServletRequest)requestWrapper.getRequest();
-	}
-
 	private static AtomicState _atomicState;
-
-	private static class PersistentHttpServletRequestWrapper1
-		extends PersistentHttpServletRequestWrapper {
-
-		private PersistentHttpServletRequestWrapper1(
-			HttpServletRequest httpServletRequest) {
-
-			super(httpServletRequest);
-		}
-
-	}
-
-	private static class PersistentHttpServletRequestWrapper2
-		extends PersistentHttpServletRequestWrapper {
-
-		private PersistentHttpServletRequestWrapper2(
-			HttpServletRequest httpServletRequest) {
-
-			super(httpServletRequest);
-		}
-
-	}
 
 }
