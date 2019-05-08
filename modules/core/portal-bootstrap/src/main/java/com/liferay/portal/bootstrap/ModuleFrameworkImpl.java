5f965ceb62f55a9867e9d543de58f0ac931c82a6
Preston-Crary <preston.crary@liferay.com>
2019-05-08 10:25:49 -0700

    LPS-95269 Remove dead code.

--- modules/core/portal-bootstrap/src/main/java/com/liferay/portal/bootstrap/ModuleFrameworkImpl.java
+++ modules/core/portal-bootstrap/src/main/java/com/liferay/portal/bootstrap/ModuleFrameworkImpl.java
@@ -756,24 +756,10 @@ public class ModuleFrameworkImpl implements ModuleFramework {
 		Properties extraProperties = PropsUtil.getProperties(
 			PropsKeys.MODULE_FRAMEWORK_PROPERTIES, true);
 
-		Parameters extraCapabilitiesParameters = OSGiHeader.parseHeader(
-			extraProperties.getProperty(
-				Constants.FRAMEWORK_SYSTEMCAPABILITIES_EXTRA));
-
 		Attributes attributes = _getExtraManifestAttributes();
 
 		String provideCapability = attributes.getValue(
 			Constants.PROVIDE_CAPABILITY);
-
-		Parameters provideCapabilityParameters = new Parameters(
-			provideCapability);
-
-		if (!extraCapabilitiesParameters.isEmpty()) {
-			extraCapabilitiesParameters.putAll(provideCapabilityParameters);
-		}
-		else {
-			extraCapabilitiesParameters = provideCapabilityParameters;
-		}
 
 		extraProperties.setProperty(
 			Constants.FRAMEWORK_SYSTEMCAPABILITIES_EXTRA, provideCapability);
