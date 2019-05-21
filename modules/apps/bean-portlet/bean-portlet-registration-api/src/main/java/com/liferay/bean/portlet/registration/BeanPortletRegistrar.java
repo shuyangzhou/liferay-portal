9295ad9d9245628288084dc061302f180d6297dc
Neil Griffin <neil.griffin.scm@gmail.com>
2019-05-20 21:18:23 -0400

    LPS-93976 Develop the bean-portlet-registration-api module

---
+++ modules/apps/bean-portlet/bean-portlet-registration-api/src/main/java/com/liferay/bean/portlet/registration/BeanPortletRegistrar.java
@@ -0,0 +1,43 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.bean.portlet.registration;
+
+import aQute.bnd.annotation.ProviderType;
+
+import com.liferay.bean.portlet.extension.BeanFilterMethodFactory;
+import com.liferay.bean.portlet.extension.BeanFilterMethodInvoker;
+import com.liferay.bean.portlet.extension.BeanPortletMethodFactory;
+import com.liferay.bean.portlet.extension.BeanPortletMethodInvoker;
+
+import java.util.Set;
+
+import javax.servlet.ServletContext;
+
+/**
+ * @author Neil Griffin
+ */
+@ProviderType
+public interface BeanPortletRegistrar {
+
+	public void register(
+		Set<Class<?>> discoveredClasses, ServletContext servletContext,
+		BeanFilterMethodFactory beanFilterMethodFactory,
+		BeanFilterMethodInvoker beanFilterMethodInvoker,
+		BeanPortletMethodFactory beanPortletMethodFactory,
+		BeanPortletMethodInvoker beanPortletMethodInvoker);
+
+	public void unregister(ServletContext servletContext);
+
+}
