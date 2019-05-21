f65017842a41f912847583b4960adbfe43d76042
Neil Griffin <neil.griffin.scm@gmail.com>
2019-05-21 08:10:37 -0400

    LPS-93978 Develop the bean-portlet-spring-extension

---
+++ modules/apps/bean-portlet/bean-portlet-spring-extension/src/main/java/com/liferay/bean/portlet/spring/extension/internal/SpringBeanFilterMethod.java
@@ -0,0 +1,45 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.bean.portlet.spring.extension.internal;
+
+import com.liferay.bean.portlet.extension.BeanFilterMethod;
+
+import java.lang.reflect.Method;
+
+import org.springframework.beans.factory.BeanFactory;
+
+/**
+ * @author Neil Griffin
+ */
+public class SpringBeanFilterMethod implements BeanFilterMethod {
+
+	public SpringBeanFilterMethod(
+		BeanFactory beanFactory, Method method, Class<?> beanType) {
+
+		_beanFactory = beanFactory;
+		_method = method;
+		_beanType = beanType;
+	}
+
+	@Override
+	public Object invoke(Object... args) throws ReflectiveOperationException {
+		return _method.invoke(_beanFactory.getBean(_beanType), args);
+	}
+
+	private final BeanFactory _beanFactory;
+	private final Class<?> _beanType;
+	private final Method _method;
+
+}
