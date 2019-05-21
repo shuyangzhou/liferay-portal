ddbfc3090528cc9426f783687d99ba9de89b90f9
Neil Griffin <neil.griffin.scm@gmail.com>
2019-05-20 21:57:17 -0400

    LPS-93977 Refactor the bean-portlet-cdi-extension to use the APIs found in bean-portlet-extension-api and bean-portlet-registration-api

---
+++ modules/apps/bean-portlet/bean-portlet-cdi-extension/src/main/java/com/liferay/bean/portlet/cdi/extension/internal/CDIBeanPortletMethod.java
@@ -0,0 +1,68 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.bean.portlet.cdi.extension.internal;
+
+import com.liferay.bean.portlet.extension.BaseBeanPortletMethod;
+import com.liferay.bean.portlet.extension.BeanPortletMethodType;
+
+import java.lang.reflect.Method;
+
+import java.util.Set;
+
+import javax.enterprise.context.spi.CreationalContext;
+import javax.enterprise.inject.spi.Bean;
+import javax.enterprise.inject.spi.BeanManager;
+
+/**
+ * @author Neil Griffin
+ */
+public class CDIBeanPortletMethod extends BaseBeanPortletMethod {
+
+	public CDIBeanPortletMethod(
+		Method method, BeanPortletMethodType beanPortletMethodType,
+		BeanManager beanManager, Class<?> beanType) {
+
+		super(method, beanPortletMethodType);
+
+		_beanManager = beanManager;
+		_beanType = beanType;
+	}
+
+	@Override
+	public Class<?> getBeanType() {
+		return _beanType;
+	}
+
+	@Override
+	public Object invoke(Object... args) throws ReflectiveOperationException {
+		Set<Bean<?>> beans = _beanManager.getBeans(_beanType);
+
+		Bean<?> bean = _beanManager.resolve(beans);
+
+		CreationalContext creationalContext =
+			_beanManager.createCreationalContext(bean);
+
+		Object beanInstance = _beanManager.getReference(
+			bean, bean.getBeanClass(), creationalContext);
+
+		Method method = getMethod();
+
+		return method.invoke(beanInstance, args);
+	}
+
+	private final BeanManager _beanManager;
+	private final Class<?> _beanType;
+
+}
