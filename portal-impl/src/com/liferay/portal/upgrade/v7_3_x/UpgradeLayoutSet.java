a3c7170dc967f03cdf98e5739120b175cc2a04f0
Preston-Crary <preston.crary@liferay.com>
2019-07-16 18:23:09 -0700

    LPS-97974 and LPS-97975 Use alter() when dropping columns.

--- portal-impl/src/com/liferay/portal/upgrade/v7_3_x/UpgradeLayoutSet.java
+++ portal-impl/src/com/liferay/portal/upgrade/v7_3_x/UpgradeLayoutSet.java
@@ -15,26 +15,20 @@
 package com.liferay.portal.upgrade.v7_3_x;
 
 import com.liferay.portal.kernel.upgrade.UpgradeProcess;
-import com.liferay.portal.kernel.version.Version;
-import com.liferay.portal.upgrade.util.PortalUpgradeProcessRegistry;
-
-import java.util.TreeMap;
+import com.liferay.portal.upgrade.v7_3_x.util.LayoutSetTable;
 
 /**
- * @author Alicia García
+ * @author Preston Crary
  */
-public class PortalUpgradeProcessRegistryImpl
-	implements PortalUpgradeProcessRegistry {
+public class UpgradeLayoutSet extends UpgradeProcess {
 
 	@Override
-	public void registerUpgradeProcesses(
-		TreeMap<Version, UpgradeProcess> upgradeProcesses) {
+	protected void doUpgrade() throws Exception {
+		alter(
+			LayoutSetTable.class, new AlterTableDropColumn("headId"),
+			new AlterTableDropColumn("head"));
 
-		upgradeProcesses.put(new Version(6, 0, 0), new UpgradeSchema());
-
-		upgradeProcesses.put(new Version(6, 0, 1), new UpgradeLayout());
-
-		upgradeProcesses.put(new Version(6, 0, 2), new UpgradeLayoutSet());
+		runSQL("drop table LayoutSetVersion");
 	}
 
 }
