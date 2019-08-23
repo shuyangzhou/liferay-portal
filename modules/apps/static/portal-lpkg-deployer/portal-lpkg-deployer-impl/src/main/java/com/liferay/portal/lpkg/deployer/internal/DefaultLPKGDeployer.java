e855e0040b0717e8e1107413d1e6ae27efc0998a
Matthew Tambara <matthew.tambara@liferay.com>
2019-08-23 13:29:43 -0700

    LPS-100271 Don't use stream

--- modules/apps/static/portal-lpkg-deployer/portal-lpkg-deployer-impl/src/main/java/com/liferay/portal/lpkg/deployer/internal/DefaultLPKGDeployer.java
+++ modules/apps/static/portal-lpkg-deployer/portal-lpkg-deployer-impl/src/main/java/com/liferay/portal/lpkg/deployer/internal/DefaultLPKGDeployer.java
@@ -41,12 +41,11 @@ import java.io.OutputStream;
 
 import java.net.URL;
 
-import java.nio.file.FileVisitOption;
+import java.nio.file.DirectoryStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
-import java.nio.file.attribute.BasicFileAttributes;
 
 import java.util.ArrayList;
 import java.util.Collection;
@@ -59,13 +58,10 @@ import java.util.Map;
 import java.util.Properties;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
-import java.util.function.BiPredicate;
 import java.util.jar.Attributes;
 import java.util.jar.JarFile;
 import java.util.jar.JarOutputStream;
 import java.util.jar.Manifest;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipFile;
 
@@ -575,41 +571,40 @@ public class DefaultLPKGDeployer implements LPKGDeployer {
 			return Collections.emptyList();
 		}
 
-		BiPredicate<Path, BasicFileAttributes> matcher = (path, attributes) -> {
-			if (attributes.isDirectory()) {
-				return false;
-			}
+		List<File> files = new ArrayList<>();
 
-			String pathName = StringUtil.toLowerCase(
-				String.valueOf(path.getFileName()));
+		try (DirectoryStream<Path> directoryStream = Files.newDirectoryStream(
+				dirPath)) {
 
-			if (!pathName.endsWith(extension)) {
-				return false;
-			}
+			for (Path path : directoryStream) {
+				String pathName = StringUtil.toLowerCase(
+					String.valueOf(path.getFileName()));
 
-			if (checkFileName && !_isValid(pathName)) {
-				if (_log.isWarnEnabled()) {
-					_log.warn(
-						"Override file " + path +
-							" has an invalid name and will be ignored");
+				if (!pathName.endsWith(extension)) {
+					continue;
 				}
 
-				return false;
+				if (checkFileName && !_isValid(pathName)) {
+					if (_log.isWarnEnabled()) {
+						_log.warn(
+							"Override file " + path +
+								" has an invalid name and will be ignored");
+					}
+
+					continue;
+				}
+
+				files.add(path.toFile());
 			}
-
-			return true;
-		};
-
-		try (Stream<Path> stream = Files.find(
-				dirPath, Integer.MAX_VALUE, matcher,
-				FileVisitOption.FOLLOW_LINKS)) {
-
-			return stream.map(
-				Path::toFile
-			).collect(
-				Collectors.toList()
-			);
 		}
+
+		File dirFile = dirPath.toFile();
+
+		for (File subdir : dirFile.listFiles(File::isDirectory)) {
+			files.addAll(_scanFiles(subdir.toPath(), extension, checkFileName));
+		}
+
+		return files;
 	}
 
 	private Set<String> _toFileNames(List<File> jarFiles, List<File> warFiles) {
