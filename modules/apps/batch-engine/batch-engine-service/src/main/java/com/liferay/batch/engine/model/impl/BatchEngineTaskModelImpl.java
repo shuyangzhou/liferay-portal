9564fccdca2d1fe876b905a23bdff76d630329fb
Ivica Cardic <ivica.cardic@liferay.com>
2019-09-16 09:30:25 +0200

    COMMERCE-1067 - batch-engine-service - Autogenerated

--- modules/apps/batch-engine/batch-engine-service/src/main/java/com/liferay/batch/engine/model/impl/BatchEngineTaskModelImpl.java
+++ modules/apps/batch-engine/batch-engine-service/src/main/java/com/liferay/batch/engine/model/impl/BatchEngineTaskModelImpl.java
@@ -125,9 +125,11 @@ public class BatchEngineTaskModelImpl
 
 	public static final long COMPANYID_COLUMN_BITMASK = 1L;
 
-	public static final long UUID_COLUMN_BITMASK = 2L;
+	public static final long EXECUTESTATUS_COLUMN_BITMASK = 2L;
 
-	public static final long BATCHENGINETASKID_COLUMN_BITMASK = 4L;
+	public static final long UUID_COLUMN_BITMASK = 4L;
+
+	public static final long BATCHENGINETASKID_COLUMN_BITMASK = 8L;
 
 	public static void setEntityCacheEnabled(boolean entityCacheEnabled) {
 		_entityCacheEnabled = entityCacheEnabled;
@@ -583,7 +585,17 @@ public class BatchEngineTaskModelImpl
 
 	@Override
 	public void setExecuteStatus(String executeStatus) {
+		_columnBitmask |= EXECUTESTATUS_COLUMN_BITMASK;
+
+		if (_originalExecuteStatus == null) {
+			_originalExecuteStatus = _executeStatus;
+		}
+
 		_executeStatus = executeStatus;
+	}
+
+	public String getOriginalExecuteStatus() {
+		return GetterUtil.getString(_originalExecuteStatus);
 	}
 
 	@Override
@@ -756,6 +768,9 @@ public class BatchEngineTaskModelImpl
 		batchEngineTaskModelImpl._setModifiedDate = false;
 
 		batchEngineTaskModelImpl._contentBlobModel = null;
+
+		batchEngineTaskModelImpl._originalExecuteStatus =
+			batchEngineTaskModelImpl._executeStatus;
 
 		batchEngineTaskModelImpl._columnBitmask = 0;
 	}
@@ -1017,6 +1032,7 @@ public class BatchEngineTaskModelImpl
 	private Date _endTime;
 	private String _errorMessage;
 	private String _executeStatus;
+	private String _originalExecuteStatus;
 	private String _operation;
 	private Date _startTime;
 	private String _version;
