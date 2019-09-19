b69b262a0f190d3277bf60324a3cac41231bed5b
Samuel Tran <samuel.tran@liferay.com>
2019-09-19 16:39:14 -0700

    LPS-101084 Register UpgradeViewCount for AssetEntry

--- portal-impl/src/com/liferay/portal/upgrade/v7_3_x/PortalUpgradeProcessRegistryImpl.java
+++ portal-impl/src/com/liferay/portal/upgrade/v7_3_x/PortalUpgradeProcessRegistryImpl.java
@@ -14,8 +14,10 @@
 
 package com.liferay.portal.upgrade.v7_3_x;
 
+import com.liferay.asset.kernel.model.AssetEntry;
 import com.liferay.portal.kernel.upgrade.UpgradeCTModel;
 import com.liferay.portal.kernel.upgrade.UpgradeProcess;
+import com.liferay.portal.kernel.upgrade.UpgradeViewCount;
 import com.liferay.portal.kernel.version.Version;
 import com.liferay.portal.upgrade.util.PortalUpgradeProcessRegistry;
 import com.liferay.portal.upgrade.v7_3_x.util.LayoutTable;
@@ -42,6 +44,11 @@ public class PortalUpgradeProcessRegistryImpl
 			new Version(6, 0, 3), new UpgradeCTModel(LayoutTable.class));
 
 		upgradeProcesses.put(new Version(6, 0, 4), new UpgradeAssetCategory());
+
+		upgradeProcesses.put(
+			new Version(6, 0, 5),
+			new UpgradeViewCount(
+				"AssetEntry", AssetEntry.class, "entryId", "viewCount"));
 	}
 
 }
