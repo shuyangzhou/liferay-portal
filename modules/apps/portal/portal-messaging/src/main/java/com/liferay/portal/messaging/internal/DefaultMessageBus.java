89afc823ebaf5e95aae541e88a1a36ae9f1bb003
tina.tian <tina.tian@liferay.com>
2019-10-11 16:03:08 -0700

    LPS-102995 Add support to interrupt message sent to async destination

--- modules/apps/portal/portal-messaging/src/main/java/com/liferay/portal/messaging/internal/DefaultMessageBus.java
+++ modules/apps/portal/portal-messaging/src/main/java/com/liferay/portal/messaging/internal/DefaultMessageBus.java
@@ -25,6 +25,7 @@ import com.liferay.portal.kernel.messaging.Message;
 import com.liferay.portal.kernel.messaging.MessageBus;
 import com.liferay.portal.kernel.messaging.MessageBusEventListener;
 import com.liferay.portal.kernel.messaging.MessageListener;
+import com.liferay.portal.kernel.messaging.async.AsyncDestinationInterrupter;
 import com.liferay.portal.kernel.util.ListUtil;
 import com.liferay.portal.kernel.util.MapUtil;
 import com.liferay.portal.kernel.util.ObjectValuePair;
@@ -300,6 +301,24 @@ public class DefaultMessageBus implements ManagedServiceFactory, MessageBus {
 
 		message.setDestinationName(destinationName);
 
+		if (destination instanceof BaseAsyncDestination) {
+			AsyncDestinationInterrupter asyncDestinationInterrupter =
+				_asyncDestinationInterrupter;
+
+			if ((asyncDestinationInterrupter != null) &&
+				asyncDestinationInterrupter.interrupt(message)) {
+
+				if (_log.isDebugEnabled()) {
+					_log.debug(
+						StringBundler.concat(
+							"Interrupt message ", message,
+							" sent to async destination ", destinationName));
+				}
+
+				return;
+			}
+		}
+
 		destination.send(message);
 	}
 
@@ -559,6 +578,13 @@ public class DefaultMessageBus implements ManagedServiceFactory, MessageBus {
 
 	private static final Log _log = LogFactoryUtil.getLog(
 		DefaultMessageBus.class);
+
+	@Reference(
+		cardinality = ReferenceCardinality.OPTIONAL,
+		policy = ReferencePolicy.DYNAMIC,
+		policyOption = ReferencePolicyOption.GREEDY
+	)
+	private volatile AsyncDestinationInterrupter _asyncDestinationInterrupter;
 
 	private final Map<String, Destination> _destinations =
 		new ConcurrentHashMap<>();
