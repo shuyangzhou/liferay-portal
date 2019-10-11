09c2fc006a4aa2abb2ed0864bab1951ed0943483
tina.tian <tina.tian@liferay.com>
2019-10-11 14:07:28 -0700

    LPS-102995 Add integration test

---
+++ modules/apps/change-tracking/change-tracking-test/src/testIntegration/java/com/liferay/change/tracking/internal/test/MessageBusCTTest.java
@@ -0,0 +1,166 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.change.tracking.internal.test;
+
+import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
+import com.liferay.change.tracking.model.CTCollection;
+import com.liferay.change.tracking.model.CTMessage;
+import com.liferay.change.tracking.service.CTCollectionLocalService;
+import com.liferay.change.tracking.service.CTMessageLocalService;
+import com.liferay.counter.kernel.service.CounterLocalService;
+import com.liferay.petra.lang.SafeClosable;
+import com.liferay.portal.kernel.change.tracking.CTCollectionThreadLocal;
+import com.liferay.portal.kernel.json.JSONFactory;
+import com.liferay.portal.kernel.messaging.Destination;
+import com.liferay.portal.kernel.messaging.DestinationConfiguration;
+import com.liferay.portal.kernel.messaging.DestinationFactory;
+import com.liferay.portal.kernel.messaging.Message;
+import com.liferay.portal.kernel.messaging.MessageBus;
+import com.liferay.portal.kernel.test.rule.AggregateTestRule;
+import com.liferay.portal.kernel.test.rule.DeleteAfterTestRun;
+import com.liferay.portal.kernel.test.util.TestPropsValues;
+import com.liferay.portal.kernel.util.MapUtil;
+import com.liferay.portal.test.rule.Inject;
+import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
+
+import java.util.List;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import org.osgi.framework.Bundle;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.FrameworkUtil;
+import org.osgi.framework.ServiceRegistration;
+
+/**
+ * @author Tina Tian
+ */
+@RunWith(Arquillian.class)
+public class MessageBusCTTest {
+
+	@ClassRule
+	@Rule
+	public static final AggregateTestRule aggregateTestRule =
+		new LiferayIntegrationTestRule();
+
+	@BeforeClass
+	public static void setUpClass() {
+		Bundle bundle = FrameworkUtil.getBundle(MessageBusCTTest.class);
+
+		_bundleContext = bundle.getBundleContext();
+	}
+
+	@Before
+	public void setUp() throws Exception {
+		DestinationConfiguration destinationConfiguration =
+			new DestinationConfiguration(
+				DestinationConfiguration.DESTINATION_TYPE_SERIAL,
+				_TEST_DESTINATION_NAME);
+
+		Destination destination = _destinationFactory.createDestination(
+			destinationConfiguration);
+
+		_serviceRegistration = _bundleContext.registerService(
+			Destination.class, destination,
+			MapUtil.singletonDictionary(
+				"destination.name", _TEST_DESTINATION_NAME));
+
+		long ctCollectionId = _counterLocalService.increment();
+
+		_ctCollection = _ctCollectionLocalService.createCTCollection(
+			ctCollectionId);
+
+		_ctCollection.setCompanyId(TestPropsValues.getCompanyId());
+		_ctCollection.setUserId(TestPropsValues.getUserId());
+
+		_ctCollection = _ctCollectionLocalService.updateCTCollection(
+			_ctCollection);
+	}
+
+	@After
+	public void tearDown() {
+		_serviceRegistration.unregister();
+	}
+
+	@Test
+	public void testSendMessage() {
+		Message message = new Message();
+
+		message.setPayload("test message payload");
+
+		try (SafeClosable safeClosable1 =
+				CTCollectionThreadLocal.setCTCollectionId(
+					_ctCollection.getCtCollectionId())) {
+
+			_messageBus.sendMessage(_TEST_DESTINATION_NAME, message);
+
+			List<CTMessage> ctMessages = _ctMessageLocalService.getCTMessages(
+				_ctCollection.getCtCollectionId());
+
+			Assert.assertSame(ctMessages.toString(), 1, ctMessages.size());
+
+			CTMessage ctMessage = ctMessages.get(0);
+
+			Assert.assertEquals(
+				_ctCollection.getCompanyId(), ctMessage.getCompanyId());
+			Assert.assertEquals(
+				_ctCollection.getUserId(), ctMessage.getUserId());
+			Assert.assertEquals(
+				_TEST_DESTINATION_NAME, ctMessage.getDestinationName());
+
+			Message deSerializedMessage = (Message)_jsonFactory.deserialize(
+				ctMessage.getMessageContent());
+
+			Assert.assertEquals(
+				message.getPayload(), deSerializedMessage.getPayload());
+		}
+	}
+
+	private static final String _TEST_DESTINATION_NAME =
+		"TEST_DESTINATION_NAME";
+
+	private static BundleContext _bundleContext;
+
+	@Inject
+	private static CounterLocalService _counterLocalService;
+
+	@Inject
+	private static CTCollectionLocalService _ctCollectionLocalService;
+
+	@Inject
+	private static CTMessageLocalService _ctMessageLocalService;
+
+	@Inject
+	private static DestinationFactory _destinationFactory;
+
+	@Inject
+	private static JSONFactory _jsonFactory;
+
+	@Inject
+	private static MessageBus _messageBus;
+
+	@DeleteAfterTestRun
+	private CTCollection _ctCollection;
+
+	private ServiceRegistration<Destination> _serviceRegistration;
+
+}
