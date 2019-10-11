dd391d888f8abd11b98d317da5fa660ec8daad8a
Samuel Tran <samuel.tran@liferay.com>
2019-10-10 18:00:59 -0700

    LPS-101084 Update usages

--- portal-impl/src/com/liferay/portlet/asset/service/impl/AssetEntryLocalServiceImpl.java
+++ portal-impl/src/com/liferay/portlet/asset/service/impl/AssetEntryLocalServiceImpl.java
@@ -105,9 +105,7 @@ public class AssetEntryLocalServiceImpl extends AssetEntryLocalServiceBaseImpl {
 		// View count
 
 		_viewCountService.removeViewCount(
-			entry.getCompanyId(),
-			classNameLocalService.getClassNameId(AssetEntry.class),
-			entry.getPrimaryKey());
+			entry.getCompanyId(), AssetEntry.class, entry.getPrimaryKey());
 
 		// Social
 
