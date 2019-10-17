7d9a3cee3a0f711ee3ca76b31faf6d1f25229bf4
Samuel Tran <samuel.tran@liferay.com>
2019-10-17 15:57:20 -0700

    LPS-101084 view-count-service - Register upgrade

---
+++ modules/apps/view-count/view-count-service/src/main/java/com/liferay/view/count/internal/upgrade/ViewCountServiceUpgrade.java
@@ -0,0 +1,50 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.view.count.internal.upgrade;
+
+import com.liferay.asset.kernel.model.AssetEntry;
+import com.liferay.portal.kernel.service.ResourceActionLocalService;
+import com.liferay.portal.kernel.service.ResourcePermissionLocalService;
+import com.liferay.portal.kernel.service.RoleLocalService;
+import com.liferay.portal.kernel.upgrade.UpgradeViewCount;
+import com.liferay.portal.upgrade.registry.UpgradeStepRegistrator;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Samuel Trong Tran
+ */
+@Component(immediate = true, service = UpgradeStepRegistrator.class)
+public class ViewCountServiceUpgrade implements UpgradeStepRegistrator {
+
+	@Override
+	public void register(Registry registry) {
+		registry.register(
+			"1.0.0", "1.0.1",
+			new UpgradeViewCount(
+				"AssetEntry", AssetEntry.class, "entryId", "viewCount"));
+	}
+
+	@Reference
+	private ResourceActionLocalService _resourceActionLocalService;
+
+	@Reference
+	private ResourcePermissionLocalService _resourcePermissionLocalService;
+
+	@Reference
+	private RoleLocalService _roleLocalService;
+
+}
