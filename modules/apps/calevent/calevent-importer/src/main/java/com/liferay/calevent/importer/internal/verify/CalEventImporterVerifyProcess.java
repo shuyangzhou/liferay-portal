6bb98f0c84aee815517b1867b254cbd1521812b1
Samuel Tran <samuel.tran@liferay.com>
2019-10-22 15:32:20 -0700

    LPS-101084 Use long for view count

--- modules/apps/calevent/calevent-importer/src/main/java/com/liferay/calevent/importer/internal/verify/CalEventImporterVerifyProcess.java
+++ modules/apps/calevent/calevent-importer/src/main/java/com/liferay/calevent/importer/internal/verify/CalEventImporterVerifyProcess.java
@@ -91,7 +91,9 @@ import com.liferay.social.kernel.model.SocialActivity;
 import com.liferay.social.kernel.service.SocialActivityLocalService;
 import com.liferay.subscription.model.Subscription;
 import com.liferay.subscription.service.SubscriptionLocalService;
+import com.liferay.view.count.model.ViewCountEntry;
 import com.liferay.view.count.service.ViewCountEntryLocalService;
+import com.liferay.view.count.service.persistence.ViewCountEntryPK;
 
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -193,7 +195,7 @@ public class CalEventImporterVerifyProcess extends VerifyProcess {
 		Date endDate, Date publishDate, Date expirationDate, String mimeType,
 		String title, String description, String summary, String url,
 		String layoutUuid, int height, int width, double priority,
-		int viewCount) {
+		long viewCount) {
 
 		AssetEntry assetEntry = _assetEntryLocalService.createAssetEntry(
 			entryId);
@@ -224,9 +226,16 @@ public class CalEventImporterVerifyProcess extends VerifyProcess {
 
 		_assetEntryLocalService.updateAssetEntry(assetEntry);
 
-		_viewCountEntryLocalService.incrementViewCount(
-			companyId, _classNameLocalService.getClassNameId(AssetEntry.class),
-			entryId, viewCount);
+		ViewCountEntry viewCountEntry =
+			_viewCountEntryLocalService.createViewCountEntry(
+				new ViewCountEntryPK(
+					companyId,
+					_classNameLocalService.getClassNameId(AssetEntry.class),
+					entryId));
+
+		viewCountEntry.setViewCount(viewCount);
+
+		_viewCountEntryLocalService.addViewCountEntry(viewCountEntry);
 	}
 
 	private void _addAssetLink(
