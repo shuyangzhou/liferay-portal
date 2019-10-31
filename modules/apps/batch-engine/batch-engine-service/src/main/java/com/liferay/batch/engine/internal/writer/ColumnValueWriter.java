8de742a50c1d5e127ef4a0e3e8b87c6cdc9457ad
Ivica Cardic <ivica.cardic@liferay.com>
2019-10-31 10:55:54 +0100

    COMMERCE-1067 - batch-engine-service - SF

--- modules/apps/batch-engine/batch-engine-service/src/main/java/com/liferay/batch/engine/internal/writer/ColumnValueWriter.java
+++ modules/apps/batch-engine/batch-engine-service/src/main/java/com/liferay/batch/engine/internal/writer/ColumnValueWriter.java
@@ -30,8 +30,8 @@ import java.util.function.Consumer;
 public class ColumnValueWriter {
 
 	public void write(
-			Object item, Map<String, Field> fieldMap, List<String> fieldNames,
-			Consumer<Collection<?>> consumer)
+			Consumer<Collection<?>> consumer, Map<String, Field> fieldMap,
+			List<String> fieldNames, Object item)
 		throws IllegalAccessException {
 
 		if (fieldNames.isEmpty()) {
