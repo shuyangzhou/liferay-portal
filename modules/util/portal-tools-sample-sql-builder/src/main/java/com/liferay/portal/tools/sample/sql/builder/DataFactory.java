9a773b589a5a46bdde7f42dff9c67de4b4ae2cb1
tina.tian <tina.tian@liferay.com>
2019-11-11 17:11:46 -0800

    LPS-104353 SF, correct name first then process

--- modules/util/portal-tools-sample-sql-builder/src/main/java/com/liferay/portal/tools/sample/sql/builder/DataFactory.java
+++ modules/util/portal-tools-sample-sql-builder/src/main/java/com/liferay/portal/tools/sample/sql/builder/DataFactory.java
@@ -4545,6 +4545,9 @@ public class DataFactory {
 				if (name.endsWith(StringPool.UNDERLINE)) {
 					name = name.substring(0, name.length() - 1);
 				}
+				else if (name.equals("LPageTemplateStructureRelId")) {
+					name = "LayoutPageTemplateStructureRelId";
+				}
 
 				int type = (int)tableColumn[1];
 
@@ -4575,12 +4578,6 @@ public class DataFactory {
 					sb.append(method.invoke(baseModel));
 				}
 				else {
-					if (StringUtil.equals(
-							name, "LPageTemplateStructureRelId")) {
-
-						name = "LayoutPageTemplateStructureRelId";
-					}
-
 					Method method = clazz.getMethod("get".concat(name));
 
 					sb.append(method.invoke(baseModel));
