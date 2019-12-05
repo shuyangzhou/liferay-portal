shuyangzhou/liferay-portal

commit ddbfc3090528cc9426f783687d99ba9de89b90f9

LPS-93977 Refactor the bean-portlet-cdi-extension to use the APIs found in bean-portlet-extension-api and bean-portlet-registration-api

modules/apps/bean-portlet/bean-portlet-registration-impl/src/main/java/com/liferay/bean/portlet/registration/internal/BeanApp.java
@@ -1,0 +1,40 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.bean.portlet.registration.internal;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * @author Neil Griffin
+ */
+public interface BeanApp {
+
+	public Map<String, List<String>> getContainerRuntimeOptions();
+
+	public Set<String> getCustomPortletModes();
+
+	public String getDefaultNamespace();
+
+	public List<Event> getEvents();
+
+	public List<Map.Entry<Integer, String>> getPortletListeners();
+
+	public Map<String, PublicRenderParameter> getPublicRenderParameters();
+
+	public String getSpecVersion();
+
+}

modules/apps/bean-portlet/bean-portlet-registration-impl/src/main/java/com/liferay/bean/portlet/registration/internal/BeanAppImpl.java
@@ -1,0 +1,85 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.bean.portlet.registration.internal;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * @author Neil Griffin
+ */
+public class BeanAppImpl implements BeanApp {
+
+	public BeanAppImpl(
+		String specVersion, String defaultNamespace, List<Event> events,
+		Map<String, PublicRenderParameter> publicRenderParameters,
+		Map<String, List<String>> containerRuntimeOptions,
+		Set<String> customPortletModes,
+		List<Map.Entry<Integer, String>> portletListeners) {
+
+		_specVersion = specVersion;
+		_defaultNamespace = defaultNamespace;
+		_events = events;
+		_publicRenderParameters = publicRenderParameters;
+		_containerRuntimeOptions = containerRuntimeOptions;
+		_customPortletModes = customPortletModes;
+		_portletListeners = portletListeners;
+	}
+
+	@Override
+	public Map<String, List<String>> getContainerRuntimeOptions() {
+		return _containerRuntimeOptions;
+	}
+
+	@Override
+	public Set<String> getCustomPortletModes() {
+		return _customPortletModes;
+	}
+
+	@Override
+	public String getDefaultNamespace() {
+		return _defaultNamespace;
+	}
+
+	@Override
+	public List<Event> getEvents() {
+		return _events;
+	}
+
+	@Override
+	public List<Map.Entry<Integer, String>> getPortletListeners() {
+		return _portletListeners;
+	}
+
+	@Override
+	public Map<String, PublicRenderParameter> getPublicRenderParameters() {
+		return _publicRenderParameters;
+	}
+
+	@Override
+	public String getSpecVersion() {
+		return _specVersion;
+	}
+
+	private final Map<String, List<String>> _containerRuntimeOptions;
+	private final Set<String> _customPortletModes;
+	private final String _defaultNamespace;
+	private final List<Event> _events;
+	private final List<Map.Entry<Integer, String>> _portletListeners;
+	private final Map<String, PublicRenderParameter> _publicRenderParameters;
+	private final String _specVersion;
+
+}

modules/apps/bean-portlet/bean-portlet-cdi-extension/src/main/java/com/liferay/bean/portlet/cdi/extension/internal/BeanApp.java
@@ -1,40 +1,0 @@
-/**
- * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
- *
- * This library is free software; you can redistribute it and/or modify it under
- * the terms of the GNU Lesser General Public License as published by the Free
- * Software Foundation; either version 2.1 of the License, or (at your option)
- * any later version.
- *
- * This library is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
- * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
- * details.
- */
-
-package com.liferay.bean.portlet.cdi.extension.internal;
-
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
-
-/**
- * @author Neil Griffin
- */
-public interface BeanApp {
-
-	public Map<String, List<String>> getContainerRuntimeOptions();
-
-	public Set<String> getCustomPortletModes();
-
-	public String getDefaultNamespace();
-
-	public List<Event> getEvents();
-
-	public List<Map.Entry<Integer, String>> getPortletListeners();
-
-	public Map<String, PublicRenderParameter> getPublicRenderParameters();
-
-	public String getSpecVersion();
-
-}

modules/apps/bean-portlet/bean-portlet-cdi-extension/src/main/java/com/liferay/bean/portlet/cdi/extension/internal/BeanAppImpl.java
@@ -1,85 +1,0 @@
-/**
- * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
- *
- * This library is free software; you can redistribute it and/or modify it under
- * the terms of the GNU Lesser General Public License as published by the Free
- * Software Foundation; either version 2.1 of the License, or (at your option)
- * any later version.
- *
- * This library is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
- * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
- * details.
- */
-
-package com.liferay.bean.portlet.cdi.extension.internal;
-
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
-
-/**
- * @author Neil Griffin
- */
-public class BeanAppImpl implements BeanApp {
-
-	public BeanAppImpl(
-		String specVersion, String defaultNamespace, List<Event> events,
-		Map<String, PublicRenderParameter> publicRenderParameters,
-		Map<String, List<String>> containerRuntimeOptions,
-		Set<String> customPortletModes,
-		List<Map.Entry<Integer, String>> portletListeners) {
-
-		_specVersion = specVersion;
-		_defaultNamespace = defaultNamespace;
-		_events = events;
-		_publicRenderParameters = publicRenderParameters;
-		_containerRuntimeOptions = containerRuntimeOptions;
-		_customPortletModes = customPortletModes;
-		_portletListeners = portletListeners;
-	}
-
-	@Override
-	public Map<String, List<String>> getContainerRuntimeOptions() {
-		return _containerRuntimeOptions;
-	}
-
-	@Override
-	public Set<String> getCustomPortletModes() {
-		return _customPortletModes;
-	}
-
-	@Override
-	public String getDefaultNamespace() {
-		return _defaultNamespace;
-	}
-
-	@Override
-	public List<Event> getEvents() {
-		return _events;
-	}
-
-	@Override
-	public List<Map.Entry<Integer, String>> getPortletListeners() {
-		return _portletListeners;
-	}
-
-	@Override
-	public Map<String, PublicRenderParameter> getPublicRenderParameters() {
-		return _publicRenderParameters;
-	}
-
-	@Override
-	public String getSpecVersion() {
-		return _specVersion;
-	}
-
-	private final Map<String, List<String>> _containerRuntimeOptions;
-	private final Set<String> _customPortletModes;
-	private final String _defaultNamespace;
-	private final List<Event> _events;
-	private final List<Map.Entry<Integer, String>> _portletListeners;
-	private final Map<String, PublicRenderParameter> _publicRenderParameters;
-	private final String _specVersion;
-
-}

commit fd6104e16a11b2c461bf2a190abbb57c11f881ba

LPS-93977 Fix pass-by-value bug that was caused during refactoring in commit ddbfc3090528cc9426f783687d99ba9de89b90f9

modules/apps/bean-portlet/bean-portlet-registration-impl/src/main/java/com/liferay/bean/portlet/registration/internal/BeanApp.java
@@ -37,4 +37,21 @@ public interface BeanApp {
 
 	public String getSpecVersion();
 
+	public void setContainerRuntimeOptions(
+		Map<String, List<String>> containerRuntimeOptions);
+
+	public void setCustomPortletModes(Set<String> customPortletModes);
+
+	public void setDefaultNamespace(String defaultNamespace);
+
+	public void setEvents(List<Event> events);
+
+	public void setPortletListeners(
+		List<Map.Entry<Integer, String>> portletListeners);
+
+	public void setPublicRenderParameters(
+		Map<String, PublicRenderParameter> publicRenderParameters);
+
+	public void setSpecVersion(String specVersion);
+
 }

modules/apps/bean-portlet/bean-portlet-registration-impl/src/main/java/com/liferay/bean/portlet/registration/internal/BeanAppImpl.java
@@ -74,12 +74,53 @@ public class BeanAppImpl implements BeanApp {
 		return _specVersion;
 	}
 
-	private final Map<String, List<String>> _containerRuntimeOptions;
-	private final Set<String> _customPortletModes;
-	private final String _defaultNamespace;
-	private final List<Event> _events;
-	private final List<Map.Entry<Integer, String>> _portletListeners;
-	private final Map<String, PublicRenderParameter> _publicRenderParameters;
-	private final String _specVersion;
+	@Override
+	public void setContainerRuntimeOptions(
+		Map<String, List<String>> containerRuntimeOptions) {
+
+		_containerRuntimeOptions = containerRuntimeOptions;
+	}
+
+	@Override
+	public void setCustomPortletModes(Set<String> customPortletModes) {
+		_customPortletModes = customPortletModes;
+	}
+
+	@Override
+	public void setDefaultNamespace(String defaultNamespace) {
+		_defaultNamespace = defaultNamespace;
+	}
+
+	@Override
+	public void setEvents(List<Event> events) {
+		_events = events;
+	}
+
+	@Override
+	public void setPortletListeners(
+		List<Map.Entry<Integer, String>> portletListeners) {
+
+		_portletListeners = portletListeners;
+	}
+
+	@Override
+	public void setPublicRenderParameters(
+		Map<String, PublicRenderParameter> publicRenderParameters) {
+
+		_publicRenderParameters = publicRenderParameters;
+	}
+
+	@Override
+	public void setSpecVersion(String specVersion) {
+		_specVersion = specVersion;
+	}
+
+	private Map<String, List<String>> _containerRuntimeOptions;
+	private Set<String> _customPortletModes;
+	private String _defaultNamespace;
+	private List<Event> _events;
+	private List<Map.Entry<Integer, String>> _portletListeners;
+	private Map<String, PublicRenderParameter> _publicRenderParameters;
+	private String _specVersion;
 
 }

modules/apps/bean-portlet/bean-portlet-registration-impl/src/main/java/com/liferay/bean/portlet/registration/internal/BeanPortletRegistrarImpl.java
@@ -578,9 +578,13 @@ public class BeanPortletRegistrarImpl implements BeanPortletRegistrar {
 
 		portletListeners.addAll(beanApp.getPortletListeners());
 
-		beanApp = new BeanAppImpl(
-			specVersion, defaultNamespace, events, publicRenderParameters,
-			containerRuntimeOptions, customPortletModes, portletListeners);
+		beanApp.setSpecVersion(specVersion);
+		beanApp.setDefaultNamespace(defaultNamespace);
+		beanApp.setEvents(events);
+		beanApp.setPublicRenderParameters(publicRenderParameters);
+		beanApp.setContainerRuntimeOptions(containerRuntimeOptions);
+		beanApp.setCustomPortletModes(customPortletModes);
+		beanApp.setPortletListeners(portletListeners);
 
 		String preferencesValidator = preferencesValidatorFunction.apply(
 			configuredPortletName);