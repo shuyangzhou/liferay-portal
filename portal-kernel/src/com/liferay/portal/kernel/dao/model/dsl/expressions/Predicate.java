71e9c1e03205fa0e3f6fa092f9353fde3b4e04ae
shuyangzhou <shuyang.zhou@liferay.com>
2020-01-08 10:10:07 -0800

    LPS-106397 Adapt Query to become an Expression, so that we can avoid the PredicateClause marker interface

--- portal-kernel/src/com/liferay/portal/kernel/dao/model/dsl/expressions/Predicate.java
+++ portal-kernel/src/com/liferay/portal/kernel/dao/model/dsl/expressions/Predicate.java
@@ -17,9 +17,7 @@ package com.liferay.portal.kernel.dao.model.dsl.expressions;
 import com.liferay.petra.string.StringBundler;
 import com.liferay.portal.kernel.dao.model.dsl.ast.ASTNodeListener;
 import com.liferay.portal.kernel.dao.model.dsl.base.BaseASTNode;
-import com.liferay.portal.kernel.dao.model.dsl.clause.PredicateClause;
 import com.liferay.portal.kernel.dao.model.dsl.operands.Operand;
-import com.liferay.portal.kernel.dao.model.dsl.query.Query;
 
 import java.util.Objects;
 
@@ -30,9 +28,9 @@ public class Predicate extends BaseASTNode implements Expression<Boolean> {
 
 	public Predicate(
 		Expression<?> leftExpression, Operand operand,
-		PredicateClause predicateClause) {
+		Expression<?> rightExpression) {
 
-		this(leftExpression, operand, predicateClause, false);
+		this(leftExpression, operand, rightExpression, false);
 	}
 
 	public Predicate and(Expression<Boolean> expression) {
@@ -44,7 +42,7 @@ public class Predicate extends BaseASTNode implements Expression<Boolean> {
 			this, Operand.AND,
 			new Predicate(
 				predicate._leftExpression, predicate._operand,
-				predicate._predicateClause, true));
+				predicate._rightExpression, true));
 	}
 
 	public Expression<?> getLeftExpression() {
@@ -55,8 +53,8 @@ public class Predicate extends BaseASTNode implements Expression<Boolean> {
 		return _operand;
 	}
 
-	public PredicateClause getPredicateClause() {
-		return _predicateClause;
+	public Expression<?> getRightExpression() {
+		return _rightExpression;
 	}
 
 	public boolean isWrapParentheses() {
@@ -72,7 +70,7 @@ public class Predicate extends BaseASTNode implements Expression<Boolean> {
 			this, Operand.OR,
 			new Predicate(
 				predicate._leftExpression, predicate._operand,
-				predicate._predicateClause, true));
+				predicate._rightExpression, true));
 	}
 
 	@Override
@@ -87,16 +85,7 @@ public class Predicate extends BaseASTNode implements Expression<Boolean> {
 		sb.append(_operand);
 		sb.append(" ");
 
-		if (_predicateClause instanceof Query) {
-			sb.append("(");
-
-			_predicateClause.toSQL(sb, astNodeListener);
-
-			sb.append(")");
-		}
-		else {
-			_predicateClause.toSQL(sb, astNodeListener);
-		}
+		_rightExpression.toSQL(sb, astNodeListener);
 
 		if (_wrapParentheses) {
 			sb.append(")");
@@ -105,17 +94,17 @@ public class Predicate extends BaseASTNode implements Expression<Boolean> {
 
 	private Predicate(
 		Expression<?> leftExpression, Operand operand,
-		PredicateClause predicateClause, boolean wrapParentheses) {
+		Expression<?> rightExpression, boolean wrapParentheses) {
 
 		_leftExpression = Objects.requireNonNull(leftExpression);
 		_operand = Objects.requireNonNull(operand);
-		_predicateClause = Objects.requireNonNull(predicateClause);
+		_rightExpression = Objects.requireNonNull(rightExpression);
 		_wrapParentheses = wrapParentheses;
 	}
 
 	private final Expression<?> _leftExpression;
 	private final Operand _operand;
-	private final PredicateClause _predicateClause;
+	private final Expression<?> _rightExpression;
 	private final boolean _wrapParentheses;
 
 }
