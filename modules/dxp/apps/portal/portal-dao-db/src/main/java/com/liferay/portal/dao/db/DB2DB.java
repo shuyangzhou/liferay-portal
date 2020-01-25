894a6109c680c010a232411576662ce36274f10f
Alberto Chaparro <alberto.chaparro@liferay.com>
2020-01-24 22:43:06 -0800

    LPS-107396 I think we do not need this anymore

--- modules/dxp/apps/portal/portal-dao-db/src/main/java/com/liferay/portal/dao/db/DB2DB.java
+++ modules/dxp/apps/portal/portal-dao-db/src/main/java/com/liferay/portal/dao/db/DB2DB.java
@@ -65,11 +65,6 @@ public class DB2DB extends BaseDB {
 	}
 
 	@Override
-	public boolean isSupportsAlterColumnType() {
-		return _SUPPORTS_ALTER_COLUMN_TYPE;
-	}
-
-	@Override
 	public boolean isSupportsInlineDistinct() {
 		return _SUPPORTS_INLINE_DISTINCT;
 	}
@@ -340,8 +335,6 @@ public class DB2DB extends BaseDB {
 		Types.BLOB, Types.BLOB, Types.SMALLINT, Types.TIMESTAMP, Types.DOUBLE,
 		Types.INTEGER, Types.BIGINT, Types.VARCHAR, Types.CLOB, Types.VARCHAR
 	};
-
-	private static final boolean _SUPPORTS_ALTER_COLUMN_TYPE = true;
 
 	private static final boolean _SUPPORTS_INLINE_DISTINCT = false;
 
