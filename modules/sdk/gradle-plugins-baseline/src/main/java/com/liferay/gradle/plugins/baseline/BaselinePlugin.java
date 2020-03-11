2ec7a92f58aa26247962be6382b12d79296eb313
Peter Shin <peter.shin@liferay.com>
2020-03-11 09:45:10 -0700

    LPS-110200 Run baseline tasks in parallel

--- modules/sdk/gradle-plugins-baseline/src/main/java/com/liferay/gradle/plugins/baseline/BaselinePlugin.java
+++ modules/sdk/gradle-plugins-baseline/src/main/java/com/liferay/gradle/plugins/baseline/BaselinePlugin.java
@@ -268,8 +268,6 @@ public class BaselinePlugin implements Plugin<Project> {
 			baselineConfigurationExtension.getLowestMajorVersion();
 
 		if (lowestMajorVersion != null) {
-			BaselineTask previousVersionBaselineTask = baselineTask;
-
 			int maxMajorVersion = versionNumber.getMajor();
 
 			if ((versionNumber.getMinor() == 0) &&
@@ -293,9 +291,7 @@ public class BaselinePlugin implements Plugin<Project> {
 						true);
 				}
 
-				previousVersionBaselineTask.dependsOn(majorVersionBaselineTask);
-
-				previousVersionBaselineTask = majorVersionBaselineTask;
+				baselineTask.dependsOn(majorVersionBaselineTask);
 			}
 		}
 		else if (baselineConfigurationExtension.
