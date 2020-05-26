de339c4ee9338e9871a159576b0267d6690d331f
Dante Wang <dante.wang@liferay.com>
2020-05-26 16:29:23 +0800

    LPS-112056 Deprecate getPortletResourceActions(Portlet) and replace usages. This method now only applies to PORTAL portlet (key = 90) and creates differences in portlet actions between DB and in-memory ResourceActionsBag.

--- portal-kernel/src/com/liferay/portal/kernel/security/permission/ResourceActionsUtil.java
+++ portal-kernel/src/com/liferay/portal/kernel/security/permission/ResourceActionsUtil.java
@@ -161,6 +161,10 @@ public class ResourceActionsUtil {
 		return getResourceActions().getPortletNames();
 	}
 
+	/**
+	 * @deprecated As of Athanasius (7.3.x), with no direct replacement
+	 */
+	@Deprecated
 	public static List<String> getPortletResourceActions(Portlet portlet) {
 		return getResourceActions().getPortletResourceActions(portlet);
 	}
