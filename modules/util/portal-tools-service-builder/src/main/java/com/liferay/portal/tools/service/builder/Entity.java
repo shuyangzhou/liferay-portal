f89b5b2c223169da18eb34a383640d51134c4d65
arthurchan35 <arthurchan35@gmail.com>
2020-08-28 15:20:37 +0800

    LPS-120266 Introduce variable name attritube for service entity

--- modules/util/portal-tools-service-builder/src/main/java/com/liferay/portal/tools/service/builder/Entity.java
+++ modules/util/portal-tools-service-builder/src/main/java/com/liferay/portal/tools/service/builder/Entity.java
@@ -97,24 +97,24 @@ public class Entity implements Comparable<Entity> {
 	public Entity(ServiceBuilder serviceBuilder, String name) {
 		this(
 			serviceBuilder, null, null, null, name, null, null, null, null,
-			false, false, false, false, true, true, null, null, null, null,
-			null, true, false, false, false, false, false, null, false, null,
-			null, false, null, null, null, null, null, null, null, null, null,
-			null, false);
+			null, false, false, false, false, true, true, null, null, null,
+			null, null, true, false, false, false, false, false, null, false,
+			null, null, false, null, null, null, null, null, null, null, null,
+			null, null, false);
 	}
 
 	public Entity(
 		ServiceBuilder serviceBuilder, String packagePath,
 		String apiPackagePath, String portletShortName, String name,
-		String pluralName, String humanName, String table, String alias,
-		boolean uuid, boolean uuidAccessor, boolean externalReferenceCode,
-		boolean localService, boolean remoteService, boolean persistence,
-		String persistenceClassName, String finderClassName, String dataSource,
-		String sessionFactory, String txManager, boolean cacheEnabled,
-		boolean changeTrackingEnabled, boolean dynamicUpdateEnabled,
-		boolean jsonEnabled, boolean mvccEnabled, boolean trashEnabled,
-		String uadApplicationName, boolean uadAutoDelete, String uadOutputPath,
-		String uadPackagePath, boolean deprecated,
+		String variableName, String pluralName, String humanName, String table,
+		String alias, boolean uuid, boolean uuidAccessor,
+		boolean externalReferenceCode, boolean localService,
+		boolean remoteService, boolean persistence, String persistenceClassName,
+		String finderClassName, String dataSource, String sessionFactory,
+		String txManager, boolean cacheEnabled, boolean changeTrackingEnabled,
+		boolean dynamicUpdateEnabled, boolean jsonEnabled, boolean mvccEnabled,
+		boolean trashEnabled, String uadApplicationName, boolean uadAutoDelete,
+		String uadOutputPath, String uadPackagePath, boolean deprecated,
 		List<EntityColumn> pkEntityColumns,
 		List<EntityColumn> regularEntityColumns,
 		List<EntityColumn> blobEntityColumns,
@@ -129,6 +129,7 @@ public class Entity implements Comparable<Entity> {
 		_apiPackagePath = apiPackagePath;
 		_portletShortName = portletShortName;
 		_name = name;
+		_variableName = variableName;
 		_pluralName = GetterUtil.getString(
 			pluralName, serviceBuilder.formatPlural(name));
 		_humanName = GetterUtil.getString(
@@ -774,6 +775,10 @@ public class Entity implements Comparable<Entity> {
 	}
 
 	public String getVarName() {
+		if (Validator.isNotNull(_variableName)) {
+			return _variableName;
+		}
+
 		return TextFormatter.format(_name, TextFormatter.I);
 	}
 
@@ -1328,6 +1333,7 @@ public class Entity implements Comparable<Entity> {
 	private List<String> _unresolvedReferenceEntityNames;
 	private final boolean _uuid;
 	private final boolean _uuidAccessor;
+	private final String _variableName;
 	private Entity _versionedEntity;
 	private Entity _versionEntity;
 
