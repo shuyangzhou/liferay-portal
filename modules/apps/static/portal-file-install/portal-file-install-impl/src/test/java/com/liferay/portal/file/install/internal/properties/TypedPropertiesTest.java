8e38ad8a44626a84640e2f409fd77086e2f612d6
shuyangzhou <shuyang.zhou@liferay.com>
2020-08-28 00:38:10 -0700

    LPS-118855 Simplify

--- modules/apps/static/portal-file-install/portal-file-install-impl/src/test/java/com/liferay/portal/file/install/internal/properties/TypedPropertiesTest.java
+++ modules/apps/static/portal-file-install/portal-file-install-impl/src/test/java/com/liferay/portal/file/install/internal/properties/TypedPropertiesTest.java
@@ -21,6 +21,7 @@ import java.io.StringReader;
 import java.io.StringWriter;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Set;
 
 import org.junit.Assert;
@@ -65,12 +66,7 @@ public class TypedPropertiesTest {
 		TypedProperties typedProperties = _createTypedProperties(line);
 
 		Assert.assertEquals(
-			new ArrayList<String>() {
-				{
-					add("testValue1");
-					add("testValue2");
-				}
-			},
+			Arrays.asList("testValue1", "testValue2"),
 			typedProperties.get("testKey"));
 
 		_assertSave(typedProperties, line);
