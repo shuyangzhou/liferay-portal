93b107d907bb681e32566368d6edd1f297ac287f
Matija Petanjek <matija.petanjek@liferay.com>
2020-09-07 18:05:50 +0200

    COMMERCE-2475 dispatch - .sql remove old entity table and indexes

--- modules/apps/dispatch/dispatch-service/src/main/resources/META-INF/sql/tables.sql
+++ modules/apps/dispatch/dispatch-service/src/main/resources/META-INF/sql/tables.sql
@@ -29,21 +29,3 @@ create table DispatchTask (
 	type_ VARCHAR(75) null,
 	typeSettings VARCHAR(75) null
 );
-
-create table DispatchTrigger (
-	mvccVersion LONG default 0 not null,
-	dispatchTriggerId LONG not null primary key,
-	companyId LONG,
-	userId LONG,
-	userName VARCHAR(75) null,
-	createDate DATE null,
-	modifiedDate DATE null,
-	active_ BOOLEAN,
-	cronExpression VARCHAR(75) null,
-	endDate DATE null,
-	name VARCHAR(75) null,
-	startDate DATE null,
-	system_ BOOLEAN,
-	type_ VARCHAR(75) null,
-	typeSettings TEXT null
-);
