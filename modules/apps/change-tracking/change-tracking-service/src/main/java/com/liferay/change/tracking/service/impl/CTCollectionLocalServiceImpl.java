2222ec7a1d6299fad6a95a59df941c634965ea6c
Preston-Crary <preston.crary@liferay.com>
2020-12-09 18:42:26 -0800

    LPS-124713 Call super.deactivate().

--- modules/apps/change-tracking/change-tracking-service/src/main/java/com/liferay/change/tracking/service/impl/CTCollectionLocalServiceImpl.java
+++ modules/apps/change-tracking/change-tracking-service/src/main/java/com/liferay/change/tracking/service/impl/CTCollectionLocalServiceImpl.java
@@ -744,7 +744,10 @@ public class CTCollectionLocalServiceImpl
 	}
 
 	@Deactivate
+	@Override
 	protected void deactivate() {
+		super.deactivate();
+
 		_constraintResolverServiceTrackerMap.close();
 
 		_ctDisplayRendererServiceTrackerMap.close();
