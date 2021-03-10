8f8a779c12703aaef96b67ddf107ecc8ff5635c6
lily.chi <lily.chi@liferay.com>
2021-03-10 18:13:59 +0800

    COMMERCE-4993 Simplify, remove repeat loop

--- modules/util/portal-tools-sample-sql-builder/src/main/java/com/liferay/portal/tools/sample/sql/builder/DataFactory.java
+++ modules/util/portal-tools-sample-sql-builder/src/main/java/com/liferay/portal/tools/sample/sql/builder/DataFactory.java
@@ -1240,21 +1240,6 @@ public class DataFactory {
 			commerceCatalogModel.getName(), false);
 	}
 
-	public List<GroupModel> newCommerceCatalogGroupModels(
-		List<CommerceCatalogModel> commerceCatalogModels) {
-
-		List<GroupModel> groupModels = new ArrayList<>(
-			commerceCatalogModels.size());
-
-		for (CommerceCatalogModel commerceCatalogModel :
-				commerceCatalogModels) {
-
-			groupModels.add(newCommerceCatalogGroupModel(commerceCatalogModel));
-		}
-
-		return groupModels;
-	}
-
 	public CommerceCatalogModel newCommerceCatalogModel(
 		CommerceCurrencyModel commerceCurrencyModel, int count) {
 
