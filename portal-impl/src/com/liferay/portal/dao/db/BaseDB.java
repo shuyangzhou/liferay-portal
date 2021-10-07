8933bb063ba98055c0eb82bbd14896da8ce23b2a
Sergio Alonso Manzanedo <sergio.alonso@liferay.com>
2021-10-07 17:24:45 +0200

    LPS-140390 When database name is oracle we create a function-based index with substr function, if applies.

--- portal-impl/src/com/liferay/portal/dao/db/BaseDB.java
+++ portal-impl/src/com/liferay/portal/dao/db/BaseDB.java
@@ -861,6 +861,20 @@ public abstract class BaseDB implements DB {
 				new Filter(dbType.getName())),
 			-1);
 
+		String dbName = dbType.getName();
+
+		if (dbName.equals(DBType.ORACLE.toString())) {
+			return _applyOracleMaxStringIndexLengthLimitation(
+				template, stringIndexMaxLength);
+		}
+
+		return _applyMySQLMaxStringIndexLengthLimitation(
+			template, stringIndexMaxLength);
+	}
+
+	private String _applyMySQLMaxStringIndexLengthLimitation(
+		String template, int stringIndexMaxLength) {
+
 		Matcher matcher = _columnLengthPattern.matcher(template);
 
 		if (stringIndexMaxLength < 0) {
@@ -887,6 +901,57 @@ public abstract class BaseDB implements DB {
 		return sb.toString();
 	}
 
+	private String _applyOracleMaxStringIndexLengthLimitation(
+		String template, int stringIndexMaxLength) {
+
+		Matcher matcher = _columnOraclePattern.matcher(template);
+
+		StringBuffer sb = new StringBuffer();
+
+		if (stringIndexMaxLength < 0) {
+			while (matcher.find()) {
+				String column = matcher.group(1);
+
+				matcher.appendReplacement(sb, column);
+			}
+		}
+		else {
+			while (matcher.find()) {
+				String column = matcher.group(1);
+
+				StringBundler replacementSB = new StringBundler(5);
+
+				if (column.startsWith("(")) {
+					replacementSB.append("\\(substr\\(");
+					replacementSB.append(column.substring(1))
+					replacementSB.append(", 1, ");
+					replacementSB.append(stringIndexMaxLength)
+					replacementSB.append("\\)");
+				}
+				else {
+					replacementSB.append("substr\\(");
+					replacementSB.append(column);
+					replacementSB.append(", 1, ");
+					replacementSB.append(stringIndexMaxLength);
+					replacementSB.append("\\)");
+				}
+
+				int length = Integer.valueOf(matcher.group(2));
+
+				if (length > stringIndexMaxLength) {
+					matcher.appendReplacement(sb, replacementSB.toString());
+				}
+				else {
+					matcher.appendReplacement(sb, column);
+				}
+			}
+		}
+
+		matcher.appendTail(sb);
+
+		return sb.toString();
+	}
+
 	private static final boolean _SUPPORTS_ALTER_COLUMN_NAME = true;
 
 	private static final boolean _SUPPORTS_ALTER_COLUMN_TYPE = true;
@@ -903,6 +968,8 @@ public abstract class BaseDB implements DB {
 
 	private static final Pattern _columnLengthPattern = Pattern.compile(
 		"\\[\\$COLUMN_LENGTH:(\\d+)\\$\\]");
+	private static final Pattern _columnOraclePattern = Pattern.compile(
+		"(\\S+)\\[\\$COLUMN_LENGTH:(\\d+)\\$\\]");
 	private static final Pattern _templatePattern;
 
 	static {
