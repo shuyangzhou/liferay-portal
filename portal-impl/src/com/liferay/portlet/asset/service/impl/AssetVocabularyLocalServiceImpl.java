cd8055d94cd2e4ff7655952b610ae36f05fc14cf
amosfong <amos.fong@liferay.com>
2022-02-01 08:59:52 -0800

    LPS-143575 set externalReferenceCode to primKey if it's blank

--- portal-impl/src/com/liferay/portlet/asset/service/impl/AssetVocabularyLocalServiceImpl.java
+++ portal-impl/src/com/liferay/portlet/asset/service/impl/AssetVocabularyLocalServiceImpl.java
@@ -203,10 +203,6 @@ public class AssetVocabularyLocalServiceImpl
 
 		long vocabularyId = counterLocalService.increment();
 
-		if (Validator.isNull(externalReferenceCode)) {
-			externalReferenceCode = String.valueOf(vocabularyId);
-		}
-
 		_validateExternalReferenceCode(externalReferenceCode, groupId);
 
 		AssetVocabulary vocabulary = assetVocabularyPersistence.create(
@@ -671,6 +667,10 @@ public class AssetVocabularyLocalServiceImpl
 	private void _validateExternalReferenceCode(
 			String externalReferenceCode, long groupId)
 		throws PortalException {
+
+		if (Validator.isNull(externalReferenceCode)) {
+			return;
+		}
 
 		AssetVocabulary assetVocabulary =
 			assetVocabularyPersistence.fetchByG_ERC(
