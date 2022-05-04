shuyangzhou/liferay-portal

commit 617e44105411fb66e5d5eb47f0b8233c069aa0e0

LPS-46550 Use UploadException when LiferayFileItem.threshold.size is exceeded

portal-impl/src/com/liferay/portal/upload/UploadServletRequestImpl.java
@@ -24,6 +24,7 @@ import com.liferay.portal.kernel.upload.UploadServletRequest;
 import com.liferay.portal.kernel.util.ArrayUtil;
 import com.liferay.portal.kernel.util.FileUtil;
 import com.liferay.portal.kernel.util.PropsKeys;
+import com.liferay.portal.kernel.util.StringBundler;
 import com.liferay.portal.kernel.util.SystemProperties;
 import com.liferay.portal.kernel.util.Validator;
 import com.liferay.portal.util.PrefsPropsUtil;
@@ -108,7 +109,29 @@ public class UploadServletRequestImpl
 
 					List<String> values = _regularParameters.get(fieldName);
 
-					values.add(liferayFileItem.getString());
+					if (liferayFileItem.getSize() >
+							LiferayFileItem.THRESHOLD_SIZE) {
+
+						StringBundler sb = new StringBundler(5);
+
+						sb.append("The field ");
+						sb.append(fieldName);
+						sb.append(" exceeds its maximum permitted size of ");
+						sb.append(LiferayFileItem.THRESHOLD_SIZE);
+						sb.append(" bytes");
+
+						UploadException uploadException = new UploadException(
+							sb.toString());
+
+						uploadException.setExceededLiferayFileItemSizeLimit(
+							true);
+						uploadException.setExceededSizeLimit(true);
+
+						request.setAttribute(
+							WebKeys.UPLOAD_EXCEPTION, uploadException);
+					}
+
+					values.add(liferayFileItem.getEncodedString());
 
 					continue;
 				}

portal-service/src/com/liferay/portal/kernel/upload/UploadException.java
@@ -37,14 +37,25 @@ public class UploadException extends PortalException {
 		super(cause);
 	}
 
+	public boolean isExceededLiferayFileItemSizeLimit() {
+		return _exceededLiferayFileItemSizeLimit;
+	}
+
 	public boolean isExceededSizeLimit() {
 		return _exceededSizeLimit;
+	}
+
+	public void setExceededLiferayFileItemSizeLimit(
+		boolean exceededLiferayFileItemSizeLimit) {
+
+		_exceededLiferayFileItemSizeLimit = exceededLiferayFileItemSizeLimit;
 	}
 
 	public void setExceededSizeLimit(boolean exceededSizeLimit) {
 		_exceededSizeLimit = exceededSizeLimit;
 	}
 
+	private boolean _exceededLiferayFileItemSizeLimit;
 	private boolean _exceededSizeLimit;
 
 }

commit 5a7212647d4801ad9af713c726e6331ce6f677b9

LPS-X Remove the weird FileItem.setString()/FileItem.getEncodedString(), hide the logic inside FileItem.getString(). This fixes a bug caused by 617e4410, in that commit, it changed "liferayFileItem.getString()" to "liferayFileItem.getEncodedString()" which is disabling the LiferayFileItem.THRESHOLD_SIZE as a side affect.

portal-impl/src/com/liferay/portal/upload/LiferayFileItem.java
@@ -53,7 +53,7 @@ public class LiferayFileItem extends DiskFileItem implements FileItem {
 
 	public LiferayFileItem(
 		String fieldName, String contentType, boolean formField,
-		String fileName, int sizeThreshold, File repository) {
+		String fileName, int sizeThreshold, File repository, String encoding) {
 
 		super(
 			fieldName, contentType, formField, fileName, sizeThreshold,
@@ -62,6 +62,7 @@ public class LiferayFileItem extends DiskFileItem implements FileItem {
 		_fileName = fileName;
 		_sizeThreshold = sizeThreshold;
 		_repository = repository;
+		_encoding = encoding;
 	}
 
 	@Override
@@ -77,11 +78,6 @@ public class LiferayFileItem extends DiskFileItem implements FileItem {
 
 			return ContentTypes.APPLICATION_OCTET_STREAM;
 		}
-	}
-
-	@Override
-	public String getEncodedString() {
-		return _encodedString;
 	}
 
 	@Override
@@ -185,21 +181,16 @@ public class LiferayFileItem extends DiskFileItem implements FileItem {
 			return StringPool.BLANK;
 		}
 
-		if (_encodedString == null) {
-			return super.getString();
+		if (isFormField() && (_encoding != null)) {
+			try {
+				return super.getString(_encoding);
+			}
+			catch (UnsupportedEncodingException unsupportedEncodingException) {
+				_log.error(unsupportedEncodingException);
+			}
 		}
 
-		return _encodedString;
-	}
-
-	@Override
-	public void setString(String encode) {
-		try {
-			_encodedString = getString(encode);
-		}
-		catch (UnsupportedEncodingException unsupportedEncodingException) {
-			_log.error(unsupportedEncodingException);
-		}
+		return super.getString();
 	}
 
 	@Override
@@ -242,7 +233,7 @@ public class LiferayFileItem extends DiskFileItem implements FileItem {
 
 	private static int _counter;
 
-	private String _encodedString;
+	private final String _encoding;
 	private String _fileName;
 	private final File _repository;
 	private final int _sizeThreshold;

portal-impl/src/com/liferay/portal/upload/LiferayFileItemFactory.java
@@ -23,7 +23,9 @@ import org.apache.commons.fileupload.disk.DiskFileItemFactory;
  */
 public class LiferayFileItemFactory extends DiskFileItemFactory {
 
-	public LiferayFileItemFactory(File tempDir, int sizeThreshold) {
+	public LiferayFileItemFactory(
+		File tempDir, int sizeThreshold, String encoding) {
+
 		_tempDir = tempDir;
 
 		if (sizeThreshold > 0) {
@@ -32,6 +34,8 @@ public class LiferayFileItemFactory extends DiskFileItemFactory {
 		else {
 			_sizeThreshold = _DEFAULT_SIZE;
 		}
+
+		_encoding = encoding;
 	}
 
 	@Override
@@ -41,11 +45,12 @@ public class LiferayFileItemFactory extends DiskFileItemFactory {
 
 		return new LiferayFileItem(
 			fieldName, contentType, formField, fileName, _sizeThreshold,
-			_tempDir);
+			_tempDir, _encoding);
 	}
 
 	private static final int _DEFAULT_SIZE = 1024;
 
+	private final String _encoding;
 	private final int _sizeThreshold;
 	private final File _tempDir;
 

portal-impl/src/com/liferay/portal/upload/UploadServletRequestImpl.java
@@ -82,7 +82,8 @@ public class UploadServletRequestImpl
 
 			ServletFileUpload servletFileUpload = new ServletFileUpload(
 				new LiferayFileItemFactory(
-					_getTempDir(location), fileSizeThreshold));
+					_getTempDir(location), fileSizeThreshold,
+					httpServletRequest.getCharacterEncoding()));
 
 			long uploadServletRequestImplMaxSize =
 				UploadServletRequestConfigurationHelperUtil.getMaxSize();
@@ -148,9 +149,6 @@ public class UploadServletRequestImpl
 				}
 
 				if (liferayFileItem.isFormField()) {
-					liferayFileItem.setString(
-						httpServletRequest.getCharacterEncoding());
-
 					String fieldName = liferayFileItem.getFieldName();
 
 					if (!_regularParameters.containsKey(fieldName)) {
@@ -176,7 +174,7 @@ public class UploadServletRequestImpl
 							WebKeys.UPLOAD_EXCEPTION, uploadException);
 					}
 
-					values.add(liferayFileItem.getEncodedString());
+					values.add(liferayFileItem.getString());
 
 					continue;
 				}

portal-kernel/src/com/liferay/portal/kernel/upload/FileItem.java
@@ -33,8 +33,6 @@ public interface FileItem {
 
 	public String getContentType();
 
-	public String getEncodedString();
-
 	public String getFieldName();
 
 	public String getFileName();
@@ -62,8 +60,6 @@ public interface FileItem {
 	public boolean isFormField();
 
 	public boolean isInMemory();
-
-	public void setString(String encode);
 
 	public void write(File file) throws Exception;
 