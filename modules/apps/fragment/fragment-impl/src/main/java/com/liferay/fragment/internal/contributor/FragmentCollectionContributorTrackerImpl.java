shuyangzhou/liferay-portal

commit a103c34142edf7901e441f3db62e79dc7f9d5cca

LPS-109350 Register corresponding service when fragment collection contributor was added to be able to depend on it

modules/apps/fragment/fragment-impl/src/main/java/com/liferay/fragment/internal/contributor/FragmentCollectionContributorTrackerImpl.java
@@ -16,6 +16,7 @@ package com.liferay.fragment.internal.contributor;
 
 import com.liferay.fragment.constants.FragmentConstants;
 import com.liferay.fragment.contributor.FragmentCollectionContributor;
+import com.liferay.fragment.contributor.FragmentCollectionContributorRegistration;
 import com.liferay.fragment.contributor.FragmentCollectionContributorTracker;
 import com.liferay.fragment.model.FragmentEntry;
 import com.liferay.fragment.model.FragmentEntryLink;
@@ -28,17 +29,20 @@ import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.util.AggregateResourceBundleLoader;
+import com.liferay.portal.kernel.util.HashMapDictionary;
 import com.liferay.portal.kernel.util.ListUtil;
 import com.liferay.portal.kernel.util.MapUtil;
 import com.liferay.portal.kernel.util.ResourceBundleLoader;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Dictionary;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Locale;
 import java.util.Map;
 import java.util.Objects;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -273,7 +277,8 @@ public class FragmentCollectionContributorTrackerImpl
 	private static final Log _log = LogFactoryUtil.getLog(
 		FragmentCollectionContributorTrackerImpl.class);
 
-	private volatile Map<String, FragmentEntry> _fragmentEntries;
+	private volatile Map<String, FragmentEntry> _fragmentEntries =
+		new ConcurrentHashMap<>();
 
 	@Reference
 	private FragmentEntryLinkLocalService _fragmentEntryLinkLocalService;
@@ -295,9 +300,29 @@ public class FragmentCollectionContributorTrackerImpl
 		public FragmentCollectionContributor addingService(
 			ServiceReference<FragmentCollectionContributor> serviceReference) {
 
-			_fragmentEntries = null;
+			FragmentCollectionContributor fragmentCollectionContributor =
+				_bundleContext.getService(serviceReference);
 
-			return _bundleContext.getService(serviceReference);
+			if (_fragmentEntries == null) {
+				_fragmentEntries = new ConcurrentHashMap<>();
+			}
+
+			_fragmentEntries.putAll(
+				_getFragmentEntries(fragmentCollectionContributor));
+
+			Dictionary<String, Object> properties = new HashMapDictionary<>();
+
+			properties.put(
+				"fragment.collection.key",
+				serviceReference.getProperty("fragment.collection.key"));
+
+			_bundleContext.registerService(
+				FragmentCollectionContributorRegistration.class,
+				new FragmentCollectionContributorRegistration() {
+				},
+				properties);
+
+			return fragmentCollectionContributor;
 		}
 
 		@Override

modules/apps/fragment/fragment-api/src/main/java/com/liferay/fragment/contributor/FragmentCollectionContributorRegistration.java
@@ -1,0 +1,21 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.fragment.contributor;
+
+/**
+ * @author Pavel Savinov
+ */
+public interface FragmentCollectionContributorRegistration {
+}

commit 675a1de559e0966ad74a25c79f2f273a5018d93f

LPS-X Can not just leak out registered service. This will cause AddDefaultLayoutPortalInstanceLifecycleListener not being unregistered when FragmentCollectionContributorTrackerImpl is being unregistered. Caused by a103c34142edf7901e441f3db62e79dc7f9d5cca and f409fd21bc9ab18c45d3c9fbc18f35a29bd9ff9d

modules/apps/fragment/fragment-impl/src/main/java/com/liferay/fragment/internal/contributor/FragmentCollectionContributorTrackerImpl.java
@@ -43,6 +43,7 @@ import java.util.concurrent.ConcurrentHashMap;
 
 import org.osgi.framework.BundleContext;
 import org.osgi.framework.ServiceReference;
+import org.osgi.framework.ServiceRegistration;
 import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Deactivate;
@@ -251,6 +252,8 @@ public class FragmentCollectionContributorTrackerImpl
 	@Reference
 	private FragmentEntryLinkLocalService _fragmentEntryLinkLocalService;
 
+	private final Map<FragmentCollectionContributor, ServiceRegistration<?>>
+		_serviceRegistrations = new HashMap<>();
 	private ServiceTrackerMap<String, FragmentCollectionContributor>
 		_serviceTrackerMap;
 
@@ -295,13 +298,16 @@ public class FragmentCollectionContributorTrackerImpl
 				}
 			}
 
-			_bundleContext.registerService(
-				FragmentCollectionContributorRegistration.class,
-				new FragmentCollectionContributorRegistration() {
-				},
-				MapUtil.singletonDictionary(
-					"fragment.collection.key",
-					serviceReference.getProperty("fragment.collection.key")));
+			_serviceRegistrations.put(
+				fragmentCollectionContributor,
+				_bundleContext.registerService(
+					FragmentCollectionContributorRegistration.class,
+					new FragmentCollectionContributorRegistration() {
+					},
+					MapUtil.singletonDictionary(
+						"fragment.collection.key",
+						serviceReference.getProperty(
+							"fragment.collection.key"))));
 
 			return fragmentCollectionContributor;
 		}
@@ -316,6 +322,11 @@ public class FragmentCollectionContributorTrackerImpl
 		public void removedService(
 			ServiceReference<FragmentCollectionContributor> serviceReference,
 			FragmentCollectionContributor fragmentCollectionContributor) {
+
+			ServiceRegistration<?> serviceRegistration =
+				_serviceRegistrations.remove(fragmentCollectionContributor);
+
+			serviceRegistration.unregister();
 
 			for (FragmentComposition fragmentComposition :
 					fragmentCollectionContributor.getFragmentCompositions()) {