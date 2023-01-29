shuyangzhou/liferay-portal

commit 08a1c988317e3a65946e068bb94ba712d15812a7

COMMERCE-9707 Add greedy/dynamic to be able to use the new model resource permission implementation

modules/apps/account/account-admin-web/src/main/java/com/liferay/account/admin/web/internal/security/permission/resource/AccountEntryPermission.java
@@ -23,6 +23,7 @@ import com.liferay.portal.kernel.security.permission.resource.ModelResourcePermi
 
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
+import org.osgi.service.component.annotations.ReferencePolicyOption;
 
 /**
  * @author Pei-Jung Lan
@@ -65,6 +66,7 @@ public class AccountEntryPermission {
 	}
 
 	@Reference(
+		policyOption = ReferencePolicyOption.GREEDY,
 		target = "(model.class.name=com.liferay.account.model.AccountEntry)",
 		unbind = "-"
 	)

modules/apps/account/account-admin-web/src/main/java/com/liferay/account/admin/web/internal/portlet/action/DeleteAccountEntryAddressesMVCActionCommand.java
@@ -31,6 +31,8 @@ import javax.portlet.ActionResponse;
 
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
+import org.osgi.service.component.annotations.ReferencePolicy;
+import org.osgi.service.component.annotations.ReferencePolicyOption;
 
 /**
  * @author Pei-Jung Lan
@@ -81,9 +83,11 @@ public class DeleteAccountEntryAddressesMVCActionCommand
 	}
 
 	@Reference(
+		policy = ReferencePolicy.DYNAMIC,
+		policyOption = ReferencePolicyOption.GREEDY,
 		target = "(model.class.name=com.liferay.account.model.AccountEntry)"
 	)
-	private ModelResourcePermission<AccountEntry>
+	private volatile ModelResourcePermission<AccountEntry>
 		_accountEntryModelResourcePermission;
 
 	@Reference

modules/apps/account/account-admin-web/src/main/java/com/liferay/account/admin/web/internal/portlet/action/EditAccountEntryAddressMVCActionCommand.java
@@ -39,6 +39,8 @@ import javax.portlet.ActionResponse;
 
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
+import org.osgi.service.component.annotations.ReferencePolicy;
+import org.osgi.service.component.annotations.ReferencePolicyOption;
 
 /**
  * @author Pei-Jung Lan
@@ -180,9 +182,11 @@ public class EditAccountEntryAddressMVCActionCommand
 	}
 
 	@Reference(
+		policy = ReferencePolicy.DYNAMIC,
+		policyOption = ReferencePolicyOption.GREEDY,
 		target = "(model.class.name=com.liferay.account.model.AccountEntry)"
 	)
-	private ModelResourcePermission<AccountEntry>
+	private volatile ModelResourcePermission<AccountEntry>
 		_accountEntryModelResourcePermission;
 
 	@Reference

modules/apps/account/account-service/src/main/java/com/liferay/account/internal/manager/CurrentAccountEntryManagerImpl.java
@@ -39,6 +39,8 @@ import java.util.Objects;
 
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
+import org.osgi.service.component.annotations.ReferencePolicy;
+import org.osgi.service.component.annotations.ReferencePolicyOption;
 
 /**
  * @author Pei-Jung Lan
@@ -190,9 +192,11 @@ public class CurrentAccountEntryManagerImpl
 	private AccountEntryLocalService _accountEntryLocalService;
 
 	@Reference(
+		policy = ReferencePolicy.DYNAMIC,
+		policyOption = ReferencePolicyOption.GREEDY,
 		target = "(model.class.name=com.liferay.account.model.AccountEntry)"
 	)
-	private ModelResourcePermission<AccountEntry>
+	private volatile ModelResourcePermission<AccountEntry>
 		_accountEntryModelResourcePermission;
 
 	@Reference

modules/apps/account/account-service/src/main/java/com/liferay/account/internal/search/spi/model/permission/UserSearchPermissionFilterContributor.java
@@ -36,6 +36,8 @@ import java.util.List;
 
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
+import org.osgi.service.component.annotations.ReferencePolicy;
+import org.osgi.service.component.annotations.ReferencePolicyOption;
 
 /**
  * @author Drew Brokke
@@ -124,9 +126,11 @@ public class UserSearchPermissionFilterContributor
 		UserSearchPermissionFilterContributor.class);
 
 	@Reference(
+		policy = ReferencePolicy.DYNAMIC,
+		policyOption = ReferencePolicyOption.GREEDY,
 		target = "(model.class.name=com.liferay.account.model.AccountEntry)"
 	)
-	private ModelResourcePermission<AccountEntry>
+	private volatile ModelResourcePermission<AccountEntry>
 		_accountEntryModelResourcePermission;
 
 	@Reference

modules/apps/account/account-service/src/main/java/com/liferay/account/internal/security/permission/resource/AccountGroupModelResourcePermission.java
@@ -32,6 +32,8 @@ import java.util.Objects;
 
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
+import org.osgi.service.component.annotations.ReferencePolicy;
+import org.osgi.service.component.annotations.ReferencePolicyOption;
 
 /**
  * @author Pei-Jung Lan
@@ -129,9 +131,11 @@ public class AccountGroupModelResourcePermission
 	}
 
 	@Reference(
+		policy = ReferencePolicy.DYNAMIC,
+		policyOption = ReferencePolicyOption.GREEDY,
 		target = "(model.class.name=com.liferay.account.model.AccountEntry)"
 	)
-	private ModelResourcePermission<AccountEntry>
+	private volatile ModelResourcePermission<AccountEntry>
 		_accountEntryModelResourcePermission;
 
 	@Reference

modules/apps/account/account-service/src/main/java/com/liferay/account/internal/security/permission/resource/AccountRoleModelResourcePermission.java
@@ -35,6 +35,8 @@ import java.util.Objects;
 
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
+import org.osgi.service.component.annotations.ReferencePolicy;
+import org.osgi.service.component.annotations.ReferencePolicyOption;
 
 /**
  * @author Pei-Jung Lan
@@ -182,9 +184,11 @@ public class AccountRoleModelResourcePermission
 	private AccountEntryLocalService _accountEntryLocalService;
 
 	@Reference(
+		policy = ReferencePolicy.DYNAMIC,
+		policyOption = ReferencePolicyOption.GREEDY,
 		target = "(model.class.name=com.liferay.account.model.AccountEntry)"
 	)
-	private ModelResourcePermission<AccountEntry>
+	private volatile ModelResourcePermission<AccountEntry>
 		_accountEntryModelResourcePermission;
 
 	@Reference

modules/apps/account/account-service/src/main/java/com/liferay/account/service/impl/AccountEntryOrganizationRelServiceImpl.java
@@ -24,6 +24,8 @@ import com.liferay.portal.kernel.security.permission.resource.ModelResourcePermi
 
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
+import org.osgi.service.component.annotations.ReferencePolicy;
+import org.osgi.service.component.annotations.ReferencePolicyOption;
 
 /**
  * @author Brian Wing Shun Chan
@@ -91,9 +93,11 @@ public class AccountEntryOrganizationRelServiceImpl
 	}
 
 	@Reference(
+		policy = ReferencePolicy.DYNAMIC,
+		policyOption = ReferencePolicyOption.GREEDY,
 		target = "(model.class.name=com.liferay.account.model.AccountEntry)"
 	)
-	private ModelResourcePermission<AccountEntry>
+	private volatile ModelResourcePermission<AccountEntry>
 		_accountEntryModelResourcePermission;
 
 }

commit eeed54482493e27484322686a1590ac56a2ec131

LPS-X AccountEntryPermission's reference to ModelResourcePermission needs to be dynamic to avoid startup time component re-activation. Caused by 08a1c988

modules/apps/account/account-admin-web/src/main/java/com/liferay/account/admin/web/internal/security/permission/resource/AccountEntryPermission.java
@@ -23,6 +23,7 @@ import com.liferay.portal.kernel.security.permission.resource.ModelResourcePermi
 
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
+import org.osgi.service.component.annotations.ReferencePolicy;
 import org.osgi.service.component.annotations.ReferencePolicyOption;
 
 /**
@@ -66,14 +67,18 @@ public class AccountEntryPermission {
 	}
 
 	@Reference(
+		policy = ReferencePolicy.DYNAMIC,
 		policyOption = ReferencePolicyOption.GREEDY,
-		target = "(model.class.name=com.liferay.account.model.AccountEntry)",
-		unbind = "-"
+		target = "(model.class.name=com.liferay.account.model.AccountEntry)"
 	)
 	protected void setModelResourcePermission(
 		ModelResourcePermission<AccountEntry> modelResourcePermission) {
 
 		_accountEntryModelResourcePermission = modelResourcePermission;
+	}
+
+	protected void unsetModelResourcePermission(
+		ModelResourcePermission<AccountEntry> modelResourcePermission) {
 	}
 
 	private static final Log _log = LogFactoryUtil.getLog(