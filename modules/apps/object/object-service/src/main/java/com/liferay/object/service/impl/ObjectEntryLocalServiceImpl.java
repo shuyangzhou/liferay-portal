e7aa8be37db72ae9c75e7242b4d2dde671b80f66
Igor Beslic <igor.beslic@liferay.com>
2023-03-29 22:53:58 +0200

    LPS-177837 object-service - Fix this crazy line (call member method getValues directly, not over aggregated ServiceUtil class) - this also caused unexpected behaviour during DataGuard deletions

--- modules/apps/object/object-service/src/main/java/com/liferay/object/service/impl/ObjectEntryLocalServiceImpl.java
+++ modules/apps/object/object-service/src/main/java/com/liferay/object/service/impl/ObjectEntryLocalServiceImpl.java
@@ -410,7 +410,7 @@ public class ObjectEntryLocalServiceImpl
 	public ObjectEntry deleteObjectEntry(ObjectEntry objectEntry)
 		throws PortalException {
 
-		Map<String, Serializable> values = objectEntry.getValues();
+		Map<String, Serializable> values = getValues(objectEntry);
 
 		objectEntry = objectEntryPersistence.remove(objectEntry);
 
