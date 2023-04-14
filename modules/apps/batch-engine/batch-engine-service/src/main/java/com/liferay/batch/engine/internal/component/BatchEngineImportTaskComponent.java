64f001952abbc5af09934c92738b1ea767e66e3e
Raymond Augé <raymond.auge@liferay.com>
2023-04-14 17:47:09 -0400

    LPS-181729 Refactor BatchEngineTaskItemDelegateRegistry to wait for ResourceImpls to become active

---
+++ modules/apps/batch-engine/batch-engine-service/src/main/java/com/liferay/batch/engine/internal/component/BatchEngineImportTaskComponent.java
@@ -0,0 +1,125 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.batch.engine.internal.component;
+
+import com.liferay.batch.engine.BatchEngineImportTaskExecutor;
+import com.liferay.batch.engine.BatchEngineTaskExecuteStatus;
+import com.liferay.batch.engine.BatchEngineTaskOperation;
+import com.liferay.batch.engine.constants.BatchEngineImportTaskConstants;
+import com.liferay.batch.engine.internal.unit.BatchEngineUnitProcessorImpl;
+import com.liferay.batch.engine.model.BatchEngineImportTask;
+import com.liferay.batch.engine.service.BatchEngineImportTaskLocalService;
+import com.liferay.batch.engine.unit.BatchEngineUnit;
+import com.liferay.batch.engine.unit.BatchEngineUnitConfiguration;
+import com.liferay.petra.executor.PortalExecutorManager;
+import com.liferay.petra.string.StringBundler;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.util.StringUtil;
+import com.liferay.portal.vulcan.batch.engine.VulcanBatchEngineTaskItemDelegate;
+
+import java.util.Map;
+import java.util.concurrent.ExecutorService;
+
+import org.apache.felix.scr.ext.annotation.DSExt;
+
+import org.osgi.service.component.ComponentContext;
+import org.osgi.service.component.ComponentInstance;
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Raymond Augé
+ */
+@Component(factory = "batch.engine.import.task.component", service = {})
+@DSExt.PersistentFactoryComponent
+public class BatchEngineImportTaskComponent {
+
+	@Activate
+	protected void activate(
+			ComponentContext componentContext, Map<String, Object> properties)
+		throws Exception {
+
+		ComponentInstance<BatchEngineImportTaskComponent> componentInstance =
+			componentContext.getComponentInstance();
+
+		BatchEngineUnitConfiguration batchEngineUnitConfiguration =
+			(BatchEngineUnitConfiguration)properties.get(
+				"batchEngineUnitConfiguration");
+
+		BatchEngineUnit batchEngineUnit = (BatchEngineUnit)properties.get(
+			"batchEngineUnit");
+
+		byte[] content = (byte[])properties.get("content");
+		String contentType = (String)properties.get("contentType");
+
+		ExecutorService executorService =
+			_portalExecutorManager.getPortalExecutor(
+				BatchEngineUnitProcessorImpl.class.getName());
+
+		BatchEngineImportTask batchEngineImportTask =
+			_batchEngineImportTaskLocalService.addBatchEngineImportTask(
+				null, batchEngineUnitConfiguration.getCompanyId(),
+				batchEngineUnitConfiguration.getUserId(), 100,
+				batchEngineUnitConfiguration.getCallbackURL(),
+				batchEngineUnitConfiguration.getClassName(), content,
+				StringUtil.toUpperCase(contentType),
+				BatchEngineTaskExecuteStatus.INITIAL.name(),
+				batchEngineUnitConfiguration.getFieldNameMappingMap(),
+				BatchEngineImportTaskConstants.IMPORT_STRATEGY_ON_ERROR_FAIL,
+				BatchEngineTaskOperation.CREATE.name(),
+				batchEngineUnitConfiguration.getParameters(),
+				batchEngineUnitConfiguration.getTaskItemDelegateName());
+
+		executorService.submit(
+			() -> {
+				try {
+					_batchEngineImportTaskExecutor.execute(
+						batchEngineImportTask);
+
+					if (_log.isInfoEnabled()) {
+						_log.info(
+							StringBundler.concat(
+								"Successfully deployed batch engine file ",
+								batchEngineUnit.getFileName(), " ",
+								batchEngineUnit.getDataFileName()));
+					}
+				}
+				finally {
+					componentInstance.dispose();
+				}
+			});
+	}
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		BatchEngineImportTaskComponent.class);
+
+	@Reference
+	private BatchEngineImportTaskExecutor _batchEngineImportTaskExecutor;
+
+	@Reference
+	private BatchEngineImportTaskLocalService
+		_batchEngineImportTaskLocalService;
+
+	@Reference
+	private PortalExecutorManager _portalExecutorManager;
+
+	@Reference
+	@SuppressWarnings("rawtypes")
+	private Map.Entry<Map<String, Object>, VulcanBatchEngineTaskItemDelegate>
+		_vulcanBatchEngineTaskItemDelegate;
+
+}
--- modules/apps/batch-engine/batch-engine-service/src/main/java/com/liferay/batch/engine/internal/unit/BatchEngineUnitProcessorImpl.java
+++ modules/apps/batch-engine/batch-engine-service/src/main/java/com/liferay/batch/engine/internal/unit/BatchEngineUnitProcessorImpl.java
@@ -14,12 +14,6 @@
 
 package com.liferay.batch.engine.internal.unit;
 
-import com.liferay.batch.engine.BatchEngineImportTaskExecutor;
-import com.liferay.batch.engine.BatchEngineTaskExecuteStatus;
-import com.liferay.batch.engine.BatchEngineTaskOperation;
-import com.liferay.batch.engine.constants.BatchEngineImportTaskConstants;
-import com.liferay.batch.engine.model.BatchEngineImportTask;
-import com.liferay.batch.engine.service.BatchEngineImportTaskLocalService;
 import com.liferay.batch.engine.unit.BatchEngineUnit;
 import com.liferay.batch.engine.unit.BatchEngineUnitConfiguration;
 import com.liferay.batch.engine.unit.BatchEngineUnitProcessor;
@@ -35,19 +29,19 @@ import com.liferay.portal.kernel.model.Company;
 import com.liferay.portal.kernel.service.CompanyLocalService;
 import com.liferay.portal.kernel.service.UserLocalService;
 import com.liferay.portal.kernel.util.File;
+import com.liferay.portal.kernel.util.HashMapDictionaryBuilder;
 import com.liferay.portal.kernel.util.PropsKeys;
 import com.liferay.portal.kernel.util.PropsUtil;
-import com.liferay.portal.kernel.util.StringUtil;
 import com.liferay.portal.kernel.util.Validator;
 
 import java.io.InputStream;
 import java.io.Serializable;
 
 import java.util.Map;
-import java.util.concurrent.ExecutorService;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
 
+import org.osgi.service.component.ComponentFactory;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
 
@@ -81,6 +75,7 @@ public class BatchEngineUnitProcessorImpl implements BatchEngineUnitProcessor {
 		}
 	}
 
+	@SuppressWarnings("unchecked")
 	private void _processBatchEngineUnit(BatchEngineUnit batchEngineUnit)
 		throws Exception {
 
@@ -130,36 +125,21 @@ public class BatchEngineUnitProcessorImpl implements BatchEngineUnitProcessor {
 			return;
 		}
 
-		ExecutorService executorService =
-			_portalExecutorManager.getPortalExecutor(
-				BatchEngineUnitProcessorImpl.class.getName());
-
-		BatchEngineImportTask batchEngineImportTask =
-			_batchEngineImportTaskLocalService.addBatchEngineImportTask(
-				null, batchEngineUnitConfiguration.getCompanyId(),
-				batchEngineUnitConfiguration.getUserId(), 100,
-				batchEngineUnitConfiguration.getCallbackURL(),
-				batchEngineUnitConfiguration.getClassName(), content,
-				StringUtil.toUpperCase(contentType),
-				BatchEngineTaskExecuteStatus.INITIAL.name(),
-				batchEngineUnitConfiguration.getFieldNameMappingMap(),
-				BatchEngineImportTaskConstants.IMPORT_STRATEGY_ON_ERROR_FAIL,
-				BatchEngineTaskOperation.CREATE.name(),
-				batchEngineUnitConfiguration.getParameters(),
-				batchEngineUnitConfiguration.getTaskItemDelegateName());
-
-		executorService.submit(
-			() -> {
-				_batchEngineImportTaskExecutor.execute(batchEngineImportTask);
-
-				if (_log.isInfoEnabled()) {
-					_log.info(
-						StringBundler.concat(
-							"Successfully deployed batch engine file ",
-							batchEngineUnit.getFileName(), " ",
-							batchEngineUnit.getDataFileName()));
-				}
-			});
+		_componentFactory.newInstance(
+			HashMapDictionaryBuilder.<String, Object>put(
+				"_vulcanBatchEngineTaskItemDelegate.target",
+				StringBundler.concat(
+					"(batch.engine.entity.class.name=",
+					batchEngineUnitConfiguration.getClassName(), ")")
+			).put(
+				"batchEngineUnit", batchEngineUnit
+			).put(
+				"batchEngineUnitConfiguration", batchEngineUnitConfiguration
+			).put(
+				"content", content
+			).put(
+				"contentType", contentType
+			).build());
 	}
 
 	private BatchEngineUnitConfiguration _updateBatchEngineUnitConfiguration(
@@ -205,14 +185,13 @@ public class BatchEngineUnitProcessorImpl implements BatchEngineUnitProcessor {
 		BatchEngineUnitProcessorImpl.class);
 
 	@Reference
-	private BatchEngineImportTaskExecutor _batchEngineImportTaskExecutor;
-
-	@Reference
-	private BatchEngineImportTaskLocalService
-		_batchEngineImportTaskLocalService;
-
-	@Reference
 	private CompanyLocalService _companyLocalService;
+
+	@Reference(
+		target = "(component.factory=batch.engine.import.task.component)"
+	)
+	@SuppressWarnings("rawtypes")
+	private ComponentFactory _componentFactory;
 
 	@Reference
 	private File _file;
