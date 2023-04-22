6fcad049e46b715d5bec8ed5524c3918b1ac6c0b
shuyangzhou <shuyang.zhou@liferay.com>
2023-04-21 18:55:05 -0700

    LPS-X Remove useless DBProcessContext

--- modules/apps/portal/portal-upgrade-impl/src/test/java/com/liferay/portal/upgrade/internal/registry/UpgradeStepRegistryTest.java
+++ modules/apps/portal/portal-upgrade-impl/src/test/java/com/liferay/portal/upgrade/internal/registry/UpgradeStepRegistryTest.java
@@ -14,7 +14,6 @@
 
 package com.liferay.portal.upgrade.internal.registry;
 
-import com.liferay.portal.kernel.dao.db.DBProcessContext;
 import com.liferay.portal.kernel.upgrade.DummyUpgradeStep;
 import com.liferay.portal.kernel.upgrade.UpgradeProcess;
 import com.liferay.portal.kernel.upgrade.UpgradeStep;
@@ -239,7 +238,7 @@ public class UpgradeStepRegistryTest {
 	private static class TestUpgradeStep implements UpgradeStep {
 
 		@Override
-		public void upgrade(DBProcessContext dbProcessContext) {
+		public void upgrade() {
 		}
 
 	}
