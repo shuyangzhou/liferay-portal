8c36b06d856502948b9de35bb44708ea55dbd864
shuyangzhou <shuyang.zhou@liferay.com>
2023-06-20 22:02:14 -0700

    LPS-X Avoid creating release for service bundle. Service bundle's release entry is created when InitialUpgradeStep is executed.

--- modules/apps/portal/portal-verify-extender/src/main/java/com/liferay/portal/verify/extender/internal/osgi/commands/VerifyProcessTrackerOSGiCommands.java
+++ modules/apps/portal/portal-verify-extender/src/main/java/com/liferay/portal/verify/extender/internal/osgi/commands/VerifyProcessTrackerOSGiCommands.java
@@ -21,6 +21,7 @@ import com.liferay.osgi.service.tracker.collections.map.ServiceReferenceMapper;
 import com.liferay.osgi.service.tracker.collections.map.ServiceTrackerMap;
 import com.liferay.osgi.service.tracker.collections.map.ServiceTrackerMapFactory;
 import com.liferay.petra.string.StringBundler;
+import com.liferay.petra.string.StringPool;
 import com.liferay.portal.events.StartupHelperUtil;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
@@ -228,7 +229,9 @@ public class VerifyProcessTrackerOSGiCommands {
 
 						_executeVerifyProcess(verifyProcess, release);
 					}
-					else if (release == null) {
+					else if ((release == null) &&
+							 !_isServiceBundle(serviceReference.getBundle())) {
+
 						release = _releaseLocalService.createRelease(
 							_counterLocalService.increment());
 
@@ -295,7 +298,7 @@ public class VerifyProcessTrackerOSGiCommands {
 		try {
 			Bundle bundle = FrameworkUtil.getBundle(verifyProcess.getClass());
 
-			if (release == null) {
+			if ((release == null) && !_isServiceBundle(bundle)) {
 
 				// Verification state must be persisted even though not all
 				// verifiers are associated with a database service
@@ -315,20 +318,26 @@ public class VerifyProcessTrackerOSGiCommands {
 
 				verifyProcess.verify();
 
-				release.setVerified(true);
-				release.setState(ReleaseConstants.STATE_GOOD);
+				if (release != null) {
+					release.setVerified(true);
+					release.setState(ReleaseConstants.STATE_GOOD);
+				}
 			}
 			catch (VerifyException verifyException) {
 				_log.error(verifyException);
 
-				release.setVerified(false);
-				release.setState(ReleaseConstants.STATE_VERIFY_FAILURE);
+				if (release != null) {
+					release.setVerified(false);
+					release.setState(ReleaseConstants.STATE_VERIFY_FAILURE);
+				}
 			}
 			finally {
 				UpgradeLogContext.clearContext();
 			}
 
-			_releaseLocalService.updateRelease(release);
+			if (release != null) {
+				_releaseLocalService.updateRelease(release);
+			}
 		}
 		finally {
 			NotificationThreadLocal.setEnabled(true);
@@ -382,6 +391,19 @@ public class VerifyProcessTrackerOSGiCommands {
 		}
 
 		return false;
+	}
+
+	private boolean _isServiceBundle(Bundle bundle) {
+		Dictionary<String, String> headers = bundle.getHeaders(
+			StringPool.BLANK);
+
+		if ((headers.get("Liferay-Service") == null) &&
+			(headers.get("Liferay-Spring-Context") == null)) {
+
+			return false;
+		}
+
+		return true;
 	}
 
 	private static final Log _log = LogFactoryUtil.getLog(
