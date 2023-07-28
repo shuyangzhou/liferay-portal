12bbf05121f1c261e96f7bd5f827a45770a5641c
shuyangzhou <shuyang.zhou@liferay.com>
2023-07-27 22:17:02 -0700

    LPS-192210 Merge DependencyManagerSync into DependencyManagerSyncUtil

--- portal-kernel/src/com/liferay/portal/kernel/dependency/manager/DependencyManagerSyncUtil.java
+++ portal-kernel/src/com/liferay/portal/kernel/dependency/manager/DependencyManagerSyncUtil.java
@@ -5,10 +5,20 @@
 
 package com.liferay.portal.kernel.dependency.manager;
 
-import com.liferay.portal.kernel.util.ServiceProxyFactory;
+import com.liferay.portal.kernel.concurrent.DefaultNoticeableFuture;
+import com.liferay.portal.kernel.concurrent.FutureListener;
+import com.liferay.portal.kernel.concurrent.SystemExecutorServiceUtil;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.util.GetterUtil;
+import com.liferay.portal.kernel.util.PropsKeys;
+import com.liferay.portal.kernel.util.PropsUtil;
 
 import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Future;
 import java.util.concurrent.FutureTask;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author Shuyang Zhou
@@ -16,22 +26,81 @@ import java.util.concurrent.FutureTask;
 public class DependencyManagerSyncUtil {
 
 	public static void registerSyncCallable(Callable<Void> syncCallable) {
-		_dependencyManagerSync.registerSyncCallable(syncCallable);
+		_addFutureListener(
+			_syncCallableDefaultNoticeableFuture,
+			future -> {
+				try {
+					syncCallable.call();
+				}
+				catch (Exception exception) {
+					_log.error("Unable to sync callable", exception);
+				}
+			});
 	}
 
 	public static void registerSyncFutureTask(
 		FutureTask<Void> syncFutureTask, String taskName) {
 
-		_dependencyManagerSync.registerSyncFutureTask(syncFutureTask, taskName);
+		_addFutureListener(
+			_syncFutureTaskDefaultNoticeableFuture,
+			future -> {
+				syncFutureTask.run();
+
+				try {
+					syncFutureTask.get(_SYNC_TIMEOUT, TimeUnit.SECONDS);
+				}
+				catch (Exception exception) {
+					_log.error("Unable to sync future", exception);
+				}
+			});
+
+		if (!syncFutureTask.isDone()) {
+			ExecutorService systemExecutorService =
+				SystemExecutorServiceUtil.getExecutorService();
+
+			systemExecutorService.submit(
+				SystemExecutorServiceUtil.renameThread(
+					syncFutureTask, taskName));
+		}
 	}
 
 	public static void sync() {
-		_dependencyManagerSync.sync();
+		if (_syncCallableDefaultNoticeableFuture.isDone()) {
+			return;
+		}
+
+		_syncFutureTaskDefaultNoticeableFuture.run();
+
+		_syncCallableDefaultNoticeableFuture.run();
 	}
 
-	private static volatile DependencyManagerSync _dependencyManagerSync =
-		ServiceProxyFactory.newServiceTrackedInstance(
-			DependencyManagerSync.class, DependencyManagerSyncUtil.class,
-			"_dependencyManagerSync", true);
+	private static void _addFutureListener(
+		DefaultNoticeableFuture<Void> defaultNoticeableFuture,
+		FutureListener<Void> futureListener) {
+
+		defaultNoticeableFuture.addFutureListener(
+			new FutureListener<Void>() {
+
+				@Override
+				public void complete(Future<Void> future) {
+					futureListener.complete(future);
+
+					defaultNoticeableFuture.removeFutureListener(this);
+				}
+
+			});
+	}
+
+	private static final long _SYNC_TIMEOUT = GetterUtil.getInteger(
+		PropsUtil.get(PropsKeys.DEPENDENCY_MANAGER_SYNC_TIMEOUT), 60);
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		DependencyManagerSyncUtil.class);
+
+	private static final DefaultNoticeableFuture<Void>
+		_syncCallableDefaultNoticeableFuture = new DefaultNoticeableFuture<>();
+	private static final DefaultNoticeableFuture<Void>
+		_syncFutureTaskDefaultNoticeableFuture =
+			new DefaultNoticeableFuture<>();
 
 }
