a861a914cdad30f85c6790736daede487c47aef8
shuyangzhou <shuyang.zhou@liferay.com>
2023-08-06 08:48:19 -0700

    LPS-X Defer default layout creation to runtime to avoid startup time service availability racing.

---
+++ modules/apps/site/site-welcome-site-initializer/src/main/java/com/liferay/site/welcome/site/initializer/internal/instance/lifecycle/DefaultLayoutCreatorImpl.java
@@ -0,0 +1,99 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2023 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.site.welcome.site.initializer.internal.instance.lifecycle;
+
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.model.Group;
+import com.liferay.portal.kernel.model.Role;
+import com.liferay.portal.kernel.model.User;
+import com.liferay.portal.kernel.model.role.RoleConstants;
+import com.liferay.portal.kernel.security.auth.PrincipalThreadLocal;
+import com.liferay.portal.kernel.security.permission.PermissionChecker;
+import com.liferay.portal.kernel.security.permission.PermissionCheckerFactory;
+import com.liferay.portal.kernel.security.permission.PermissionThreadLocal;
+import com.liferay.portal.kernel.service.LayoutLocalService;
+import com.liferay.portal.kernel.service.RoleLocalService;
+import com.liferay.portal.kernel.service.ServiceContext;
+import com.liferay.portal.kernel.service.ServiceContextThreadLocal;
+import com.liferay.portal.kernel.service.UserLocalService;
+import com.liferay.site.initializer.SiteInitializer;
+import com.liferay.sites.kernel.util.DefaultLayoutCreator;
+
+import java.util.List;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Pavel Savinov
+ */
+@Component(service = DefaultLayoutCreator.class)
+public class DefaultLayoutCreatorImpl implements DefaultLayoutCreator {
+
+	@Override
+	public void create(Group group) throws PortalException {
+		String name = PrincipalThreadLocal.getName();
+
+		PermissionChecker permissionChecker =
+			PermissionThreadLocal.getPermissionChecker();
+
+		try {
+			User user = _getUser(group.getCompanyId());
+
+			PrincipalThreadLocal.setName(user.getUserId());
+
+			PermissionThreadLocal.setPermissionChecker(
+				_defaultPermissionCheckerFactory.create(user));
+
+			ServiceContextThreadLocal.pushServiceContext(new ServiceContext());
+
+			_siteInitializer.initialize(group.getGroupId());
+		}
+		finally {
+			PrincipalThreadLocal.setName(name);
+
+			PermissionThreadLocal.setPermissionChecker(permissionChecker);
+
+			ServiceContextThreadLocal.popServiceContext();
+		}
+	}
+
+	private User _getUser(long companyId) throws PortalException {
+		Role role = _roleLocalService.fetchRole(
+			companyId, RoleConstants.ADMINISTRATOR);
+
+		if (role == null) {
+			return _userLocalService.getGuestUser(companyId);
+		}
+
+		List<User> adminUsers = _userLocalService.getRoleUsers(
+			role.getRoleId(), 0, 1);
+
+		if (adminUsers.isEmpty()) {
+			return _userLocalService.getGuestUser(companyId);
+		}
+
+		return adminUsers.get(0);
+	}
+
+	@Reference
+	private PermissionCheckerFactory _defaultPermissionCheckerFactory;
+
+	@Reference
+	private LayoutLocalService _layoutLocalService;
+
+	@Reference
+	private RoleLocalService _roleLocalService;
+
+	@Reference(
+		target = "(site.initializer.key=com.liferay.site.initializer.welcome)"
+	)
+	private SiteInitializer _siteInitializer;
+
+	@Reference
+	private UserLocalService _userLocalService;
+
+}
