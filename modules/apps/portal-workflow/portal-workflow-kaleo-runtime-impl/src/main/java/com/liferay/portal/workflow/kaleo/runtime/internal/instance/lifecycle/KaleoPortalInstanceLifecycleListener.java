a20fa657582e6c53e15a6e02a3f7289ed077d19e
shuyangzhou <shuyang.zhou@liferay.com>
2023-08-23 13:41:35 -0700

    LPS-X Postpone KaleoPortalInstanceLifecycleListener to avoid service availability race condition

--- modules/apps/portal-workflow/portal-workflow-kaleo-runtime-impl/src/main/java/com/liferay/portal/workflow/kaleo/runtime/internal/instance/lifecycle/KaleoPortalInstanceLifecycleListener.java
+++ modules/apps/portal-workflow/portal-workflow-kaleo-runtime-impl/src/main/java/com/liferay/portal/workflow/kaleo/runtime/internal/instance/lifecycle/KaleoPortalInstanceLifecycleListener.java
@@ -5,17 +5,11 @@
 
 package com.liferay.portal.workflow.kaleo.runtime.internal.instance.lifecycle;
 
-import com.liferay.portal.instance.lifecycle.BasePortalInstanceLifecycleListener;
+import com.liferay.portal.instance.lifecycle.InitialRequestPortalInstanceLifecycleListener;
 import com.liferay.portal.instance.lifecycle.PortalInstanceLifecycleListener;
-import com.liferay.portal.kernel.model.Company;
-import com.liferay.portal.workflow.kaleo.runtime.WorkflowEngine;
-import com.liferay.portal.workflow.kaleo.runtime.internal.messaging.KaleoWorkflowMessagingConfigurator;
+import com.liferay.portal.kernel.security.permission.PermissionChecker;
+import com.liferay.portal.kernel.security.permission.PermissionThreadLocal;
 import com.liferay.portal.workflow.kaleo.runtime.manager.PortalKaleoManager;
-import com.liferay.portal.workflow.kaleo.service.KaleoConditionLocalService;
-import com.liferay.portal.workflow.kaleo.service.KaleoDefinitionLocalService;
-import com.liferay.portal.workflow.kaleo.service.KaleoNodeLocalService;
-import com.liferay.portal.workflow.kaleo.service.KaleoTaskLocalService;
-import com.liferay.portal.workflow.kaleo.service.KaleoTransitionLocalService;
 
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
@@ -25,36 +19,24 @@ import org.osgi.service.component.annotations.Reference;
  */
 @Component(service = PortalInstanceLifecycleListener.class)
 public class KaleoPortalInstanceLifecycleListener
-	extends BasePortalInstanceLifecycleListener {
+	extends InitialRequestPortalInstanceLifecycleListener {
 
 	@Override
-	public void portalInstanceRegistered(Company company) throws Exception {
-		_portalKaleoManager.deployKaleoDefaults(company.getCompanyId());
+	protected void doPortalInstanceRegistered(long companyId) throws Exception {
+		PermissionChecker permissionChecker =
+			PermissionThreadLocal.getPermissionChecker();
+
+		PermissionThreadLocal.setPermissionChecker(null);
+
+		try {
+			_portalKaleoManager.deployKaleoDefaults(companyId);
+		}
+		finally {
+			PermissionThreadLocal.setPermissionChecker(permissionChecker);
+		}
 	}
 
 	@Reference
-	private KaleoConditionLocalService _kaleoConditionLocalService;
-
-	@Reference
-	private KaleoDefinitionLocalService _kaleoDefinitionLocalService;
-
-	@Reference
-	private KaleoNodeLocalService _kaleoNodeLocalService;
-
-	@Reference
-	private KaleoTaskLocalService _kaleoTaskLocalService;
-
-	@Reference
-	private KaleoTransitionLocalService _kaleoTransitionLocalService;
-
-	@Reference
-	private KaleoWorkflowMessagingConfigurator
-		_kaleoWorkflowMessagingConfigurator;
-
-	@Reference
 	private PortalKaleoManager _portalKaleoManager;
-
-	@Reference
-	private WorkflowEngine _workflowEngine;
 
 }
