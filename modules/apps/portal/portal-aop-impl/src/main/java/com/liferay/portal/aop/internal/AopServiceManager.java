5ef9cd3b13165848c1f573b60235374c5db385ab
shuyangzhou <shuyang.zhou@liferay.com>
2023-10-17 18:03:33 -0700

    LPS-X SystemChecker integration

--- modules/apps/portal/portal-aop-impl/src/main/java/com/liferay/portal/aop/internal/AopServiceManager.java
+++ modules/apps/portal/portal-aop-impl/src/main/java/com/liferay/portal/aop/internal/AopServiceManager.java
@@ -7,6 +7,8 @@ package com.liferay.portal.aop.internal;
 
 import com.liferay.osgi.service.tracker.collections.map.ServiceTrackerMap;
 import com.liferay.osgi.service.tracker.collections.map.ServiceTrackerMapFactory;
+import com.liferay.osgi.util.osgi.commands.OSGiCommands;
+import com.liferay.petra.io.unsync.UnsyncStringWriter;
 import com.liferay.petra.reflect.ReflectionUtil;
 import com.liferay.petra.string.StringBundler;
 import com.liferay.portal.aop.AopService;
@@ -14,9 +16,16 @@ import com.liferay.portal.events.StartupHelperUtil;
 import com.liferay.portal.kernel.concurrent.SystemExecutorServiceUtil;
 import com.liferay.portal.kernel.util.ArrayUtil;
 import com.liferay.portal.kernel.util.GetterUtil;
+import com.liferay.portal.kernel.util.HashMapDictionaryBuilder;
+import com.liferay.portal.kernel.util.MapUtil;
+import com.liferay.portal.osgi.debug.SystemChecker;
 import com.liferay.portal.spring.transaction.TransactionExecutor;
 
+import java.io.PrintWriter;
+
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.FutureTask;
@@ -28,6 +37,7 @@ import org.osgi.framework.Constants;
 import org.osgi.framework.Filter;
 import org.osgi.framework.InvalidSyntaxException;
 import org.osgi.framework.ServiceReference;
+import org.osgi.framework.ServiceRegistration;
 import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Deactivate;
@@ -62,10 +72,30 @@ public class AopServiceManager {
 			new AopServiceServiceTrackerCustomizer());
 
 		_aopServiceServiceTracker.open();
+
+		_serviceRegistrations.add(
+			bundleContext.registerService(
+				SystemChecker.class, new AopServiceRegistrarSystemChecker(),
+				MapUtil.singletonDictionary(Constants.SERVICE_RANKING, 1)));
+
+		_serviceRegistrations.add(
+			bundleContext.registerService(
+				OSGiCommands.class, new AopServiceRegistrarOSGiCommands(),
+				HashMapDictionaryBuilder.put(
+					"osgi.command.function", "failures"
+				).put(
+					"osgi.command.scope", "aop"
+				).build()));
 	}
 
 	@Deactivate
 	protected void deactivate() {
+		for (ServiceRegistration<?> serviceRegistration :
+				_serviceRegistrations) {
+
+			serviceRegistration.unregister();
+		}
+
 		_aopServiceServiceTracker.close();
 
 		_transactionExecutorServiceTrackerMap.close();
@@ -77,6 +107,38 @@ public class AopServiceManager {
 			properties.get("parallel.enabled"), StartupHelperUtil.isDBWarmed());
 	}
 
+	private String _collectRegistrationFailures() {
+		Throwable aggregatedThrowable = null;
+
+		Map<?, Supplier<AopServiceRegistrar>> trackedMap =
+			_aopServiceServiceTracker.getTracked();
+
+		for (Supplier<AopServiceRegistrar> supplier : trackedMap.values()) {
+			try {
+				supplier.get();
+			}
+			catch (Throwable throwable) {
+				if (aggregatedThrowable == null) {
+					aggregatedThrowable = throwable;
+				}
+				else {
+					aggregatedThrowable.addSuppressed(throwable);
+				}
+			}
+		}
+
+		if (aggregatedThrowable == null) {
+			return null;
+		}
+
+		UnsyncStringWriter unsyncStringWriter = new UnsyncStringWriter();
+
+		aggregatedThrowable.printStackTrace(
+			new PrintWriter(unsyncStringWriter));
+
+		return unsyncStringWriter.toString();
+	}
+
 	private ServiceTracker<AopService, Supplier<AopServiceRegistrar>>
 		_aopServiceServiceTracker;
 	private BundleContext _bundleContext;
@@ -85,6 +147,8 @@ public class AopServiceManager {
 	@Reference(target = "(&(bean.id=transactionExecutor)(original.bean=true))")
 	private TransactionExecutor _portalTransactionExecutor;
 
+	private final List<ServiceRegistration<?>> _serviceRegistrations =
+		new ArrayList<>();
 	private ServiceTrackerMap<Long, TransactionExecutor>
 		_transactionExecutorServiceTrackerMap;
 
@@ -134,6 +198,38 @@ public class AopServiceManager {
 		}
 
 		private final AopServiceRegistrar _aopServiceRegistrar;
+
+	}
+
+	private class AopServiceRegistrarOSGiCommands implements OSGiCommands {
+
+		public String failures() {
+			return _collectRegistrationFailures();
+		}
+
+	}
+
+	private class AopServiceRegistrarSystemChecker implements SystemChecker {
+
+		@Override
+		public String check() {
+			return _collectRegistrationFailures();
+		}
+
+		@Override
+		public String getName() {
+			return "Aop Service Registrar System Checker";
+		}
+
+		@Override
+		public String getOSGiCommand() {
+			return "aop:failures";
+		}
+
+		@Override
+		public String toString() {
+			return getName();
+		}
 
 	}
 
