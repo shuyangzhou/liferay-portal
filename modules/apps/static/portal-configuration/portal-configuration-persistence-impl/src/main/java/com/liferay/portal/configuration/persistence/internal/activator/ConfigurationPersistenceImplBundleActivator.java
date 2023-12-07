0561d551d3c395ec16ae2083eeebd5071f6c2cb1
shuyangzhou <shuyang.zhou@liferay.com>
2023-12-06 16:42:30 -0800

    LPS-X Getting DataSource from InfrastructureUtil

--- modules/apps/static/portal-configuration/portal-configuration-persistence-impl/src/main/java/com/liferay/portal/configuration/persistence/internal/activator/ConfigurationPersistenceImplBundleActivator.java
+++ modules/apps/static/portal-configuration/portal-configuration-persistence-impl/src/main/java/com/liferay/portal/configuration/persistence/internal/activator/ConfigurationPersistenceImplBundleActivator.java
@@ -10,18 +10,13 @@ import com.liferay.portal.configuration.persistence.internal.ConfigurationPersis
 import com.liferay.portal.configuration.persistence.internal.upgrade.ConfigurationUpgradeStepFactoryImpl;
 import com.liferay.portal.configuration.persistence.upgrade.ConfigurationUpgradeStepFactory;
 import com.liferay.portal.kernel.util.HashMapDictionaryBuilder;
-
-import java.util.Collection;
-import java.util.Iterator;
-
-import javax.sql.DataSource;
+import com.liferay.portal.kernel.util.InfrastructureUtil;
 
 import org.apache.felix.cm.PersistenceManager;
 
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
 import org.osgi.framework.Constants;
-import org.osgi.framework.ServiceReference;
 import org.osgi.framework.ServiceRegistration;
 
 /**
@@ -31,23 +26,9 @@ public class ConfigurationPersistenceImplBundleActivator
 	implements BundleActivator {
 
 	@Override
-	public void start(BundleContext bundleContext) throws Exception {
-		Collection<ServiceReference<DataSource>> serviceReferences =
-			bundleContext.getServiceReferences(
-				DataSource.class, "(bean.id=liferayDataSource)");
-
-		if ((serviceReferences == null) || serviceReferences.isEmpty()) {
-			throw new IllegalStateException(
-				"Liferay data source is not available");
-		}
-
-		Iterator<ServiceReference<DataSource>> iterator =
-			serviceReferences.iterator();
-
-		_serviceReference = iterator.next();
-
+	public void start(BundleContext bundleContext) {
 		_configurationPersistenceManager = new ConfigurationPersistenceManager(
-			bundleContext, bundleContext.getService(_serviceReference));
+			bundleContext, InfrastructureUtil.getDataSource());
 
 		_configurationPersistenceManager.start();
 
@@ -74,7 +55,7 @@ public class ConfigurationPersistenceImplBundleActivator
 	}
 
 	@Override
-	public void stop(BundleContext bundleContext) throws Exception {
+	public void stop(BundleContext bundleContext) {
 		if (_configurationUpgradeStepFactoryServiceRegistration != null) {
 			_configurationUpgradeStepFactoryServiceRegistration.unregister();
 		}
@@ -84,10 +65,6 @@ public class ConfigurationPersistenceImplBundleActivator
 		}
 
 		_configurationPersistenceManager.stop();
-
-		if (_serviceReference != null) {
-			bundleContext.ungetService(_serviceReference);
-		}
 	}
 
 	private ConfigurationPersistenceManager _configurationPersistenceManager;
@@ -95,6 +72,5 @@ public class ConfigurationPersistenceImplBundleActivator
 		_configurationPersistenceManagerServiceRegistration;
 	private ServiceRegistration<ConfigurationUpgradeStepFactory>
 		_configurationUpgradeStepFactoryServiceRegistration;
-	private ServiceReference<DataSource> _serviceReference;
 
 }
