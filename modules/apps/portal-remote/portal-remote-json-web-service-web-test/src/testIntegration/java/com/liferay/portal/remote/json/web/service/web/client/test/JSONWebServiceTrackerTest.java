034ac12245a19b068f4ea11ac8768d6b139abee8
shuyangzhou <shuyang.zhou@liferay.com>
2023-12-11 13:04:02 -0800

    LPS-X Rename package

--- modules/apps/portal-remote/portal-remote-json-web-service-web-test/src/testIntegration/java/com/liferay/portal/remote/json/web/service/web/client/test/JSONWebServiceTrackerTest.java
+++ modules/apps/portal-remote/portal-remote-json-web-service-web-test/src/testIntegration/java/com/liferay/portal/remote/json/web/service/web/client/test/JSONWebServiceTrackerTest.java
@@ -3,7 +3,7 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-package com.liferay.portal.remote.json.web.service.extender.client.test;
+package com.liferay.portal.remote.json.web.service.web.client.test;
 
 import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
 import com.liferay.petra.string.StringBundler;
