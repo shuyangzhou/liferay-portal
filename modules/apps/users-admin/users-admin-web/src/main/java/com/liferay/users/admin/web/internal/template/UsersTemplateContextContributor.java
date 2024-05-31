324dae7e90585f22cdf327af0e6693583679720f
Brian Chan <brian.chan@liferay.com>
2024-05-31 08:15:32 -0300

    LPD-27237 Sort

--- modules/apps/users-admin/users-admin-web/src/main/java/com/liferay/users/admin/web/internal/template/UsersTemplateContextContributor.java
+++ modules/apps/users-admin/users-admin-web/src/main/java/com/liferay/users/admin/web/internal/template/UsersTemplateContextContributor.java
@@ -48,7 +48,6 @@ public class UsersTemplateContextContributor
 		User user1 = themeDisplay.getUser();
 
 		contextObjects.put("is_default_user", user1.isDefaultUser());
-
 		contextObjects.put(
 			"is_female",
 			new LazyValue(
@@ -64,7 +63,7 @@ public class UsersTemplateContextContributor
 
 					return null;
 				}));
-
+		contextObjects.put("is_guest_user", user1.isGuestUser());
 		contextObjects.put(
 			"is_male",
 			new LazyValue(
@@ -80,42 +79,9 @@ public class UsersTemplateContextContributor
 
 					return null;
 				}));
-
-		contextObjects.put(
-			"user_birthday",
-			new LazyValue(
-				() -> {
-					try {
-						Contact contact = user1.getContact();
-
-						return contact.getBirthday();
-					}
-					catch (PortalException portalException) {
-						_log.error(portalException);
-					}
-
-					return null;
-				}));
-
-		contextObjects.put("is_guest_user", user1.isGuestUser());
 		contextObjects.put("is_setup_complete", user1.isSetupComplete());
-
 		contextObjects.put("language", themeDisplay.getLanguageId());
-
 		contextObjects.put("language_id", user1.getLanguageId());
-		contextObjects.put("user_comments", user1.getComments());
-		contextObjects.put("user_email_address", user1.getEmailAddress());
-		contextObjects.put("user_first_name", user1.getFirstName());
-		contextObjects.put(
-			"user_greeting", HtmlUtil.escape(user1.getGreeting()));
-		contextObjects.put("user_id", user1.getUserId());
-		contextObjects.put("user_initialized", true);
-		contextObjects.put("user_last_login_ip", user1.getLastLoginIP());
-		contextObjects.put("user_last_name", user1.getLastName());
-		contextObjects.put("user_login_ip", user1.getLoginIP());
-		contextObjects.put("user_middle_name", user1.getMiddleName());
-		contextObjects.put("user_name", user1.getFullName());
-
 		contextObjects.put(
 			"user2",
 			new LazyValue(
@@ -134,7 +100,33 @@ public class UsersTemplateContextContributor
 
 					return null;
 				}));
+		contextObjects.put(
+			"user_birthday",
+			new LazyValue(
+				() -> {
+					try {
+						Contact contact = user1.getContact();
 
+						return contact.getBirthday();
+					}
+					catch (PortalException portalException) {
+						_log.error(portalException);
+					}
+
+					return null;
+				}));
+		contextObjects.put("user_comments", user1.getComments());
+		contextObjects.put("user_email_address", user1.getEmailAddress());
+		contextObjects.put("user_first_name", user1.getFirstName());
+		contextObjects.put(
+			"user_greeting", HtmlUtil.escape(user1.getGreeting()));
+		contextObjects.put("user_id", user1.getUserId());
+		contextObjects.put("user_initialized", true);
+		contextObjects.put("user_last_login_ip", user1.getLastLoginIP());
+		contextObjects.put("user_last_name", user1.getLastName());
+		contextObjects.put("user_login_ip", user1.getLoginIP());
+		contextObjects.put("user_middle_name", user1.getMiddleName());
+		contextObjects.put("user_name", user1.getFullName());
 		contextObjects.put(
 			"w3c_language_id",
 			LocaleUtil.toW3cLanguageId(themeDisplay.getLanguageId()));
