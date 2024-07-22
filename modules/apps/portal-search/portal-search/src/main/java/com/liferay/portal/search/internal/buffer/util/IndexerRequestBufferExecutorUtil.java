729cc27f09b93a61a8192b795eb23640d5500c1c
gustavolimav <gustavo.lima@liferay.com>
2024-07-22 14:53:07 -0300

    LPD-25633 portal-search: If there is just one indexerRequest we dont need to call merge
     https://liferay.atlassian.net/browse/LPD-25633

--- modules/apps/portal-search/portal-search/src/main/java/com/liferay/portal/search/internal/buffer/util/IndexerRequestBufferExecutorUtil.java
+++ modules/apps/portal-search/portal-search/src/main/java/com/liferay/portal/search/internal/buffer/util/IndexerRequestBufferExecutorUtil.java
@@ -65,7 +65,9 @@ public class IndexerRequestBufferExecutorUtil {
 		Collection<IndexerRequest> indexerRequests =
 			indexerRequestBuffer.getIndexerRequests();
 
-		if (!BufferOverflowThreadLocal.isOverflowMode()) {
+		if (!BufferOverflowThreadLocal.isOverflowMode() &&
+			(indexerRequests.size() > 1)) {
+
 			indexerRequests = _mergeIndexerRequests(indexerRequests);
 		}
 
