b90fb912763c83acbe3c88b6d58a55e15ab03ea9
shuyangzhou <shuyang.zhou@liferay.com>
2024-08-14 08:18:27 -0700

    LPD-25633 ObjectEntryWorkflowHandler calls ObjectEntryLocalService.updateStatus() which does an ObjectEntry reindexing already, remove the double reindexing in the servce call.

--- modules/apps/object/object-service/src/main/java/com/liferay/object/service/impl/ObjectEntryLocalServiceImpl.java
+++ modules/apps/object/object-service/src/main/java/com/liferay/object/service/impl/ObjectEntryLocalServiceImpl.java
@@ -332,8 +332,6 @@ public class ObjectEntryLocalServiceImpl
 
 		_startWorkflowInstance(userId, objectEntry, serviceContext);
 
-		_reindex(objectEntry);
-
 		boolean clearObjectEntryIdsMap =
 			ObjectActionThreadLocal.isClearObjectEntryIdsMap();
 
@@ -1581,8 +1579,6 @@ public class ObjectEntryLocalServiceImpl
 		_deleteFileEntries(
 			objectEntry.getValues(), objectEntry.getObjectDefinitionId(),
 			transientValues);
-
-		_reindex(objectEntry);
 
 		_executeObjectActions(
 			objectEntry.getCompanyId(),
