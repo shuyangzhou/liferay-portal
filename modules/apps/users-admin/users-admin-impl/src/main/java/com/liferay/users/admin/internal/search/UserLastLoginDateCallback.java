7aeb8d70bc2f81bdc1584ecbd758ea9b4b503087
shuyangzhou <shuyang.zhou@liferay.com>
2024-09-04 13:28:36 -0700

    LPD-X Batch process user update last log in

--- modules/apps/users-admin/users-admin-impl/src/main/java/com/liferay/users/admin/internal/search/UserLastLoginDateCallback.java
+++ modules/apps/users-admin/users-admin-impl/src/main/java/com/liferay/users/admin/internal/search/UserLastLoginDateCallback.java
@@ -11,10 +11,18 @@ import com.liferay.portal.kernel.search.Document;
 import com.liferay.portal.kernel.search.DocumentImpl;
 import com.liferay.portal.kernel.search.Field;
 import com.liferay.portal.kernel.search.Indexable;
+import com.liferay.portal.kernel.util.BatchProcessor;
 import com.liferay.portal.search.index.UpdateDocumentIndexWriter;
 import com.liferay.portal.search.model.uid.UIDFactory;
+import com.liferay.portal.util.PropsValues;
+
+import java.util.AbstractMap;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Deactivate;
 import org.osgi.service.component.annotations.Reference;
 
 /**
@@ -44,9 +52,45 @@ public class UserLastLoginDateCallback implements Indexable.Callback {
 		document.addKeyword(Field.UID, _uidFactory.getUID(user));
 		document.addDate("lastLoginDate", user.getLastLoginDate());
 
-		_updateDocumentIndexWriter.updateDocumentPartially(
-			user.getCompanyId(), document, false);
+		_batchProcessor.add(
+			new AbstractMap.SimpleImmutableEntry<>(
+				user.getCompanyId(), document));
 	}
+
+	@Deactivate
+	protected void deactivate() {
+		_batchProcessor.close();
+	}
+
+	private void _flush(List<Map.Entry<Long, Document>> entries) {
+		Map<Long, Map<String, Document>> documentsMap = new HashMap<>();
+
+		for (Map.Entry<Long, Document> entry : entries) {
+			Map<String, Document> documents = documentsMap.computeIfAbsent(
+				entry.getKey(), key -> new HashMap<>());
+
+			Document document = entry.getValue();
+
+			Field field = document.getField(Field.ENTRY_CLASS_PK);
+
+			documents.put(field.getValue(), document);
+		}
+
+		for (Map.Entry<Long, Map<String, Document>> entry :
+				documentsMap.entrySet()) {
+
+			Map<String, Document> documents = entry.getValue();
+
+			_updateDocumentIndexWriter.updateDocumentsPartially(
+				entry.getKey(), documents.values(), false);
+		}
+	}
+
+	private final BatchProcessor<Map.Entry<Long, Document>> _batchProcessor =
+		new BatchProcessor<>(
+			UserLastLoginDateCallback.class.getName(), this::_flush,
+			PropsValues.USERS_UPDATE_LAST_LOGIN_BATCH_INTERVAL,
+			PropsValues.USERS_UPDATE_LAST_LOGIN_BATCH_SIZE);
 
 	@Reference
 	private UIDFactory _uidFactory;
