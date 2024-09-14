c0d707947d5397ad504fe8603afa2ed06cd6eb07
shuyangzhou <shuyang.zhou@liferay.com>
2024-09-14 11:09:14 -0700

    LPD-35723 Add BulkDeleteCacheThreadLocal and apply to ObjectDefinition delete. Later if this turns out to be a common requirement, we can turn it into AOP support.

---
+++ portal-kernel/src/com/liferay/portal/kernel/util/BulkDeleteCacheThreadLocal.java
@@ -0,0 +1,43 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2024 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.portal.kernel.util;
+
+import com.liferay.petra.lang.CentralizedThreadLocal;
+import com.liferay.petra.lang.SafeCloseable;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Supplier;
+
+/**
+ * @author Shuyang Zhou
+ */
+public class BulkDeleteCacheThreadLocal {
+
+	public static <T> T getBulkDeleteCache(
+		String ownerName, Supplier<T> supplier) {
+
+		Map<String, Object> bulkDeleteCacheMap =
+			_bulkDeleteCacheThreadLocal.get();
+
+		if (bulkDeleteCacheMap == null) {
+			return null;
+		}
+
+		return (T)bulkDeleteCacheMap.computeIfAbsent(
+			ownerName, key -> supplier.get());
+	}
+
+	public static SafeCloseable openBulkDeleteMode() {
+		return _bulkDeleteCacheThreadLocal.setWithSafeCloseable(
+			new HashMap<>());
+	}
+
+	private static final CentralizedThreadLocal<Map<String, Object>>
+		_bulkDeleteCacheThreadLocal = new CentralizedThreadLocal<>(
+			BulkDeleteCacheThreadLocal.class + "._bulkDeleteCacheThreadLocal");
+
+}
