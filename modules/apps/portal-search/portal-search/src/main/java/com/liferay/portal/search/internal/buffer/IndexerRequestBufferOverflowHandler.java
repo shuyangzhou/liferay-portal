3bcff3b62a978ebc3f1494259448f5c096813234
shuyangzhou <shuyang.zhou@liferay.com>
2024-09-16 16:11:13 -0700

    LPD-35723 When you have to flush, flush the whole buffer not just a bit of it.
    # breaking
    
    ## What modules/apps/portal-search/portal-search-api/src/main/java/com/liferay/portal/search/configuration/IndexerRegistryConfiguration.java
    
    Configuration value minimumBufferAvailabilityPercentage() has been removed.
    
    ## Why
    
    This is no longer used, the whole buffer will be flushed upon overflowing.
    
    ----

--- modules/apps/portal-search/portal-search/src/main/java/com/liferay/portal/search/internal/buffer/IndexerRequestBufferOverflowHandler.java
+++ modules/apps/portal-search/portal-search/src/main/java/com/liferay/portal/search/internal/buffer/IndexerRequestBufferOverflowHandler.java
@@ -5,18 +5,11 @@
 
 package com.liferay.portal.search.internal.buffer;
 
-import com.liferay.petra.string.StringBundler;
-import com.liferay.portal.configuration.metatype.bnd.util.ConfigurableUtil;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
-import com.liferay.portal.search.configuration.IndexerRegistryConfiguration;
 import com.liferay.portal.search.internal.buffer.util.IndexerRequestBufferExecutorUtil;
 
-import java.util.Map;
-
-import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
-import org.osgi.service.component.annotations.Modified;
 
 /**
  * @author Michael C. Han
@@ -41,56 +34,18 @@ public class IndexerRequestBufferOverflowHandler {
 			return;
 		}
 
-		int numRequests = Math.round(
-			currentBufferSize -
-				Math.abs(maxBufferSize * _minimumBufferAvailabilityPercentage));
+		try {
+			BufferOverflowThreadLocal.setOverflowMode(true);
 
-		if (numRequests > 0) {
-			try {
-				BufferOverflowThreadLocal.setOverflowMode(true);
-
-				IndexerRequestBufferExecutorUtil.execute(
-					indexerRequestBuffer, numRequests);
-			}
-			finally {
-				BufferOverflowThreadLocal.setOverflowMode(false);
-			}
+			IndexerRequestBufferExecutorUtil.execute(
+				indexerRequestBuffer, currentBufferSize);
+		}
+		finally {
+			BufferOverflowThreadLocal.setOverflowMode(false);
 		}
 	}
-
-	@Activate
-	@Modified
-	protected void activate(Map<String, Object> properties) {
-		IndexerRegistryConfiguration indexerRegistryConfiguration =
-			ConfigurableUtil.createConfigurable(
-				IndexerRegistryConfiguration.class, properties);
-
-		float minimumBufferAvailabilityPercentage =
-			indexerRegistryConfiguration.minimumBufferAvailabilityPercentage();
-
-		if ((minimumBufferAvailabilityPercentage > 1) ||
-			(minimumBufferAvailabilityPercentage < 0.1)) {
-
-			if (_log.isWarnEnabled()) {
-				_log.warn(
-					StringBundler.concat(
-						"Invalid minimum buffer availability percentage: ",
-						minimumBufferAvailabilityPercentage,
-						", using default value",
-						_DEFAULT_MINIMUM_BUFFER_AVAILABILITY_PERCENTAGE));
-			}
-		}
-
-		_minimumBufferAvailabilityPercentage =
-			minimumBufferAvailabilityPercentage;
-	}
-
-	private static final float _DEFAULT_MINIMUM_BUFFER_AVAILABILITY_PERCENTAGE =
-		0.90F;
 
 	private static final Log _log = LogFactoryUtil.getLog(
 		IndexerRequestBufferOverflowHandler.class);
-
-	private volatile float _minimumBufferAvailabilityPercentage;
 
 }
