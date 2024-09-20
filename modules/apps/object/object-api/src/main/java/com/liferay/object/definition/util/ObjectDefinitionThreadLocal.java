38f7de3c6d193c59ba20545ae29b980eb5af31f0
shuyangzhou <shuyang.zhou@liferay.com>
2024-09-20 07:13:55 -0700

    LPD-35723 Bulk delete ObjectEntry related tables

---
+++ modules/apps/object/object-api/src/main/java/com/liferay/object/definition/util/ObjectDefinitionThreadLocal.java
@@ -0,0 +1,39 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2024 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.object.definition.util;
+
+import com.liferay.object.entry.util.ObjectEntryThreadLocal;
+import com.liferay.petra.lang.CentralizedThreadLocal;
+import com.liferay.petra.lang.SafeCloseable;
+
+/**
+ * @author Shuyang Zhou
+ */
+public class ObjectDefinitionThreadLocal {
+
+	public static boolean isDeleteObjectDefinitionId(long objectDefinitionId) {
+		Long deleteObjectDefinitionId =
+			_deleteObjectDefinitionIdThreadLocal.get();
+
+		if ((deleteObjectDefinitionId != null) &&
+			(deleteObjectDefinitionId == objectDefinitionId)) {
+
+			return true;
+		}
+
+		return false;
+	}
+
+	public static SafeCloseable setDeleteObjectDefinitionId(long id) {
+		return _deleteObjectDefinitionIdThreadLocal.setWithSafeCloseable(id);
+	}
+
+	private static final CentralizedThreadLocal<Long>
+		_deleteObjectDefinitionIdThreadLocal = new CentralizedThreadLocal<>(
+			ObjectEntryThreadLocal.class +
+				"._deleteObjectDefinitionIdThreadLocal");
+
+}
