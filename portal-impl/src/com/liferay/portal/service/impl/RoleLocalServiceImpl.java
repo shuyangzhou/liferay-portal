fb7534d4076a078d6ca8e5e961493de8ee8f0ebe
shuyangzhou <shuyang.zhou@liferay.com>
2024-10-09 15:57:38 -0700

    Debug print

--- portal-impl/src/com/liferay/portal/service/impl/RoleLocalServiceImpl.java
+++ portal-impl/src/com/liferay/portal/service/impl/RoleLocalServiceImpl.java
@@ -64,6 +64,7 @@ import com.liferay.portal.kernel.portlet.PortletProviderUtil;
 import com.liferay.portal.kernel.search.Indexer;
 import com.liferay.portal.kernel.search.IndexerRegistryUtil;
 import com.liferay.portal.kernel.search.SearchException;
+import com.liferay.portal.kernel.search.dummy.DummyIndexer;
 import com.liferay.portal.kernel.security.permission.ActionKeys;
 import com.liferay.portal.kernel.security.permission.PermissionThreadLocal;
 import com.liferay.portal.kernel.service.ClassNameLocalService;
@@ -2091,6 +2092,10 @@ public class RoleLocalServiceImpl extends RoleLocalServiceBaseImpl {
 	protected void reindex(long userId) throws SearchException {
 		Indexer<User> indexer = IndexerRegistryUtil.nullSafeGetIndexer(
 			User.class);
+
+		if (indexer.getClass() != DummyIndexer.class) {
+			new Exception("***Using " + indexer + " to index " + userId + " by thread " + Thread.currentThread()).printStackTrace(System.out);
+		}
 
 		indexer.reindex(_userLocalService.fetchUser(userId));
 	}
