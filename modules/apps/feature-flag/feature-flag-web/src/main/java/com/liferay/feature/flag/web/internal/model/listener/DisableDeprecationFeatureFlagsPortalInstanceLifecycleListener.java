62146a9c89b9701f591667959e938ce3423652bf
shuyangzhou <shuyang.zhou@liferay.com>
2024-11-01 13:13:33 -0700

    LPD-40974 Ensure rerun safety

--- modules/apps/feature-flag/feature-flag-web/src/main/java/com/liferay/feature/flag/web/internal/model/listener/DisableDeprecationFeatureFlagsPortalInstanceLifecycleListener.java
+++ modules/apps/feature-flag/feature-flag-web/src/main/java/com/liferay/feature/flag/web/internal/model/listener/DisableDeprecationFeatureFlagsPortalInstanceLifecycleListener.java
@@ -7,11 +7,18 @@ package com.liferay.feature.flag.web.internal.model.listener;
 
 import com.liferay.feature.flag.web.internal.feature.flag.FeatureFlagsBag;
 import com.liferay.feature.flag.web.internal.feature.flag.FeatureFlagsBagProvider;
+import com.liferay.petra.string.StringPool;
 import com.liferay.portal.instance.lifecycle.BasePortalInstanceLifecycleListener;
 import com.liferay.portal.instance.lifecycle.PortalInstanceLifecycleListener;
 import com.liferay.portal.kernel.feature.flag.FeatureFlag;
 import com.liferay.portal.kernel.feature.flag.FeatureFlagType;
+import com.liferay.portal.kernel.feature.flag.constants.FeatureFlagConstants;
 import com.liferay.portal.kernel.model.Company;
+import com.liferay.portal.kernel.portlet.PortalPreferences;
+import com.liferay.portal.kernel.service.PortalPreferencesLocalService;
+import com.liferay.portal.kernel.util.GetterUtil;
+import com.liferay.portal.kernel.util.PortletKeys;
+import com.liferay.portlet.PortalPreferencesWrapper;
 
 import java.util.List;
 
@@ -27,9 +34,31 @@ public class DisableDeprecationFeatureFlagsPortalInstanceLifecycleListener
 
 	@Override
 	public void portalInstanceRegistered(Company company) throws Exception {
+		long companyId = company.getCompanyId();
+
+		PortalPreferencesWrapper portalPreferencesWrapper =
+			(PortalPreferencesWrapper)
+				_portalPreferencesLocalService.getPreferences(
+					companyId, PortletKeys.PREFS_OWNER_TYPE_COMPANY);
+
+		PortalPreferences portalPreferences =
+			portalPreferencesWrapper.getPortalPreferencesImpl();
+
+		if (GetterUtil.getBoolean(
+				portalPreferences.getValue(
+					FeatureFlagConstants.FEATURE_FLAG, _KEY, null))) {
+
+			return;
+		}
+
+		portalPreferences.setValue(
+			FeatureFlagConstants.FEATURE_FLAG, _KEY, StringPool.TRUE);
+
+		_portalPreferencesLocalService.updatePreferences(
+			companyId, PortletKeys.PREFS_OWNER_TYPE_COMPANY, portalPreferences);
+
 		FeatureFlagsBag featureFlagsBag =
-			_featureFlagsBagProvider.getOrCreateFeatureFlagsBag(
-				company.getCompanyId());
+			_featureFlagsBagProvider.getOrCreateFeatureFlagsBag(companyId);
 
 		List<FeatureFlag> deprecationFeatureFlags =
 			featureFlagsBag.getFeatureFlags(
@@ -37,11 +66,16 @@ public class DisableDeprecationFeatureFlagsPortalInstanceLifecycleListener
 
 		for (FeatureFlag deprecationFeatureFlag : deprecationFeatureFlags) {
 			_featureFlagsBagProvider.setEnabled(
-				company.getCompanyId(), deprecationFeatureFlag.getKey(), false);
+				companyId, deprecationFeatureFlag.getKey(), false);
 		}
 	}
 
+	private static final String _KEY = "DeprecationFeatureFlagsProcessed";
+
 	@Reference
 	private FeatureFlagsBagProvider _featureFlagsBagProvider;
+
+	@Reference
+	private PortalPreferencesLocalService _portalPreferencesLocalService;
 
 }
