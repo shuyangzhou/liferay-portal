4032009b19a72e42d4f87237b7f0d6c47f495957
Peter Shin <peter.shin@liferay.com>
2024-11-27 16:02:53 -0600

    Revert "LPD-37838 SF"
    This reverts commit d52e08da955e2dc437068dde6edbcedcb456f0c1.

--- modules/sdk/gradle-plugins-defaults/src/main/java/com/liferay/gradle/plugins/defaults/LiferayOSGiDefaultsPlugin.java
+++ modules/sdk/gradle-plugins-defaults/src/main/java/com/liferay/gradle/plugins/defaults/LiferayOSGiDefaultsPlugin.java
@@ -1599,7 +1599,7 @@ public class LiferayOSGiDefaultsPlugin implements Plugin<Project> {
 
 		Properties properties = GUtil.loadProperties(file);
 
-		String dependencies = properties.getProperty("dependencies");
+		final String dependencies = properties.getProperty("dependencies");
 
 		if (Validator.isNull(dependencies)) {
 			return;
