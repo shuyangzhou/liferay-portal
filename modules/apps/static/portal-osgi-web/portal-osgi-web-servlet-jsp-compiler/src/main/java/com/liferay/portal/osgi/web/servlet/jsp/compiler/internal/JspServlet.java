9288579b56a0da7d7b0ae57bb855d6d7b72661cd
shuyangzhou <shuyang.zhou@liferay.com>
2024-12-31 15:19:04 -0800

    LPD-X Forward compatibility, avoid directly implementing ServletContext

--- modules/apps/static/portal-osgi-web/portal-osgi-web-servlet-jsp-compiler/src/main/java/com/liferay/portal/osgi/web/servlet/jsp/compiler/internal/JspServlet.java
+++ modules/apps/static/portal-osgi-web/portal-osgi-web-servlet-jsp-compiler/src/main/java/com/liferay/portal/osgi/web/servlet/jsp/compiler/internal/JspServlet.java
@@ -19,6 +19,7 @@ import com.liferay.portal.kernel.util.GetterUtil;
 import com.liferay.portal.kernel.util.HashMapBuilder;
 import com.liferay.portal.kernel.util.ListUtil;
 import com.liferay.portal.kernel.util.PropsUtil;
+import com.liferay.portal.kernel.util.ProxyUtil;
 import com.liferay.portal.kernel.util.StringUtil;
 import com.liferay.portal.util.PropsValues;
 import com.liferay.taglib.servlet.JspFactorySwapper;
@@ -39,7 +40,6 @@ import java.net.URL;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Enumeration;
-import java.util.EventListener;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
@@ -52,19 +52,12 @@ import java.util.regex.Pattern;
 
 import javax.naming.NamingException;
 
-import javax.servlet.Filter;
-import javax.servlet.FilterRegistration;
 import javax.servlet.RequestDispatcher;
-import javax.servlet.Servlet;
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
-import javax.servlet.ServletRegistration;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
-import javax.servlet.SessionCookieConfig;
-import javax.servlet.SessionTrackingMode;
-import javax.servlet.descriptor.JspConfigDescriptor;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -269,7 +262,11 @@ public class JspServlet extends HttpServlet {
 				}
 
 				private final ServletContext _jspServletContext =
-					new ServletContextWrapper(servletContext);
+					ProxyUtil.newDelegateProxyInstance(
+						ServletContext.class.getClassLoader(),
+						ServletContext.class,
+						new ServletContextDelegate(servletContext),
+						servletContext);
 
 			});
 
@@ -546,212 +543,16 @@ public class JspServlet extends HttpServlet {
 
 	}
 
-	private class ServletContextWrapper implements ServletContext {
+	private class ServletContextDelegate {
 
-		@Override
-		public FilterRegistration.Dynamic addFilter(
-			String filterName, Class<? extends Filter> filterClass) {
-
-			return _servletContext.addFilter(filterName, filterClass);
-		}
-
-		@Override
-		public FilterRegistration.Dynamic addFilter(
-			String filterName, Filter filter) {
-
-			return _servletContext.addFilter(filterName, filter);
-		}
-
-		@Override
-		public FilterRegistration.Dynamic addFilter(
-			String filterName, String className) {
-
-			return _servletContext.addFilter(filterName, className);
-		}
-
-		@Override
-		public void addListener(Class<? extends EventListener> listenerClass) {
-			_servletContext.addListener(listenerClass);
-		}
-
-		@Override
-		public void addListener(String className) {
-			_servletContext.addListener(className);
-		}
-
-		@Override
-		public <T extends EventListener> void addListener(T listener) {
-			_servletContext.addListener(listener);
-		}
-
-		@Override
-		public ServletRegistration.Dynamic addServlet(
-			String servletName, Class<? extends Servlet> servletClass) {
-
-			return _servletContext.addServlet(servletName, servletClass);
-		}
-
-		@Override
-		public ServletRegistration.Dynamic addServlet(
-			String servletName, Servlet servlet) {
-
-			return _servletContext.addServlet(servletName, servlet);
-		}
-
-		@Override
-		public ServletRegistration.Dynamic addServlet(
-			String servletName, String className) {
-
-			return _servletContext.addServlet(servletName, className);
-		}
-
-		@Override
-		public <T extends Filter> T createFilter(Class<T> clazz)
-			throws ServletException {
-
-			return _servletContext.createFilter(clazz);
-		}
-
-		@Override
-		public <T extends EventListener> T createListener(Class<T> clazz)
-			throws ServletException {
-
-			return _servletContext.createListener(clazz);
-		}
-
-		@Override
-		public <T extends Servlet> T createServlet(Class<T> clazz)
-			throws ServletException {
-
-			return _servletContext.createServlet(clazz);
-		}
-
-		@Override
-		public void declareRoles(String... roleNames) {
-			_servletContext.declareRoles(roleNames);
-		}
-
-		@Override
-		public boolean equals(Object object) {
-			if (!(object instanceof ServletContext)) {
-				return false;
-			}
-
-			ServletContext servletContext = (ServletContext)object;
-
-			if (object instanceof ServletContextWrapper) {
-				ServletContextWrapper servletContextWrapper =
-					(ServletContextWrapper)object;
-
-				servletContext = servletContextWrapper._servletContext;
-			}
-
-			return servletContext.equals(_servletContext);
-		}
-
-		@Override
-		public Object getAttribute(String name) {
-			return _servletContext.getAttribute(name);
-		}
-
-		@Override
-		public Enumeration<String> getAttributeNames() {
-			return _servletContext.getAttributeNames();
-		}
-
-		@Override
 		public ClassLoader getClassLoader() {
 			return _jspBundleClassloader;
 		}
 
-		@Override
-		public ServletContext getContext(String uripath) {
-			return _servletContext.getContext(uripath);
-		}
-
-		@Override
 		public String getContextPath() {
 			return _contextPath;
 		}
 
-		@Override
-		public Set<SessionTrackingMode> getDefaultSessionTrackingModes() {
-			return _servletContext.getDefaultSessionTrackingModes();
-		}
-
-		@Override
-		public int getEffectiveMajorVersion() {
-			return _servletContext.getEffectiveMajorVersion();
-		}
-
-		@Override
-		public int getEffectiveMinorVersion() {
-			return _servletContext.getEffectiveMinorVersion();
-		}
-
-		@Override
-		public Set<SessionTrackingMode> getEffectiveSessionTrackingModes() {
-			return _servletContext.getEffectiveSessionTrackingModes();
-		}
-
-		@Override
-		public FilterRegistration getFilterRegistration(String filterName) {
-			return _servletContext.getFilterRegistration(filterName);
-		}
-
-		@Override
-		public Map<String, ? extends FilterRegistration>
-			getFilterRegistrations() {
-
-			return _servletContext.getFilterRegistrations();
-		}
-
-		@Override
-		public String getInitParameter(String name) {
-			return _servletContext.getInitParameter(name);
-		}
-
-		@Override
-		public Enumeration<String> getInitParameterNames() {
-			return _servletContext.getInitParameterNames();
-		}
-
-		@Override
-		public JspConfigDescriptor getJspConfigDescriptor() {
-			return _servletContext.getJspConfigDescriptor();
-		}
-
-		@Override
-		public int getMajorVersion() {
-			return _servletContext.getMajorVersion();
-		}
-
-		@Override
-		public String getMimeType(String file) {
-			return _servletContext.getMimeType(file);
-		}
-
-		@Override
-		public int getMinorVersion() {
-			return _servletContext.getMinorVersion();
-		}
-
-		@Override
-		public RequestDispatcher getNamedDispatcher(String name) {
-			return _servletContext.getNamedDispatcher(name);
-		}
-
-		@Override
-		public String getRealPath(String path) {
-			return _servletContext.getRealPath(path);
-		}
-
-		@Override
-		public RequestDispatcher getRequestDispatcher(String path) {
-			return _servletContext.getRequestDispatcher(path);
-		}
-
-		@Override
 		public URL getResource(String path) {
 			try {
 				if ((path == null) || path.equals(StringPool.BLANK)) {
@@ -810,7 +611,6 @@ public class JspServlet extends HttpServlet {
 			return null;
 		}
 
-		@Override
 		public InputStream getResourceAsStream(String path) {
 			URL url = getResource(path);
 
@@ -830,7 +630,6 @@ public class JspServlet extends HttpServlet {
 			}
 		}
 
-		@Override
 		public Set<String> getResourcePaths(String path) {
 			Set<String> paths = _servletContext.getResourcePaths(path);
 
@@ -852,112 +651,11 @@ public class JspServlet extends HttpServlet {
 			return paths;
 		}
 
-		@Override
-		public String getServerInfo() {
-			return _servletContext.getServerInfo();
-		}
-
-		/**
-		 * @deprecated As of Judson (7.1.x)
-		 */
-		@Deprecated
-		@Override
-		public Servlet getServlet(String name) throws ServletException {
-			return _servletContext.getServlet(name);
-		}
-
-		@Override
 		public String getServletContextName() {
 			return _servletContextName;
 		}
 
-		/**
-		 * @deprecated As of Judson (7.1.x)
-		 */
-		@Deprecated
-		@Override
-		public Enumeration<String> getServletNames() {
-			return _servletContext.getServletNames();
-		}
-
-		@Override
-		public ServletRegistration getServletRegistration(String servletName) {
-			return _servletContext.getServletRegistration(servletName);
-		}
-
-		@Override
-		public Map<String, ? extends ServletRegistration>
-			getServletRegistrations() {
-
-			return _servletContext.getServletRegistrations();
-		}
-
-		/**
-		 * @deprecated As of Judson (7.1.x)
-		 */
-		@Deprecated
-		@Override
-		public Enumeration<Servlet> getServlets() {
-			return _servletContext.getServlets();
-		}
-
-		@Override
-		public SessionCookieConfig getSessionCookieConfig() {
-			return _servletContext.getSessionCookieConfig();
-		}
-
-		@Override
-		public int hashCode() {
-			return _servletContext.hashCode();
-		}
-
-		/**
-		 * @deprecated As of Judson (7.1.x)
-		 */
-		@Deprecated
-		@Override
-		public void log(Exception exception, String message) {
-			_servletContext.log(exception, message);
-		}
-
-		@Override
-		public void log(String message) {
-			_servletContext.log(message);
-		}
-
-		@Override
-		public void log(String message, Throwable throwable) {
-			_servletContext.log(message, throwable);
-		}
-
-		@Override
-		public void removeAttribute(String name) {
-			_servletContext.removeAttribute(name);
-		}
-
-		@Override
-		public void setAttribute(String name, Object value) {
-			_servletContext.setAttribute(name, value);
-		}
-
-		@Override
-		public boolean setInitParameter(String name, String value) {
-			return _servletContext.setInitParameter(name, value);
-		}
-
-		@Override
-		public void setSessionTrackingModes(
-			Set<SessionTrackingMode> sessionTrackingModes) {
-
-			_servletContext.setSessionTrackingModes(sessionTrackingModes);
-		}
-
-		@Override
-		public String toString() {
-			return _servletContext.toString();
-		}
-
-		private ServletContextWrapper(ServletContext servletContext) {
+		private ServletContextDelegate(ServletContext servletContext) {
 			_servletContext = servletContext;
 
 			_contextPath = servletContext.getContextPath();
