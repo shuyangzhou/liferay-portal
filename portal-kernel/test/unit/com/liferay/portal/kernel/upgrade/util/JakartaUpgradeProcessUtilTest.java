d5ddfeb585c7d3beb1eb66c9f45986d3a051feb7
Luis Ortiz <luis.ortiz@liferay.com>
2025-04-24 12:58:34 +0200

    LPD-52636 Adding more unit tests to cover new cases

--- portal-kernel/test/unit/com/liferay/portal/kernel/upgrade/util/JakartaUpgradeProcessUtilTest.java
+++ portal-kernel/test/unit/com/liferay/portal/kernel/upgrade/util/JakartaUpgradeProcessUtilTest.java
@@ -7,15 +7,110 @@ package com.liferay.portal.kernel.upgrade.util;
 
 import com.liferay.portal.kernel.util.HashMapBuilder;
 
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 import java.util.Map;
 
+import org.junit.After;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
+
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
 
 /**
  * @author Luis Ortiz
  */
 public class JakartaUpgradeProcessUtilTest {
+
+	@Before
+	public void setUp() {
+		_mockitoAnnotationsAutoCloseable = MockitoAnnotations.openMocks(this);
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		_mockitoAnnotationsAutoCloseable.close();
+	}
+
+	@Test
+	public void testGetSelectResultSetData() throws SQLException {
+		String columnName = "testColumn";
+		String columnValue = "javax.servlet.http.HttpServlet";
+		Object primaryKey1Value = 1;
+		Object primaryKey2Value = "test";
+		String[] primaryKeyColumnNames = {"primaryKey1", "primaryKey2"};
+
+		Mockito.when(
+			_resultSet.getObject("primaryKey1")
+		).thenReturn(
+			primaryKey1Value
+		);
+
+		Mockito.when(
+			_resultSet.getObject("primaryKey2")
+		).thenReturn(
+			primaryKey2Value
+		);
+
+		Mockito.when(
+			_resultSet.getString("testColumn")
+		).thenReturn(
+			columnValue
+		);
+
+		Object[] result = JakartaUpgradeProcessUtil.getSelectResultSetData(
+			columnName, primaryKeyColumnNames, _resultSet);
+
+		Assert.assertArrayEquals(
+			new Object[] {primaryKey1Value, primaryKey2Value, columnValue},
+			result);
+	}
+
+	@Test
+	public void testGetSelectSQL() {
+		String columnName = "testColumn";
+		String[] primaryKeyColumnNames = {"primaryKey1"};
+		String tableName = "testTable";
+
+		Assert.assertEquals(
+			"select primaryKey1, testColumn from testTable where testColumn " +
+				"is not null",
+			JakartaUpgradeProcessUtil.getSelectSQL(
+				columnName, primaryKeyColumnNames, tableName));
+
+		primaryKeyColumnNames = new String[] {"primaryKey1", "primaryKey2"};
+
+		Assert.assertEquals(
+			"select primaryKey1, primaryKey2, testColumn from testTable " +
+				"where testColumn is not null",
+			JakartaUpgradeProcessUtil.getSelectSQL(
+				columnName, primaryKeyColumnNames, tableName));
+	}
+
+	@Test
+	public void testGetUpdateSQL() {
+		String columnName = "testColumn";
+		String[] primaryKeyColumnNames = {"primaryKey1"};
+		String tableName = "testTable";
+
+		Assert.assertEquals(
+			"update testTable set testColumn = ? where primaryKey1 = ?",
+			JakartaUpgradeProcessUtil.getUpdateSQL(
+				columnName, primaryKeyColumnNames, tableName));
+
+		primaryKeyColumnNames = new String[] {"primaryKey1", "primaryKey2"};
+
+		Assert.assertEquals(
+			"update testTable set testColumn = ? where primaryKey1 = ? and " +
+				"primaryKey2 = ?",
+			JakartaUpgradeProcessUtil.getUpdateSQL(
+				columnName, primaryKeyColumnNames, tableName));
+	}
 
 	@Test
 	public void testReplace() {
@@ -119,6 +214,42 @@ public class JakartaUpgradeProcessUtilTest {
 			).build());
 	}
 
+	@Test
+	public void testUpdateJakartaValue() throws SQLException {
+		char[] customSeparators = {};
+		String[] primaryKeyColumnNames = {"id", "uuid"};
+		Object[] values = {1L, "uuid", "import javax.portlet.Portlet"};
+
+		Assert.assertEquals(
+			"(1, uuid)",
+			JakartaUpgradeProcessUtil.updateJakartaValue(
+				customSeparators, _preparedStatement, primaryKeyColumnNames,
+				values));
+
+		values = new Object[] {1L, "uuid", "import jakarta.portlet.Portlet"};
+
+		Assert.assertNull(
+			JakartaUpgradeProcessUtil.updateJakartaValue(
+				customSeparators, _preparedStatement, primaryKeyColumnNames,
+				values));
+
+		values = new Object[] {1L, "uuid", null};
+
+		Assert.assertNull(
+			JakartaUpgradeProcessUtil.updateJakartaValue(
+				customSeparators, _preparedStatement, primaryKeyColumnNames,
+				values));
+
+		customSeparators = new char[] {'$'};
+		values = new Object[] {1L, "uuid", "import javax$portlet$Portlet"};
+
+		Assert.assertEquals(
+			"(1, uuid)",
+			JakartaUpgradeProcessUtil.updateJakartaValue(
+				customSeparators, _preparedStatement, primaryKeyColumnNames,
+				values));
+	}
+
 	private void _testReplace(Map<String, String> replacements) {
 		for (Map.Entry<String, String> entry : replacements.entrySet()) {
 			Assert.assertEquals(
@@ -137,5 +268,13 @@ public class JakartaUpgradeProcessUtilTest {
 					entry.getKey(), customSeparators));
 		}
 	}
+
+	private AutoCloseable _mockitoAnnotationsAutoCloseable;
+
+	@Mock
+	private PreparedStatement _preparedStatement;
+
+	@Mock
+	private ResultSet _resultSet;
 
 }
