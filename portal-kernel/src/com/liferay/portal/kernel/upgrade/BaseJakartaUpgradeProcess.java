936e286d43be48779b33bb3d20d57dd692037e66
Luis Ortiz <luis.ortiz@liferay.com>
2025-04-24 11:02:02 +0200

    LPD-52636 Moving some logic to the Util to reuse it later in the Verify process

--- portal-kernel/src/com/liferay/portal/kernel/upgrade/BaseJakartaUpgradeProcess.java
+++ portal-kernel/src/com/liferay/portal/kernel/upgrade/BaseJakartaUpgradeProcess.java
@@ -7,14 +7,14 @@ package com.liferay.portal.kernel.upgrade;
 
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.security.auth.CompanyThreadLocal;
 import com.liferay.portal.kernel.upgrade.util.JakartaUpgradeProcessUtil;
-import com.liferay.portal.kernel.util.SetUtil;
 import com.liferay.portal.kernel.util.StringBundler;
 
-import java.util.HashMap;
-import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.CopyOnWriteArraySet;
 
 /**
  * @author Luis Ortiz
@@ -23,7 +23,8 @@ public abstract class BaseJakartaUpgradeProcess extends UpgradeProcess {
 
 	@Override
 	protected void doUpgrade() throws Exception {
-		Map<String, Map<String, Set<String>>> modifiedData = new HashMap<>();
+		Map<String, Map<String, Set<String>>> modifiedData =
+			new ConcurrentHashMap<>();
 
 		for (String[] tableAndColumnNames : getTableAndColumnNames()) {
 			String columnName = tableAndColumnNames[1];
@@ -33,125 +34,56 @@ public abstract class BaseJakartaUpgradeProcess extends UpgradeProcess {
 			String[] primaryKeyColumnNames = getPrimaryKeyColumnNames(
 				connection, tableName);
 
-			StringBundler selectSB = new StringBundler();
-
-			selectSB.append("select ");
-
-			StringBundler updateSB = new StringBundler();
-
-			updateSB.append("update ");
-			updateSB.append(tableName);
-			updateSB.append(" set ");
-			updateSB.append(columnName);
-			updateSB.append(" = ? where ");
-
-			for (String primaryKeyColumnName : primaryKeyColumnNames) {
-				selectSB.append(primaryKeyColumnName);
-				selectSB.append(", ");
-
-				updateSB.append(primaryKeyColumnName);
-				updateSB.append(" = ?");
-				updateSB.append(" and ");
-			}
-
-			updateSB.setIndex(updateSB.index() - 1);
-
-			selectSB.append(columnName);
-			selectSB.append(" from ");
-			selectSB.append(tableName);
-
 			processConcurrently(
-				selectSB.toString(), updateSB.toString(),
-				resultSet -> {
-					Object[] result =
-						new Object[primaryKeyColumnNames.length + 1];
-
-					int i = 0;
-
-					for (String primaryKeyColumnName : primaryKeyColumnNames) {
-						result[i] = resultSet.getObject(primaryKeyColumnName);
-						i++;
-					}
-
-					result[i] = resultSet.getString(columnName);
-
-					return result;
-				},
+				JakartaUpgradeProcessUtil.getSelectSQL(
+					columnName, primaryKeyColumnNames, tableName),
+				JakartaUpgradeProcessUtil.getUpdateSQL(
+					columnName, primaryKeyColumnNames, tableName),
+				resultSet -> JakartaUpgradeProcessUtil.getSelectResultSetData(
+					columnName, primaryKeyColumnNames, resultSet),
 				(values, preparedStatement) -> {
-					String javaxValue = (String)values[values.length - 1];
-					String jakartaValue;
+					String modifiedKey =
+						JakartaUpgradeProcessUtil.updateJakartaValue(
+							getCustomSeparators(), preparedStatement,
+							primaryKeyColumnNames, values);
 
-					if (getCustomSeparators().length > 0) {
-						jakartaValue = JakartaUpgradeProcessUtil.replace(
-							javaxValue,
-							SetUtil.fromArray(getCustomSeparators()));
-					}
-					else {
-						jakartaValue = JakartaUpgradeProcessUtil.replace(
-							javaxValue);
+					if (modifiedKey == null) {
+						return;
 					}
 
-					if (javaxValue.length() != jakartaValue.length()) {
-						int i = 1;
+					Map<String, Set<String>> modifiedColumns =
+						modifiedData.computeIfAbsent(
+							tableName, key -> new ConcurrentHashMap<>());
 
-						for (String primaryKeyColumnName :
-								primaryKeyColumnNames) {
+					Set<String> modifiedKeys = modifiedColumns.computeIfAbsent(
+						columnName, key -> new CopyOnWriteArraySet<>());
 
-							preparedStatement.setObject(i, values[i - 1]);
-							i++;
-						}
-
-						preparedStatement.setString(i, jakartaValue);
-
-						preparedStatement.addBatch();
-
-						Map<String, Set<String>> modifiedColumns =
-							modifiedData.computeIfAbsent(
-								tableName, key -> new HashMap<>());
-
-						Set<String> modifiedKeys =
-							modifiedColumns.computeIfAbsent(
-								columnName, key -> new HashSet<>());
-
-						StringBundler keySB = new StringBundler();
-
-						int j = 1;
-
-						for (String primaryKeyColumnName :
-								primaryKeyColumnNames) {
-
-							keySB.append(values[j]);
-							keySB.append(", ");
-							j++;
-						}
-
-						keySB.setIndex(keySB.index() - 1);
-
-						modifiedKeys.add(keySB.toString());
-					}
+					modifiedKeys.add(modifiedKey);
 				},
 				StringBundler.concat(
 					"Unable to update javax references in table ", tableName,
-					" column ", columnName));
+					" column ", columnName, " for company ",
+					CompanyThreadLocal.getCompanyId(
+					).toString()));
 		}
 
 		if (_log.isInfoEnabled()) {
 			for (Map.Entry<String, Map<String, Set<String>>> entry :
 					modifiedData.entrySet()) {
 
+				Map<String, Set<String>> columnNames = entry.getValue();
 				String tableName = entry.getKey();
-				Map<String, Set<String>> data = entry.getValue();
 
 				for (Map.Entry<String, Set<String>> columnEntry :
-						data.entrySet()) {
+						columnNames.entrySet()) {
 
 					String columnName = columnEntry.getKey();
-					Set<String> values = columnEntry.getValue();
+					Set<String> keys = columnEntry.getValue();
 
 					StringBundler sb = new StringBundler();
 
-					for (String value : values) {
-						sb.append(value);
+					for (String key : keys) {
+						sb.append(key);
 						sb.append(", ");
 					}
 
@@ -160,6 +92,9 @@ public abstract class BaseJakartaUpgradeProcess extends UpgradeProcess {
 					_log.info(
 						StringBundler.concat(
 							"Table/column ", tableName, "/", columnName,
+							" for company ",
+							CompanyThreadLocal.getCompanyId(
+							).toString(),
 							" has been upgraded for next IDs: ",
 							sb.toString()));
 				}
@@ -167,8 +102,8 @@ public abstract class BaseJakartaUpgradeProcess extends UpgradeProcess {
 		}
 	}
 
-	protected Character[] getCustomSeparators() {
-		return new Character[0];
+	protected char[] getCustomSeparators() {
+		return new char[0];
 	}
 
 	protected abstract String[][] getTableAndColumnNames();
