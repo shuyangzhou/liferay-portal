bf7e990fc4429aae6ac6a6a114089b5a0cd8d6a5
shuyangzhou <shuyang.zhou@liferay.com>
2025-09-02 15:39:38 -0700

    Objects-36 Inline _withFeatureFlagManager()

--- portal-kernel/src/com/liferay/portal/kernel/feature/flag/FeatureFlagManagerUtil.java
+++ portal-kernel/src/com/liferay/portal/kernel/feature/flag/FeatureFlagManagerUtil.java
@@ -10,13 +10,13 @@ import com.liferay.portal.kernel.json.JSONFactoryUtil;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.module.service.Snapshot;
+import com.liferay.portal.kernel.security.auth.CompanyThreadLocal;
 import com.liferay.portal.kernel.util.GetterUtil;
 import com.liferay.portal.kernel.util.MapUtil;
 import com.liferay.portal.kernel.util.PropsUtil;
 
 import java.util.Dictionary;
 import java.util.function.Function;
-import java.util.function.Supplier;
 
 import org.osgi.framework.BundleContext;
 import org.osgi.framework.ServiceReference;
@@ -44,15 +44,37 @@ public class FeatureFlagManagerUtil {
 	}
 
 	public static String getJSON(long companyId) {
-		return _withFeatureFlagManager(
-			featureFlagManager -> featureFlagManager.getJSON(companyId),
-			() -> _JSON);
+		FeatureFlagManager featureFlagManager =
+			_featureFlagManagerSnapshot.get();
+
+		if (featureFlagManager != null) {
+			return featureFlagManager.getJSON(companyId);
+		}
+
+		if (_log.isInfoEnabled()) {
+			_log.info(
+				"No feature flag manager service found. Returning the " +
+					"default value.");
+		}
+
+		return _JSON;
 	}
 
 	public static boolean isEnabled(long companyId, String key) {
-		return _withFeatureFlagManager(
-			featureFlagManager -> featureFlagManager.isEnabled(companyId, key),
-			() -> GetterUtil.getBoolean(PropsUtil.get("feature.flag." + key)));
+		FeatureFlagManager featureFlagManager =
+			_featureFlagManagerSnapshot.get();
+
+		if (featureFlagManager != null) {
+			return featureFlagManager.isEnabled(companyId, key);
+		}
+
+		if (_log.isInfoEnabled()) {
+			_log.info(
+				"No feature flag manager service found. Returning the " +
+					"default value.");
+		}
+
+		return GetterUtil.getBoolean(PropsUtil.get("feature.flag." + key));
 	}
 
 	/**
@@ -61,9 +83,7 @@ public class FeatureFlagManagerUtil {
 	 */
 	@Deprecated
 	public static boolean isEnabled(String key) {
-		return _withFeatureFlagManager(
-			featureFlagManager -> featureFlagManager.isEnabled(key),
-			() -> GetterUtil.getBoolean(PropsUtil.get("feature.flag." + key)));
+		return isEnabled(CompanyThreadLocal.getCompanyId(), key);
 	}
 
 	public static <T> ServiceRegistration<T> registerService(
@@ -74,25 +94,6 @@ public class FeatureFlagManagerUtil {
 		return new FeatureFlaggedServiceRegistration<>(
 			bundleContext, featureFlagKey, serviceClass, serviceFunction,
 			servicePropertiesFunction);
-	}
-
-	private static <T> T _withFeatureFlagManager(
-		Function<FeatureFlagManager, T> function, Supplier<T> supplier) {
-
-		FeatureFlagManager featureFlagManager =
-			_featureFlagManagerSnapshot.get();
-
-		if (featureFlagManager != null) {
-			return function.apply(featureFlagManager);
-		}
-
-		if (_log.isInfoEnabled()) {
-			_log.info(
-				"No feature flag manager service found. Returning the " +
-					"default value.");
-		}
-
-		return supplier.get();
 	}
 
 	private static final String _JSON = String.valueOf(
